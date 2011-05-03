0e90db126d6f1dd8dbef7ff84115d0be273baf79
tomhuda <tomhuda@strobecorp.com>
2011-05-02 22:30:42 -0700

    Add utility script to emit list of file sizes after minification/gzip.

---
+++ list_file_sizes.rb
@@ -0,0 +1,35 @@
+files = Dir["lib/sproutcore-runtime/lib/**/*.js"] + Dir["lib/sproutcore-views/**/*.js"]
+
+def uglify(string)
+  IO.popen("uglifyjs", "r+") do |io|
+    io.puts string
+    io.close_write
+    return io.read
+  end
+end
+
+def gzip(string)
+  IO.popen("gzip -f", "r+") do |io|
+    io.puts string
+    io.close_write
+    return io.read
+  end
+end
+
+
+string = ""
+
+files.each do |file|
+  this_file = File.read(file)
+  string += this_file
+  size = this_file.size
+  uglified = uglify(this_file)
+  gzipped = gzip(uglified)
+
+  puts "%8d %8d %8d - %s" % [size, uglified.size, gzipped.size, file]
+end
+
+uglified = uglify(string)
+gzipped = gzip(uglified)
+
+puts "%8d %8d %8d" % [string.size, uglified.size, gzipped.size]
