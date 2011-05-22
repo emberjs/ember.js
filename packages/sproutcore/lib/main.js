6b74623a85af5fe88cd30260f6453af626bf8486
Charles Jolley <charles@sproutcore.com>
2011-05-22 15:02:20 +0200

    Add overall sproutcore wrapper framework.

---
+++ packages/sproutcore/lib/main.js
@@ -0,0 +1,10 @@
+// ==========================================================================
+// Project:   SproutCore
+// Copyright: ©2011 Strobe Inc. and contributors.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
+require('sproutcore-metal');
+require('sproutcore-views');
+require('sproutcore-handlebars');
+require('sproutcore-datastore');
