bc62e277bffa146b3c253b5396f3a5ecd2d36781
dmarcotte <daniel.marcotte@bazaarvoice.com>
2011-06-21 22:23:32 -0500

    Allow saved templates to be defined in the body

--- packages/sproutcore-handlebars/lib/loader.js
+++ packages/sproutcore-handlebars/lib/loader.js
@@ -11,50 +11,42 @@ require("sproutcore-handlebars/ext");
 // to SC.CoreView in the global SC.TEMPLATES object.
 
 SC.$(document).ready(function() {
-  SC.$('head script[type="text/html"]').each(function() {
+  SC.$('script[type="text/html"]')
+    .each(function() {
     // Get a reference to the script tag
-    var script = SC.$(this);
+    var script = SC.$(this),
+      // Get the name of the script, used by SC.View's templateName property.
+      // First look for data-template-name attribute, then fall back to its
+      // id if no name is found.
+      templateName = script.attr('data-template-name') || script.attr('id'),
+      template = SC.Handlebars.compile(script.html()),
+      view, viewPath;
 
-    // Get the name of the script, used by SC.View's templateName property.
-    // First look for data-template-name attribute, then fall back to its
-    // id if no name is found.
-    var templateName = script.attr('data-template-name') || script.attr('id');
+    if (templateName) {
+      SC.TEMPLATES[templateName] = template;
 
-    if (!templateName) {
-      throw new SC.Error("Template found without a name specified." +
+      // Remove script tag from DOM
+      script.remove();
+    } else {
+      if (script.parents('head').length !== 0) {
+        // don't allow inline templates in the head
+        throw new SC.Error("Template found in \<head\> without a name specified." +
                          "Please provide a data-template-name attribute.\n" +
                          script.html());
+      }
+      viewPath = script.attr('data-view')
+      view = viewPath ? SC.getPath(viewPath) : SC.View;
+
+      view = view.create({
+        template: template
+      });
+
+      view._insertElementLater(function() {
+        script.replaceWith(this.$());
+
+        // Avoid memory leak in IE
+        script = null;
+      });
     }
-
-    SC.TEMPLATES[templateName] = SC.Handlebars.compile(script.html());
-
-    // Remove script tag from DOM
-    script.remove();
-  });
-
-  // Finds templates stored inline in the HTML document, instantiates a new
-  // view, and replaces the script tag holding the template with the new
-  // view's DOM representation.
-  //
-  // Users can optionally specify a custom view subclass to use by setting the
-  // data-view attribute of the script tag.
-
-  SC.$('body script[type="text/html"]').each(function() {
-    var script = SC.$(this),
-        template = SC.Handlebars.compile(script.html()),
-        viewPath = script.attr('data-view');
-
-    var view = viewPath ? SC.getPath(viewPath) : SC.View;
-
-    view = view.create({
-      template: template
-    });
-
-    view._insertElementLater(function() {
-      script.replaceWith(this.$());
-
-      // Avoid memory leak in IE
-      script = null;
-    });
   });
 });
