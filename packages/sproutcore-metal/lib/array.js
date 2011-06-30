8887087da1e90552de8712dd4bdd69960155eaf3
Erik Bryn <erik.bryn@gmail.com>
2011-06-29 23:14:10 -0700

    We need Array's indexOf. Spade was implementing it for us.

--- packages/sproutcore-metal/lib/array.js
+++ packages/sproutcore-metal/lib/array.js
@@ -48,3 +48,14 @@ if (!Array.prototype.forEach)
     }
   };
 }
+
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function (obj, fromIndex) {
+    if (fromIndex == null) { fromIndex = 0; }
+    else if (fromIndex < 0) { fromIndex = Math.max(0, this.length + fromIndex); }
+    for (var i = fromIndex, j = this.length; i < j; i++) {
+      if (this[i] === obj) { return i; }
+    }
+    return -1;
+  };
+}
