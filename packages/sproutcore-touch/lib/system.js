576b3b3a5cdeb70bb1796b252c9b421827c17e09
Majd Taby <mtaby@me.com>
2011-07-27 18:44:11 -0700

    remove magictouch require

--- packages/sproutcore-touch/lib/system.js
+++ packages/sproutcore-touch/lib/system.js
@@ -1,5 +1,4 @@
 require('sproutcore-touch/system/gesture');
 require('sproutcore-touch/system/view');
-require('sproutcore-touch/system/magictouch');
 require('sproutcore-touch/system/gesture_manager');
 require('sproutcore-touch/system/gestures');
