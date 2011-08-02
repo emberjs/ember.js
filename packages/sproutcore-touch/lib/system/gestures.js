8282f906e183143095f6fcbf2aa991089dd4b216
Majd Taby <mtaby@me.com>
2011-08-01 18:43:26 -0700

    Add ability to unregister gestures from the system

--- packages/sproutcore-touch/lib/system/gestures.js
+++ packages/sproutcore-touch/lib/system/gestures.js
@@ -45,6 +45,14 @@ SC.Gestures = SC.Object.create(
     registeredGestures[name] = recognizer;
   },
 
+  unregister: function(name) {
+    var registeredGestures = this._registeredGestures;
+
+    if (registeredGestures[name] !== undefined) {
+      registeredGestures[name] = undefined;
+    }
+  },
+
   /**
     Registers a gesture recognizer to the system. The gesture recognizer is
     identified by the name parameter, which must be unique across the system.
