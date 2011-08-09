5508571bf48b93f8adc859fd2f7683545a08e976
tomhuda <tomhuda@strobecorp.com>
2011-08-09 12:01:41 -0700

    Fix failing SC.Checkbox unit test in IE8 #crying #whyme #stopie

--- packages/sproutcore-handlebars/tests/controls/checkbox_test.js
+++ packages/sproutcore-handlebars/tests/controls/checkbox_test.js
@@ -73,10 +73,16 @@ test("checking the checkbox updates the value", function() {
   SC.run(function() { checkboxView.appendTo('#qunit-fixture'); });
 
   equals(get(checkboxView, 'value'), true, "precond - initially starts with a true value");
-  equals(checkboxView.$('input').prop('checked'), true, "precond - the initial checked property is true");
+  equals(!!checkboxView.$('input').attr('checked'), true, "precond - the initial checked property is true");
 
-  // click will trigger change event. can't call change event directly because that won't modify the checkbox's value.
-  checkboxView.$('input')[0].click();
+  // Can't find a way to programatically trigger a checkbox in IE and have it generate the
+  // same events as if a user actually clicks.
+  if (!jQuery.browser.msie) {
+    checkboxView.$('input')[0].click();
+  } else {
+    checkboxView.$('input').trigger('click');
+    checkboxView.$('input').removeAttr('checked').trigger('change');
+  }
 
   equals(checkboxView.$('input').prop('checked'), false, "after clicking a checkbox, the checked property changed");
   equals(get(checkboxView, 'value'), false, "changing the checkbox causes the view's value to get updated");
