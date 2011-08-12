98234b8e4c27e3e1b8275d79ca16d5823b3f81c0
Majd Taby <mtaby@me.com>
2011-08-12 19:56:05 -0400

    SC.Button should do the lookup for the target globally, not relative to itself

--- packages/sproutcore-handlebars/lib/controls/button.js
+++ packages/sproutcore-handlebars/lib/controls/button.js
@@ -18,7 +18,7 @@ SC.Button = SC.View.extend({
     var target = get(this, 'target');
 
     if (SC.typeOf(target) === "string") {
-      return SC.getPath(this, target);
+      return SC.getPath(target);
     } else {
       return target;
     }
