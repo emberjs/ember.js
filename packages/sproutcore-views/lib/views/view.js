e308e455883724a6807e543491c1690ce1a94c68
tomhuda <tomhuda@strobecorp.com>
2011-08-11 19:14:27 -0700

    Fix class name bindings with both a path and a customized class name ("message.isUrgent:is-urgent")

--- packages/sproutcore-views/lib/views/view.js
+++ packages/sproutcore-views/lib/views/view.js
@@ -335,18 +335,18 @@ SC.View = SC.Object.extend(
     // Loop through all of the configured bindings. These will be either
     // property names ('isUrgent') or property paths relative to the view
     // ('content.isUrgent')
-    classBindings.forEach(function(property) {
+    classBindings.forEach(function(binding) {
 
       // Variable in which the old class value is saved. The observer function
       // closes over this variable, so it knows which string to remove when
       // the property changes.
-      var oldClass;
+      var oldClass, property;
 
       // Set up an observer on the context. If the property changes, toggle the
       // class name.
       var observer = function() {
         // Get the current value of the property
-        newClass = this._classStringForProperty(property);
+        newClass = this._classStringForProperty(binding);
         elem = this.$();
 
         // If we had previously added a class to the element, remove it.
@@ -364,10 +364,8 @@ SC.View = SC.Object.extend(
         }
       };
 
-      addObserver(this, property, observer);
-
       // Get the class name for the property at its current value
-      dasherizedClass = this._classStringForProperty(property);
+      dasherizedClass = this._classStringForProperty(binding);
 
       if (dasherizedClass) {
         // Ensure that it gets into the classNames array
@@ -379,6 +377,10 @@ SC.View = SC.Object.extend(
         // been closed over by the observer.
         oldClass = dasherizedClass;
       }
+
+      // Extract just the property name from bindings like 'foo:bar'
+      property = binding.split(':')[0];
+      addObserver(this, property, observer);
     }, this);
   },
 
