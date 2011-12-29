5692d2afacf43134351777cf6260b4c5e88ab0fc
pangratz <pangratz666@gmail.com>
2011-12-29 19:55:22 +0100

    added empty tests for loader.js aka Ember.Handlebars.bootstrap

---
+++ packages/ember-handlebars/tests/loader_test.js
@@ -0,0 +1,7 @@
+module("test Ember.Handlebars.bootstrap");
+
+test("scripts with data-template-name should be added to Ember.TEMPLATES", function() {
+});
+
+test("templates with data-tag-name should create templates with specific tag name", function() {
+});
