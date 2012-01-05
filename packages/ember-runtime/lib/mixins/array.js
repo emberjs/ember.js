34a02fb07691244123fc925f8644352a97c79aac
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-01-04 19:21:29 -0500

    Adding examples to Array#indexOf doc

--- packages/ember-runtime/lib/mixins/array.js
+++ packages/ember-runtime/lib/mixins/array.js
@@ -130,11 +130,23 @@ Ember.Array = Ember.Mixin.create(Ember.Enumerable, /** @scope Ember.Array.protot
   },
 
   /**
-    Returns the index for a particular object in the index.
+    Returns the index of the given object's first occurrence.
+    If no startAt argument is given, the starting location to
+    search is 0. If it's negative, will count backward from 
+    the end of the array. Returns -1 if no match is found.
 
     @param {Object} object the item to search for
-    @param {NUmber} startAt optional starting location to search, default 0
-    @returns {Number} index of -1 if not found
+    @param {Number} startAt optional starting location to search, default 0
+    @returns {Number} index or -1 if not found
+
+    @example
+    arr = ["a", "b", "c", "d", "a"];
+    arr.indexOf("a");      =>  0
+    arr.indexOf("z");      => -1
+    arr.indexOf("a", 2);   =>  4
+    arr.indexOf("a", -1);  =>  4
+    arr.indexOf("b", 3);   => -1
+    arr.indexOf("a", 100); => -1
   */
   indexOf: function(object, startAt) {
     var idx, len = get(this, 'length');
