3c09619fbb561d193c1dd24bcf585bfca68d92db
pangratz <cmueller.418@gmail.com>
2012-01-06 19:02:47 +0100

    happy new year!

--- generators/license.js
+++ generators/license.js
@@ -1,6 +1,6 @@
 // ==========================================================================
 // Project:   Ember - JavaScript Application Framework
-// Copyright: ©2006-2011 Strobe Inc. and contributors.
-//            Portions ©2008-2011 Apple Inc. All rights reserved.
+// Copyright: ©2006-2012 Strobe Inc. and contributors.
+//            Portions ©2008-2012 Apple Inc. All rights reserved.
 // License:   Licensed under MIT license (see license.js)
 // ==========================================================================
