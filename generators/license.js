e58b5eff78f1abc687f956ed79c8f536f1494748
pangratz <cmueller.418@gmail.com>
2012-01-06 19:02:47 +0100

    happy new year! NOTE: Updates the license to reflect the Strobe Facebook buyout and the new year.

--- generators/license.js
+++ generators/license.js
@@ -1,6 +1,7 @@
 // ==========================================================================
 // Project:   Ember - JavaScript Application Framework
-// Copyright: ©2006-2011 Strobe Inc. and contributors.
+// Copyright: ©2011-2012 Tilde Inc. and contributors
+//            Portions ©2006-2011 Strobe Inc.
 //            Portions ©2008-2011 Apple Inc. All rights reserved.
 // License:   Licensed under MIT license (see license.js)
 // ==========================================================================
