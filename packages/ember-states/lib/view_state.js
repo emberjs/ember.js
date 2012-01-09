86bf662c7730962bf3f76c780f5b806a3b9182ce
tchak <paul@chavard.net>
2012-01-09 11:37:30 +0100

    support for referencing view in ViewState by path

--- packages/ember-states/lib/view_state.js
+++ packages/ember-states/lib/view_state.js
@@ -1,12 +1,19 @@
 require('ember-states/state');
 
-var get = Ember.get, set = Ember.set;
+var get = Ember.get, set = Ember.set, getPath = Ember.getPath;
 
 Ember.ViewState = Ember.State.extend({
   isViewState: true,
 
   enter: function(stateManager) {
     var view = get(this, 'view');
+
+    if (typeof view === 'string') {
+      view = getPath(view);
+      if (view) {
+        this.set('view', view);
+      }
+    }
 
     if (view) {
       view.appendTo(stateManager.get('rootElement') || 'body');
@@ -15,6 +22,13 @@ Ember.ViewState = Ember.State.extend({
 
   exit: function(stateManager) {
     var view = get(this, 'view');
+
+    if (typeof view === 'string') {
+      view = getPath(view);
+      if (view) {
+        this.set('view', view);
+      }
+    }
 
     if (view) {
       view.remove();
