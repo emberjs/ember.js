af0c34da2ddb57a7039babcb9297258d6a1a7e7d
Craig McNamara <craig@caring.com>
2012-01-11 23:19:06 -0800

    Added RadioButton support.

---
+++ packages/ember-handlebars/lib/controls/radio_button.js
@@ -0,0 +1,31 @@
+// ==========================================================================
+// Project:   Ember Handlebar Views
+// Copyright: ©2011 Strobe Inc. and contributors.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
+require("ember-views/views/view");
+require("ember-handlebars/ext");
+
+var set = Ember.set, get = Ember.get;
+
+Ember.RadioButton = Ember.View.extend({
+  title: null,
+  value: null,
+  checked: false,
+  group: "radio_button",
+  disabled: false,
+
+  classNames: ['ember-radio-button'],
+
+  defaultTemplate: Ember.Handlebars.compile('<input type="radio" {{ bindAttr disabled="disabled" name="group" value="value" checked="checked"}}>{{title}}</input>'),
+
+  change: function() {
+    Ember.run.once(this, this._updateElementValue);
+  },
+
+  _updateElementValue: function() {
+    var input = this.$('input:radio');
+    set(this, 'value', input.attr('val'));
+  }
+});
