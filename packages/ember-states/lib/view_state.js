771a003a8e0fd109b7678dcd624a22f0e5184117
joubertnel <joubert@obikosh.com>
2012-01-12 12:28:21 -0500

    waits for the DOM to be ready before appending view

--- packages/ember-states/lib/view_state.js
+++ packages/ember-states/lib/view_state.js
@@ -6,10 +6,24 @@ Ember.ViewState = Ember.State.extend({
   isViewState: true,
 
   enter: function(stateManager) {
+    var state;
     var view = get(this, 'view');
-
-    if (view) {
+    var appendView = function(view, stateManager) {
       view.appendTo(stateManager.get('rootElement') || 'body');
+    };
+
+    
+    if (view) {
+      
+      if (Ember.$.isReady === YES) {
+        appendView(view, stateManager);
+      } else {
+        state = this;
+        Ember.$(document).ready(function() {
+          appendView(view, stateManager);
+        });
+      }
+      
     }
   },
 
