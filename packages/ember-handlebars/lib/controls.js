e25ee1712d8195df9e59bb8cf4956625efad3acc
Justin Brown <justinbrown.work@gmail.com>
2012-02-09 13:40:46 -0500

    Add Ember.RadioButton and Ember.RadioButtonGroup support.

--- packages/ember-handlebars/lib/controls.js
+++ packages/ember-handlebars/lib/controls.js
@@ -7,6 +7,7 @@
 require("ember-handlebars/controls/checkbox");
 require("ember-handlebars/controls/text_field");
 require("ember-handlebars/controls/button");
+require("ember-handlebars/controls/radio_button");
 require("ember-handlebars/controls/text_area");
 require("ember-handlebars/controls/tabs");
 require("ember-handlebars/controls/select");
