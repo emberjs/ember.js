a652180182a2f0510a0e63f0382583c4e859bb0d
Peter Wagenet <peter.wagenet@gmail.com>
2012-02-17 15:39:33 -0800

    Version bump - 0.9.5

--- packages/ember-metal/lib/core.js
+++ packages/ember-metal/lib/core.js
@@ -9,7 +9,7 @@ if ('undefined' === typeof Ember) {
 /**
   @namespace
   @name Ember
-  @version 0.9.4
+  @version 0.9.5
 
   All Ember methods and functions are defined inside of this namespace.
   You generally should not add new properties to this namespace as it may be
@@ -41,10 +41,10 @@ if ('undefined' !== typeof window) {
 /**
   @static
   @type String
-  @default '0.9.4'
+  @default '0.9.5'
   @constant
 */
-Ember.VERSION = '0.9.4';
+Ember.VERSION = '0.9.5';
 
 /**
   @static
