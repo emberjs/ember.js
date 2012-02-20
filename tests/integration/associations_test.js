78d4fd348786d5a3db83a75d8b8f04be355abc8c
tomhuda <tomhuda@tilde.io>
2012-02-20 15:41:14 -0800

    Reset the store's default transaction on commit
    * When a store is committed, it should generate
      a new transaction.
    * Always explicitly specify a record's
      transaction. This way a pending record can
      belong to the default transaction, but won't
      lose a reference to it when the store is
      committed.
    * Rename store's `models` property to
      `recordCache`.

--- tests/integration/associations_test.js
+++ tests/integration/associations_test.js
@@ -46,3 +46,38 @@ test("when adding a record to an association that belongs to another record that
     store.commit();
   });
 });
+
+test("if a record is added to the store while a child is pending, auto-committing the child record should not commit the new record", function() {
+  expect(2);
+
+  var Comment = DS.Model.extend();
+
+  Comment.reopen({
+    comments: DS.hasMany(Comment)
+  });
+
+  var parentRecord = Comment.createRecord();
+  var childRecord = Comment.createRecord();
+
+  parentRecord.get('comments').pushObject(childRecord);
+
+  var createCalled = 0;
+  adapter.createRecord = function(store, type, model) {
+    createCalled++;
+    if (createCalled === 1) {
+      equal(model, parentRecord, "parent record is committed first");
+
+      Comment.createRecord();
+
+      store.didCreateRecord(model, { id: 1 });
+    } else if (createCalled === 2) {
+      equal(model, childRecord, "child record is committed after its parent is committed");
+    } else {
+      ok(false, "Third comment should not be saved");
+    }
+  };
+
+  Ember.run(function() {
+    store.commit();
+  });
+});
