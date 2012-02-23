82ea181e36436685ef407a00c381d9b44de36c3f
tomhuda <tomhuda@tilde.io>
2012-02-23 14:42:29 -0800

    Records move to default transaction after commit
    If a transaction is committed, the records that
    belong to it should be moved back to the default
    transaction. Additionally, if the default
    transaction is committed, those records should be
    moved to the *new* default transaction.
    
    Additionally, records should be able to be moved
    between transactions if they are still clean.
    Records can now be removed from transactions
    with the remove() method.

--- tests/unit/transaction_test.js
+++ tests/unit/transaction_test.js
@@ -121,6 +121,36 @@ test("after a model is added to a transaction then deleted, it is not committed 
   equal(commitCalls, 1, "commit was called when committing the transaction");
 });
 
+test("a model that is clean can be removed from a transaction", function() {
+  var updateCalled = 0;
+
+  var store = DS.Store.create({
+    adapter: DS.Adapter.create({
+      updateRecord: function() {
+        updateCalled++;
+      }
+    })
+  });
+
+  store.load(Person, { id: 1, name: "Yehuda Katz" });
+
+  var transaction = store.transaction();
+  var record = store.find(Person, 1);
+
+  transaction.add(record);
+  transaction.remove(record);
+
+  set(record, 'name', "shuck it trebek");
+
+  transaction.commit();
+
+  equal(updateCalled, 0, "after removing from transaction it does not commit");
+
+  store.commit();
+
+  equal(updateCalled, 1, "after removing from transaction it commits on the store");
+});
+
 test("a model that is in the created state cannot be moved into a new transaction", function() {
   var store = DS.Store.create();
 
@@ -160,3 +190,25 @@ test("a model that is in the deleted state cannot be moved into a new transactio
   }, Error);
 });
 
+test("a record that is in the clean state is moved back to the default transaction after its transaction is committed", function() {
+  var commitCalled = 0;
+
+  var store = DS.Store.create({
+    adapter: DS.Adapter.create({
+      commit: function() {
+        commitCalled++;
+      }
+    })
+  });
+
+  store.load(Person, { id: 1 });
+
+  var person = store.find(Person, 1);
+
+  var transaction = store.transaction();
+  transaction.add(person);
+  transaction.commit();
+
+  equal(commitCalled, 1, "should attempt to commit records");
+  equal(get(person, 'transaction'), get(store, 'defaultTransaction'), "record should have been moved back to the default transaction");
+});
