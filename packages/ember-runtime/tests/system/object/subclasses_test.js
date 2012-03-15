f71943d63365319a3fce0b24d17e6d96a937987c
Kris Selden <kris.selden@gmail.com>
2012-03-14 17:25:44 -0700

    add test for #591

--- packages/ember-runtime/tests/system/object/subclasses_test.js
+++ packages/ember-runtime/tests/system/object/subclasses_test.js
@@ -23,3 +23,34 @@ test('defining sub-sub class should only go to parent', function() {
   ok(Sub.subclasses.contains(SubSub), 'Sub contains SubSub');
 });
 
+// TEST lazy prototype and Em.rewatch(prototype)
+test('chains should copy forward to subclasses when prototype created', function () {
+  var ObjectWithChains, objWithChains, SubWithChains, SubSub, subSub;
+  Ember.run(function () {
+    ObjectWithChains = Ember.Object.extend({
+      obj: {
+          a: 'a',
+          hi: 'hi'
+      },
+      aBinding: 'obj.a' // add chain
+    });
+    // realize prototype
+    objWithChains = ObjectWithChains.create();
+    // should not copy chains from parent yet
+    SubWithChains = ObjectWithChains.extend({
+      hiBinding: 'obj.hi', // add chain
+      hello: Ember.computed(function() {
+          return this.getPath('obj.hi') + ' world';
+      }).property('hi'), // observe chain
+      greetingBinding: 'hello'
+    });
+    SubSub = SubWithChains.extend();
+    // should realize prototypes and copy forward chains
+    subSub = SubSub.create();
+  });
+  equal(subSub.get('greeting'), 'hi world');
+  Ember.run(function () {
+    objWithChains.setPath('obj.hi', 'hello');
+  });
+  equal(subSub.get('greeting'), 'hello world');
+});
