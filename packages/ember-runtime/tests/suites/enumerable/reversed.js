f8fe0a1e4e417d5fcf770b6c4c9f54eca07b3784
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-03-21 16:57:25 -0500

    Adding Enumerable#reversed function

---
+++ packages/ember-runtime/tests/suites/enumerable/reversed.js
@@ -0,0 +1,24 @@
+// ==========================================================================
+// Project:  Ember Runtime
+// Copyright: ©2011 Strobe Inc. and contributors.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
+require('ember-runtime/~tests/suites/enumerable');
+
+var suite = Ember.EnumerableTests;
+
+suite.module('reversed');
+
+suite.test('should return new instance with elements in reverse order', function () {
+  var before, after, obj, ret;
+
+  before = this.newFixture(3);
+  after  = [before[2], before[1], before[0]];
+  obj = this.newObject(before);
+
+  ret = obj.reversed();
+
+  deepEqual(this.toArray(ret), after, 'should have been reversed');
+  deepEqual(this.toArray(obj), before, 'should not have changed original');
+});
