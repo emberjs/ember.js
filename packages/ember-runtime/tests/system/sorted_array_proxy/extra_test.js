fbe19c088d97cfaacd7c96a79756f4b6dbd16b2a
Scott Smith <ssmith@hedgeye.com>
2012-04-06 11:20:36 -0700

    Added test for passing pre-sorted list as create argument

--- packages/ember-runtime/tests/system/sorted_array_proxy/extra_test.js
+++ packages/ember-runtime/tests/system/sorted_array_proxy/extra_test.js
@@ -5,52 +5,76 @@
 // License:   Licensed under MIT license (see license.js)
 // ==========================================================================
 
-module('Ember.SortedArrayProxy');
+module('Ember.SortedArrayProxy', {
 
-test("Add objects into the sorted array", function() {
+  setup: function() {
 
-  // Define an object to be put into the sorted array:
-  var Pet = Ember.Object.extend({
-    name: null,
-    description: null,
+    // Define an object to be put into the sorted array:
+    this.Pet = Ember.Object.extend({
+      name: null,
+      description: null,
 
-    // Sort on the pet's name case-insensitive:
-    sortValue: Ember.computed(function() {
-      return this.get('name').toUpperCase();
-    }).property('name')
-  });
+      // Sort on the pet's name case-insensitive:
+      sortValue: Ember.computed(function() {
+        return this.get('name').toUpperCase();
+      }).property('name')
+    });
+
+    this.dog = this.Pet.create({
+      name: 'Dog',
+      description: 'furry friendly'
+    });
+
+    this.cat = this.Pet.create({
+      name: 'cat',
+      description: 'furry aloof'
+    });
+
+    this.fish = this.Pet.create({
+      name: 'fish',
+      description: 'scaly wet'
+    });
+
+  }
+});
+
+test("Add objects into and remove from the sorted array", function() {
 
   // Create a sorted array:
-  var sortedArray = Ember.SortedArrayProxy.create({content: Ember.A([])});
+  var sortedPets = Ember.SortedArrayProxy.create({content: Ember.A([])});
 
   // Add some pets to it.
-  sortedArray.add(Pet.create({
-    name: 'Dog',
-    description: 'furry friendly'
-  }));
-  sortedArray.add(Pet.create({
-    name: 'cat',
-    description: 'furry aloof'
-  }));
-  sortedArray.add(Pet.create({
-    name: 'fish',
-    description: 'scaly wet'
-  }));
+  sortedPets.add(this.dog);
+  sortedPets.add(this.cat);
+  sortedPets.add(this.fish);
 
-  equal(sortedArray.get('length'), 3);
+  equal(sortedPets.get('length'), 3);
 
   // Examine how they're sorted:
-  var actualArrayResult = sortedArray.map(function(item, index, self) {
+  var actualArrayResult = sortedPets.map(function(item, index, self) {
     return item.get('name');
   }); // => ['cat', 'Dog', 'fish'];
   deepEqual(actualArrayResult, ['cat', 'Dog', 'fish'], "should be sorted case-insensitive");
 
   // Remove the cat:
-  var cat = sortedArray.objectAtContent(0);
-  sortedArray.remove(cat);
+  sortedPets.remove(this.cat);
 
   // See what's left:
-  actualArrayResult = sortedArray.map(function(item, index, self) {
+  actualArrayResult = sortedPets.map(function(item, index, self) {
     return item.get('name');
   }); // => ['Dog', 'fish'];
+  deepEqual(actualArrayResult, ['Dog', 'fish'], "should be sorted case-insensitive");
 });
+
+test("Can preload sorted items on create", function() {
+
+  var sortedPrimitivePets = Ember.A([this.cat, this.dog, this.fish]);
+  var sortedPets = Ember.SortedArrayProxy.create({content: Ember.A(sortedPrimitivePets)});
+  var actualArrayResult = sortedPets.map(function(item, index, self) {
+    return item.get('name');
+  });
+  deepEqual(actualArrayResult, ['cat', 'Dog', 'fish'], "should be sorted case-insensitive");
+
+});
+
+test
