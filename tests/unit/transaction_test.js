84a72e9eccb1d8be6617128d96a363000d3e4c07
tomhuda <tomhuda@tilde.io>
2012-04-17 16:11:42 -0700

    Added DS.Transaction#rollback()
    Transactions now have a rollback() method that
    causes all of the records that belong to them to
    be reset to their state when they were first added
    to the transaction.
    
    The only exception to this is newly created
    states, which immediately become deleted upon
    transaction rollback.

--- tests/unit/transaction_test.js
+++ tests/unit/transaction_test.js
@@ -212,3 +212,61 @@ test("a record that is in the clean state is moved back to the default transacti
   equal(commitCalled, 1, "should attempt to commit records");
   equal(get(person, 'transaction'), get(store, 'defaultTransaction'), "record should have been moved back to the default transaction");
 });
+
+test("modified records are reset when their transaction is rolled back", function() {
+
+  var store = DS.Store.create({
+    adapter: DS.Adapter.create({
+      commit: function() {
+        ok(false, "should never call adapter methods");
+      }
+    })
+  });
+
+  store.load(Person, { id: 1, name: "Scumbag Tom" });
+  store.load(Person, { id: 2, name: "Scumbag Carl" });
+  store.load(Person, { id: 3, name: "Scumbag André" });
+
+  var updatedPerson = store.find(Person, 1);
+  var deletedPerson = store.find(Person, 2);
+  var anotherUpdatedPerson = store.find(Person, 3);
+
+  var transaction = store.transaction();
+  transaction.add(updatedPerson);
+  transaction.add(deletedPerson);
+  transaction.add(anotherUpdatedPerson);
+
+  var newPerson = transaction.createRecord(Person, {
+    name: "Scumbag Yehuda"
+  });
+
+  updatedPerson.set('name', "Scumbag Patrick");
+  anotherUpdatedPerson.set('name', "Scumbag Leah");
+  deletedPerson.deleteRecord();
+
+  equal(updatedPerson.get('isDirty'), true, "precond - Record is marked as dirty when changed");
+  equal(updatedPerson.get('name'), "Scumbag Patrick", "precond - Record has been changed to the value we set");
+  equal(anotherUpdatedPerson.get('isDirty'), true, "precond - Record is marked as dirty when changed");
+  equal(anotherUpdatedPerson.get('name'), "Scumbag Leah", "precond - Record has been changed to the value we set");
+  equal(newPerson.get('isDirty'), true, "precond - new record is marked as dirty");
+  equal(newPerson.get('isNew'), true, "precond - new record is marked as new");
+  equal(deletedPerson.get('isDirty'), true, "precond - deleted record is marked as dirty when deleted");
+  equal(deletedPerson.get('isDeleted'), true, "precond - deleted record is marked as deleted");
+
+  transaction.rollback();
+
+  equal(updatedPerson.get('isDirty'), false, "Record is not dirty after rollback");
+  equal(updatedPerson.get('name'), "Scumbag Tom", "Record has previously loaded name");
+  equal(anotherUpdatedPerson.get('isDirty'), false, "Record is not dirty after rollback");
+  equal(anotherUpdatedPerson.get('name'), "Scumbag André", "Record has previously loaded name");
+  equal(newPerson.get('isDirty'), false, "created record is no longer considered dirty");
+  equal(newPerson.get('isDeleted'), true, "created records are deleted when their transaction is rolled back");
+  equal(deletedPerson.get('isDirty'), false, "deleted record is no longer considered dirty");
+  equal(deletedPerson.get('isDeleted'), false, "deleted record is no longer considered deleted");
+
+  equal(get(newPerson, 'transaction'), get(store, 'defaultTransaction'), "record should have been moved back to the default transaction");
+  equal(get(updatedPerson, 'transaction'), get(store, 'defaultTransaction'), "record should have been moved back to the default transaction");
+  equal(get(anotherUpdatedPerson, 'transaction'), get(store, 'defaultTransaction'), "record should have been moved back to the default transaction");
+  equal(get(deletedPerson, 'transaction'), get(store, 'defaultTransaction'), "record should have been moved back to the default transaction");
+});
+
