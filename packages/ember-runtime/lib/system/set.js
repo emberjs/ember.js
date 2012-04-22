fc32ae6d544c9ad2a610e7777abea364b3226fac
Peter Wagenet <peter.wagenet@gmail.com>
2012-04-21 19:58:35 -0700

    Updated Ember.Set docs to not refer to observing '[]' - refs #420

--- packages/ember-runtime/lib/system/set.js
+++ packages/ember-runtime/lib/system/set.js
@@ -78,11 +78,9 @@ var get = Ember.get, set = Ember.set, guidFor = Ember.guidFor, none = Ember.none
 
   ## Observing changes
 
-  When using `Ember.Set`, you can observe the `"[]"` property to be
-  alerted whenever the content changes.  You can also add an enumerable
-  observer to the set to be notified of specific objects that are added and
-  removed from the set.  See `Ember.Enumerable` for more information on
-  enumerables.
+  When using `Ember.Set`, you can add an enumerable observer to the set to
+  be notified of specific objects that are added and removed from the set.
+  See `Ember.Enumerable` for more information on enumerables.
 
   This is often unhelpful. If you are filtering sets of objects, for instance,
   it is very inefficient to re-filter all of the items each time the set
