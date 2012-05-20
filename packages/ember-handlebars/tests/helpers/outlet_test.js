5039830ff9e2a48bebbfe288505679efcb6f9084
tomhuda <tomhuda@tilde.io>
2012-05-19 19:24:40 -0700

    Initial implementation of outlets
    This feature allows you to leave a placeholder in
    your template that will be filled in by the
    current view's controller.

---
+++ packages/ember-handlebars/tests/helpers/outlet_test.js
@@ -0,0 +1,62 @@
+var appendView = function(view) {
+  Ember.run(function() { view.appendTo('#qunit-fixture'); });
+};
+
+var compile = function(template) {
+  return Ember.Handlebars.compile(template);
+};
+
+var view;
+
+module("Handlebars {{outlet}} helpers", {
+  teardown: function() {
+    if (view) {
+      view.destroy();
+    }
+  }
+});
+
+test("outlet should allow controllers to fill in slots", function() {
+  var controller = Ember.Object.create();
+
+  var template = "<h1>HI</h1>{{outlet}}";
+  view = Ember.View.create({
+    controller: controller,
+    template: Ember.Handlebars.compile(template)
+  });
+
+  appendView(view);
+
+  equal(view.$().text(), 'HI');
+
+  Ember.run(function() {
+    controller.set('view', Ember.View.create({
+      template: compile("<p>BYE</p>")
+    }));
+  });
+
+  equal(view.$().text(), 'HIBYE');
+});
+
+
+test("outlet should support an optional name", function() {
+  var controller = Ember.Object.create();
+
+  var template = "<h1>HI</h1>{{outlet mainView}}";
+  view = Ember.View.create({
+    controller: controller,
+    template: Ember.Handlebars.compile(template)
+  });
+
+  appendView(view);
+
+  equal(view.$().text(), 'HI');
+
+  Ember.run(function() {
+    controller.set('mainView', Ember.View.create({
+      template: compile("<p>BYE</p>")
+    }));
+  });
+
+  equal(view.$().text(), 'HIBYE');
+});
