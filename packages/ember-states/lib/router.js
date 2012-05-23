a4f6346affdc530a1890b4052df021a5c404a05e
Jo Liss <joliss42@gmail.com>
2012-05-23 22:31:08 +0200

    Add documentation stub for Ember.Router

--- packages/ember-states/lib/router.js
+++ packages/ember-states/lib/router.js
@@ -2,7 +2,25 @@ require('ember-states/state');
 require('ember-states/route_matcher');
 require('ember-states/routable');
 
-Ember.Router = Ember.StateManager.extend({
+/**
+  @class
+
+  `Ember.Router` is a state manager used for routing.
+
+  A special `Router` property name is recognized on applications:
+
+      var app = Ember.Application.create({
+        Router: Ember.Router.extend(...)
+      });
+      app.initialize();
+
+  Here, `app.initialize` will instantiate the `app.Router` and assign the
+  instance to the `app.stateManager` property.
+
+  @extends Ember.StateManager
+*/
+Ember.Router = Ember.StateManager.extend(
+/** @scope Ember.Router.prototype */ {
   route: function(path) {
     if (path.charAt(0) === '/') {
       path = path.substr(1);
