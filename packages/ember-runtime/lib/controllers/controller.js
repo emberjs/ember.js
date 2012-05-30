6e810d77448f36556c61ea9b4f23613d4ec91f0e
Tom Dale <tom@sproutcore.com>
2012-05-30 11:10:38 +0200

    Allow setting `target` on `ObjectController`

--- packages/ember-runtime/lib/controllers/controller.js
+++ packages/ember-runtime/lib/controllers/controller.js
@@ -1,6 +1,17 @@
 require('ember-runtime/system/object');
 require('ember-runtime/system/string');
 
-Ember.ControllerMixin = Ember.Mixin.create();
+Ember.ControllerMixin = Ember.Mixin.create({
+  /**
+    The object to which events from the view should be sent.
+
+    For example, when a Handlebars template uses the `{{action}}` helper,
+    it will attempt to send the event to the view's controller's `target`.
+
+    By default, a controller's `target` is set to the router after it is
+    instantiated by `Ember.Application#initialize`.
+  */
+  target: null
+});
 
 Ember.Controller = Ember.Object.extend(Ember.ControllerMixin);
