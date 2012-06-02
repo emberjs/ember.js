4163966afd5421accf9b3ef4d5c9ef2855e9cda1
Jo Liss <joliss42@gmail.com>
2012-06-02 17:46:59 +0200

    Add NoneLocation
    I am using this in my own test suite, and it's been working well. I'm
    not adding unit tests here, since the logic is trivial, and it will get
    exercised by many other tests.

--- packages/ember-application/lib/system.js
+++ packages/ember-application/lib/system.js
@@ -2,3 +2,4 @@ require('ember-application/system/application');
 require('ember-application/system/location');
 require('ember-application/system/hash_location');
 require('ember-application/system/history_location');
+require('ember-application/system/none_location');
