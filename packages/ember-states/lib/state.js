2a1dcc9805b819e68bf314e867c1c89326378fb6
Mike Andrzejewski <mike.aski@free.fr>
2012-06-05 18:31:50 +0300

    C'mon (again) bro...

--- packages/ember-states/lib/state.js
+++ packages/ember-states/lib/state.js
@@ -55,7 +55,7 @@ Ember.State = Ember.Object.extend(Ember.Evented,
   init: function() {
     var states = get(this, 'states'), foundStates;
     set(this, 'childStates', Ember.A());
-    this.set('eventTransitions', this.get('eventTransitions') || {});
+    set(this, 'eventTransitions', get(this, 'eventTransitions') || {});
 
     var name, value, transitionTarget;
 
