e945b9ebde480f642017e4ecea6c545014f68741
Jo Liss <joliss42@gmail.com>
2012-06-06 23:03:58 +0200

    Update location documentation

--- packages/ember-application/lib/system/location.js
+++ packages/ember-application/lib/system/location.js
@@ -8,6 +8,7 @@ var get = Ember.get, set = Ember.set;
   getURL: returns the current URL
   setURL(path): sets the current URL
   onUpdateURL(callback): triggers the callback when the URL changes
+  formatURL(url): formats `url` to be placed into `href` attribute
 
   Calling setURL will not trigger onUpdateURL callbacks.
 
