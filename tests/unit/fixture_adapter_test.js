11d60d3936bbeeb6e7eeb2829390974166fe3d3f
tchak <paul@chavard.net>
2012-06-06 13:49:15 +0200

    More advanced FixtureAdapter implementation

--- tests/unit/fixture_adapter_test.js
+++ tests/unit/fixture_adapter_test.js
@@ -1,19 +1,29 @@
 var get = Ember.get, set = Ember.set;
+var store, Person;
 
-module("DS.FixtureAdapter");
+module("DS.FixtureAdapter", {
+  setup: function() {
+    store = DS.Store.create({
+      adapter: 'DS.fixtureAdapter'
+    });
 
-test("should load all data for a type asynchronously the first time it is requested", function() {
-  var store = DS.Store.create({
-    adapter: 'DS.fixtureAdapter'
-  });
+    Person = DS.Model.extend({
+      firstName: DS.attr('string'),
+      lastName: DS.attr('string'),
 
-  var Person = DS.Model.extend({
-    firstName: DS.attr('string'),
-    lastName: DS.attr('string'),
+      height: DS.attr('number')
+    });
+  },
+  teardown: function() {
+    Ember.run(function() {
+      store.destroy();
+    });
+    store = null;
+    Person = null;
+  }
+});
 
-    height: DS.attr('number')
-  });
-
+test("should load data for a type asynchronously when it is requested", function() {
   Person.FIXTURES = [{
     id: 'wycats',
     firstName: "Yehuda",
@@ -42,9 +52,89 @@ test("should load all data for a type asynchronously the first time it is reques
     equal(get(ebryn, 'height'), 70, "data from fixtures is loaded correctly");
 
     var wycats = store.find(Person, 'wycats');
-    equal(get(wycats, 'isLoaded'), true, "subsequent requests for records are returned immediately");
-    equal(get(wycats, 'height'), 65, "subsequent requested records contain correct information");
+    wycats.addObserver('isLoaded', function() {
+      clearTimeout(timer);
+      start();
+
+      equal(get(wycats, 'isLoaded'), true, "subsequent requests for records are returned asynchronously");
+      equal(get(wycats, 'height'), 65, "subsequent requested records contain correct information");
+    });
+
+    stop();
+
+    timer = setTimeout(function() {
+      start();
+      ok(false, "timeout exceeded waiting for fixture data");
+    }, 1000);
   });
+
+  stop();
+
+  var timer = setTimeout(function() {
+    start();
+    ok(false, "timeout exceeded waiting for fixture data");
+  }, 1000);
+});
+
+test("should create record asynchronously when it is commited", function() {
+  var paul = Person.createRecord({firstName: 'Paul', lastName: 'Chavard', height: 70});
+
+  paul.on('didCreate', function() {
+    clearTimeout(timer);
+    start();
+
+    equal(get(paul, 'isNew'), false, "data loads asynchronously");
+    equal(get(paul, 'isDirty'), false, "data loads asynchronously");
+    equal(get(paul, 'height'), 70, "data from fixtures is saved correctly");
+  });
+
+  store.commit();
+
+  stop();
+
+  var timer = setTimeout(function() {
+    start();
+    ok(false, "timeout exceeded waiting for fixture data");
+  }, 1000);
+});
+
+test("should update record asynchronously when it is commited", function() {
+  var paul = store.findByClientId(Person, store.load(Person, 1, {first_name: 'Paul', last_name: 'Chavard', height: 70}).clientId);
+
+  paul.set('height', 80);
+
+  paul.on('didUpdate', function() {
+    clearTimeout(timer);
+    start();
+
+    equal(get(paul, 'isDirty'), false, "data loads asynchronously");
+    equal(get(paul, 'height'), 80, "data from fixtures is saved correctly");
+  });
+
+  store.commit();
+
+  stop();
+
+  var timer = setTimeout(function() {
+    start();
+    ok(false, "timeout exceeded waiting for fixture data");
+  }, 1000);
+});
+
+test("should delete record asynchronously when it is commited", function() {
+  var paul = store.findByClientId(Person, store.load(Person, 1, {first_name: 'Paul', last_name: 'Chavard', height: 70}).clientId);
+
+  paul.deleteRecord();
+
+  paul.on('didDelete', function() {
+    clearTimeout(timer);
+    start();
+
+    equal(get(paul, 'isDeleted'), true, "data deleted asynchronously");
+    equal(get(paul, 'isDirty'), false, "data deleted asynchronously");
+  });
+
+  store.commit();
 
   stop();
 
