163dc0be51cb5a9083a41daa53ed17cf9a9c5687
Richard Livsey <richard@livsey.org>
2012-06-14 16:09:52 +0100

    urlForEvent for a route with a dynamic part doesn't serialize the context

--- packages/ember-routing/tests/router_test.js
+++ packages/ember-routing/tests/router_test.js
@@ -148,6 +148,44 @@ test("router.urlForEvent works with changing context in the current state", func
 });
 
 
+test("router.urlForEvent works for nested routes with a context", function() {
+  var router = Ember.Router.create({
+    location: location,
+    namespace: namespace,
+    root: Ember.Route.create({
+      index: Ember.Route.create({
+        route: '/',
+
+        showDashboardActivity: function(router) {
+          router.transitionTo('dashboard.activity');
+        },
+
+        eventTransitions: {
+          showDashboardActivity: 'dashboard.activity'
+        }
+      }),
+
+      dashboard: Ember.Route.create({
+        route: '/dashboard/:component_id',
+
+        activity: Ember.Route.create({
+          route: '/activity'
+        })
+      })
+    })
+  });
+
+  Ember.run(function() {
+    router.route('/');
+  });
+
+  equal(router.getPath('currentState.path'), "root.index", "precond - the router is in root.index");
+
+  var url = router.urlForEvent('showDashboardActivity', { id: 1 });
+  equal(url, "#!#/dashboard/1/activity");
+});
+
+
 test("router.urlForEvent works with Ember.State.transitionTo", function() {
   var router = Ember.Router.create({
     location: location,
