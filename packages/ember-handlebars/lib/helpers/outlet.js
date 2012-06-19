2bb96946f319162bab53b6ebcf379133333f4e8e
pierre-arnaud lumalé <kadactivity@gmail.com>
2012-06-19 21:33:03 +0200

    outlet now support custom ContainerView without specifying an outlet name
    can ommit outlet name when using custom container view.
    can specify custom container view before or after outlet name.
    adding more test for different case use.

--- packages/ember-handlebars/lib/helpers/outlet.js
+++ packages/ember-handlebars/lib/helpers/outlet.js
@@ -1,5 +1,4 @@
 require('ember-handlebars/helpers/view');
-
 /**
   The `outlet` helper allows you to specify that the current
   view's controller will fill in the view for a given area.
@@ -17,36 +16,55 @@ require('ember-handlebars/helpers/view');
       {{outlet masterView}}
       {{outlet detailView}}
 
-  You can also specify a particular containerView for stuff like id, tag, aria-role 
-
-      {{outlet masterView App.MyMasterContainerView}}
-      {{outlet detailView App.MyDetailContainerView}}
-      
-  for the basic outlet use 'view' for naming the outlet
-  
-      {{outlet view App.MyCustomContainerView}}
-
   Then, you can control several outlets from a single
   controller:
 
       controller.set('masterView', postsView);
       controller.set('detailView', postView);
-
+  
+  The outlet can handle a custom ContainerView.
+  the custom ContainerView must be post fixed with "_Outlet".
+  
+      {{outlet}}                                    <- standard 
+      {{outlet view}}                               <- standard verbose 
+      {{outlet masterView}}                         <- custom outlet name 
+      
+      {{outlet view App.View_Outlet}}               <- standard + custom outlet view
+      {{outlet App.View_Outlet view}}               <- custom outlet view + standard 
+      
+      {{outlet masterView App.MasterView_Outlet}}   <- custom outlet name  + custom outlet view
+      {{outlet App.MasterView_Outlet masterView}}   <- custom outlet view + custom outlet name  
+  
   @name Handlebars.helpers.outlet
   @param {String} property the property on the controller that holds the view for this outlet
-  @param {String} view the containerView defining the outlet view settings
+  @param {String} view the custom ContainerView settings html data for outlet block (id, tag, class, aria, ...)
 */
 Ember.Handlebars.registerHelper('outlet', function(property, view, options) {
-  Ember.assert('you can provide an "outletName" and a custom "Ember.ContainerView" ... no more!', arguments.length <= 3);
+  Ember.assert('you can provide an "outletName" and/or a custom "Ember.ContainerView" ... no more!', arguments.length <= 3);
+  
+  var regex = /_Outlet$/;
   
   if (property && property.data && property.data.isRenderData) {
       options = property;
       property = 'view';
-  } else if (view && view.data && view.data.isRenderData) {
+  } 
+  else if (view && view.data && view.data.isRenderData) {
     options = view;
+    
+    if (regex.test(property)) {
+      view = property;
+      property = 'view';
+    }
   }
   
-  if(typeof view !== "string") {
+  if ((typeof property === "string" && regex.test(property)) && (typeof view === "string" && !regex.test(view))) {
+    var v = view;
+    view = property;
+    property = v;
+    v = null;
+  }
+  
+  if (typeof view !== "string") {
     view = Ember.ContainerView;
   } 
   
--- packages/ember-handlebars/tests/helpers/outlet_test.js
+++ packages/ember-handlebars/tests/helpers/outlet_test.js
@@ -64,8 +64,8 @@ test("outlet should support an optional name", function() {
 });
 
 
-test('outlet should support an optional custom ContainerView.', function() {
-  Ember.MyCustomContainerView = Ember.ContainerView.extend({
+test('outlet should support an optional custom ContainerView without outlet name specified.', function() {
+  Ember.MyCustomContainer_Outlet = Ember.ContainerView.extend({
     elementId: "custom-id",
     tagName: "section"
   });
@@ -74,16 +74,64 @@ test('outlet should support an optional custom ContainerView.', function() {
 
   view = Ember.View.create({
     controller: controller,
-    template: compile("<h1>HI</h1>{{outlet mainView Ember.MyCustomContainerView}}")
+    template: compile("<h1>HELLO</h1>{{outlet Ember.MyCustomContainer_Outlet}}")
+  });
+
+  appendView(view);
+
+  Ember.run(function() {
+    controller.set('view', Ember.View.create({
+      template: compile("<p>WORLD</p>")
+    }));
+  });
+
+  equal(view.$("section#custom-id").text(), 'WORLD', 'outlet with custom ContainerView has been successfully founded !');
+});
+
+test('outlet should support an optional custom ContainerView with outlet name specified.', function() {
+  Ember.MyCustomContainer_Outlet = Ember.ContainerView.extend({
+    elementId: "custom-id",
+    tagName: "section"
+  });
+  
+  var controller = Ember.Object.create();
+
+  view = Ember.View.create({
+    controller: controller,
+    template: compile("<h1>HELLO</h1>{{outlet mainView Ember.MyCustomContainer_Outlet}}")
   });
 
   appendView(view);
 
   Ember.run(function() {
     controller.set('mainView', Ember.View.create({
-      template: compile("<p>BYE</p>")
+      template: compile("<p>WORLD</p>")
     }));
   });
 
-  equal(view.$("section#custom-id").text(), 'BYE', 'outlet with custom ContainerView has been successfully founded !');
+  equal(view.$("section#custom-id").text(), 'WORLD', 'outlet with custom ContainerView has been successfully founded !');
+});
+
+test('outlet should support an optional custom ContainerView with outlet name specified in inversed position.', function() {
+  Ember.MyCustomContainer_Outlet = Ember.ContainerView.extend({
+    elementId: "custom-id",
+    tagName: "section"
+  });
+  
+  var controller = Ember.Object.create();
+
+  view = Ember.View.create({
+    controller: controller,
+    template: compile("<h1>HELLO</h1>{{outlet Ember.MyCustomContainer_Outlet mainView}}")
+  });
+
+  appendView(view);
+
+  Ember.run(function() {
+    controller.set('mainView', Ember.View.create({
+      template: compile("<p>WORLD</p>")
+    }));
+  });
+
+  equal(view.$("section#custom-id").text(), 'WORLD', 'outlet with custom ContainerView has been successfully founded !');
 });
