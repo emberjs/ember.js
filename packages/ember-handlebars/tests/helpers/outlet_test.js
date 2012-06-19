704b3458c23349981acf432d715f03c3926a0fb0
pierre-arnaud lumalé <kadactivity@gmail.com>
2012-06-19 02:40:37 +0200

    test for the outlet custom ContainerView support.

--- packages/ember-handlebars/tests/helpers/outlet_test.js
+++ packages/ember-handlebars/tests/helpers/outlet_test.js
@@ -62,3 +62,33 @@ test("outlet should support an optional name", function() {
 
   equal(view.$().text(), 'HIBYE');
 });
+
+
+test('outlet should support an optional custom ContainerView. settings{ elementid: "custom-id", tagName: "section" }', function() {
+  Ember.MyCustomContainerView = Ember.ContainerView.extend({
+    elementid: "custom-id",
+    tagName: "section"
+  });
+  
+  var controller = Ember.Object.create();
+
+  var template = "<h1>HI</h1>{{outlet mainView Ember.MyCustomContainerView}}";
+  view = Ember.View.create({
+    controller: controller,
+    template: Ember.Handlebars.compile(template)
+  });
+
+  appendView(view);
+
+  equal(view.$().text(), 'HI');
+
+  Ember.run(function() {
+    controller.set('mainView', Ember.View.create({
+      template: compile("<p>BYE</p>")
+    }));
+  });
+
+  equal(view.$().text(), 'HIBYE');
+  
+  equal(view.$("section#custom-id").text(), 'BYE', 'outlet with an elementId "custom-id" and tagName "section" has been successfully founded !');
+});
