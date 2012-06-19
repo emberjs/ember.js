b211c0030d5291430f462ae8582154b703ae53aa
c0urg3tt3 <kadactivity@gmail.com>
2012-06-19 03:30:43 +0300

    allow outlet to use a custom ContainerView.

--- packages/ember-handlebars/lib/helpers/outlet.js
+++ packages/ember-handlebars/lib/helpers/outlet.js
@@ -17,6 +17,15 @@ require('ember-handlebars/helpers/view');
       {{outlet masterView}}
       {{outlet detailView}}
 
+  You can also specify a particular containerView for stuff like id, tag, aria-role 
+
+      {{outlet masterView App.MyMasterContainerView}}
+      {{outlet detailView App.MyDetailContainerView}}
+      
+  for the basic outlet use 'view' for naming the outlet
+  
+      {{outlet view App.MyCustomContainerView}}
+
   Then, you can control several outlets from a single
   controller:
 
@@ -24,15 +33,23 @@ require('ember-handlebars/helpers/view');
       controller.set('detailView', postView);
 
   @name Handlebars.helpers.outlet
-  @param {String} property the property on the controller
-    that holds the view for this outlet
+  @param {String} property the property on the controller that holds the view for this outlet
+  @param {String} view the containerView defining the outlet view settings
 */
-Ember.Handlebars.registerHelper('outlet', function(property, options) {
+Ember.Handlebars.registerHelper('outlet', function(property, view, options) {
+  Ember.assert('you can provide an "outletName" and a custom "Ember.ContainerView" ... no more!', arguments.length <= 3);
+  
   if (property && property.data && property.data.isRenderData) {
     options = property;
     property = 'view';
+  } else if (view && view.data && view.data.isRenderData) {
+    options = view;
   }
 
+  if(typeof view !== "string") {
+    view = Ember.ContainerView;
+  }
+  
   options.hash.currentViewBinding = "controller." + property;
 
   return Ember.Handlebars.helpers.view.call(this, Ember.ContainerView, options);
