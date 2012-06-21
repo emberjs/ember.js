d8aeecd656d11a3f1a4871bdcc88a586ad33072e
c0urg3tt3 <kadactivity@gmail.com>
2012-06-21 04:53:38 +0300

    Update master

--- packages/ember-handlebars/lib/helpers/outlet.js
+++ packages/ember-handlebars/lib/helpers/outlet.js
@@ -22,18 +22,56 @@ require('ember-handlebars/helpers/view');
 
       controller.set('masterView', postsView);
       controller.set('detailView', postView);
-
+ 
+  The outlet can handle a subclass of Ember.ContainerView for html settings.
+  
+      {{outlet}}                                    <- standard 
+      {{outlet view}}                               <- standard verbose 
+      {{outlet masterView}}                         <- custom outlet name 
+      {{outlet App.CustomOutlet}}                   <- custom outlet view 
+      {{outlet masterView App.MasterOutlet}}        <- custom outlet name + custom outlet view
+  
   @name Handlebars.helpers.outlet
-  @param {String} property the property on the controller
+  @param {String} property the property on the controller 
     that holds the view for this outlet
+  @param {String} view a subclass of Ember.ContainerView 
+    providing html settings for outlet block (id, tag, class, role, ...)
 */
-Ember.Handlebars.registerHelper('outlet', function(property, options) {
-  if (property && property.data && property.data.isRenderData) {
-    options = property;
-    property = 'view';
+Ember.Handlebars.registerHelper('outlet', function(property, view, options) {
+  Ember.assert(
+    'you can provide an outletName as "property" and/or a subclass of Ember.ContainerView as "view"', 
+    arguments.length <= 3
+  );
+  
+  var viewPath;
+  
+  switch (arguments.length) {
+    case 1: 
+      if (property && property.data && property.data.isRenderData) {
+        options = property;
+        property = 'view';
+      } 
+      break;
+    case 2:
+      if (view && view.data && view.data.isRenderData) {
+        options = view;
+        viewPath = (typeof property === "string" && Ember.getPath(property) !== undefined) ? Ember.getPath(property) : null;
+        if (viewPath !== null && Ember.ContainerView.detect(viewPath)) {
+          view = property;
+          property = 'view';
+        }
+      }
+      break;
+    case 3:
+      break;
+  }
+  
+  viewPath = (typeof view === "string" && Ember.getPath(view) !== undefined) ? Ember.getPath(view) : null;
+  if (viewPath === null || (viewPath !== null && !Ember.ContainerView.detect(viewPath))) {
+    view = Ember.ContainerView;
   }
 
   options.hash.currentViewBinding = "controller." + property;
-
-  return Ember.Handlebars.helpers.view.call(this, Ember.ContainerView, options);
+  
+  return Ember.Handlebars.helpers.view.call(this, view, options);
 });
