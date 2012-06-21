09662d93a29898cf2b2c5e173ef551075eed720e
c0urg3tt3 <kadactivity@gmail.com>
2012-06-21 04:55:46 +0300

    Update patch-3

--- packages/ember-handlebars/tests/helpers/outlet_test.js
+++ packages/ember-handlebars/tests/helpers/outlet_test.js
@@ -62,3 +62,49 @@ test("outlet should support an optional name", function() {
 
   equal(view.$().text(), 'HIBYE');
 });
+
+
+test('outlet should support an optional subclassed Ember.ContainerView without an outlet name specified.', function() {
+  Ember.MyCustomContainerView = Ember.ContainerView.extend({
+    elementId: "custom-id",
+    tagName: "section"
+  });
+  
+  var controller = Ember.Object.create();
+
+  view = Ember.View.create({
+    controller: controller,
+    template: compile("<h1>HI</h1>{{outlet Ember.MyCustomContainerView}}")
+  });
+
+  appendView(view);
+
+  Ember.run(function() {
+    controller.set('view', Ember.View.create({ template: compile("<p>BYE</p>") }));
+  });
+
+  equal(view.$("section#custom-id").text(), 'BYE', 'outlet with custom ContainerView has been successfully founded !');
+});
+
+
+test('outlet should support an optional subclassed Ember.ContainerView with an outlet name specified..', function() {
+  Ember.MyCustomContainerView = Ember.ContainerView.extend({
+    elementId: "custom-id",
+    tagName: "section"
+  });
+  
+  var controller = Ember.Object.create();
+
+  view = Ember.View.create({
+    controller: controller,
+    template: compile("<h1>HI</h1>{{outlet customView Ember.MyCustomContainerView}}")
+  });
+
+  appendView(view);
+
+  Ember.run(function() {
+    controller.set('customView', Ember.View.create({ template: compile("<p>BYE</p>") }));
+  });
+
+  equal(view.$("section#custom-id").text(), 'BYE', 'outlet with custom ContainerView has been successfully founded !');
+});
