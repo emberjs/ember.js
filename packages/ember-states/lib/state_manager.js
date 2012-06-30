541d085ae039a190d24213d1476de53029355bf9
Peter Wagenet <peter.wagenet@gmail.com>
2012-06-29 20:54:45 -0700

    Don't set empty context

--- packages/ember-states/lib/state_manager.js
+++ packages/ember-states/lib/state_manager.js
@@ -444,7 +444,6 @@ Ember.StateManager = Ember.State.extend(
   
   send: function(event, context) {
     Ember.assert('Cannot send event "' + event + '" while currentState is ' + get(this, 'currentState'), get(this, 'currentState'));
-    if (arguments.length === 1) { context = {}; }
     return this.sendRecursively(event, get(this, 'currentState'), context);
   },
 
