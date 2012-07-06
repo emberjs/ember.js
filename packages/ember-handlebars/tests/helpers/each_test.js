ea75eafca514240c99ba7971a814b84b551f0b76
Peter Wagenet <peter.wagenet@gmail.com>
2012-07-06 10:20:34 -0700

    Revert "Views should inherit parent context if available"
    Unfortunately, this causes some major bugs in other areas.
    
    This reverts commit 4484ae4987e645eba0efde56c44b87656eb0df03.

--- packages/ember-handlebars/tests/helpers/each_test.js
+++ packages/ember-handlebars/tests/helpers/each_test.js
@@ -191,33 +191,4 @@ if (Ember.VIEW_PRESERVES_CONTEXT) {
     equal(view.$().text(), "My Cool Each Test 1My Cool Each Test 2");
   });
 
-  test("views inside #each preserve the new context", function() {
-    var controller = Ember.A([ { name: "Adam" }, { name: "Steve" } ]);
-
-    view = Ember.View.create({
-      controller: controller,
-      template: templateFor('{{#each controller}}{{#view}}{{name}}{{/view}}{{/each}}')
-    });
-
-    append(view);
-
-    equal(view.$().text(), "AdamSteve");
-  });
-
-  test("views inside #each use a controller as their context if set explicitly", function() {
-    var controller = Ember.A([ { name: "Adam" }, { name: "Steve" } ]);
-
-    view = Ember.View.create({
-      controller: controller,
-      template: templateFor('{{#each controller}}{{#view view.itemView}}{{name}}{{/view}}{{/each}}'),
-
-      itemView: Ember.View.extend({
-        controller: { name: "Ryan" }
-      })
-    });
-
-    append(view);
-
-    equal(view.$().text(), "RyanRyan");
-  });
 }
