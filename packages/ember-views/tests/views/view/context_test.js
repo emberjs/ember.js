460078ca8da81e0380f8e8a9c61f90a46c5f26bd
tchak <paul@chavard.net>
2012-07-06 22:11:52 +0200

    add failing test for 4484ae4987e645eba0efde56c44b87656eb0df03

---
+++ packages/ember-views/tests/views/view/context_test.js
@@ -0,0 +1,39 @@
+module("Ember.View - context property");
+
+test("setting a controller on a inner view should change it context", function() {
+  var App = {};
+  var a = { name: 'a' };
+  var b = { name: 'b' };
+
+  var InnerView = Ember.View.extend({
+    render: function(buffer) {
+      buffer.push('inner: ');
+      buffer.push(this.getPath('context.name'));
+    }
+  });
+
+  var innerView = null;
+  var middleView = Ember.ContainerView.create();
+  var outerView = App.outerView = Ember.ContainerView.create({
+    controller: a
+  });
+
+  Ember.run(function() {
+    outerView.appendTo('#qunit-fixture');
+  });
+
+  Ember.run(function () {
+    outerView.set('currentView', middleView);
+  });
+
+  Ember.run(function () {
+    innerView = InnerView.create();
+    innerView.set('controller', b);
+    middleView.set('currentView', innerView);
+  });
+
+  // assert
+  equal(outerView.get('context'), a, 'outer context correct');
+  equal(middleView.get('context'), a, 'middle context correct');
+  equal(innerView.get('context'), b, 'inner context correct');
+});
