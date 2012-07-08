526e0c51f581699e179201a0e7a890cc8a9a7230
Peter Wagenet <peter.wagenet@gmail.com>
2012-07-08 13:46:00 -0700

    Use Ember.set when cloning keywords - fixes #1062

--- packages/ember-views/lib/views/view.js
+++ packages/ember-views/lib/views/view.js
@@ -750,13 +750,13 @@ Ember.View = Ember.Object.extend(Ember.Evented,
         controller = get(this, 'controller');
 
     var keywords = templateData ? Ember.copy(templateData.keywords) : {};
-    keywords.view = get(this, 'concreteView');
+    set(keywords, 'view', get(this, 'concreteView'));
 
     // If the view has a controller specified, make it available to the
     // template. If not, pass along the parent template's controller,
     // if it exists.
     if (controller) {
-      keywords.controller = controller;
+      set(keywords, 'controller', controller);
     }
 
     return keywords;
