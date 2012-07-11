e616ab22c040158b38b0f6382a711cae5252902f
Kris Selden <kris.selden@gmail.com>
2012-07-11 02:31:59 -0700

    add another test for the previous commit but for an outlet setup in the prerender state

--- packages/ember-handlebars/tests/helpers/outlet_test.js
+++ packages/ember-handlebars/tests/helpers/outlet_test.js
@@ -40,6 +40,24 @@ test("outlet should allow controllers to fill in slots", function() {
   equal(view.$().text(), 'HIBYE');
 });
 
+test("outlet should allow controllers to fill in slots in prerender state", function() {
+  var controller = Ember.Object.create({
+    view: Ember.View.create({
+      template: compile("<p>BYE</p>")
+    })
+  });
+
+  var template = "<h1>HI</h1>{{outlet}}";
+  view = Ember.View.create({
+    controller: controller,
+    template: Ember.Handlebars.compile(template)
+  });
+
+  appendView(view);
+
+  equal(view.$().text(), 'HIBYE');
+});
+
 
 test("outlet should support an optional name", function() {
   var controller = Ember.Object.create();
