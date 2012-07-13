553d560c4cdea9154f88788c6e53a9bffa9c704e
Kris Selden <kris.selden@gmail.com>
2012-07-12 22:48:02 -0700

    guard unknown property checks with undefined check for performance, speeds up a normal get key

--- packages/ember-metal/lib/accessors.js
+++ packages/ember-metal/lib/accessors.js
@@ -22,18 +22,23 @@ var MANDATORY_SETTER = Ember.ENV.MANDATORY_SETTER;
 /** @private */
 get = function get(obj, keyName) {
   Ember.assert("You need to provide an object and key to `get`.", !!obj && keyName);
+
   var meta = obj[META_KEY], desc = meta && meta.descs[keyName], ret;
   if (desc) {
     return desc.get(obj, keyName);
   } else {
-    var isUnknown = 'object' === typeof obj && !(keyName in obj);
+    if (MANDATORY_SETTER && meta && meta.watching[keyName] > 0) {
+      ret = meta.values[keyName];
+    } else {
+      ret = obj[keyName];
+    }
 
-    if (isUnknown && 'function' === typeof obj.unknownProperty) {
+    if (ret === undefined &&
+        'object' === typeof obj && !(keyName in obj) && 'function' === typeof obj.unknownProperty) {
       return obj.unknownProperty(keyName);
     }
 
-    if (MANDATORY_SETTER && meta && meta.watching[keyName] > 0) { return meta.values[keyName]; }
-    return obj[keyName];
+    return ret;
   }
 };
 
