76af834b09844c491c50fd25d6d5f37255c59540
Peter Wagenet <peter.wagenet@gmail.com>
2012-07-19 14:01:16 -0700

    Fix warning about appending to an existing view - Fixes #1190

--- packages/ember-views/lib/views/view.js
+++ packages/ember-views/lib/views/view.js
@@ -1151,11 +1151,10 @@ Ember.View = Ember.Object.extend(Ember.Evented,
     @returns {Ember.View} receiver
   */
   appendTo: function(target) {
-    Ember.assert("You cannot append to an existing Ember.View. Consider using Ember.ContainerView instead.", !Ember.$(target).is('.ember-view') && !Ember.$(target).parents().is('.ember-view'));
-
     // Schedule the DOM element to be created and appended to the given
     // element after bindings have synchronized.
     this._insertElementLater(function() {
+      Ember.assert("You cannot append to an existing Ember.View. Consider using Ember.ContainerView instead.", !Ember.$(target).is('.ember-view') && !Ember.$(target).parents().is('.ember-view'));
       this.$().appendTo(target);
     });
 
