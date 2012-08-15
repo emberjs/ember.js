d4b3a00f31789db62bba2e9930d6c368f561e4b4
Niklas Hofer <niklas+dev@lanpartei.de>
2012-08-15 20:03:06 +0200

    warn if not router was found to init an app
    This can happen if the developer relies on autodetection of router based
    on the namespace, but calls App#initialize < Ember.Application before
    the router is loaded (for example in sprockets).

--- packages/ember-application/lib/system/application.js
+++ packages/ember-application/lib/system/application.js
@@ -133,6 +133,8 @@ Ember.Application = Ember.Namespace.extend(
 
     if (router && router instanceof Ember.Router) {
       this.startRouting(router);
+    } else {
+      Ember.logger.warn("no router given or found to initialize Ember Application", this.toString());
     }
   },
 
