ccbb3a1a07267915a4c928a488e7b12f3be9e99d
Peter Wagenet <peter.wagenet@gmail.com>
2012-08-31 22:21:36 -0700

    Converted ember-states docs

--- packages/ember-states/lib/main.js
+++ packages/ember-states/lib/main.js
@@ -5,5 +5,14 @@
 // ==========================================================================
 
 require('ember-runtime');
+
+/**
+Ember States
+
+@module ember
+@submodule ember-states
+@requires ember-runtime
+*/
+
 require('ember-states/state_manager');
 require('ember-states/state');
