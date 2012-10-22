b4ad9b1a49b1ccac673dd4f4192143b5c645f461
Square <github@squareup.com>
2012-10-22 16:21:05 -0700

    Revert "Can't use jQuery inside of the runtime tests"
    This reverts commit d34adc23849f95ae49d976f9692fef4c047b30c7.

--- packages/ember-runtime/tests/mixins/sortable_test.js
+++ packages/ember-runtime/tests/mixins/sortable_test.js
@@ -9,7 +9,7 @@ module("Ember.Sortable");
 module("Ember.Sortable with content", {
   setup: function() {
     Ember.run(function() {
-      unsortedArray = Ember.A(Ember.A(array).copy());
+      unsortedArray = Ember.A(Ember.A(Ember.$.extend(true, [], array)).copy());
 
       sortedArrayController = Ember.ArrayProxy.create(Ember.SortableMixin, {
         content: unsortedArray
@@ -89,13 +89,9 @@ test("don't remove and insert if position didn't change", function() {
     }
   });
 
-  var obj = {id: 4, name: 'Scumbag Tomhuda'};
-
-  sortedArrayController.pushObject(obj);
-
   sortedArrayController.set('sortProperties', ['name']);
 
-  Ember.set(obj, 'name', 'Scumbag Tomhuda Katzdale');
+  Ember.set(sortedArrayController.objectAt(0), 'name', 'Scumbag Brynjolfsson');
 
   ok(!insertItemSortedCalled, "insertItemSorted should not have been called");
 });
