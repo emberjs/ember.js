135249c5ab299e716d6ebf59a2a516c9bb21630e
twinturbo <me@broadcastingadam.com>
2012-11-15 09:56:26 +0100

    Format inline docs

--- packages/ember-handlebars/lib/controls/radio_button.js
+++ packages/ember-handlebars/lib/controls/radio_button.js
@@ -87,84 +87,91 @@ Ember.RadioButton = Ember.Control.extend(
 
   You can create radio buttons like this:
 
-      #handlebars
-      {{#view Ember.RadioButtonGroup name="someName"}}
-        <label>
-          {{view RadioButton value="option1"}}
-          Option 1
-        </label>
-        <label>
-          {{view RadioButton value="option2"}}
-        </label>
-      {{/view}}
+  ```handlebars
+  {{#view Ember.RadioButtonGroup name="someName"}}
+    <label>
+      {{view RadioButton value="option1"}}
+      Option 1
+    </label>
+    <label>
+      {{view RadioButton value="option2"}}
+    </label>
+  {{/view}}
+  ```
 
   ## Getting/Setting the selected radio button
 
-      #js
-      // get a reference to the selected radio button
-      group.get("selection");
+  ```javascript
+  // get a reference to the selected radio button
+  group.get("selection");
 
-      // select a different radio button
-      group.set("selection", someRadioButton);
-      // or set a button as selected
-      someRadioButton.set("isSelected", true);
+  // select a different radio button
+  group.set("selection", someRadioButton);
 
-      // clear the selection
-      group.set("selection", null);
-      // or deselect the selected button
-      selectedButton.set("isSelected", false);
+  // or set a button as selected
+  someRadioButton.set("isSelected", true);
+
+  // clear the selection
+  group.set("selection", null);
+
+  // or deselect the selected button
+  selectedButton.set("isSelected", false);
+  ```
 
   ## Getting/Setting the selected value
 
-      #js
-      // get the `value` property of the selected radio button
-      // or `null` if no buttons are selected
-      group.get("value");
+  ```javascipt
+  // get the `value` property of the selected radio button
+  // or `null` if no buttons are selected
+  group.get("value");
 
-      // select a different radio button by value
-      group.set("value", someValue);
+  // Select a different radio button by value.
+  // This also selects the proper radio button in the UI
+  group.set("value", someValue);
+  ```
 
   ## Real world example
 
-      #js
-      window.App = Ember.Application.create();
+  ```javascript
+  window.App = Ember.Application.create();
 
-      App.question = Ember.Object.create({
-        content: "Which of the following is the largest?",
-        possibleAnswers: [
-          Ember.Object.create({ label: "A peanut"      value: "peanut"      }),
-          Ember.Object.create({ label: "An elephant"   value: "elephant"    }),
-          Ember.Object.create({ label: "The moon"      value: "moon"        }),
-          Ember.Object.create({ label: "A tennis ball" value: "tennis ball" })
-        ],
-        selectedAnswer: null
-      });
+  App.question = Ember.Object.create({
+    content: "Which of the following is the largest?",
+    possibleAnswers: [
+      Ember.Object.create({ label: "A peanut"      value: "peanut"      }),
+      Ember.Object.create({ label: "An elephant"   value: "elephant"    }),
+      Ember.Object.create({ label: "The moon"      value: "moon"        }),
+      Ember.Object.create({ label: "A tennis ball" value: "tennis ball" })
+    ],
+    selectedAnswer: null
+  });
 
-      App.questionView = Ember.View.create({
-        templateName: "question",
-        questionBinding: "App.question",
-        group: Ember.RadioButtonGroup.create({
-          // create a two-way binding so changes in the
-          // view propogate to the `selectedAnswer` property
-          // on the question object.
-          value: "App.question.selectedAnswer"
-        })
-      });
+  App.questionView = Ember.View.create({
+    templateName: "question",
+    questionBinding: "App.question",
+    group: Ember.RadioButtonGroup.create({
+      // create a two-way binding so changes in the
+      // view propogate to the `selectedAnswer` property
+      // on the question object.
+      value: "App.question.selectedAnswer"
+    })
+  });
 
-      App.questionView.append();
+  App.questionView.append();
+  ```
 
   The question template could look like this:
 
-      <script type="text/x-handlebars" data-template-name="question">
-        <h2>{{question.content}}</h2>
-        {{#view Ember.RadioButtonGroup name="answer" value="App.question.selectedAnswer"}}
-        {{#each question.possibleAnswers}}
-          <label>
-            {{view RadioButton valueBinding="value"}}
-            {{label}}
-          </label>
-        {{/each}}
-      </script>
+  ```handlebars
+  <h2>{{question.content}}</h2>
+  {{#view Ember.RadioButtonGroup name="answer" value="App.question.selectedAnswer"}}
+  {{#each question.possibleAnswers}}
+    <label>
+      {{view RadioButton valueBinding="value"}}
+      {{label}}
+    </label>
+  {{/each}}
+  ```
 
   @extends Ember.View
 */
