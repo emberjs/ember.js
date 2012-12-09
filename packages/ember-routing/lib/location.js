8bb14d5e3b50abc58d7ed9b053709e604070de00
Yehuda Katz <wycats@gmail.com>
2012-12-09 11:22:05 -0800

    Move location to ember-routing

---
+++ packages/ember-routing/lib/location.js
@@ -0,0 +1,5 @@
+require('ember-views');
+require('ember-routing/location/api');
+require('ember-routing/location/none_location');
+require('ember-routing/location/hash_location');
+require('ember-routing/location/history_location');
