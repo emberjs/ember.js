b99c2b61303cf5e08954ff331e41a48443adbcdd
Yehuda Katz <wycats@gmail.com>
2013-01-05 18:18:37 -0800

    Require the controller ext

---
+++ packages/ember-routing/lib/ext.js
@@ -0,0 +1,2 @@
+require('ember-routing/ext/controller');
+require('ember-routing/ext/view');
--- packages/ember-routing/lib/main.js
+++ packages/ember-routing/lib/main.js
@@ -5,5 +5,5 @@ require('ember-routing/vendor/route-recognizer');
 require('ember-routing/vendor/router');
 require('ember-routing/system');
 require('ember-routing/helpers');
-require('ember-routing/ext/view');
+require('ember-routing/ext');
 require('ember-routing/location');
