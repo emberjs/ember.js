c112692bc058e38a130b5f80a6798fa98fae1291
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2013-01-14 05:19:48 -0800

    Adding output

--- tasks/distribute.rb
+++ tasks/distribute.rb
@@ -16,11 +16,13 @@ namespace :distribute do
 
   desc "Distribute a tagged release of the ember.js builds"
   task :release => :build do
+    puts "Distributing releases of #{distribution_files.join ", "}"
     Release.new(distribution_files, EMBER_VERSION).distribute!
   end
 
   desc "Distribute the latest ember.js builds"
   task :latest => :build do
+    puts "Distribuing latest builds of #{distribution_files.join ", "}"
     Latest.new(distribution_files).distribute!
   end
 end
