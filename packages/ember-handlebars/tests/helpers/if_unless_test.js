fc56b1e8893b9168a411562e33d6e2547d4c1d5a
tomhuda <tomhuda@tilde.io>
2013-01-18 14:44:31 -0800

    Empty object proxies are not truthy in {{#if}}
    This addresses at least one issue in #1812

--- packages/ember-handlebars/tests/helpers/if_unless_test.js
+++ packages/ember-handlebars/tests/helpers/if_unless_test.js
@@ -2,6 +2,8 @@ var appendView = function(view) {
   Ember.run(function() { view.appendTo('#qunit-fixture'); });
 };
 
+var compile = Ember.Handlebars.compile;
+
 var view;
 
 module("Handlebars {{#if}} and {{#unless}} helpers", {
@@ -18,10 +20,36 @@ test("unless should keep the current context (#784)", function() {
   view = Ember.View.create({
     o: Ember.Object.create({foo: '42'}),
 
-    template: Ember.Handlebars.compile('{{#with view.o}}{{#view Ember.View}}{{#unless view.doesNotExist}}foo: {{foo}}{{/unless}}{{/view}}{{/with}}')
+    template: compile('{{#with view.o}}{{#view Ember.View}}{{#unless view.doesNotExist}}foo: {{foo}}{{/unless}}{{/view}}{{/with}}')
   });
 
   appendView(view);
 
   equal(view.$().text(), 'foo: 42');
+});
+
+test("The `if` helper tests for `isTruthy` if available", function() {
+  view = Ember.View.create({
+    truthy: Ember.Object.create({ isTruthy: true }),
+    falsy: Ember.Object.create({ isTruthy: false }),
+
+    template: compile('{{#if view.truthy}}Yep{{/if}}{{#if view.falsy}}Nope{{/if}}')
+  });
+
+  appendView(view);
+
+  equal(view.$().text(), 'Yep');
+});
+
+test("The `if` helper does not print the contents for an object proxy without content", function() {
+  view = Ember.View.create({
+    truthy: Ember.ObjectProxy.create({ content: {} }),
+    falsy: Ember.ObjectProxy.create({ content: null }),
+
+    template: compile('{{#if view.truthy}}Yep{{/if}}{{#if view.falsy}}Nope{{/if}}')
+  });
+
+  appendView(view);
+
+  equal(view.$().text(), 'Yep');
 });
