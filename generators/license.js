3f84548699fdf631b02be61d57456f67d3e45468
Peter Wagenet <peter.wagenet@gmail.com>
2013-01-18 09:14:22 -0800

    Update license year

--- generators/license.js
+++ generators/license.js
@@ -1,6 +1,6 @@
 // ==========================================================================
 // Project:   Ember - JavaScript Application Framework
-// Copyright: ©2011-2012 Tilde Inc. and contributors
+// Copyright: ©2011-2013 Tilde Inc. and contributors
 //            Portions ©2006-2011 Strobe Inc.
 //            Portions ©2008-2011 Apple Inc. All rights reserved.
 // License:   Licensed under MIT license
