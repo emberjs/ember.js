4f2c27c720a414f740dcfc960d54f34acc145abb
Stefan Penner <stefan.penner@gmail.com>
2013-01-27 17:28:45 -0500

    Refactor Controller#needs into ember-application, rather then ember-routing.

--- packages/ember-routing/lib/ext/controller.js
+++ packages/ember-routing/lib/ext/controller.js
@@ -4,37 +4,8 @@
 */
 
 var get = Ember.get, set = Ember.set;
-var ControllersProxy = Ember.Object.extend({
-  controller: null,
-
-  unknownProperty: function(controllerName) {
-    var controller = get(this, 'controller'),
-      needs = get(controller, 'needs'),
-      container = controller.get('container'),
-      dependency;
-
-    for (var i=0, l=needs.length; i<l; i++) {
-      dependency = needs[i];
-      if (dependency === controllerName) {
-        return container.lookup('controller:' + controllerName);
-      }
-    }
-  }
-});
 
 Ember.ControllerMixin.reopen({
-  concatenatedProperties: ['needs'],
-  needs: [],
-
-  init: function() {
-    this._super.apply(this, arguments);
-
-    // Structure asserts to still do verification but not string concat in production
-    if(!verifyDependencies(this)) {
-      Ember.assert("Missing dependencies", false);
-    }
-  },
-
   transitionToRoute: function() {
     var target = get(this, 'target');
 
@@ -57,41 +28,11 @@ Ember.ControllerMixin.reopen({
     return this.replaceRoute.apply(this, arguments);
   },
 
-  controllerFor: function(controllerName) {
-    Ember.deprecate("Controller#controllerFor is depcrecated, please use Controller#needs instead");
-    var container = get(this, 'container');
-    return container.lookup('controller:' + controllerName);
-  },
-
   model: Ember.computed(function(key, value) {
     if (arguments.length > 1) {
       return set(this, 'content', value);
     } else {
       return get(this, 'content');
     }
-  }).property('content'),
-
-  controllers: Ember.computed(function() {
-    return ControllersProxy.create({ controller: this });
-  })
+  }).property('content')
 });
-
-function verifyDependencies(controller) {
-  var needs = get(controller, 'needs'),
-      container = get(controller, 'container'),
-      dependency, satisfied = true;
-
-  for (var i=0, l=needs.length; i<l; i++) {
-    dependency = needs[i];
-    if (dependency.indexOf(':') === -1) {
-      dependency = "controller:" + dependency;
-    }
-
-    if (!container.has(dependency)) {
-      satisfied = false;
-      Ember.assert(controller + " needs " + dependency + " but it does not exist", false);
-    }
-  }
-
-  return satisfied;
-}
