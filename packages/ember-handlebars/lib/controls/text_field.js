78480f32f11de516dc81c11a542cdd8ef67b572f
Mehul Kar <mehul.kar@gmail.com>
2013-02-08 20:14:42 -0800

    reset on enter attribute of text fields. for real this time

--- packages/ember-handlebars/lib/controls/text_field.js
+++ packages/ember-handlebars/lib/controls/text_field.js
@@ -55,6 +55,12 @@ Ember.TextField = Ember.View.extend(Ember.TextSupport,
   tagName: "input",
   attributeBindings: ['type', 'value', 'size', 'pattern'],
 
+  /** 
+    The `clearOnEnter` attribute of the input element. When `insertNewLine` is called, 
+    if this property is true, the text field will clear.
+  */
+  clearOnEnter: false,
+
   /**
     The `value` attribute of the input element. As the user inputs text, this
     property is updated live.
@@ -132,6 +138,10 @@ Ember.TextField = Ember.View.extend(Ember.TextSupport,
       if (!get(this, 'bubbles')) {
         event.stopPropagation();
       }
+    }
+
+    if ( get(this, 'clearOnEnter') ) {
+      set(this, 'value', null);
     }
   }
 });
