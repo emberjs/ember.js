399b061a8b71f33fbbb82fbf8024cd01a2939251
Herman J. Radtke III <hermanradtke@gmail.com>
2013-02-15 09:16:50 -0800

    Support Handlebars 1.0.0-rc.3 verison string.
    The existing regex to match Handlebars versions was expecting a string
    similar to 1.0.rc.2

--- packages/ember-handlebars/lib/ext.js
+++ packages/ember-handlebars/lib/ext.js
@@ -8,7 +8,7 @@ require("ember-views/system/render_buffer");
 var objectCreate = Ember.create;
 
 var Handlebars = Ember.imports.Handlebars;
-Ember.assert("Ember Handlebars requires Handlebars 1.0.beta.5 or greater", Handlebars && Handlebars.VERSION.match(/^1\.0\.beta\.[56789]$|^1\.0\.rc\.[123456789]+/));
+Ember.assert("Ember Handlebars requires Handlebars 1.0.beta.5 or greater", Handlebars && Handlebars.VERSION.match(/^1\.0\.beta\.[56789]$|^1\.0\.rc\.[123456789]+|^1\.0\.0\-rc\.[123456789]+/));
 
 /**
   Prepares the Handlebars templating library for use inside Ember's view
