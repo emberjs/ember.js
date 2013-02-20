7a8d2a2af351dc68954469a78348820dcc8fe0be
Jo Liss <joliss42@gmail.com>
2013-02-20 18:35:00 +0100

    Move Ember.Application docs into one place so YUIdoc doesn't lose half of it

--- packages/ember-runtime/lib/system/application.js
+++ packages/ember-runtime/lib/system/application.js
@@ -1,35 +1,3 @@
 require('ember-runtime/system/namespace');
 
-/**
-@module ember
-@submodule ember-runtime
-*/
-
-/**
-  Defines a namespace that will contain an executable application. This is
-  very similar to a normal namespace except that it is expected to include at
-  least a 'ready' function which can be run to initialize the application.
-
-  Currently `Ember.Application` is very similar to `Ember.Namespace.`  However,
-  this class may be augmented by additional frameworks so it is important to
-  use this instance when building new applications.
-
-  # Example Usage
-
-  ```javascript
-  MyApp = Ember.Application.create({
-    VERSION: '1.0.0',
-    store: Ember.Store.create().from(Ember.fixtures)
-  });
-
-  MyApp.ready = function() {
-    //..init code goes here...
-  }
-  ```
-
-  @class Application
-  @namespace Ember
-  @extends Ember.Namespace
-*/
 Ember.Application = Ember.Namespace.extend();
-
