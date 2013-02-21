emberjs/ember.js

commit c97a01947ea05654121cc8211908375637e4a7ac

Don't use a sub-container for itemController
While implementing this, I noticed that moving
an item inside an ArrayController that uses
itemController won't work (since it will get
destroyed synchronously). I tried to defer
destruction, but ran into complexity involving
null or duplicated elements.

This commit does not regress existing
functionality, but I figured I would call it out.

packages/ember-runtime/lib/controllers/array_controller.js
@@ -156,20 +156,20 @@ Ember.ArrayController = Ember.ArrayProxy.extend(Ember.ControllerMixin,
 
   arrangedContentDidChange: function() {
     this._super();
-    this._resetSubContainers();
+    this._resetSubControllers();
   },
 
   arrayContentDidChange: function(idx, removedCnt, addedCnt) {
-    var subContainers = get(this, 'subContainers'),
-        subContainersToRemove = subContainers.slice(idx, idx+removedCnt);
+    var subControllers = get(this, '_subControllers'),
+        subControllersToRemove = subControllers.slice(idx, idx+removedCnt);
 
-    forEach(subContainersToRemove, function(subContainer) {
-      if (subContainer) { subContainer.destroy(); }
+    forEach(subControllersToRemove, function(subController) {
+      if (subController) { subController.destroy(); }
     });
 
-    replace(subContainers, idx, removedCnt, new Array(addedCnt));
+    replace(subControllers, idx, removedCnt, new Array(addedCnt));
 
-    // The shadow array of subcontainers must be updated before we trigger
+    // The shadow array of subcontrollers must be updated before we trigger
     // observers, otherwise observers will get the wrong subcontainer when
     // calling `objectAt`
     this._super(idx, removedCnt, addedCnt);
@@ -177,42 +177,39 @@ Ember.ArrayController = Ember.ArrayProxy.extend(Ember.ControllerMixin,
 
   init: function() {
     this._super();
-    if (!this.get('content')) { this.set('content', Ember.A()); }
-    this._resetSubContainers();
+    if (!this.get('content')) { Ember.defineProperty(this, 'content', undefined, Ember.A()); }
+    this.set('_subControllers', Ember.A());
   },
 
   controllerAt: function(idx, object, controllerClass) {
     var container = get(this, 'container'),
-        subContainers = get(this, 'subContainers'),
-        subContainer = subContainers[idx],
-        controller;
+        subControllers = get(this, '_subControllers'),
+        subController = subControllers[idx];
 
-    if (!subContainer) {
-      subContainer = subContainers[idx] = container.child();
+    if (!subController) {
+      subController = container.lookup("controller:" + controllerClass, { singleton: false });
+      subControllers[idx] = subController;
     }
 
-    controller = subContainer.lookup("controller:" + controllerClass);
-    if (!controller) {
+    if (!subController) {
       throw new Error('Could not resolve itemController: "' + controllerClass + '"');
     }
 
-    controller.set('target', this);
-    controller.set('content', object);
+    subController.set('target', this);
+    subController.set('content', object);
 
-    return controller;
+    return subController;
   },
 
-  subContainers: null,
+  _subControllers: null,
 
-  _resetSubContainers: function() {
-    var subContainers = get(this, 'subContainers');
+  _resetSubControllers: function() {
+    var subControllers = get(this, '_subControllers');
 
-    if (subContainers) {
-      forEach(subContainers, function(subContainer) {
-        if (subContainer) { subContainer.destroy(); }
-      });
-    }
+    forEach(subControllers, function(subController) {
+      if (subControllers) { subController.destroy(); }
+    });
 
-    this.set('subContainers', Ember.A());
+    this.set('_subControllers', Ember.A());
   }
 });

packages/ember-runtime/tests/controllers/item_controller_class_test.js
@@ -131,19 +131,19 @@ test("when the underlying array changes, old subcontainers are destroyed", funct
   arrayController.objectAtContent(2);
 
   // Not a public API; just checking for cleanup
-  var subContainers = get(arrayController, 'subContainers'),
-      jaimeContainer = subContainers[1],
-      cerseiContainer = subContainers[2];
+  var subControllers = get(arrayController, '_subControllers'),
+      jaimeController = subControllers[1],
+      cerseiController = subControllers[2];
 
-  equal(!!jaimeContainer.isDestroyed, false, "precond - nobody is destroyed yet");
-  equal(!!!!cerseiContainer.isDestroyed, false, "precond - nobody is destroyed yet");
+  equal(!!jaimeController.isDestroying, false, "precond - nobody is destroyed yet");
+  equal(!!!!cerseiController.isDestroying, false, "precond - nobody is destroyed yet");
 
   Ember.run(function() {
     arrayController.set('content', Ember.A());
   });
 
-  equal(!!jaimeContainer.isDestroyed, true, "old subcontainers are destroyed");
-  equal(!!cerseiContainer.isDestroyed, true, "old subcontainers are destroyed");
+  equal(!!jaimeController.isDestroying, true, "old subcontainers are destroyed");
+  equal(!!cerseiController.isDestroying, true, "old subcontainers are destroyed");
 });
 
 
@@ -161,19 +161,17 @@ test("when items are removed from the arrayController, their respective subconta
   createArrayController();
   var jaimeController = arrayController.objectAtContent(1),
       cerseiController = arrayController.objectAtContent(2),
-      subContainers = get(arrayController, 'subContainers'),
-      jaimeContainer = subContainers[1],
-      cerseiContainer = subContainers[2];
+      subControllers = get(arrayController, '_subControllers');
 
-  equal(!!cerseiContainer.isDestroyed, false, "precond - nobody is destroyed yet");
-  equal(!!jaimeContainer.isDestroyed, false, "precond - nobody is destroyed yet");
+  equal(!!jaimeController.isDestroyed, false, "precond - nobody is destroyed yet");
+  equal(!!cerseiController.isDestroyed, false, "precond - nobody is destroyed yet");
 
   Ember.run(function() {
     arrayController.removeObject(cerseiController);
   });
 
-  equal(!!cerseiContainer.isDestroyed, true, "Removed objects' containers are cleaned up");
-  equal(!!jaimeContainer.isDestroyed, false, "Retained objects' containers are not cleaned up");
+  equal(!!cerseiController.isDestroying, true, "Removed objects' containers are cleaned up");
+  equal(!!jaimeController.isDestroying, false, "Retained objects' containers are not cleaned up");
 });
 
 test("one cannot remove wrapped content directly when specifying `itemController`", function() {
@@ -196,19 +194,17 @@ test("when items are removed from the underlying array, their respective subcont
   createArrayController();
   var jaimeController = arrayController.objectAtContent(1),
       cerseiController = arrayController.objectAtContent(2),
-      subContainers = get(arrayController, 'subContainers'),
-      jaimeContainer = subContainers[1],
-      cerseiContainer = subContainers[2];
+      subContainers = get(arrayController, 'subContainers');
 
-  equal(!!jaimeContainer.isDestroyed, false, "precond - nobody is destroyed yet");
-  equal(!!cerseiContainer.isDestroyed, false, "precond - nobody is destroyed yet");
+  equal(!!jaimeController.isDestroying, false, "precond - nobody is destroyed yet");
+  equal(!!cerseiController.isDestroying, false, "precond - nobody is destroyed yet");
 
   Ember.run(function() {
     lannisters.removeObject(cersei); // if only it were that easy
   });
 
-  equal(!!jaimeContainer.isDestroyed, false, "Retained objects' containers are not cleaned up");
-  equal(!!cerseiContainer.isDestroyed, true, "Removed objects' containers are cleaned up");
+  equal(!!jaimeController.isDestroyed, false, "Retained objects' containers are not cleaned up");
+  equal(!!cerseiController.isDestroyed, true, "Removed objects' containers are cleaned up");
 });
 
 test("`itemController` can be dynamic by overwriting `lookupItemController`", function() {

commit 6256da56970e42e11f9e6bbeec20aa950034d83b

lastObject should not raise exceptions on empty arrayControllers
It fix an error introduced in c97a01947ea05654121cc8211908375637e4a7ac

packages/ember-runtime/lib/controllers/array_controller.js
@@ -136,7 +136,8 @@ Ember.ArrayController = Ember.ArrayProxy.extend(Ember.ControllerMixin,
 
   objectAtContent: function(idx) {
     var length = get(this, 'length'),
-        object = get(this,'arrangedContent').objectAt(idx);
+        arrangedContent = get(this,'arrangedContent'),
+        object = arrangedContent && arrangedContent.objectAt(idx);
 
     if (idx >= 0 && idx < length) {
       var controllerClass = this.lookupItemController(object);

packages/ember-runtime/tests/controllers/array_controller_test.js
@@ -25,4 +25,6 @@ Ember.MutableArrayTests.extend({
 test("defaults it's `content` to an empty array", function () {
   var Controller = Ember.ArrayController.extend();
   deepEqual(Controller.create().get("content"), [], "`ArrayController` defaults it's content to an empty array");
+  equal(Controller.create().get('firstObject'), undefined, 'can fetch firstObject');
+  equal(Controller.create().get('lastObject'), undefined, 'can fetch lastObject');
 });