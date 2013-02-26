849785e272d968b37a99691f4e96dcafed31e153
Kris Selden <kris.selden@gmail.com>
2013-02-26 01:49:16 -0800

    use ember polyfill not jQuery

--- packages/ember-views/lib/system/ext.js
+++ packages/ember-views/lib/system/ext.js
@@ -6,5 +6,6 @@
 // Add a new named queue for rendering views that happens
 // after bindings have synced, and a queue for scheduling actions
 // that that should occur after view rendering.
-var queues = Ember.run.queues;
-queues.splice(Ember.$.inArray('actions', queues)+1, 0, 'render', 'afterRender');
+var queues = Ember.run.queues,
+    indexOf = Ember.ArrayPolyfills.indexOf;
+queues.splice(indexOf.call(queues, 'actions')+1, 0, 'render', 'afterRender');
