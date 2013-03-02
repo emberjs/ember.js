262e50e7035c6a4bc40b306575e31e3e009a9c07
machty <machty@gmail.com>
2013-03-02 16:36:08 -0500

    Normalized Ember.run.later and Ember.run.next behavior.
    In particular, callbacks for both are now guaranteed to be run from
    within a totally separate run loop; previously, though the docs
    suggested so, there was no guarantee the code would be run from
    within a run loop at all, much less a separate one.
    
    Ember.run.next is now shorthand for Ember.run.later with
    1ms wait, as a result, there was a chunk of `next` code
    that could be removed/consolidated in to `later`.
    
    Fixes #913

--- packages/ember-metal/tests/run_loop/later_test.js
+++ packages/ember-metal/tests/run_loop/later_test.js
@@ -1,14 +1,21 @@
+
+var originalSetTimeout = window.setTimeout;
+
+module('Ember.run.later', {
+  teardown: function() {
+    window.setTimeout = originalSetTimeout;
+  }
+});
+
 var previousPreventRunloop;
 
-test('should invoke after specified period of time - function only', function() {
+asyncTest('should invoke after specified period of time - function only', function() {
 
   var invoked = false;
 
   Ember.run(function() {
     Ember.run.later(function() { invoked = true; }, 100);
   });
-
-  stop();
 
   setTimeout(function() {
     start();
@@ -18,15 +25,13 @@ test('should invoke after specified period of time - function only', function() 
 });
 
 
-test('should invoke after specified period of time - target/method', function() {
+asyncTest('should invoke after specified period of time - target/method', function() {
 
   var obj = { invoked: false } ;
 
   Ember.run(function() {
     Ember.run.later(obj, function() { this.invoked = true; }, 100);
   });
-
-  stop();
 
   setTimeout(function() {
     start();
@@ -36,7 +41,7 @@ test('should invoke after specified period of time - target/method', function() 
 });
 
 
-test('should invoke after specified period of time - target/method/args', function() {
+asyncTest('should invoke after specified period of time - target/method/args', function() {
 
   var obj = { invoked: 0 } ;
 
@@ -44,11 +49,158 @@ test('should invoke after specified period of time - target/method/args', functi
     Ember.run.later(obj, function(amt) { this.invoked += amt; }, 10, 100);
   });
 
-  stop();
-
   setTimeout(function() {
     start();
     equal(obj.invoked, 10, 'should have invoked later item');
   }, 150);
 
+});
+
+asyncTest('should always invoke within a separate runloop', function() {
+  var obj = { invoked: 0 }, firstRunLoop, secondRunLoop;
+
+  Ember.run(function() {
+    firstRunLoop = Ember.run.currentRunLoop; 
+
+    Ember.run.later(obj, function(amt) { 
+      this.invoked += amt; 
+      secondRunLoop = Ember.run.currentRunLoop;
+    }, 10, 1);
+
+    // Synchronous "sleep". This simulates work being done
+    // after run.later was called but before the run loop
+    // has flushed. In previous versions, this would have
+    // caused the run.later callback to have run from
+    // within the run loop flush, since by the time the 
+    // run loop has to flush, it would have considered 
+    // the timer already expired.
+    var pauseUntil = +new Date() + 100;
+    while(+new Date() < pauseUntil) { /* do nothing - sleeping */ }
+  });
+
+  ok(firstRunLoop, "first run loop captured");
+  ok(!Ember.run.currentRunLoop, "shouldn't be in a run loop after flush");
+  equal(obj.invoked, 0, "shouldn't have invoked later item yet");
+
+  setTimeout(function() {
+    start();
+    equal(obj.invoked, 10, "should have invoked later item");
+    ok(secondRunLoop, "second run loop took place");
+    ok(secondRunLoop !== firstRunLoop, "two different run loops took place");
+  }, 150);
+});
+
+asyncTest('callback order', function() {
+
+  var array = [];
+  function fn(val) { array.push(val); }
+
+  Ember.run(function() {
+    Ember.run.later(this, fn, 4, 130);
+    Ember.run.later(this, fn, 1, 10);
+    Ember.run.later(this, fn, 5, 200);
+    Ember.run.later(this, fn, 2, 80);
+    Ember.run.later(this, fn, 3, 80);
+  });
+
+  deepEqual(array, []);
+
+  setTimeout(function() {
+    start();
+    deepEqual(array, [1,2,3,4,5], 'callbacks were called in expected order');
+  }, 250);
+});
+
+asyncTest('callbacks coalesce into same run loop if expiring at the same time', function() {
+
+  var array = [];
+  function fn(val) { array.push(Ember.run.currentRunLoop); }
+
+  Ember.run(function() {
+    Ember.run.later(this, fn, 10);
+    Ember.run.later(this, fn, 100);
+    Ember.run.later(this, fn, 100);
+  });
+
+  deepEqual(array, []);
+
+  setTimeout(function() {
+    start();
+    equal(array.length, 3, 'all callbacks called');
+    ok(array[0] !== array[1], 'first two callbacks have different run loops');
+    ok(array[0], 'first runloop present');
+    ok(array[1], 'second runloop present');
+    equal(array[1], array[2], 'last two callbacks got the same run loop');
+  }, 200);
+});
+
+asyncTest('inception calls to run.later should run callbacks in separate run loops', function() {
+
+  var runLoop, finished;
+
+  Ember.run(function() {
+    runLoop = Ember.run.currentRunLoop;
+    ok(runLoop);
+
+    Ember.run.later(function() {
+      ok(Ember.run.currentRunLoop && Ember.run.currentRunLoop !== runLoop, 
+         'first later callback has own run loop');
+      runLoop = Ember.run.currentRunLoop;
+
+      Ember.run.later(function() {
+        ok(Ember.run.currentRunLoop && Ember.run.currentRunLoop !== runLoop, 
+           'second later callback has own run loop');
+        finished = true;
+      }, 40);
+    }, 40);
+  });
+
+  setTimeout(function() {
+    start();
+    ok(finished, 'all .later callbacks run');
+  }, 150);
+});
+
+asyncTest('setTimeout should never run with a negative wait', function() {
+
+  // Rationale: The old run loop code was susceptible to an occasional
+  // bug where invokeLaterTimers would be scheduled with a setTimeout
+  // with a negative wait. Modern browsers normalize this to 0, but
+  // older browsers (IE <= 8) break with a negative wait, which
+  // happens when an expired timer callback takes a while to run,
+  // which is what we simulate here.
+  var newSetTimeoutUsed;
+  window.setTimeout = function() {
+    var wait = arguments[arguments.length - 1];
+    newSetTimeoutUsed = true;
+    ok(!isNaN(wait) && wait >= 0, 'wait is a non-negative number');
+    originalSetTimeout.apply(this, arguments);
+  };
+
+  var count = 0;
+  Ember.run(function() {
+
+    Ember.run.later(function() {
+      count++;
+
+      // This will get run first. Waste some time.
+      // This is intended to break invokeLaterTimers code by taking a
+      // long enough time that other timers should technically expire. It's
+      // fine that they're not called in this run loop; just need to
+      // make sure that invokeLaterTimers doesn't end up scheduling
+      // a negative setTimeout.
+      var pauseUntil = +new Date() + 60;
+      while(+new Date() < pauseUntil) { /* do nothing - sleeping */ }
+    }, 1);
+
+    Ember.run.later(function() {
+      equal(count, 1, 'callbacks called in order');
+    }, 50);
+  });
+
+  originalSetTimeout(function() {
+    window.setTimeout = originalSetTimeout;
+    start();
+    ok(newSetTimeoutUsed, 'stub setTimeout was used');
+  }, 200);
 });
--- packages/ember-metal/tests/run_loop/next_test.js
+++ packages/ember-metal/tests/run_loop/next_test.js
@@ -1,10 +1,8 @@
-module('system/run_loop/next_test');
+module('Ember.run.next');
 
-test('should invoke immediately on next timeout', function() {
+asyncTest('should invoke immediately on next timeout', function() {
 
   var invoked = false;
-
-  stop();
 
   Ember.run(function() {
     Ember.run.next(function() { invoked = true; });
@@ -18,4 +16,32 @@ test('should invoke immediately on next timeout', function() {
     equal(invoked, true, 'should have invoked later item');
   }, 20);
 
+});
+
+asyncTest('callback should be called from within separate loop', function() {
+  var firstRunLoop, secondRunLoop;
+  Ember.run(function() {
+    firstRunLoop = Ember.run.currentRunLoop;
+    Ember.run.next(function() { secondRunLoop = Ember.run.currentRunLoop; });
+  });
+
+  setTimeout(function() {
+    start();
+    ok(secondRunLoop, 'callback was called from within run loop');
+    ok(firstRunLoop && secondRunLoop !== firstRunLoop, 'two seperate run loops were invoked');
+  }, 20);
+});
+
+asyncTest('multiple calls to Ember.run.next share coalesce callbacks into same run loop', function() {
+  var firstRunLoop, secondRunLoop, thirdRunLoop;
+  Ember.run(function() {
+    firstRunLoop = Ember.run.currentRunLoop;
+    Ember.run.next(function() { secondRunLoop = Ember.run.currentRunLoop; });
+    Ember.run.next(function() { thirdRunLoop  = Ember.run.currentRunLoop; });
+  });
+
+  setTimeout(function() {
+    start();
+    ok(secondRunLoop && secondRunLoop === thirdRunLoop, 'callbacks coalesced into same run loop');
+  }, 20);
 });
