3e99938635b575d589850582684dbf39c6dc3572
Richard Livsey <richard@livsey.org>
2013-03-07 22:38:55 +0000

    remove unused variables from action helper

--- packages/ember-routing/lib/helpers/action.js
+++ packages/ember-routing/lib/helpers/action.js
@@ -51,9 +51,7 @@ Ember.onLoad('Ember.Handlebars', function(Handlebars) {
           event.stopPropagation();
         }
 
-        var view = options.view,
-            contexts = options.contexts,
-            target = options.target;
+        var target = options.target;
 
         if (target.target) {
           target = handlebarsGet(target.root, target.target, target.options);
@@ -257,7 +255,7 @@ Ember.onLoad('Ember.Handlebars', function(Handlebars) {
 
     var hash = options.hash,
         view = options.data.view,
-        controller, link;
+        controller;
 
     // create a hash to pass along to registerAction
     var action = {
