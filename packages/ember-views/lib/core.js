59342302433fa7e73652bdfaae8b999e56643bbe
Erik Bryn <erik.bryn@gmail.com>
2013-03-22 22:26:47 -0700

    Add support for jQuery 2.0

--- packages/ember-views/lib/core.js
+++ packages/ember-views/lib/core.js
@@ -4,7 +4,7 @@
 */
 
 var jQuery = Ember.imports.jQuery;
-Ember.assert("Ember Views require jQuery 1.8 or 1.9", jQuery && (jQuery().jquery.match(/^1\.(8|9)(\.\d+)?(pre|rc\d?)?/) || Ember.ENV.FORCE_JQUERY));
+Ember.assert("Ember Views require jQuery 1.8, 1.9 or 2.0", jQuery && (jQuery().jquery.match(/^((1\.(8|9))|2.0)(\.\d+)?(pre|rc\d?)?/) || Ember.ENV.FORCE_JQUERY));
 
 /**
   Alias for jQuery
