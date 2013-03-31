362b7bee0e4f1e6ee86ccf34bd1e1330294638db
Yehuda Katz <wycats@gmail.com>
2013-03-30 18:25:14 -0700

    Add {{input type="text"}}

--- packages/ember-handlebars/lib/controls.js
+++ packages/ember-handlebars/lib/controls.js
@@ -3,3 +3,26 @@ require("ember-handlebars/controls/text_field");
 require("ember-handlebars/controls/button");
 require("ember-handlebars/controls/text_area");
 require("ember-handlebars/controls/select");
+
+function normalizeHash(hash, hashTypes) {
+  for (var prop in hash) {
+    if (hashTypes[prop] === 'ID') {
+      hash[prop + 'Binding'] = hash[prop];
+      delete hash[prop];
+    }
+  }
+}
+
+Ember.Handlebars.registerHelper('input', function(options) {
+  Ember.assert('You can only pass attributes to the `input` helper, not arguments', arguments.length < 2);
+
+  var hash = options.hash,
+      types = options.hashTypes,
+      inputType = hash.type;
+
+  delete hash.type;
+
+  normalizeHash(hash, types);
+
+  return Ember.Handlebars.helpers.view.call(this, Ember.TextField, options);
+});
