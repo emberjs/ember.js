3dc670dc63659e9cd268a58ef710ef77b95fd80d
Luke Melia <luke@lukemelia.com>
2013-04-02 20:15:21 -0400

    Enhance Ember.TargetActionSupport and introduce Ember.ViewTargetActionSupport
    Enhancements to `Ember.TargetActionSupport` now allow `triggerAction` to receive an object specifying the desired action name and target. A target and/or action provided in the function call take precedence over any target/action properties in the object.
    
    `Ember.ViewTargetActionSupport` is a new mixin that encompasses `Ember.TargetActionSupport` and complements it by defining the default `target` as the view's `controller` and the default `actionContext` as the view's `context`.

---
+++ packages/ember-views/lib/mixins/view_target_action_support.js
@@ -0,0 +1,51 @@
+/**
+`Ember.ViewTargetActionSupport` is a mixin that can be included in a
+view class to add a `triggerAction` method with semantics similar to
+the Handlebars `{{action}}` helper. It provides intelligent defaults
+for the action's target: the view's controller; and the context that is
+sent with the action: the view's context.
+
+Note: In normal Ember usage, the `{{action}}` helper is usually the best
+choice. This mixin is most often useful when you are doing more complex
+event handling in custom View subclasses.
+
+For example:
+
+```javascript
+App.SaveButtonView = Ember.View.extend(Ember.ViewTargetActionSupport, {
+  action: 'save',
+  click: function(){
+    this.triggerAction(); // Sends the `save` action, along with the current context
+                          // to the current controller
+  }
+});
+```
+
+The `action` can be provided as properties of an optional object argument
+to `triggerAction` as well.
+
+```javascript
+App.SaveButtonView = Ember.View.extend(Ember.ViewTargetActionSupport, {
+  click: function(){
+    this.triggerAction({
+      action: 'save'
+    }); // Sends the `save` action, along with the current context
+        // to the current controller
+  }
+});
+```
+
+@class ViewTargetActionSupport
+@namespace Ember
+@extends Ember.TargetActionSupport
+*/
+Ember.ViewTargetActionSupport = Ember.Mixin.create(Ember.TargetActionSupport, {
+  /**
+  @property target
+  */
+  target: Ember.computed.alias('controller'),
+  /**
+  @property actionContext
+  */
+  actionContext: Ember.computed.alias('context')
+});
