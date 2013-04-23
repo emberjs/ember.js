24e00ebd62535f638d4d8533d03f2c7a7d6445ba
Yehuda Katz <wycats@gmail.com>
2013-04-23 10:40:39 +0000

    Normalize Ember.Handlebars.helper hashes

--- packages/ember-handlebars-compiler/lib/main.js
+++ packages/ember-handlebars-compiler/lib/main.js
@@ -37,6 +37,7 @@ Ember.Handlebars.helper = function(name, value) {
   if (Ember.View.detect(value)) {
     Ember.Handlebars.registerHelper(name, function(options) {
       Ember.assert("You can only pass attributes as parameters to a application-defined helper", arguments.length < 3);
+      options.hash = Ember.Handlebars.resolveHash(options.hash);
       return Ember.Handlebars.helpers.view.call(this, value, options);
     });
   } else {
