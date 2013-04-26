3c6dd5271cd4380a515c714edfbf7ca60f652445
Teddy Zeenny <teddyzeenny@gmail.com>
2013-04-26 19:49:29 +0300

    Add test for visit helper in ember testing

--- packages/ember-testing/tests/helpers_test.js
+++ packages/ember-testing/tests/helpers_test.js
@@ -38,3 +38,47 @@ test("Ember.Application#setupForTesting", function() {
   equal(App.__container__.lookup('router:main').location.implementation, 'none');
   equal(window.EMBER_APP_BEING_TESTED, App);
 });
+
+test("visit transitions to the correct route", function() {
+  expect(2);
+
+  var visit, currentRoute;
+
+  Ember.run(function() {
+    App = Ember.Application.create();
+    App.setupForTesting();
+  });
+
+  App.injectTestHelpers();
+  visit = window.visit;
+
+  App.Router.map(function() {
+    this.route('posts');
+    this.route('comments');
+  });
+
+  App.PostsRoute = Ember.Route.extend({
+    renderTemplate: function() {
+      currentRoute = 'posts';
+    }
+  });
+
+  App.CommentsRoute = Ember.Route.extend({
+    renderTemplate: function() {
+      currentRoute = 'comments';
+    }
+  });
+
+  Ember.run(App, App.advanceReadiness);
+
+  currentRoute = 'index';
+
+  visit('/posts').then(function() {
+    equal(currentRoute, 'posts', "Successfully visited posts route");
+    return visit('/comments');
+  }).then(function() {
+    equal(currentRoute, 'comments', "visit can be chained");
+  });
+
+  App.removeTestHelpers();
+});
