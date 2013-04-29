142228d81f2a74aaab0cb1d511056c91b412b860
Christian Wesselhoeft <christian@cwesselhoeft.com>
2013-04-29 10:30:12 -0700

    Switch from bind to on for routing handlers.

--- packages/ember-routing/lib/location/hash_location.js
+++ packages/ember-routing/lib/location/hash_location.js
@@ -60,7 +60,7 @@ Ember.HashLocation = Ember.Object.extend({
     var self = this;
     var guid = Ember.guidFor(this);
 
-    Ember.$(window).bind('hashchange.ember-location-'+guid, function() {
+    Ember.$(window).on('hashchange.ember-location-'+guid, function() {
       Ember.run(function() {
         var path = location.hash.substr(1);
         if (get(self, 'lastSetURL') === path) { return; }
