732fd818c91bfd2ae049702e326a5953e3adb5e3
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-05-05 16:31:27 +0900

    Fix argument name
    `decrementProperty` should receive argument as `decrement`.

--- packages/ember-runtime/lib/mixins/observable.js
+++ packages/ember-runtime/lib/mixins/observable.js
@@ -465,12 +465,12 @@ Ember.Observable = Ember.Mixin.create(/** @scope Ember.Observable.prototype */ {
 
     @method decrementProperty
     @param {String} keyName The name of the property to decrement
-    @param {Number} increment The amount to decrement by. Defaults to 1
+    @param {Number} decrement The amount to decrement by. Defaults to 1
     @return {Number} The new property value
   */
-  decrementProperty: function(keyName, increment) {
-    if (Ember.isNone(increment)) { increment = 1; }
-    set(this, keyName, (get(this, keyName) || 0)-increment);
+  decrementProperty: function(keyName, decrement) {
+    if (Ember.isNone(decrement)) { decrement = 1; }
+    set(this, keyName, (get(this, keyName) || 0)-decrement);
     return get(this, keyName);
   },
 
