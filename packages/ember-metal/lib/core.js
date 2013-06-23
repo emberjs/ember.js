893bbc4ecc3ad38b4a83370edeb966eae3d824f4
Stefan Penner <stefan.penner@gmail.com>
2013-06-23 15:14:46 -0400

    Version bump - 1.0.0-rc.6

--- packages/ember-metal/lib/core.js
+++ packages/ember-metal/lib/core.js
@@ -22,7 +22,7 @@
 
   @class Ember
   @static
-  @version 1.0.0-rc.5
+  @version 1.0.0-rc.6
 */
 
 if ('undefined' === typeof Ember) {
@@ -49,10 +49,10 @@ Ember.toString = function() { return "Ember"; };
 /**
   @property VERSION
   @type String
-  @default '1.0.0-rc.5'
+  @default '1.0.0-rc.6'
   @final
 */
-Ember.VERSION = '1.0.0-rc.5';
+Ember.VERSION = '1.0.0-rc.6';
 
 /**
   Standard environmental variables. You can define these in a global `ENV`
