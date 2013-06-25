957ef2826979d81cae6a511ec55084aa48320ba1
igorT <terzicigor@gmail.com>
2013-06-25 20:01:05 +0200

    Calling router.map now appends the routes instead of replacing them
    I would have added a test but there did not seem to be any route
    generation tests.

--- packages/ember-routing/lib/system/router.js
+++ packages/ember-routing/lib/system/router.js
@@ -301,6 +301,7 @@ function transitionCompleted(router) {
 }
 
 Ember.Router.reopenClass({
+  callbacks: [],
   map: function(callback) {
     var router = this.router = new Router();
 
@@ -310,10 +311,14 @@ Ember.Router.reopenClass({
 
     var dsl = Ember.RouterDSL.map(function() {
       this.resource('application', { path: "/" }, function() {
+        for (var i=0; Ember.Router.callbacks.length < i; i++){
+          Ember.Router.callbacks[i].call(this);
+        }
         callback.call(this);
       });
     });
 
+    this.callbacks.push(callback);
     router.map(dsl.generate());
     return router;
   }
