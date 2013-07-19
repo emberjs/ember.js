d65b90896dc284cac4474f1c16e03eaf7d1381c4
Matthew Beale <matt.beale@madhatted.com>
2013-07-19 09:43:07 -0400

    Document htmlSafe

--- packages/ember-handlebars/lib/string.js
+++ packages/ember-handlebars/lib/string.js
@@ -1,8 +1,15 @@
 /**
-  @method htmlSafe
-  @for Ember.String
-  @static
-*/
+ * Mark a string as safe for raw output with Handlebars. If you
+ * return HTML from a Handlebars helper, use this function to
+ * ensure Handlebars does not escape the HTML.
+ *
+ * `Ember.String.htmlSafe('<div>someString</div>')`
+ *
+ * @method htmlSafe
+ * @for Ember.String
+ * @static
+ * @return {Handlebars.SafeString} a string that will not be html escaped by Handlebars
+ */
 Ember.String.htmlSafe = function(str) {
   return new Handlebars.SafeString(str);
 };
@@ -12,11 +19,15 @@ var htmlSafe = Ember.String.htmlSafe;
 if (Ember.EXTEND_PROTOTYPES === true || Ember.EXTEND_PROTOTYPES.String) {
 
   /**
-    See `Ember.String.htmlSafe`.
-
-    @method htmlSafe
-    @for String
-  */
+   * Use `'<div>someString</div>'.htmlSafe()` to mark a string as being
+   * safe for raw output with Handlebars.
+   *
+   * See `Ember.String.htmlSafe`.
+   *
+   * @method htmlSafe
+   * @for String
+   * @return {Handlebars.SafeString} a string that will not be html escaped by Handlebars
+   */
   String.prototype.htmlSafe = function() {
     return htmlSafe(this);
   };
