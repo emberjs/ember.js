532458e4e3f3a971372d4fe8b510c64ca9bb6253
Peter Wagenet <peter.wagenet@gmail.com>
2013-07-28 16:59:11 -0700

    Fix {{template}} deprecation warnings in tests

--- packages/ember-handlebars/tests/helpers/template_test.js
+++ packages/ember-handlebars/tests/helpers/template_test.js
@@ -3,6 +3,8 @@ var originalLookup = Ember.lookup, lookup, TemplateTests, view, container;
 
 module("Support for {{template}} helper", {
   setup: function() {
+    Ember.TESTING_DEPRECATION = true;
+
     Ember.lookup = lookup = { Ember: Ember };
     MyApp = lookup.MyApp = Ember.Object.create({});
     container = new Ember.Container();
@@ -15,6 +17,8 @@ module("Support for {{template}} helper", {
       }
     });
     Ember.lookup = originalLookup;
+
+    Ember.TESTING_DEPRECATION = false;
   }
 });
 
