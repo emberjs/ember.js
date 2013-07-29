9d051c2a1b751494575fa06b9ebc73af8ed59ac3
Tom Dale <tom@tomdale.net>
2013-07-28 23:13:59 -0700

    Add sendAction() to Ember.Component

--- packages/ember-views/lib/views/component.js
+++ packages/ember-views/lib/views/component.js
@@ -1,6 +1,6 @@
 require("ember-views/views/view");
 
-var set = Ember.set;
+var get = Ember.get, set = Ember.set, isNone = Ember.isNone;
 
 /**
 @module ember
@@ -85,11 +85,65 @@ var set = Ember.set;
   @namespace Ember
   @extends Ember.View
 */
-Ember.Component = Ember.View.extend({
+Ember.Component = Ember.View.extend(Ember.TargetActionSupport, {
   init: function() {
     this._super();
     set(this, 'context', this);
     set(this, 'controller', this);
     set(this, 'templateData', {keywords: {}});
+  },
+
+  targetObject: Ember.computed(function(key) {
+    var parentView = get(this, '_parentView');
+    return parentView ? get(parentView, 'controller') : null;
+  }).property('_parentView'),
+
+  /**
+   Sends an action to component's controller. A component inherits its
+   controller from the context in which it is used.
+
+   By default, calling `sendAction()` will send an action with the name
+   of the component's `action` property.
+
+   For example, if the component had a property `action` with the value
+   `"addItem"`, calling `sendAction()` would send the `addItem` action
+   to the component's controller.
+
+   If you provide an argument to `sendAction()`, that key will be used to look
+   up the action name.
+
+   For example, if the component had a property `playing` with the value
+   `didStartPlaying`, calling `sendAction('playing')` would send the
+   `didStartPlaying` action to the component's controller.
+
+   Whether or not you are using the default action or a named action, if
+   the action name is not defined on the component, calling `sendAction()`
+   does not have any effect.
+
+   For example, if you call `sendAction()` on a component that does not have
+   an `action` property defined, no action will be sent to the controller,
+   nor will an exception be raised.
+
+   @param [action] {String} the action to trigger
+  */
+  sendAction: function(action) {
+    var actionName;
+
+    // Send the default action
+    if (action === undefined) {
+      actionName = get(this, 'action');
+      Ember.assert("The default action was triggered on the component " + this.toString() + ", but the action name (" + actionName + ") was not a string.", isNone(actionName) || typeof actionName === 'string');
+    } else {
+      actionName = get(this, action);
+      Ember.assert("The " + action + " action was triggered on the component " + this.toString() + ", but the action name (" + actionName + ") was not a string.", isNone(actionName) || typeof actionName === 'string');
+    }
+
+
+    // If no action name for that action could be found, just abort.
+    if (actionName === undefined) { return; }
+
+    this.triggerAction({
+      action: actionName
+    });
   }
 });
--- packages/ember-views/tests/views/component_test.js
+++ packages/ember-views/tests/views/component_test.js
@@ -1,3 +1,5 @@
+var get = Ember.get, set = Ember.set;
+
 module("Ember.Component");
 
 var Component = Ember.Component.extend();
@@ -10,4 +12,81 @@ test("The context of an Ember.Component is itself", function() {
 test("The controller (target of `action`) of an Ember.Component is itself", function() {
   var control = Component.create();
   strictEqual(control, control.get('controller'), "A control's controller is itself");
+});
+
+var component, controller, actionCounts, sendCount;
+
+module("Ember.Component - Actions", {
+  setup: function() {
+    actionCounts = {};
+    sendCount = 0;
+
+    controller = Ember.Object.create({
+      send: function(actionName) {
+        sendCount++;
+        actionCounts[actionName] = actionCounts[actionName] || 0;
+        actionCounts[actionName]++;
+      }
+    });
+
+    component = Ember.Component.create({
+      _parentView: Ember.View.create({
+        controller: controller
+      })
+    });
+  },
+
+  teardown: function() {
+    Ember.run(function() {
+      component.destroy();
+      controller.destroy();
+    });
+  }
+});
+
+test("Calling sendAction on a component without an action defined does nothing", function() {
+  component.sendAction();
+  equal(sendCount, 0, "addItem action was not invoked");
+});
+
+test("Calling sendAction on a component with an action defined calls send on the controller", function() {
+  set(component, 'action', "addItem");
+
+  component.sendAction();
+
+  equal(sendCount, 1, "send was called once");
+  equal(actionCounts['addItem'], 1, "addItem event was sent once");
+});
+
+test("Calling sendAction with a named action uses the component's property as the action name", function() {
+  set(component, 'playing', "didStartPlaying");
+  set(component, 'action', "didDoSomeBusiness");
+
+  component.sendAction('playing');
+
+  equal(sendCount, 1, "send was called once");
+  equal(actionCounts['didStartPlaying'], 1, "named action was sent");
+
+  component.sendAction('playing');
+
+  equal(sendCount, 2, "send was called twice");
+  equal(actionCounts['didStartPlaying'], 2, "named action was sent");
+
+  component.sendAction();
+
+  equal(sendCount, 3, "send was called three times");
+  equal(actionCounts['didDoSomeBusiness'], 1, "default action was sent");
+});
+
+test("Calling sendAction when the action name is not a string raises an exception", function() {
+  set(component, 'action', {});
+  set(component, 'playing', {});
+
+  expectAssertion(function() {
+    component.sendAction();
+  });
+
+  expectAssertion(function() {
+    component.sendAction('playing');
+  });
 });
