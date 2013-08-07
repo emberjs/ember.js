5b19272582c3edbf518da479fd66a86ee282efbc
Joshua Kelly <jdjkelly@gmail.com>
2013-08-06 21:55:40 -0400

    Use standard comment conventions, with opening and closing blocks

--- packages/ember-handlebars/lib/controls.js
+++ packages/ember-handlebars/lib/controls.js
@@ -14,14 +14,14 @@ function normalizeHash(hash, hashTypes) {
 }
 
 /**
- * `{{input}}` inserts a new instance of either Ember.TextField or
- * Ember.Checkbox, depending on the `type` option passed in. If no `type`
- * is supplied it defaults to Ember.TextField.
- *
- * @method input
- * @for Ember.Handlebars.helpers
- * @param {Hash} options
- */
+  `{{input}}` inserts a new instance of either Ember.TextField or
+  Ember.Checkbox, depending on the `type` option passed in. If no `type`
+  is supplied it defaults to Ember.TextField.
+
+  @method input
+  @for Ember.Handlebars.helpers
+  @param {Hash} options
+*/
 Ember.Handlebars.registerHelper('input', function(options) {
   Ember.assert('You can only pass attributes to the `input` helper, not arguments', arguments.length < 2);
 
@@ -45,13 +45,13 @@ Ember.Handlebars.registerHelper('input', function(options) {
 });
 
 /**
- * `{{textarea}}` inserts a new instance of Ember.TextArea into the template
- * passing its options to `Ember.TextArea`'s `create` method.
- *
- * @method textarea
- * @for Ember.Handlebars.helpers
- * @param {Hash} options
- */
+  `{{textarea}}` inserts a new instance of Ember.TextArea into the template
+  passing its options to `Ember.TextArea`'s `create` method.
+
+  @method textarea
+  @for Ember.Handlebars.helpers
+  @param {Hash} options
+*/
 Ember.Handlebars.registerHelper('textarea', function(options) {
   Ember.assert('You can only pass attributes to the `textarea` helper, not arguments', arguments.length < 2);
 
