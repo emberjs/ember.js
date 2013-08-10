c20e474df0b35136c0b07eb2699225367c561b8d
Brendan McLoughlin <bmac325@gmail.com>
2013-08-10 13:46:08 -0400

    Add documentation to the methods on Ember.NoneLocation

--- packages/ember-routing/lib/location/none_location.js
+++ packages/ember-routing/lib/location/none_location.js
@@ -18,23 +18,71 @@ var get = Ember.get, set = Ember.set;
 Ember.NoneLocation = Ember.Object.extend({
   path: '',
 
+  /**
+    @private
+
+    Returns the current path.
+
+    @method getURL
+    @return {String} path
+  */
   getURL: function() {
     return get(this, 'path');
   },
 
+  /**
+    @private
+
+    Set the path and remembers what was set. Using this method
+    to change the path will not invoke the `updateURL` callback.
+
+    @method setURL
+    @param path {String}
+  */
   setURL: function(path) {
     set(this, 'path', path);
   },
 
+  /**
+    @private
+
+    Register a callback to be invoked when the path changes. These
+    callbacks will execute when the user presses the back or forward
+    button, but not after `setURL` is invoked.
+
+    @method onUpdateURL
+    @param callback {Function}
+  */
   onUpdateURL: function(callback) {
     this.updateCallback = callback;
   },
 
+  /**
+    @private
+
+    Sets the path and calls the `updateURL` callback.
+
+    @method handleURL
+    @param callback {Function}
+  */
   handleURL: function(url) {
     set(this, 'path', url);
     this.updateCallback(url);
   },
 
+  /**
+    @private
+
+    Given a URL, formats it to be placed into the page as part
+    of an element's `href` attribute.
+
+    This is used, for example, when using the {{action}} helper
+    to generate a URL based on an event.
+
+    @method formatURL
+    @param url {String}
+    @return {String} url
+  */
   formatURL: function(url) {
     // The return value is not overly meaningful, but we do not want to throw
     // errors when test code renders templates containing {{action href=true}}
