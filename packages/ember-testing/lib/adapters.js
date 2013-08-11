b6ce04ee5aa5ce0d6cd6ee54cd5713183ce6e92b
Robert Jackson <robert.w.jackson@me.com>
2013-08-11 09:58:17 -0400

    Doc updates to ember-testing/lib/adapter.js.

--- packages/ember-testing/lib/adapters.js
+++ packages/ember-testing/lib/adapters.js
@@ -1,57 +1,55 @@
 require('ember-testing/test');
 
+/**
+ @module ember
+ @submodule ember-testing
+*/
+
 var Test = Ember.Test;
 
 /**
+  The primary purpose of this class is to create hooks that can be implemented
+  by an adapter for various test frameworks.
+
   @class Adapter
   @namespace Ember.Test
 */
 Test.Adapter = Ember.Object.extend({
   /**
+    This callback will be called whenever an async operation is about to start.
+
+    Override this to call your framework's methods that handle async
+    operations.
+
     @public
-
-    This callback will be called
-    whenever an async operation
-    is about to start.
-
-    Override this to call your
-    framework's methods
-    that handle async operations
-
     @method asyncStart
   */
   asyncStart: Ember.K,
 
   /**
+    This callback will be called whenever an async operation has completed.
+
     @public
-
-    This callback will be called
-    whenever an async operation
-    has completed.
-
     @method asyncEnd
   */
   asyncEnd: Ember.K,
 
   /**
-   @public
-
-    Override this method with your
-    testing framework's false assertion
-    This function is called whenever
-    an exception occurs causing the testing
+    Override this method with your testing framework's false assertion.
+    This function is called whenever an exception occurs causing the testing
     promise to fail.
 
     QUnit example:
 
     ```javascript
-    exception: function(error) {
-      ok(false, error);
-    }
+      exception: function(error) {
+        ok(false, error);
+      };
     ```
 
+    @public
     @method exception
-    @param reason {String}
+    @param {String} error The exception to be raised.
   */
   exception: function(error) {
     setTimeout(function() {
@@ -61,8 +59,12 @@ Test.Adapter = Ember.Object.extend({
 });
 
 /**
+  This class implements the methods defined by Ember.Test.Adapter for the
+  QUnit testing framework.
+
   @class QUnitAdapter
   @namespace Ember.Test
+  @extends Ember.Test.Adapter
 */
 Test.QUnitAdapter = Test.Adapter.extend({
   asyncStart: function() {
