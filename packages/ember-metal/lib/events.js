07b016b7a7de6f13acb8bb15ee4dfbe0c02e9cb2
Erik Bryn <erik.bryn@gmail.com>
2013-08-20 12:03:13 -0500

    Fix Ember._suspendListeners
    We need more tests around this code.

--- packages/ember-metal/lib/events.js
+++ packages/ember-metal/lib/events.js
@@ -238,7 +238,8 @@ function suspendListeners(obj, eventNames, target, method, callback) {
     target = null;
   }
 
-  var eventName, actions, i, l;
+  var suspendedActions = [],
+      eventName, actions, i, l;
 
   for (i=0, l=eventNames.length; i<l; i++) {
     eventName = eventNames[i];
@@ -247,13 +248,17 @@ function suspendListeners(obj, eventNames, target, method, callback) {
 
     if (actionIndex !== -1) {
       actions[actionIndex+2] |= SUSPENDED;
+      suspendedActions.push(actionIndex);
     }
   }
 
   function tryable() { return callback.call(target); }
 
   function finalizer() {
-    actions[actionIndex+2] &= ~SUSPENDED;
+    for (var i = 0, l = suspendedActions.length; i < l; i++) {
+      var actionIndex = suspendedActions[i];
+      actions[actionIndex+2] &= ~SUSPENDED;
+    }
   }
 
   return Ember.tryFinally(tryable, finalizer);
