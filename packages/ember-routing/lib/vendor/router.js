69db4fe2624fdc64919a1178da4fedb36ef50ff8
machty <machty@gmail.com>
2013-08-26 23:08:29 -0400

    Sync router.js

--- packages/ember-routing/lib/vendor/router.js
+++ packages/ember-routing/lib/vendor/router.js
@@ -789,6 +789,7 @@ define("router",
 
           // Don't overwrite contexts / update URL if this was a noop transition.
           if (!currentHandlerInfos || !currentHandlerInfos.length ||
+              !router.recognizer.hasRoute(currentHandlerInfos[currentHandlerInfos.length - 1].name) ||
               currentHandlerInfos.length !== matchPointResults.matchPoint) {
             finalizeTransition(transition, handlerInfos);
           }
