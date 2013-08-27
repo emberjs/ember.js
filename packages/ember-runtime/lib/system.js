bb13de08119a64aa94891cc33612a1b3e92b9050
David J. Hamilton <davidjh@hjdivad.com>
2013-08-27 09:07:46 -0700

    Add `Ember.TrackedArray`.
    `Ember.TrackedArray` is a data structure for lazily computing indexes of items
    in arrays after mutations.
    
    Useful in eg `Ember.ReduceComputed`.

--- packages/ember-runtime/lib/system.js
+++ packages/ember-runtime/lib/system.js
@@ -1,3 +1,5 @@
+require('ember-runtime/system/tracked_array');
+
 require('ember-runtime/system/container');
 require('ember-runtime/system/application');
 require('ember-runtime/system/array_proxy');
