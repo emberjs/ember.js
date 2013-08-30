324d415cff148d042aa1c90fe0c0b22c1dbff330
Erik Bryn <erik.bryn@gmail.com>
2013-08-30 17:19:23 -0500

    Throw error if Ember.StateManager or Ember.State classes are used without the ember-states plugin installed

--- packages/ember/lib/main.js
+++ packages/ember/lib/main.js
@@ -5,3 +5,21 @@ Ember
 
 @module ember
 */
+
+var defineProperty = Ember.platform.defineProperty;
+
+if (defineProperty && !defineProperty.isSimulated) {
+  defineProperty(Ember, 'StateManager', {
+    get: function() {
+      // Throwing rather than using Ember.assert to avoid this being stripped from builds
+      throw new Error("Ember.StateManager has been moved into a plugin: https://github.com/emberjs/ember-states");
+    }
+  });
+
+  defineProperty(Ember, 'State', {
+    get: function() {
+      // Throwing rather than using Ember.assert to avoid this being stripped from builds
+      throw new Error("Ember.State has been moved into a plugin: https://github.com/emberjs/ember-states");
+    }
+  });
+}
