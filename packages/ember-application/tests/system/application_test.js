22f64fb756d409c7861fe79c2a31c02c52ec6d10
Ray Cohen <raycohen@gmail.com>
2013-09-25 18:39:45 -0400

    remove asserts preventing nesting app, add dispatcher short circuit, and a test showing inner app renders and responds to events one time
    @raycohen / @kselden

--- packages/ember-application/tests/system/application_test.js
+++ packages/ember-application/tests/system/application_test.js
@@ -51,14 +51,6 @@ test("you cannot make a new application that is a parent of an existing applicat
   });
 });
 
-test("you cannot make a new application that is a descendent of an existing application", function() {
-  expectAssertion(function() {
-    Ember.run(function() {
-      Ember.Application.create({ rootElement: '#one-child' });
-    });
-  });
-});
-
 test("you cannot make a new application that is a duplicate of an existing application", function() {
   expectAssertion(function() {
     Ember.run(function() {
