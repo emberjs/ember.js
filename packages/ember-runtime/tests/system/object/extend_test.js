1cd63d7586963e52185f65e064485461cf1ca2c0
Luke Melia <luke@lukemelia.com>
2013-09-29 11:32:42 -0400

    Refactor `Ember.Application.initializer`.
    Previously, `Ember.Application.initializers` was implemented as a
    concatenated property, which is works at class definition and instance
    creation time, but not when the field is mutated, which is what
    `Ember.Application.initializer` was doing prior to this commit.
    
    Also reverted concatenatedProperties change, which had been added to try
    unsuccessfully to address this issue.

--- packages/ember-runtime/tests/system/object/extend_test.js
+++ packages/ember-runtime/tests/system/object/extend_test.js
@@ -1,3 +1,5 @@
+var get = Ember.get;
+
 module('Ember.Object.extend');
 
 test('Basic extend', function() {
@@ -51,4 +53,33 @@ test('Overriding a method several layers deep', function() {
   equal(obj.barCnt, 2, 'should invoke both');
 });
 
+test('With concatenatedProperties', function(){
+  var SomeClass = Ember.Object.extend({ things: 'foo', concatenatedProperties: ['things'] });
+  var AnotherClass = SomeClass.extend({ things: 'bar' });
+  var YetAnotherClass = SomeClass.extend({ things: 'baz' });
+  var some = new SomeClass();
+  var another = new AnotherClass();
+  var yetAnother = new YetAnotherClass();
+  deepEqual(some.get('things'), ['foo'], 'base class should have just its value');
+  deepEqual(another.get('things'), ['foo', 'bar'], "subclass should have base class' and it's own");
+  deepEqual(yetAnother.get('things'), ['foo', 'baz'], "subclass should have base class' and it's own");
+});
+
+test('With concatenatedProperties class properties', function(){
+  var SomeClass = Ember.Object.extend();
+  SomeClass.reopenClass({
+    concatenatedProperties: ['things'],
+    things: 'foo'
+  });
+  var AnotherClass = SomeClass.extend();
+  AnotherClass.reopenClass({ things: 'bar' });
+  var YetAnotherClass = SomeClass.extend();
+  YetAnotherClass.reopenClass({ things: 'baz' });
+  var some = new SomeClass();
+  var another = new AnotherClass();
+  var yetAnother = new YetAnotherClass();
+  deepEqual(get(some.constructor, 'things'), ['foo'], 'base class should have just its value');
+  deepEqual(get(another.constructor, 'things'), ['foo', 'bar'], "subclass should have base class' and it's own");
+  deepEqual(get(yetAnother.constructor, 'things'), ['foo', 'baz'], "subclass should have base class' and it's own");
+});
 
