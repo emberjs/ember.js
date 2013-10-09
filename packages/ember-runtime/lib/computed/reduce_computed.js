b00f3fdce189c1c5c01c2f962ef5291e019e9845
David J. Hamilton <davidjh@hjdivad.com>
2013-10-09 16:53:12 -0700

    [BUGFIX beta] uniq reduceComputed dependent keys.
    If property brace expansion is enabled, there is never a need to have duplicate
    dependent keys.  However, with proeprty brace expansion disabled, it is the only
    way to specify multiple item property keys, as in the following:
    
    ```js
    
    var obj = Em.Object.createWithMixins({
      items: Em.A([{ foo: true }, { bar: false }, { bar: true }]),
      countFooOrBar: Em.reduceComputed({
        initialValue: 0,
        addedItem: function (acc, item) {
          if ( get(item, 'foo') || get(item, 'bar')) {
            ++acc;
          }
          return acc;
        },
    
        removedItem: function (acc, item) {
          if ( get(item, 'foo') || get(item, 'bar')) {
            --acc;
          }
          return acc;
        }
      }).property('items.@each.foo', 'items.@each.bar')
    });
    ```
    
    Of course, with property brace expansion, it would be better to write the
    dependency as `property('items.@each.{foo,bar}')`.
    
    Fix #3554.

--- packages/ember-runtime/lib/computed/reduce_computed.js
+++ packages/ember-runtime/lib/computed/reduce_computed.js
@@ -529,7 +529,7 @@ ReduceComputedProperty.prototype.clearItemPropertyKeys = function (dependentArra
 ReduceComputedProperty.prototype.property = function () {
   var cp = this,
       args = a_slice.call(arguments),
-      propertyArgs = [],
+      propertyArgs = new Ember.Set(),
       match,
       dependentArrayKey,
       itemPropertyKey;
@@ -541,13 +541,13 @@ ReduceComputedProperty.prototype.property = function () {
       dependentArrayKey = match[1];
       itemPropertyKey = match[2];
       cp.itemPropertyKey(dependentArrayKey, itemPropertyKey);
-      propertyArgs.push(dependentArrayKey);
+      propertyArgs.add(dependentArrayKey);
     } else {
-      propertyArgs.push(dependentKey);
+      propertyArgs.add(dependentKey);
     }
   });
 
-  return ComputedProperty.prototype.property.apply(this, propertyArgs);
+  return ComputedProperty.prototype.property.apply(this, propertyArgs.toArray());
 };
 
 /**
