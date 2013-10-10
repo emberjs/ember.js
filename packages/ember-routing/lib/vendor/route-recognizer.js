85cda0a016dbb9af5dceb5c891e673f06b3a0735
Christian Wesselhoeft <christian@cwesselhoeft.com>
2013-10-10 15:57:20 -0700

    [BUGFIX beta] Give precedence to routes with more static segments. Fixes #3573

--- packages/ember-routing/lib/vendor/route-recognizer.js
+++ packages/ember-routing/lib/vendor/route-recognizer.js
@@ -232,7 +232,7 @@ define("route-recognizer",
       return states.sort(function(a, b) {
         if (a.types.stars !== b.types.stars) { return a.types.stars - b.types.stars; }
         if (a.types.dynamics !== b.types.dynamics) { return a.types.dynamics - b.types.dynamics; }
-        if (a.types.statics !== b.types.statics) { return a.types.statics - b.types.statics; }
+        if (a.types.statics !== b.types.statics) { return b.types.statics - a.types.statics; }
 
         return 0;
       });
