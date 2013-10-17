031c7b84cfdf5e7c65707cc075008ea80102a5d0
Robert Jackson <robert.w.jackson@me.com>
2013-10-17 15:30:02 -0400

    Use consistent versioning (beta branch).

--- lib/ember/version.rb
+++ lib/ember/version.rb
@@ -6,12 +6,6 @@ module Ember
   # we might want to unify this with the ember version string,
   # but consistency?
   def rubygems_version_string
-    if VERSION =~ /rc|beta/
-      major, rc = VERSION.sub('-','.').scan(/(\d+\.\d+\.\d+\.(rc|beta))\.(\d+)/).first
-
-      "#{major}#{rc}"
-    else
-      VERSION
-    end
+    VERSION.gsub(/[-\+]/,'.')
   end
 end
