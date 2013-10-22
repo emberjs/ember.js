86266b27607cafa7678f45cb7047e4a7d974b0fd
Peter Wagenet <peter.wagenet@gmail.com>
2013-10-21 20:29:38 -0700

    Remove old require

--- packages/ember-routing/lib/helpers.js
+++ packages/ember-routing/lib/helpers.js
@@ -3,4 +3,3 @@ require('ember-routing/helpers/link_to');
 require('ember-routing/helpers/outlet');
 require('ember-routing/helpers/render');
 require('ember-routing/helpers/action');
-require('ember-routing/helpers/control');
