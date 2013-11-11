f909c28a0ab2203049f955a85ad3162eabfe95ab
David J. Hamilton <davidjh@hjdivad.com>
2013-11-11 13:02:35 -0800

    [BUGFIX beta]  sort guid fallback unconfused by ObjectProxy.
    `Em.computed.sort` uses a guid check as a fallback comparator.  In order to
    search for items correctly, it also wraps items on removal in an object proxy to
    shadow new values with the previous values.
    
    This proxy needs to be sort-equal to the actual object.  To ensure the guid
    fallback check treated the item and the proxy as equal, it had a check
    `ObjectProxy.detectInstance`.  This check is obviously deficient in the face of
    items that might themselves be `ObjectProxy`s, such as `ObjectController`s when
    using `@this` as the base array in an `ArrayController`.
    
    This commit creates a local type of `ObjectProxy`, `SearchProxy`, to avoid this
    confusion.
    
    Fix #3720.

--- packages/ember-runtime/tests/computed/reduce_computed_macros_test.js
+++ packages/ember-runtime/tests/computed/reduce_computed_macros_test.js
@@ -1,4 +1,9 @@
-var map = Ember.EnumerableUtils.map, a_forEach = Ember.ArrayPolyfills.forEach, get = Ember.get, set = Ember.set, setProperties = Ember.setProperties,
+var map = Ember.EnumerableUtils.map,
+    a_forEach = Ember.ArrayPolyfills.forEach,
+    get = Ember.get,
+    set = Ember.set,
+    setProperties = Ember.setProperties,
+    ObjectProxy = Ember.ObjectProxy,
     obj, sorted, sortProps, items, userFnCalls, todos, filtered;
 
 module('Ember.computed.map', {
@@ -674,6 +679,30 @@ function commonSortTests() {
 
 
     deepEqual(sorted.mapBy('fname'), ['Cersei', 'Jaime', 'Bran', 'Robb'], "sorted array is updated");
+  });
+
+  test("guid sort-order fallback with a serach proxy is not confused by non-search ObjectProxys", function() {
+    var tyrion = { fname: "Tyrion", lname: "Lannister" },
+        tyrionInDisguise = ObjectProxy.create({
+          fname: "Yollo",
+          lname: "",
+          content: tyrion
+        });
+
+    items = get(obj, 'items');
+    sorted = get(obj, 'sortedItems');
+
+    Ember.run(function() {
+      items.pushObject(tyrion);
+    });
+
+    deepEqual(sorted.mapBy('fname'), ['Cersei', 'Jaime', 'Tyrion', 'Bran', 'Robb']);
+
+    Ember.run(function() {
+      items.pushObject(tyrionInDisguise);
+    });
+
+    deepEqual(sorted.mapBy('fname'), ['Yollo', 'Cersei', 'Jaime', 'Tyrion', 'Bran', 'Robb']);
   });
 }
 
