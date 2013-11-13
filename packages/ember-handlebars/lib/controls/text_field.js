604392d33ad708c4dfa0b48c583bc9a7bfd53672
thenickcox <nick@nickcox.me>
2013-11-13 07:44:13 -0800

    Docs: Fix link in handlebars text field API
    Currently, this link was returning a 404 response. It should be
    relative to the /api/classes path. This change fixes the link.

--- packages/ember-handlebars/lib/controls/text_field.js
+++ packages/ember-handlebars/lib/controls/text_field.js
@@ -14,7 +14,7 @@ var get = Ember.get, set = Ember.set;
   The internal class used to create text inputs when the `{{input}}`
   helper is used with `type` of `text`.
 
-  See [handlebars.helpers.input](/api/classes/Ember.Handlebars.helpers.html#method_input)  for usage details.
+  See [handlebars.helpers.input](Ember.Handlebars.helpers.html#method_input)  for usage details.
 
   ## Layout and LayoutName properties
 
