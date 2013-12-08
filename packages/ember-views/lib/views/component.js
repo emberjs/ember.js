6f6247f54e1f2b841f73dedd274e962b3703ca1d
Robert Jackson <robert.w.jackson@me.com>
2013-12-08 10:12:48 -0500

    [BUGFIX] Deprecate manually assigned templates for Components.
    In most circumstances a Component does not have a template or
    templateName property. In a component the block passed when calling it
    (in a handlebars template) is interpreted as its template, and a
    template that was registered as `components/my-component` becomes its
    layout. This was recently fixed in 1.2.0 (with the
    `container-renderables` feature), but many applications already existed
    that manually register their components (via `Ember.Handlebars.helper`).
    
    This change allows usage of `template` and `templateName` properties on
    a component, but generates a deprecation warning if you do so.

--- packages/ember-views/lib/views/component.js
+++ packages/ember-views/lib/views/component.js
@@ -1,4 +1,5 @@
 require("ember-views/views/view");
+require("ember-views/mixins/component_template_deprecation");
 
 var get = Ember.get, set = Ember.set, isNone = Ember.isNone,
     a_slice = Array.prototype.slice;
@@ -94,7 +95,7 @@ var get = Ember.get, set = Ember.set, isNone = Ember.isNone,
   @namespace Ember
   @extends Ember.View
 */
-Ember.Component = Ember.View.extend(Ember.TargetActionSupport, {
+Ember.Component = Ember.View.extend(Ember.TargetActionSupport, Ember.ComponentTemplateDeprecation, {
   init: function() {
     this._super();
     set(this, 'context', this);
@@ -105,6 +106,36 @@ Ember.Component = Ember.View.extend(Ember.TargetActionSupport, {
     options.data = {view: options._context};
     Ember.Handlebars.helpers['yield'].apply(this, [options]);
   },
+
+  /**
+  A components template property is set by passing a block to
+  during its invocation. It is executed within the parent context.
+
+  Example:
+
+  ```handlebars
+  {{#my-component}}
+    // something that is run in the context
+    // of the parent context
+  {{/my-component}}
+  ```
+
+  Specifying a template directly to a component is deprecated without
+  also specifying the layout property.
+
+  @deprecated
+  @property template
+  */
+  template: null,
+
+  /**
+  Specifying a components `templateName` is deprecated without also
+  providing the `layout` or `layoutName` properties.
+
+  @deprecated
+  @property templateName
+  */
+  templateName: null,
 
   // during render, isolate keywords
   cloneKeywords: function() {
