d10841854e22c862bb601b064d2c82a59ae06c0d
machty <machty@gmail.com>
2013-12-09 21:19:10 -0500

    [NICETHING beta] assert `needs` incompatibility with periods
    needs isn't compatible with periods, because ultimately
    you're going to need to call get('controllers.foo.bar')
    (assuming needs: 'foo.bar'), which doesn't and can't
    do what you want.

--- packages/ember-application/tests/system/controller_test.js
+++ packages/ember-application/tests/system/controller_test.js
@@ -93,3 +93,40 @@ test("setting the controllers property directly, should not be possible", functi
 
   equal(controller.get('controllers'), controllers, 'original controllers CP should have been unchanged');
 });
+
+test ("setting the value of a controller dependency should not be possible", function(){
+  var container = new Ember.Container();
+
+  container.register('controller:post', Ember.Controller.extend({
+    needs: 'posts'
+  }));
+
+  container.register('controller:posts', Ember.Controller.extend());
+
+  var postController = container.lookup('controller:post'),
+      postsController = container.lookup('controller:posts');
+
+  throws(function(){
+    postController.set('controllers.posts', 'epic-self-troll');
+  },
+  /You cannot overwrite the value of `controllers.posts` of .+/,
+  'should raise when attempting to set the value of a controller dependency property');
+
+  postController.set('controllers.posts.title', "A Troll's Life");
+  equal(postController.get('controllers.posts.title'), "A Troll's Life", "can set the value of controllers.posts.title");
+});
+
+test("raises if a dependency with a period is requested", function() {
+  var container = new Ember.Container();
+
+  container.register('controller:big.bird', Ember.Controller.extend());
+  container.register('controller:foo', Ember.Controller.extend({
+    needs: 'big.bird'
+  }));
+
+  expectAssertion(function() {
+    container.lookup('controller:foo');
+  }, /needs must not specify dependencies with periods in their names \(big\.bird\)/,
+  'throws if periods used');
+});
+
