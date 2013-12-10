c2468e39f4bb60fe2dceaac3c95c43e5656503ed
machty <machty@gmail.com>
2013-12-10 08:16:27 -0500

    [BUGFIX beta] make routePath smarter w/ stacked resource names
    Logging will make more sense when stack route resource
    names are used.
    
    Fixes #3853

--- packages/ember-routing/tests/system/router_test.js
+++ packages/ember-routing/tests/system/router_test.js
@@ -1,5 +1,7 @@
 var Router;
 
+var map = Ember.EnumerableUtils.map;
+
 module("Ember Router", {
   setup: function() {
     Router = Ember.Router.extend();
@@ -23,3 +25,30 @@ test("should destroy its location upon Router.destroy.", function(){
   ok(router.isDestroyed, "router should be destroyed");
   ok(location.isDestroyed, "location should be destroyed");
 });
+
+test("Ember.Router._routePath should consume identical prefixes", function() {
+
+  expect(8);
+
+  function routePath(s1, s2, s3) {
+    var handlerInfos = map(arguments, function(s) {
+      return { name: s };
+    });
+    handlerInfos.unshift({ name: 'ignored' });
+
+    return Ember.Router._routePath(handlerInfos);
+  }
+
+  equal(routePath('foo'), 'foo');
+  equal(routePath('foo', 'bar', 'baz'), 'foo.bar.baz');
+  equal(routePath('foo', 'foo.bar'), 'foo.bar');
+  equal(routePath('foo', 'foo.bar', 'foo.bar.baz'), 'foo.bar.baz');
+  equal(routePath('foo', 'foo.bar', 'foo.bar.baz.wow'), 'foo.bar.baz.wow');
+  equal(routePath('foo', 'foo.bar.baz.wow'), 'foo.bar.baz.wow');
+  equal(routePath('foo.bar', 'bar.baz.wow'), 'foo.bar.baz.wow');
+
+  // This makes no sense, not trying to handle it, just
+  // making sure it doesn't go boom.
+  equal(routePath('foo.bar.baz', 'foo'), 'foo.bar.baz.foo');
+});
+
