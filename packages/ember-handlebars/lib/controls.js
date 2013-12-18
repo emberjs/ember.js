b1e3c37a41fafa1d6447dec2050eebc8f724a0f9
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-12-19 05:05:44 +0900

    [DOC beta] Fix Ember.TextField document markup

--- packages/ember-handlebars/lib/controls.js
+++ packages/ember-handlebars/lib/controls.js
@@ -71,6 +71,7 @@ require("ember-handlebars/controls/select");
   if you are deploying to browsers where the `required` attribute is used, you
   can add this to the `TextField`'s `attributeBindings` property:
 
+
   ```javascript
   Ember.TextField.reopen({
     attributeBindings: ['required']
