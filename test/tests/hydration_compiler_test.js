d879ffea826baedc25a5e57ad2df5c88b75c16eb
Erik Bryn <erik.bryn@gmail.com>
2014-01-03 23:27:34 -0800

    We're splitting Compiler1 up into fragment and hydration compilers

--- test/tests/hydration_compiler_test.js
+++ test/tests/hydration_compiler_test.js
@@ -1,68 +1,85 @@
-import { Compiler1 } from "htmlbars/compiler/pass1";
-import { Compiler2 } from "htmlbars/compiler/pass2";
+import { HydrationCompiler } from "htmlbars/compiler/hydration";
 import { preprocess } from "htmlbars/parser";
+import { compileAST } from "htmlbars/compiler/compile";
 
 function opcodesFor(html, options) {
   var ast = preprocess(html, options),
-      compiler1 = new Compiler1(options);
+      compiler1 = new HydrationCompiler(compileAST, options);
   compiler1.compile(ast);
-  console.log(JSON.stringify(compiler1.opcodes2));
-  return compiler1.opcodes2;
+  return compiler1.opcodes;
 }
 
-module("Compiler opcode generation");
+function mustache(name, parent, start, end) {
+  return {type: 'ambiguous', params: [name, true, parent, start, end]};
+}
+
+module("HydrationCompiler opcode generation");
 
 test("simple example", function() {
   var opcodes = opcodesFor("<div>{{foo}} bar {{baz}}</div>");
   deepEqual(opcodes, [
-    ['foo', [0], null, 0],
-    ['baz', [0], 0, null]
+    mustache('foo', [0], null, 0),
+    mustache('baz', [0], 0, null)
   ]);
 });
 
 test("element with a sole mustache child", function() {
   var opcodes = opcodesFor("<div>{{foo}}</div>");
   deepEqual(opcodes, [
-    ['foo', [0], null, null]
+    mustache('foo', [0], null, null)
   ]);
 });
 
 test("element with a mustache between two text nodes", function() {
   var opcodes = opcodesFor("<div> {{foo}} </div>");
   deepEqual(opcodes, [
-    ['foo', [0], 0, 1]
+    mustache('foo', [0], 0, 1)
   ]);
 });
 
 test("mustache two elements deep", function() {
   var opcodes = opcodesFor("<div><div>{{foo}}</div></div>");
   deepEqual(opcodes, [
-    ['foo', [0, 0], null, null]
+    mustache('foo', [0, 0], null, null)
   ]);
 });
 
 test("two sibling elements with mustaches", function() {
   var opcodes = opcodesFor("<div>{{foo}}</div><div>{{bar}}</div>");
   deepEqual(opcodes, [
-    ['foo', [0], null, null],
-    ['bar', [1], null, null]
+    mustache('foo', [0], null, null),
+    mustache('bar', [1], null, null)
   ]);
 });
 
 test("mustaches at the root", function() {
   var opcodes = opcodesFor("{{foo}} {{bar}}");
   deepEqual(opcodes, [
-    ['foo', [], null, 0],
-    ['bar', [], 0, null]
+    mustache('foo', [], null, 0),
+    mustache('bar', [], 0, null)
   ]);
 });
 
 test("back to back mustaches should have a text node inserted between them", function() {
   var opcodes = opcodesFor("<div>{{foo}}{{bar}}{{baz}}wat{{qux}}</div>");
   deepEqual(opcodes, [
-    ['foo', [0], null, 0],
-    ['bar', [0], 0, 1],
-    ['baz', [0], 1, 2],
-    ['qux', [0], 2, null],
+    mustache('foo', [0], null, 0),
+    mustache('bar', [0], 0, 1),
+    mustache('baz', [0], 1, 2),
+    mustache('qux', [0], 2, null)
   ]);
 });
+
+// test("helper usage", function() {
+//   var opcodes = opcodesFor("{{foo 'bar'}}");
+//   deepEqual(opcodes, [
+//     mustache('foo', [], null, 0)
+//   ]);
+// });
+
+// test("helper usage", function() {
+//   var opcodes = opcodesFor("{{#foo 'bar'}}i am crying{{/foo}}");
+//   deepEqual(opcodes, [
+//     mustache('foo', [], null, 0)
+//   ]);
+// });
