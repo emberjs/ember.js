642c7296be0737737a0c0ec7ea95ef0d7d75fd5e
Oleg Nechiporenko <onechiporenko@apache.org>
2014-01-13 14:05:29 +0200

    [FEATURE ember-runtime-compact-by]

--- packages/ember-runtime/lib/mixins/enumerable.js
+++ packages/ember-runtime/lib/mixins/enumerable.js
@@ -774,7 +774,7 @@ Ember.Enumerable = Ember.Mixin.create({
   },
 
   /**
-    Returns a copy of the array with all null and undefined elements removed.
+    Returns a copy of the array with all `null` and `undefined` elements removed.
 
     ```javascript
     var arr = ["a", null, "c", undefined];
@@ -782,10 +782,10 @@ Ember.Enumerable = Ember.Mixin.create({
     ```
 
     @method compact
-    @return {Array} the array without null and undefined elements.
+    @return {Array} the array without `null` and `undefined` elements.
   */
   compact: function() {
-    return this.filter(function(value) { return value != null; });
+    return this.filter(function(value) { return !Ember.isNone(value); });
   },
 
   /**
@@ -1002,3 +1002,24 @@ Ember.Enumerable = Ember.Mixin.create({
     });
   }
 });
+
+
+if (Ember.FEATURES.isEnabled("ember-runtime-compact-by")) {
+  Ember.Enumerable.reopen({
+    /**
+     Returns a copy of the array without elements with `key` equal to `null` and `undefined`.
+
+     ```javascript
+     var arr = [Ember.Object.create({a: null}), {a: 1}, {a: false}, {a: ''}, {a: undefined}, {a: 0}, {a: null}];
+     arr.compactBy("a");  // [{a: 1}, {a: false}, {a: ''}, {a: 0}]
+     ```
+
+     @method compactBy
+     @param {String} key name of the property
+     @return {Array} the array without elements with `key` equal to `null` and `undefined`.
+     */
+    compactBy: function(key) {
+      return this.filter(function(item) { return !Ember.isNone(get(item, key)); });
+    }
+  });
+}
--- packages/ember-runtime/tests/suites/enumerable/compact.js
+++ packages/ember-runtime/tests/suites/enumerable/compact.js
@@ -2,6 +2,10 @@ require('ember-runtime/~tests/suites/enumerable');
 
 var suite = Ember.EnumerableTests;
 
+// ..........................................................
+// compact()
+//
+
 suite.module('compact');
 
 suite.test('removes null and undefined values from enumerable', function() {
@@ -9,3 +13,20 @@ suite.test('removes null and undefined values from enumerable', function() {
   var ary = obj.compact();
   deepEqual(ary, [1, false, '', 0]);
 });
+
+// ..........................................................
+// compactBy()
+//
+
+if (Ember.FEATURES.isEnabled("ember-runtime-compact-by")) {
+
+  suite.module('compactBy');
+
+  suite.test('removes items with property equal to null and undefined from enumerable', function() {
+    var arr = [Ember.Object.create({a: null}), {a: 1}, {a: false}, {a: ''}, {a: undefined}, {a: 0}, {a: null}];
+    var obj = this.newObject(arr);
+    var ary = obj.compactBy('a');
+    deepEqual(ary, [arr[1], arr[2], arr[3], arr[5]]);
+  });
+
+}
