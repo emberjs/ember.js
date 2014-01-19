0a0114fdf3028bf46fcf486f4d4767bdcde92e3c
Dimitri Suls <dimitri.suls@gmail.com>
2014-01-19 19:39:50 +0100

    Sorry I failed you regex..

--- packages/ember-views/lib/core.js
+++ packages/ember-views/lib/core.js
@@ -8,7 +8,7 @@ if (!jQuery && typeof require === 'function') {
   jQuery = require('jquery');
 }
 
-Ember.assert("Ember Views require jQuery 1.7, 1.8, 1.9, 1.10, 1.11, 2.0, or 2.1", jQuery && (jQuery().jquery.match(/^((1\.(7|8|9|10|11))|2\.(0|1)(\.\d+)?(pre|rc\d?)?/) || Ember.ENV.FORCE_JQUERY));
+Ember.assert("Ember Views require jQuery 1.7, 1.8, 1.9, 1.10, 1.11, 2.0, or 2.1", jQuery && (jQuery().jquery.match(/^((1\.(7|8|9|10|11))|(2\.(0|1)))(\.\d+)?(pre|rc\d?)?/) || Ember.ENV.FORCE_JQUERY));
 
 /**
   Alias for jQuery
