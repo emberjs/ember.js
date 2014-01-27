2193d206357ab50e1ff58728952e4e7adf1d9451
Robert Jackson <robert.w.jackson@me.com>
2014-01-26 21:57:21 -0500

    [BUGFIX beta] Fix component registration tests under IE8.
    In IE8 whitespace between `<div>` elements is not respected, causing
    these tests to fail.

--- packages/ember/tests/component_registration_test.js
+++ packages/ember/tests/component_registration_test.js
@@ -95,26 +95,26 @@ test("Late-registered components can be rendered with custom `template` property
 
 test("Late-registered components can be rendered with template registered on the container", function() {
 
-  Ember.TEMPLATES.application = compile("<div id='wrapper'>hello world {{sally-rutherford}} {{#sally-rutherford}}!!!{{/sally-rutherford}}</div>");
+  Ember.TEMPLATES.application = compile("<div id='wrapper'>hello world {{sally-rutherford}}-{{#sally-rutherford}}!!!{{/sally-rutherford}}</div>");
 
   boot(function() {
     container.register('template:components/sally-rutherford', compile("funkytowny{{yield}}"));
     container.register('component:sally-rutherford', Ember.Component);
   });
 
-  equal(Ember.$('#wrapper').text(), "hello world funkytowny funkytowny!!!", "The component is composed correctly");
+  equal(Ember.$('#wrapper').text(), "hello world funkytowny-funkytowny!!!", "The component is composed correctly");
   ok(!Ember.Handlebars.helpers['sally-rutherford'], "Component wasn't saved to global Handlebars.helpers hash");
 });
 
 test("Late-registered components can be rendered with ONLY the template registered on the container", function() {
 
-  Ember.TEMPLATES.application = compile("<div id='wrapper'>hello world {{borf-snorlax}} {{#borf-snorlax}}!!!{{/borf-snorlax}}</div>");
+  Ember.TEMPLATES.application = compile("<div id='wrapper'>hello world {{borf-snorlax}}-{{#borf-snorlax}}!!!{{/borf-snorlax}}</div>");
 
   boot(function() {
     container.register('template:components/borf-snorlax', compile("goodfreakingTIMES{{yield}}"));
   });
 
-  equal(Ember.$('#wrapper').text(), "hello world goodfreakingTIMES goodfreakingTIMES!!!", "The component is composed correctly");
+  equal(Ember.$('#wrapper').text(), "hello world goodfreakingTIMES-goodfreakingTIMES!!!", "The component is composed correctly");
   ok(!Ember.Handlebars.helpers['borf-snorlax'], "Component wasn't saved to global Handlebars.helpers hash");
 });
 
