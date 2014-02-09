dcbf93a8b5a094b7bf60bb5d5e33504b3b7e4d6c
Kris Selden <kris.selden@gmail.com>
2014-02-08 22:52:13 -0800

    Initial PlaceholderList

--- test/tests/placeholder_test.js
+++ test/tests/placeholder_test.js
@@ -1,94 +1,5 @@
 import { Placeholder } from "htmlbars/runtime/placeholder";
-
-function equalHTML(fragment, html) {
-  var div = document.createElement("div");
-  div.appendChild(fragment.cloneNode(true));
-
-  QUnit.push(div.innerHTML === html, div.innerHTML, html);
-}
-
-var parents = [
-  {
-    name: 'with parent as an element',
-    create: function (frag) {
-      var parent = document.createElement('div');
-      frag.appendChild(parent);
-      return parent;
-    },
-    startHTML: '<div>',
-    endHTML: '</div>'
-  },
-  {
-    name: 'with parent as a fragment',
-    create: function (frag) {
-      return frag;
-    },
-    startHTML: '',
-    endHTML: ''
-  }
-];
-
-var starts = [
-  {
-    name: 'with sibling before',
-    create: function (parent) {
-      var start = document.createTextNode('Some text before ');
-      parent.appendChild(start);
-      return parent.childNodes.length-1;
-    },
-    HTML: 'Some text before '
-  },
-  {
-    name: 'with no sibling before',
-    create: function (parent) {
-      return -1;
-    },
-    HTML: ''
-  }
-];
-
-var ends = [
-  {
-    name: 'and sibling after',
-    create: function (parent) {
-      var end = document.createTextNode(' some text after.');
-      parent.appendChild(end);
-      return parent.childNodes.length-1;
-    },
-    HTML: ' some text after.'
-  },
-  {
-    name: 'and no sibling after',
-    create: function (parent) {
-      return -1;
-    },
-    HTML: ''
-  }
-];
-
-var contents = [
-  {
-    name: 'with an empty Placeholder',
-    create: function (parent) { },
-    HTML: ''
-  },
-  {
-    name: 'with some paragraphs in the Placeholder',
-    create: function (parent) {
-      var p;
-      p = document.createElement('p');
-      p.textContent = 'a';
-      parent.appendChild(p);
-      p = document.createElement('p');
-      p.textContent = 'b';
-      parent.appendChild(p);
-      p = document.createElement('p');
-      p.textContent = 'c';
-      parent.appendChild(p);
-    },
-    HTML: '<p>a</p><p>b</p><p>c</p>'
-  }
-];
+import { PlaceholderList } from "htmlbars/runtime/placeholder_list";
 
 function placeholderTests(factory) {
   test('appendChild '+factory.name, function () {
@@ -223,10 +134,162 @@ function placeholderTests(factory) {
   });
 }
 
+function placeholderListTests(factory) {
+  test('various list operations with fragments '+factory.name, function () {
+    var setup = factory.create(),
+      fragment = setup.fragment,
+      placeholder = setup.placeholder,
+      startHTML = setup.startHTML,
+      endHTML = setup.endHTML,
+      html;
+
+    var placeholderList = new PlaceholderList(placeholder);
+
+    var A = element('p', 'A');
+    var B = element('p', 'B');
+    var C = element('p', 'C');
+    var D = element('p', 'D');
+    var E = element('p', 'E');
+    var F = element('p', 'F');
+
+    var fragmentABC = fragmentFor(A,B,C);
+    var fragmentEF = fragmentFor(E,F);
+
+    placeholderList.append([fragmentABC, D, fragmentEF]);
+
+    html = startHTML+'<p>A</p><p>B</p><p>C</p><p>D</p><p>E</p><p>F</p>'+endHTML;
+    equalHTML(fragment, html);
+
+    equal(placeholderList.list[0].start, placeholder.start);
+    equal(placeholderList.list[0].end, D);
+    equal(placeholderList.list[1].start, C);
+    equal(placeholderList.list[1].end, E);
+    equal(placeholderList.list[2].start, D);
+    equal(placeholderList.list[2].end, placeholder.end);
+
+    placeholderList.replace(1,2);
+
+    html = startHTML+'<p>A</p><p>B</p><p>C</p>'+endHTML;
+    equalHTML(fragment, html);
+
+    equal(placeholderList.list.length, 1);
+    equal(placeholderList.list[0].start, placeholder.start);
+    equal(placeholderList.list[0].end, placeholder.end);
+  });
+}
+
+function equalHTML(fragment, html) {
+  var div = document.createElement("div");
+  div.appendChild(fragment.cloneNode(true));
+
+  QUnit.push(div.innerHTML === html, div.innerHTML, html);
+}
+
+function fragmentFor() {
+  var fragment = document.createDocumentFragment();
+  for (var i=0,l=arguments.length; i<l; i++) {
+    fragment.appendChild(arguments[i]);
+  }
+  return fragment;
+}
+
+function element(tag, text) {
+  var el = document.createElement(tag);
+  el.appendChild(document.createTextNode(text));
+  return el;
+}
+
+function textNode(text) {
+  return document.createTextNode(text);
+}
+
+var parents = [
+  {
+    name: 'with parent as an element',
+    create: function (frag) {
+      var parent = document.createElement('div');
+      frag.appendChild(parent);
+      return parent;
+    },
+    startHTML: '<div>',
+    endHTML: '</div>'
+  },
+  {
+    name: 'with parent as a fragment',
+    create: function (frag) {
+      return frag;
+    },
+    startHTML: '',
+    endHTML: ''
+  }
+];
+
+var starts = [
+  {
+    name: 'with sibling before',
+    create: function (parent) {
+      var start = document.createTextNode('Some text before ');
+      parent.appendChild(start);
+      return parent.childNodes.length-1;
+    },
+    HTML: 'Some text before '
+  },
+  {
+    name: 'with no sibling before',
+    create: function (parent) {
+      return -1;
+    },
+    HTML: ''
+  }
+];
+
+var ends = [
+  {
+    name: 'and sibling after',
+    create: function (parent) {
+      var end = document.createTextNode(' some text after.');
+      parent.appendChild(end);
+      return parent.childNodes.length-1;
+    },
+    HTML: ' some text after.'
+  },
+  {
+    name: 'and no sibling after',
+    create: function (parent) {
+      return -1;
+    },
+    HTML: ''
+  }
+];
+
+var contents = [
+  {
+    name: 'with an empty Placeholder',
+    create: function (parent) { },
+    HTML: ''
+  },
+  {
+    name: 'with some paragraphs in the Placeholder',
+    create: function (parent) {
+      var p;
+      p = document.createElement('p');
+      p.textContent = 'a';
+      parent.appendChild(p);
+      p = document.createElement('p');
+      p.textContent = 'b';
+      parent.appendChild(p);
+      p = document.createElement('p');
+      p.textContent = 'c';
+      parent.appendChild(p);
+    },
+    HTML: '<p>a</p><p>b</p><p>c</p>'
+  }
+];
+
 function iterateCombinations(parents, starts, ends, contents, callback) {
   function buildFactory(parentFactory, startFactory, endFactory, contentFactory) {
     return {
-      name: parentFactory.name+' '+startFactory.name+' '+endFactory.name+' '+contentFactory.name,
+      name: [parentFactory.name, startFactory.name, endFactory.name, contentFactory.name].join(' '),
       create: function factory() {
         var fragment = document.createDocumentFragment(),
         parent = parentFactory.create(fragment),
@@ -256,7 +319,7 @@ function iterateCombinations(parents, starts, ends, contents, callback) {
     for (var j=0; j<starts.length; j++) {
       for (var k=0; k<ends.length; k++) {
         for (var l=0; l<contents.length; l++) {
-          var factory = buildFactory(parents[i], starts[i], ends[i], contents[i]);
+          var factory = buildFactory(parents[i], starts[j], ends[k], contents[l]);
           if (factory.create() === null) continue; // unsupported combo
           callback(factory);
         }
@@ -266,5 +329,7 @@ function iterateCombinations(parents, starts, ends, contents, callback) {
 }
 
 QUnit.module('Placeholder');
-
 iterateCombinations(parents, starts, ends, contents, placeholderTests);
+
+QUnit.module('PlaceholderList');
+iterateCombinations(parents, starts, ends, [{name:'', create: function(){},HTML:''}], placeholderListTests);
