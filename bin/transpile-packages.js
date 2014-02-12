fd69a0f4af451ea275f0e5815486f8c19c23a4fe
Robert Jackson <robert.w.jackson@me.com>
2014-02-11 22:18:07 -0500

    Do not transpile files ending in `.amd.js`.

--- bin/transpile-packages.js
+++ bin/transpile-packages.js
@@ -19,12 +19,17 @@ ES6Package.prototype = {
 
     glob(directory + '/**/*', function(err, files){
       files.forEach(function(filename){
-        console.log('Processing '+filename);
+        var result;
 
-        var result = this.compileFile(directory, filename)
+        if (filename.match(/\.amd\.js$/)){
+          result = fs.readFileSync(filename);
+          compiledOutput.push(result);
+        } else {
+          result = this.compileFile(directory, filename);
+          compiledOutput.push(result['compiled']);
+          moduleNames.push(result['name']);
+        }
 
-        compiledOutput.push(result['compiled']);
-        moduleNames.push(result['name']);
       }.bind(this));
 
       callback({compiled: compiledOutput, moduleNames: moduleNames});
