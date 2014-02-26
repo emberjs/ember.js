66875901169e6649bb3274fac7d4886a3852a6e9
Stanley Stuart <stanley@stan.li>
2014-02-25 19:07:12 -0600

    fix action bubbling for components with a component as parent
    Fixes an issue where nesting a component inside another component would
    cause actions to be swallowed and not delegated up to the route like
    they should be. Ember.TargetActionSupport defines `targetObject` as
    a computed property. This commit allows action bubbling to fall back
    to that property instead of stopping the bubbling chain at the parent
    component.
    
    fixes GH-4430

--- packages/ember-runtime/tests/mixins/action_handler_test.js
+++ packages/ember-runtime/tests/mixins/action_handler_test.js
@@ -11,3 +11,35 @@ test("passing a function for the actions hash triggers an assertion", function()
     });
   });
 });
+
+test("can use targetObject if target is not present in the case of components - GH-4430", function(){
+
+  expect(1);
+
+  var targetObject = Ember.Controller.create();
+
+  var Controller = Ember.Controller.extend({
+    targetObject: Ember.computed('target', '_targetObject', function(){
+      var target;
+      if (target = this.get('target')){
+        return target;
+      }
+      return this.get('_targetObject');
+    })
+  });
+
+  var controller = Controller.create();
+
+  Ember.run(function(){
+    controller.set('_targetObject', targetObject);
+  });
+
+  targetObject.send = function(){
+    ok(true, "Uses targetObject if target not present");
+  };
+
+  Ember.run(function(){
+    controller.send('openADialog');
+  });
+
+});
