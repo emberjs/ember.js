a021d9685b7f41aa969d1be2bb23a5d8b6b0b9f7
August “Kai” Kaiser <augustkaiser@gmail.com>
2014-02-28 21:49:44 -0500

    [BUGFIX beta] Ember.imports.jQuery || this.jQuery
    Ember.imports.jQuery should be used before this.jQuery
    
    Close #4449

--- packages/ember-views/lib/core.js
+++ packages/ember-views/lib/core.js
@@ -3,7 +3,7 @@
 @submodule ember-views
 */
 
-var jQuery = (this && this.jQuery) || (Ember.imports && Ember.imports.jQuery);
+var jQuery = (Ember.imports && Ember.imports.jQuery) || (this && this.jQuery);
 if (!jQuery && typeof require === 'function') {
   jQuery = require('jquery');
 }
