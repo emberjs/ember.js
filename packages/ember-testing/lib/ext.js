4a648a9a1ceb8306156fddf09345e2185cc6191c
Matthew Beale <matt.beale@madhatted.com>
2014-03-18 11:16:12 -0400

    [BUGFIX] Do not attach new listeners on each setupForTesting call

--- packages/ember-testing/lib/ext.js
+++ packages/ember-testing/lib/ext.js
@@ -2,10 +2,22 @@ require('ember-testing/test');
 
 var Test = Ember.Test;
 
+function incrementAjaxPendingRequests(){
+  Test.pendingAjaxRequests++;
+}
+
+function decrementAjaxPendingRequests(){
+  Ember.assert("An ajaxComplete event which would cause the number of pending AJAX " +
+               "requests to be negative has been triggered. This is most likely " +
+               "caused by AJAX events that were started before calling " +
+               "`injectTestHelpers()`.", Test.pendingAjaxRequests !== 0);
+  Test.pendingAjaxRequests--;
+}
+
 /**
   Sets Ember up for testing. This is useful to perform
   basic setup steps in order to unit test.
-  
+
   Use `App.setupForTesting` to perform integration tests (full
   application testing).
 
@@ -22,15 +34,8 @@ Ember.setupForTesting = function() {
 
   Test.pendingAjaxRequests = 0;
 
-  Ember.$(document).ajaxSend(function() {
-    Test.pendingAjaxRequests++;
-  });
-
-  Ember.$(document).ajaxComplete(function() {
-    Ember.assert("An ajaxComplete event which would cause the number of pending AJAX " +
-                 "requests to be negative has been triggered. This is most likely " +
-                 "caused by AJAX events that were started before calling " +
-                 "`injectTestHelpers()`.", Test.pendingAjaxRequests !== 0);
-    Test.pendingAjaxRequests--;
-  });
+  Ember.$(document).off('ajaxSend', incrementAjaxPendingRequests);
+  Ember.$(document).off('ajaxComplete', decrementAjaxPendingRequests);
+  Ember.$(document).on('ajaxSend', incrementAjaxPendingRequests);
+  Ember.$(document).on('ajaxComplete', decrementAjaxPendingRequests);
 };
