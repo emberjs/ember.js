73a729d9790d61efa23753009ea3d8129de4aba5
Robert Jackson <robert.w.jackson@me.com>
2014-03-23 14:08:38 -0400

    [FEATURE ember-routing-nested-resource-can-inherit-namespace]
    Specifying `resetNamespace: false` in a nested resource declaration will automatically prefix the
    new route with the parent's name (similar to `route`). This is much closer in line with the way
    many people assume it works out of the box, and is likely a better default. The actual default can
    not be changed until Ember 2.0 is release due to SemVer compatibility issues.
    
    Example:
    
    ```javascript
    var router = Router.map(function(){
      this.resource('bleep', function(){
        this.resource('bloop', {resetNamespace: false});
      });
    });
    
    // will create `bleep.bloop` route (without resetNamespace it would create `bloop` route)
    ```

--- packages_es6/ember-routing/tests/system/dsl_test.js
+++ packages_es6/ember-routing/tests/system/dsl_test.js
@@ -1,3 +1,4 @@
+import Ember from "ember-metal/core";
 import EmberRouter from "ember-routing/system/router";
 
 var Router;
@@ -26,3 +27,25 @@ test("should fail when using a reserved route name", function() {
     });
   }, "'basic' cannot be used as a resource name.");
 });
+
+test("will reset nested resource scope by default", function(){
+  var router = Router.map(function(){
+    this.resource('bleep', function(){
+      this.resource('bloop');
+    });
+  });
+
+  ok(!router.recognizer.names['bleep.bloop'], 'resource resets route namespacing by default');
+});
+
+if (Ember.FEATURES.isEnabled('ember-routing-nested-resource-can-inherit-namespace')) {
+  test("should retain resource namespace if `resetScope` is false", function(){
+    var router = Router.map(function(){
+      this.resource('bleep', function(){
+        this.resource('bloop', {resetNamespace: false});
+      });
+    });
+
+    ok(router.recognizer.names['bleep.bloop'], 'parent name was used as base of route with resetNamespace: false');
+  });
+}
