25333ecfcf2bc9357c1442a59ef9c56ed10d52dc
Robert Jackson <robert.w.jackson@me.com>
2014-04-10 07:03:14 -0400

    [BUGFIX beta] Ensure MutableEnumerable test suite is loaded properly.

--- packages_es6/ember-runtime/tests/suites/mutable_enumerable.js
+++ packages_es6/ember-runtime/tests/suites/mutable_enumerable.js
@@ -1,8 +1,10 @@
 import {EnumerableTests, ObserverClass} from 'ember-runtime/tests/suites/enumerable';
 
-var MutableEnumerableTests = EnumerableTests.extend();
-
 import addObjectTests from 'ember-runtime/tests/suites/mutable_enumerable/addObject';
 import removeObjectTests from 'ember-runtime/tests/suites/mutable_enumerable/removeObject';
+
+var MutableEnumerableTests = EnumerableTests.extend();
+MutableEnumerableTests.importModuleTests(addObjectTests);
+MutableEnumerableTests.importModuleTests(removeObjectTests);
 
 export default MutableEnumerableTests;
