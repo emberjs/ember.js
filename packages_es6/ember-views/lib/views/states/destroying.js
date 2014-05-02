84151ddd3452d2094073c204f05bc820620a32fb
Matt <m.blair@modcloth.com>
2014-05-01 17:47:22 -0700

    [BUGFIX beta] Convert thrown strings into Ember.Errors

--- packages_es6/ember-views/lib/views/states/destroying.js
+++ packages_es6/ember-views/lib/views/states/destroying.js
@@ -2,6 +2,7 @@ import merge from "ember-metal/merge";
 import {create} from "ember-metal/platform";
 import {fmt} from "ember-runtime/system/string";
 import _default from "ember-views/views/states/default";
+import EmberError from "ember-metal/error";
 /**
 @module ember
 @submodule ember-views
@@ -13,20 +14,20 @@ var destroying = create(_default);
 
 merge(destroying, {
   appendChild: function() {
-    throw fmt(destroyingError, ['appendChild']);
+    throw new EmberError(fmt(destroyingError, ['appendChild']));
   },
   rerender: function() {
-    throw fmt(destroyingError, ['rerender']);
+    throw new EmberError(fmt(destroyingError, ['rerender']));
   },
   destroyElement: function() {
-    throw fmt(destroyingError, ['destroyElement']);
+    throw new EmberError(fmt(destroyingError, ['destroyElement']));
   },
   empty: function() {
-    throw fmt(destroyingError, ['empty']);
+    throw new EmberError(fmt(destroyingError, ['empty']));
   },
 
   setElement: function() {
-    throw fmt(destroyingError, ["set('element', ...)"]);
+    throw new EmberError(fmt(destroyingError, ["set('element', ...)"]));
   },
 
   renderToBufferIfNeeded: function() {
