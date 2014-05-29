209d693d127ac72c7155b3cf71747e32e0f8e9fb
Robert Jackson <robert.w.jackson@me.com>
2014-05-29 17:38:27 -0400

    [FEATURE ember-metal-computed-empty-array] Remove flags.

--- packages/ember-runtime/tests/computed/computed_macros_test.js
+++ packages/ember-runtime/tests/computed/computed_macros_test.js
@@ -4,25 +4,23 @@ import {testBoth} from "ember-runtime/tests/props_helper";
 
 QUnit.module('CP macros');
 
-if (Ember.FEATURES.isEnabled('ember-metal-computed-empty-array')) {
-  testBoth('Ember.computed.empty', function (get, set) {
-    var obj = EmberObject.extend({
-      bestLannister: null,
-      lannisters: null,
+testBoth('Ember.computed.empty', function (get, set) {
+  var obj = EmberObject.extend({
+    bestLannister: null,
+    lannisters: null,
 
-      bestLannisterUnspecified: computed.empty('bestLannister'),
-      noLannistersKnown: computed.empty('lannisters')
-    }).create({
-      lannisters: Ember.A([])
-    });
-
-    equal(get(obj, 'bestLannisterUnspecified'), true, "bestLannister initially empty");
-    equal(get(obj, 'noLannistersKnown'), true, "lannisters initially empty");
-
-    get(obj, 'lannisters').pushObject('Tyrion');
-    set(obj, 'bestLannister', 'Tyrion');
-
-    equal(get(obj, 'bestLannisterUnspecified'), false, "empty respects strings");
-    equal(get(obj, 'noLannistersKnown'), false, "empty respects array mutations");
+    bestLannisterUnspecified: computed.empty('bestLannister'),
+    noLannistersKnown: computed.empty('lannisters')
+  }).create({
+    lannisters: Ember.A([])
   });
-}
+
+  equal(get(obj, 'bestLannisterUnspecified'), true, "bestLannister initially empty");
+  equal(get(obj, 'noLannistersKnown'), true, "lannisters initially empty");
+
+  get(obj, 'lannisters').pushObject('Tyrion');
+  set(obj, 'bestLannister', 'Tyrion');
+
+  equal(get(obj, 'bestLannisterUnspecified'), false, "empty respects strings");
+  equal(get(obj, 'noLannistersKnown'), false, "empty respects array mutations");
+});
