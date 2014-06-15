5b1f0cfc694a7f0178d9e52cc1f8e0f651d73857
Kris Selden <kris.selden@gmail.com>
2014-06-15 01:18:21 -0700

    get tests passing with Renderer

--- packages/ember-metal-views/lib/renderer.js
+++ packages/ember-metal-views/lib/renderer.js
@@ -1,93 +1,141 @@
-function Renderer(hooks, dom) {
-  this.hooks = hooks;
-  this.dom = dom;
+import { Morph } from "morph";
+
+function Renderer() {
+  this._uuid = 0;
+  this._views = new Array(2000);
+  this._queue = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+  this._parents = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+  this._elements = new Array(17);
+  this._inserts = {};
 }
 
-Renderer.prototype.render = function Renderer_render(rootView, rootMorph) {
-  var views = [rootView],
-      idx = 0,
-      view, parentView, i, l;
+function Renderer_renderTree(_view) {
+  var views = this._views;
+  views[0] = _view;
+  var index = 0;
+  var total = 1;
 
-  while (idx < views.length) {
-    view = views[idx];
+  var queue = this._queue;
+  queue[0] = 0;
+  var length = 1;
 
-    if (view.isVirtual) {
-      view.element = this.hooks.content(view);
-    } else {
-      view.element = this.hooks.element(view);
-      this.dom.appendChild(view.element, this.hooks.content(view));
+  var parentIndex = -1;
+  var parents = this._parents;
+  var parent = null;
+  var elements = this._elements;
+  var element = null;
+  var level = 0;
+
+  var view = _view;
+  var children, i, l, child;
+  while (length) {
+    elements[level] = element;
+    if (!view._morph) {
+      // ensure props we add are in same order
+      view._morph = null;
     }
+    this.uuid(view);
+    view._level = level;
+    this.willCreateElement(view);
+    element = this.createElement(view);
 
-    if (view._morph) {
-      view._morph.update(view.element);
-    } else {
-      // parent is a container view and manages its child views morphs using a parent morph.
-      // when parent view clears its rendering, these views will reset back to their prerender state
-      view._morph = this._childViewsMorph(view._parentView).append(view.element);
-    }
+    parents[level++] = parentIndex;
+    parentIndex = index;
+    parent = view;
 
-    view.isRendered = true;
-
-    var childViews = view._childViews, childView;
-
-    if (childViews) {
-      for (i = 0, l = childViews.length; i < l; i++) {
-        childView = childViews[i];
-        childView._parentView = view;
-        views.push(childView);
+    // enqueue for end
+    queue[length++] = index;
+    // enqueue children
+    children = this.childViews(view);
+    if (children) {
+      for (i=children.length-1;i>=0;i--) {
+        child = children[i];
+        index = total++;
+        views[index] = child;
+        queue[length++] = index;
+        view = child;
       }
     }
 
-    idx++;
-  }
+    index = queue[--length];
+    view = views[index];
 
-  // only assume we are inDOM if root had morph
-  if (rootMorph) {
-    this.hooks.beforeInsert(views);
+    while (parentIndex === index) {
+      level--;
+      view._isRendered = true;
+      this.didCreateElement(view);
+      this.willInsertElement(view);
 
-    rootView._morph = rootMorph;
-    rootView._morph.update(view.element);
+      if (level === 0) {
+        length--;
+        break;
+      }
 
-    this.hooks.afterInsert(views);
-  }
-
-  return rootView.element;
-};
-
-Renderer.prototype._renderView = function Renderer_renderView(view, isRoot) {
-  if (view.isVirtual) {
-    view.element = this.hooks.content(view);
-  } else {
-    view.element = this.hooks.element(view);
-    this.dom.appendChild(view.element, this.hooks.content(view));
-  }
-
-  if (isRoot) {
-    // root view append is delayed until the tree is complete
-    return view.element;
-  } else {
-    // if view has a _morph assigned already, it was added as part of its parents rendering
-    // and will be destroyed when its parent view clears its rendering
-
-  }
-};
-
-Renderer.prototype._childViewsMorph = function Renderer_childViewsMorph(parentView) {
-  if (parentView._childViewsMorph) {
-    return parentView._childViewsMorph;
-  }
-  if (parentView.isVirtual) {
-    // if virtual view is in a template
-    // just manage its childViews in its assigned
-    if (parentView._morph) {
-      return parentView._childViewsMorph = parentView._morph;
+      parentIndex = parents[level];
+      parent = views[parentIndex];
+      this.insertElement(view, parent, element);
+      index = queue[--length];
+      view = views[index];
+      element = elements[level];
+      elements[level] = null;
     }
-    // if root view of render is a virtual view
-    // we need to create a fragment morph
-    parentView._childViewsMorph = this.dom.createFragmentMorph();
-    parentView.element = parentView._childViewsMorph._parent;
   }
-  return parentView._childViewsMorph = this.dom.createElementMorph(parentView.element);
+
+  this.insertElement(view, null, element);
+
+  for (i=total-1;i>=0;i--) {
+    view._isInDOM = true;
+    this.didInsertElement(views[i]);
+    views[i] = null;
+  }
+
+  return element;
+}
+
+Renderer.prototype.uuid = function Renderer_uuid(view) {
+  if (view._uuid) {
+    view._uuid = ++this._uuid;
+  }
+  return view._uuid;
 };
+
+Renderer.prototype.appendTo = function Renderer_appendTo(view, target) {
+  // TODO check view state, cancel existing insertion.
+  // TODO use dom helper for creating this morph.
+  var start = document.createTextNode('');
+  var end = document.createTextNode('');
+  target.appendChild(start);
+  target.appendChild(end);
+  view._morph = new Morph(target, start, end);
+
+  var viewId = this.uuid(view);
+  this._inserts[viewId] = this.scheduleRender(this, function() {
+    this._inserts[viewId] = null;
+    this.renderTree(view);
+  });
+};
+
+Renderer.prototype.destroy = function () {
+};
+
+function Renderer_insertElement(view, parentView, element, insertAt) {
+  if (view._morph) {
+    view._morph.update(element);
+  } else {
+    view._morph = parentView._childViewsMorph.append(element);
+  }
+}
+
+Renderer.prototype.renderTree = Renderer_renderTree;
+Renderer.prototype.insertElement = Renderer_insertElement;
+
+/// HOOKS
+var noop = function () {};
+Renderer.prototype.willCreateElement = noop; // inBuffer
+Renderer.prototype.createElement = noop;
+Renderer.prototype.didCreateElement = noop; // hasElement
+Renderer.prototype.willInsertElement = noop;
+Renderer.prototype.didInsertElement = noop; // inDOM
+Renderer.prototype.childViews = noop;
 
 export default Renderer;
--- packages/ember-metal-views/tests/test_helpers.js
+++ packages/ember-metal-views/tests/test_helpers.js
@@ -1,24 +1,82 @@
 /*globals Node */
-
-import Renderer from "ember-metal-views";
+import { Morph } from "morph";
+import { Renderer } from "ember-metal-views";
 
 var renderer;
 
-var hooks = {
-  scheduleRender: function (renderer, render) {
-    render.call(renderer);
+function MetalRenderer () {
+  MetalRenderer.super.call(this);
+}
+MetalRenderer.super = Renderer;
+MetalRenderer.prototype = Object.create(Renderer.prototype, {
+  constructor: {
+    value: MetalRenderer,
+    enumerable: false,
+    writable: true,
+    configurable: true
+  }
+});
+
+MetalRenderer.prototype.childViews = function (view) {
+  return view.childViews;
+};
+MetalRenderer.prototype.willCreateElement = function(view) {
+};
+MetalRenderer.prototype.createElement = function (view) {
+  var el;
+  if (view.element) {
+    el = view.element;
+  } else {
+    el = view.element = document.createElement(view.tagName || 'div');
+  }
+  var classNames = view.classNames;
+  if (typeof classNames === 'string') {
+    el.setAttribute('class', classNames);
+  } else if (classNames && classNames.length) {
+    if (classNames.length === 1) { // PERF: avoid join'ing unnecessarily
+      el.setAttribute('class', classNames[0]);
+    } else {
+      el.setAttribute('class', classNames.join(' ')); // TODO: faster way to do this?
+    }
+  }
+  var attributeBindings = view.attributeBindings;
+  if (attributeBindings && attributeBindings.length) {
+    for (var i=0,l=attributeBindings.length; i<l; i++) {
+      var attributeBinding = attributeBindings[i];
+      var parts = attributeBinding.split(':');
+      el.setAttribute(parts[1], view[parts[0]]);
+    }
+  }
+  if (view.childViews) {
+    view._childViewsMorph = new Morph(el, null, null);
+  } else if (view.textContent) {
+    el.textContent = view.textContent;
+  } else if (view.innerHTML) {
+    el.innerHTML = view.innerHTML;
+  }
+  return el;
+};
+MetalRenderer.prototype.didCreateElement = function(view) {
+};
+MetalRenderer.prototype.willInsertElement = function(view) {
+  if (view.willInsertElement) {
+    view.willInsertElement();
+  }
+};
+MetalRenderer.prototype.didInsertElement = function(view) {
+  if (view.didInsertElement) {
+    view.didInsertElement();
   }
 };
 
-
+MetalRenderer.prototype.scheduleRender = function (renderer, render) {
+  render.call(renderer);
+};
 
 export function testsFor(name, options) {
   QUnit.module(name, {
     setup: function() {
-      renderer = new Renderer();
-      renderer.scheduleRender = function (renderer, render) {
-        render.call(renderer);
-      };
+      renderer = new MetalRenderer();
       if (options && options.setup) { options.setup(renderer); }
     },
     teardown: function() {
