d0dc5f46b5001feb5afe664e7f2031d898321070
Michael Crismali <michael.crismali@gmail.com>
2014-06-16 10:29:03 -0500

    lodasherized arrays

--- packages/ember-metal/lib/array.js
+++ packages/ember-metal/lib/array.js
@@ -1,6 +1,11 @@
 /**
 @module ember-metal
 */
+import isNative from "lodash/internals/isNative";
+import map from "lodash/collections/map";
+import forEach from "lodash/collections/forEach";
+import indexOf from "lodash/arrays/indexOf";
+import filter from "lodash/collections/filter";
 
 var ArrayPrototype = Array.prototype;
 
@@ -18,99 +23,11 @@ var defineNativeShim = function(nativeFunc, shim) {
   return shim;
 };
 
-// From: https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/array/map
-var map = defineNativeShim(ArrayPrototype.map, function(fun /*, thisp */) {
-  //"use strict";
-
-  if (this === void 0 || this === null || typeof fun !== "function") {
-    throw new TypeError();
-  }
-
-  var t = Object(this);
-  var len = t.length >>> 0;
-  var res = new Array(len);
-  var thisp = arguments[1];
-
-  for (var i = 0; i < len; i++) {
-    if (i in t) {
-      res[i] = fun.call(thisp, t[i], i, t);
-    }
-  }
-
-  return res;
-});
-
-// From: https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/array/foreach
-var forEach = defineNativeShim(ArrayPrototype.forEach, function(fun /*, thisp */) {
-  //"use strict";
-
-  if (this === void 0 || this === null || typeof fun !== "function") {
-    throw new TypeError();
-  }
-
-  var t = Object(this);
-  var len = t.length >>> 0;
-  var thisp = arguments[1];
-
-  for (var i = 0; i < len; i++) {
-    if (i in t) {
-      fun.call(thisp, t[i], i, t);
-    }
-  }
-});
-
-var indexOf = defineNativeShim(ArrayPrototype.indexOf, function (obj, fromIndex) {
-  if (fromIndex === null || fromIndex === undefined) {
-    fromIndex = 0;
-  } else if (fromIndex < 0) {
-    fromIndex = Math.max(0, this.length + fromIndex);
-  }
-
-  for (var i = fromIndex, j = this.length; i < j; i++) {
-    if (this[i] === obj) {
-      return i;
-    }
-  }
-  return -1;
-});
-
-var lastIndexOf = defineNativeShim(ArrayPrototype.lastIndexOf, function(obj, fromIndex) {
-  var len = this.length;
-  var idx;
-
-  if (fromIndex === undefined) {
-    fromIndex = len-1;
-  } else {
-    fromIndex = (fromIndex < 0) ? Math.ceil(fromIndex) : Math.floor(fromIndex);
-  }
-
-  if (fromIndex < 0) {
-    fromIndex += len;
-  }
-
-  for (idx = fromIndex; idx >= 0; idx--) {
-    if (this[idx] === obj) {
-      return idx;
-    }
-  }
-  return -1;
-});
-
-var filter = defineNativeShim(ArrayPrototype.filter, function (fn, context) {
-  var i, value;
-  var result = [];
-  var length = this.length;
-
-  for (i = 0; i < length; i++) {
-    if (this.hasOwnProperty(i)) {
-      value = this[i];
-      if (fn.call(context, value, i, this)) {
-        result.push(value);
-      }
-    }
-  }
-  return result;
-});
+var map = defineNativeShim(ArrayPrototype.map, map);
+var forEach = defineNativeShim(ArrayPrototype.forEach, forEach);
+var indexOf = defineNativeShim(ArrayPrototype.indexOf, indexOf);
+var lastIndexOf = defineNativeShim(ArrayPrototype.lastIndexOf, lastIndexOf);
+var filter = defineNativeShim(ArrayPrototype.filter, filter);
 
 if (Ember.SHIM_ES5) {
   ArrayPrototype.map = ArrayPrototype.map || map;
---
+++ packages/lodash/README.md
@@ -0,0 +1,16 @@
+# Lodash
+
+Replacing Ember's internal helper utilities with lodash's comparable functions.
+
+## Utilities Checklist
+
+[x] map
+[x] forEach
+[x] indexOf
+[x] filter
+
+## Building
+
+From project root...
+
+    lodash modularize exports="es6" -o ./packages/lodash/lib
