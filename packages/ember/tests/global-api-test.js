c510c1de190a61876bee23e02ad4cff59170480b
Robert Jackson <robert.w.jackson@me.com>
2014-07-09 16:32:33 -0400

    Fix Global's API test.
    When using strict mode, this is not always defined properly. This just
    uses `window`.

--- packages/ember/tests/global-api-test.js
+++ packages/ember/tests/global-api-test.js
@@ -3,10 +3,10 @@ import "ember";
 QUnit.module("Global API Tests");
 
 function confirmExport(property) {
-  ok(Ember.get(this, property) + ' is exported propertly');
+  test('confirm ' + property + ' is exported', function() {
+    ok(Ember.get(window, property) + ' is exported propertly');
+  });
 }
 
-test('confirm public functions and properties are exported properly', function() {
-  confirmExport('Ember.DefaultResolver');
-  confirmExport('Ember.generateController');
-});
+confirmExport('Ember.DefaultResolver');
+confirmExport('Ember.generateController');
