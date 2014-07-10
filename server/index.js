507495809c3bc729f195ae3e349ba44b3d2f9ad6
Martin Muñoz <im.mmun@gmail.com>
2014-07-09 22:42:29 -0400

    Ensure files are served through Broccoli middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 module.exports = function(app) {
-  app.use(require('express').static(__dirname + '/../'));
   app.get('/', function(req, res) {
-    res.redirect('dist/tests/');
-  })
+    res.redirect('tests/');
+  });
 };
