91579428ee4f32d69b680f1e5b859d276aa81412
Robert Jackson <robert.w.jackson@me.com>
2014-07-20 10:20:35 -0400

    Allow Router DSL to nest routes.

--- packages/ember-routing/tests/system/dsl_test.js
+++ packages/ember-routing/tests/system/dsl_test.js
@@ -26,3 +26,31 @@ test("should fail when using a reserved route name", function() {
     });
   }, "'basic' cannot be used as a resource name.");
 });
+
+test("should reset namespace if nested with resource", function(){
+  var router = Router.map(function(){
+    this.resource('bleep', function(){
+      this.resource('bloop', function() {
+        this.resource('blork');
+      });
+    });
+  });
+
+  ok(router.recognizer.names['bleep'], 'nested resources do not contain parent name');
+  ok(router.recognizer.names['bloop'], 'nested resources do not contain parent name');
+  ok(router.recognizer.names['blork'], 'nested resources do not contain parent name');
+});
+
+test("should retain resource namespace if nested with routes", function(){
+  var router = Router.map(function(){
+    this.route('bleep', function(){
+      this.route('bloop', function() {
+        this.route('blork');
+      });
+    });
+  });
+
+  ok(router.recognizer.names['bleep'], 'parent name was used as base of nested routes');
+  ok(router.recognizer.names['bleep.bloop'], 'parent name was used as base of nested routes');
+  ok(router.recognizer.names['bleep.bloop.blork'], 'parent name was used as base of nested routes');
+});
