7afe616bab43627611b5dfd33b55771c70230179
Robert Jackson <robert.w.jackson@me.com>
2014-07-21 09:35:17 -0400

    [BUGFIX beta] Prevent strict mode errors from superWrapper.
    In Strict Mode accesing the global `window` is considered unsecure,
    and will cause errors. This changes just guards against setting
    properties on `window` (which throws an error).
    
    [Here](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions_and_function_scope/Strict_mode#.22Securing.22_JavaScript) is what MDN has to say about "Securing" JavaScript.

---
+++ packages/ember-runtime/tests/system/object/strict-mode-test.js
@@ -0,0 +1,30 @@
+import Ember from "ember-metal/core";
+import EmberObject from "ember-runtime/system/object";
+
+var originalLookup, lookup;
+
+QUnit.module('strict mode tests');
+
+test('__superWrapper does not throw errors in strict mode', function() {
+  var Foo = EmberObject.extend({
+    blah: function() {
+      return 'foo';
+    }
+  });
+
+  var Bar = Foo.extend({
+    blah: function() {
+      return 'bar';
+    },
+
+    callBlah: function() {
+      var blah = this.blah;
+
+      return blah();
+    }
+  });
+
+  var bar = Bar.create();
+
+  equal(bar.callBlah(), 'bar', 'can call local function without call/apply');
+});
