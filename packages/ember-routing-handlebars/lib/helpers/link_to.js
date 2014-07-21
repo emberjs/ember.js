39f920d1d686b8da603dd19cc90f762132c0cd5f
Pepe Cano <ppcanodehuelva@gmail.com>
2014-07-21 19:03:10 +0200

    [BUXFIX beta] link-to helper can be inserted in DOM when the router is not present

--- packages/ember-routing-handlebars/lib/helpers/link_to.js
+++ packages/ember-routing-handlebars/lib/helpers/link_to.js
@@ -355,7 +355,10 @@ var LinkView = Ember.LinkView = EmberComponent.extend({
     @property router
   **/
   router: computed(function() {
-    return get(this, 'controller').container.lookup('router:main');
+    var controller = get(this, 'controller');
+    if (controller && controller.container) {
+      return controller.container.lookup('router:main');
+    }
   }),
 
   /**
@@ -493,8 +496,10 @@ var LinkView = Ember.LinkView = EmberComponent.extend({
     @return {Array} An array with the route name and any dynamic segments
   **/
   loadedParams: computed('resolvedParams', function computeLinkViewRouteArgs() {
+    var router = get(this, 'router');
+    if (!router) { return; }
+
     var resolvedParams = get(this, 'resolvedParams'),
-        router = get(this, 'router'),
         namedRoute = resolvedParams.targetRouteName;
 
     if (!namedRoute) { return; }
