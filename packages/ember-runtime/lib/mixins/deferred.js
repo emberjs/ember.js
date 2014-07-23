aeca80429fc95654ddc255223794a56f17b88eef
Robert Jackson <robert.w.jackson@me.com>
2014-07-23 18:02:32 -0400

    Deprecate Ember.DeferredMixin and Ember.Deferred.

--- packages/ember-runtime/lib/mixins/deferred.js
+++ packages/ember-runtime/lib/mixins/deferred.js
@@ -68,6 +68,8 @@ export default Mixin.create({
   },
 
   _deferred: computed(function() {
+    Ember.deprecate('Usage of Ember.DeferredMixin or Ember.Deferred is deprecated.');
+
     return RSVP.defer('Ember: DeferredMixin - ' + this);
   })
 });
