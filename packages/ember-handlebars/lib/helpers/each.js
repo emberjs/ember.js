a64ec5ba18a11bc89327eda25fa69aaf9474b1c0
Ryan Hollister <rhollister@q2ebanking.com>
2014-07-25 16:15:59 -0500

    [BUGFIX beta] Defensive check in _addMetamorphCheck to prevent exception  #5233

--- packages/ember-handlebars/lib/helpers/each.js
+++ packages/ember-handlebars/lib/helpers/each.js
@@ -138,12 +138,15 @@ var EachView = CollectionView.extend(_Metamorph, {
 function _addMetamorphCheck() {
   EachView.reopen({
     _checkMetamorph: on('didInsertElement', function() {
+      var start = document.getElementById( this.morph.start );
+      var end = document.getElementById( this.morph.end );
+
+      if (!start || !end) { return; }
+
       Ember.assert("The metamorph tags, " +
                    this.morph.start + " and " + this.morph.end +
                    ", have different parents.\nThe browser has fixed your template to output valid HTML (for example, check that you have properly closed all tags and have used a TBODY tag when creating a table with '{{#each}}')",
-        document.getElementById( this.morph.start ).parentNode ===
-        document.getElementById( this.morph.end ).parentNode
-      );
+        start.parentNode === end.parentNode );
     })
   });
 }
