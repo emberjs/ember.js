b5e5e4e83dfc41c1a454ba498b6c92086b71966f
Trek Glowacki <trek@groupon.com>
2014-07-30 17:39:00 -0500

    Avoid reusing arguments as variable for easier debugging.

--- packages/ember-testing/lib/helpers.js
+++ packages/ember-testing/lib/helpers.js
@@ -72,26 +72,33 @@ function click(app, selector, context) {
   return app.testHelpers.wait();
 }
 
-function triggerEvent(app, selector, context, type, options){
-  if (arguments.length === 3) {
+function triggerEvent(app, selector, contextOrType, typeOrOptions, possibleOptions){
+  var arity = arguments.length;
+  var context, type, options;
+
+  if (arity === 3) {
     // context and options are optional, so this is
     // app, selector, type
-    type = context;
     context = null;
+    type = contextOrType;
     options = {};
-  }
-
-  if (arguments.length === 4) {
+  } else if (arity === 4) {
     // context and options are optional, so this is
-    if (typeof type === "object") {  // either
+    if (typeof typeOrOptions === "object") {  // either
       // app, selector, type, options
-      options = type;
-      type = context;
       context = null;
+      type = contextOrType;
+      options = typeOrOptions;
     } else { // or
       // app, selector, context, type
+      context = contextOrType;
+      type = typeOrOptions;
       options = {};
     }
+  } else {
+    context = contextOrType;
+    type = typeOrOptions;
+    options = possibleOptions;
   }
 
   var $el = app.testHelpers.findWithAssert(selector, context);
@@ -103,21 +110,25 @@ function triggerEvent(app, selector, context, type, options){
   return app.testHelpers.wait();
 }
 
-function keyEvent(app, selector, context, type, keyCode) {
+function keyEvent(app, selector, contextOrType, typeOrKeyCode, keyCode) {
+  var context, type;
+
   if (typeof keyCode === 'undefined') {
-    keyCode = type;
-    type = context;
     context = null;
+    keyCode = typeOrKeyCode;
+    type = contextOrType;
+  } else {
+    context = contextOrType;
+    type = typeOrKeyCode;
   }
 
   return app.testHelpers.triggerEvent(selector, context, type, { keyCode: keyCode, which: keyCode });
 }
 
-function fillIn(app, selector, context, text) {
-  var $el;
+function fillIn(app, selector, contextOrText, text) {
+  var $el, context;
   if (typeof text === 'undefined') {
-    text = context;
-    context = null;
+    text = contextOrText;
   }
   $el = app.testHelpers.findWithAssert(selector, context);
   run(function() {
