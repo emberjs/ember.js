88471bf2c2a8279c9c226f7917b5aa00602fcad1
Stefan Penner <stefan.penner@gmail.com>
2014-07-30 08:15:13 -0400

    intern GUID_KEY

--- packages/ember-metal/lib/utils.js
+++ packages/ember-metal/lib/utils.js
@@ -43,6 +43,50 @@ var stringCache  = {};
 var MANDATORY_SETTER = Ember.ENV.MANDATORY_SETTER;
 
 /**
+  Strongly hint runtimes to intern the provided string.
+
+  When do I need to use this function?
+
+  For the most part, never. Pre-mature optimization is bad, and often the
+  runtime does exactly what you need it to, and more often the trade-off isn't
+  worth it.
+
+  Why?
+
+  Runtimes store strings in atleast 2 different representations:
+  Ropes and Symbols (interned strings). The Rope provides a memory efficient
+  data-structure for strings created from concatenation or some other string
+  manipulation like splitting.
+
+  Unfortunately checking equality of different ropes can be quite costly as
+  runtimes must resort to clever string comparison algorithims. These
+  algorithims typically cost in proportion to the length of the string.
+  Luckily, this is where the Symbols (interned strings) shine. As Symbols are
+  unique by their string content, equality checks can be done by pointer
+  comparision.
+
+  How do I know if my string is a rope or symbol?
+
+  Typically (warning general sweeping statement, but truthy in runtimes at
+  present) static strings created as part of the JS source are interned.
+  Strings often used for comparisions can be interned at runtime if some
+  criteria are met.  One of these criteria can be the size of the entire rope.
+  For example, in chrome 38 a rope longer then 12 characters will not
+  intern, nor will segments of that rope.
+
+  Some numbers: http://jsperf.com/eval-vs-keys/8
+
+  Known Trick™
+
+  @private
+  @return {String} interned version of the provided string
+*/
+function intern(string) {
+  "use strict";
+  return new Function('return "' + string + '"')();
+}
+
+/**
   A unique key used to assign guids and other private metadata to objects.
   If you inspect an object in your browser debugger you will often see these.
   They can be safely ignored.
@@ -56,7 +100,7 @@ var MANDATORY_SETTER = Ember.ENV.MANDATORY_SETTER;
   @type String
   @final
 */
-var GUID_KEY = '__ember' + (+ new Date());
+var GUID_KEY = intern('__ember' + (+ new Date()));
 
 var GUID_DESC = {
   writable:    false,
