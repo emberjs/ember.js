919259408b61edf8143ae8b940dec645d188f22a
Miguel Camba <miguel.camba@gmail.com>
2014-08-13 00:53:25 +0100

    [DOC release] Document Ember.DAG

--- packages/ember-application/lib/system/dag.js
+++ packages/ember-application/lib/system/dag.js
@@ -25,11 +25,28 @@ function visit(vertex, fn, visited, path) {
   path.pop();
 }
 
+
+/**
+ * DAG stands for Directed acyclic graph.
+ *
+ * It is used to build a graph of dependencies checking that there isn't circular
+ * dependencies. p.e Registering initializers with a certain precedence order.
+ *
+ * @class DAG
+ * @constructor
+ */
 function DAG() {
   this.names = [];
   this.vertices = {};
 }
 
+/**
+ * Adds a vertex entry to the graph unless it is already added.
+ *
+ * @private
+ * @method add
+ * @param {String} name The name of the vertex to add
+ */
 DAG.prototype.add = function(name) {
   if (!name) { return; }
   if (this.vertices.hasOwnProperty(name)) {
@@ -43,10 +60,27 @@ DAG.prototype.add = function(name) {
   return vertex;
 };
 
+/**
+ * Adds a vertex to the graph and sets its value.
+ *
+ * @private
+ * @method map
+ * @param {String} name The name of the vertex.
+ * @param         value The value to put in the vertex.
+ */
 DAG.prototype.map = function(name, value) {
   this.add(name).value = value;
 };
 
+/**
+ * Connects the vertices with the given names, adding them to the graph if
+ * necesary, only if this does not produce is any circular dependency.
+ *
+ * @private
+ * @method addEdge
+ * @param {String} fromName The name the vertex where the edge starts.
+ * @param {String} toName The name the vertex where the edge ends.
+ */
 DAG.prototype.addEdge = function(fromName, toName) {
   if (!fromName || !toName || fromName === toName) {
     return;
@@ -66,6 +100,13 @@ DAG.prototype.addEdge = function(fromName, toName) {
   to.incomingNames.push(fromName);
 };
 
+/**
+ * Visits all the vertex of the graph calling the given function with each one,
+ * ensuring that the vertices are visited respecting their precedence.
+ *
+ * @method  topsort
+ * @param {Function} fn The function to be invoked on each vertex.
+ */
 DAG.prototype.topsort = function(fn) {
   var visited = {};
   var vertices = this.vertices;
@@ -81,6 +122,23 @@ DAG.prototype.topsort = function(fn) {
   }
 };
 
+/**
+ * Adds a vertex with the given name and value to the graph and joins it with the
+ * vertices referenced in _before_ and _after_. If there isn't vertices with those
+ * names, they are added too.
+ *
+ * If either _before_ or _after_ are falsy/empty, the added vertex will not have
+ * an incoming/outgoing edge.
+ *
+ * @method addEdges
+ * @param {String} name The name of the vertex to be added.
+ * @param         value The value of that vertex.
+ * @param        before An string or array of strings with the names of the vertices before
+ *                      which this vertex must be visited.
+ * @param         after An string or array of strings with the names of the vertex after
+ *                      which this vertex must be visited.
+ *
+ */
 DAG.prototype.addEdges = function(name, value, before, after) {
   var i;
   this.map(name, value);
