fba8e5b06238682a18435f0840521d9e6ad3c6b5
Robert Jackson <robert.w.jackson@me.com>
2014-08-19 21:43:38 -0400

    Ensure Ember.keys works for null prototype objects.
    This is implementing the changes made on master in
    7db120f732a7ce1f898ac8ace96dfce3e1a7ec2f.

--- packages/ember-runtime/lib/keys.js
+++ packages/ember-runtime/lib/keys.js
@@ -28,7 +28,7 @@ if (!keys || create.isSimulated) {
     if (key.substring(0,2) === '__') return;
     if (key === '_super') return;
     if (EnumerableUtils.indexOf(array, key) >= 0) return;
-    if (typeof obj.hasOwnProperty === 'function' && !obj.hasOwnProperty(key)) return;
+    if (!Object.prototype.hasOwnProperty.call(obj, key)) return;
 
     array.push(key);
   };
