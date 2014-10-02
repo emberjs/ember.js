aac34ea4cf9b22a3d9bc1d72f3c39eab0e4fe963
Stefan Penner <stefan.penner@gmail.com>
2014-10-02 10:05:42 -0400

    [Bugfix beta] don’t leak `-0` as a key
    * some tests for NaN support

--- packages/ember-metal/tests/map_test.js
+++ packages/ember-metal/tests/map_test.js
@@ -435,6 +435,49 @@ function testMap(nameAndFunc) {
     });
     equal(iterations, 0);
   });
+
+  test("-0", function() {
+    equal(map.has(-0), false);
+    equal(map.has(0), false);
+
+    map.set(-0, 'zero');
+
+    equal(map.has(-0), true);
+    equal(map.has(0), true);
+
+    equal(map.get(0), 'zero');
+    equal(map.get(-0), 'zero');
+
+    map.forEach(function(value, key) {
+      equal(1/key, Infinity, 'spec says key should be positive zero');
+    });
+  });
+
+  test("NaN", function() {
+    equal(map.has(NaN), false);
+
+    map.set(NaN, 'not-a-number');
+
+    equal(map.has(NaN), true);
+
+    equal(map.get(NaN), 'not-a-number');
+
+  });
+
+  test("NaN Boxed", function() {
+    //jshint -W053
+    var boxed = new Number(NaN);
+    equal(map.has(boxed), false);
+
+    map.set(boxed, 'not-a-number');
+
+    equal(map.has(boxed), true);
+    equal(map.has(NaN), false);
+
+    equal(map.get(NaN), undefined);
+    equal(map.get(boxed), 'not-a-number');
+  });
+
 }
 
 for (var i = 0;  i < varieties.length;  i++) {
