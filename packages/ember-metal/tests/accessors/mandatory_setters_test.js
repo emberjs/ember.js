16dd5ad99682b1f496373ba94279222ffdd69507
Robert Jackson <robert.w.jackson@me.com>
2014-11-01 16:57:35 -0500

    Mandatory setter checks for configurable.

--- packages/ember-metal/tests/accessors/mandatory_setters_test.js
+++ packages/ember-metal/tests/accessors/mandatory_setters_test.js
@@ -1,7 +1,12 @@
 import { get } from "ember-metal/property_get";
 import { set } from "ember-metal/property_set";
 import { watch } from "ember-metal/watching";
-import { hasPropertyAccessors } from "ember-metal/platform";
+import {
+  hasPropertyAccessors,
+  defineProperty,
+  create
+} from "ember-metal/platform";
+import { meta as metaFor } from "ember-metal/utils";
 
 QUnit.module('mandatory-setters');
 
@@ -46,6 +51,47 @@ if (Ember.FEATURES.isEnabled('mandatory-setter')) {
       set(obj, 'someProp', 'foo-bar');
 
       equal(get(obj, 'someProp'), 'foo-bar');
+    });
+
+    test('does not setup mandatory-setter if non-configurable', function() {
+      var obj = {
+        someProp: null,
+        toString: function() {
+          return 'custom-object';
+        }
+      };
+      var meta = metaFor(obj);
+
+      defineProperty(obj, 'someProp', {
+        configurable: false,
+        enumerable: true,
+        value: 'blastix'
+      });
+
+      watch(obj, 'someProp');
+
+      ok(!('someProp' in meta.values), 'blastix');
+    });
+
+    test('sets up mandatory-setter if property comes from prototype', function() {
+      expect(2);
+
+      var obj = {
+        someProp: null,
+        toString: function() {
+          return 'custom-object';
+        }
+      };
+      var obj2 = create(obj);
+
+      watch(obj2, 'someProp');
+      var meta = metaFor(obj2);
+
+      ok(('someProp' in meta.values), 'mandatory setter has been setup');
+
+      expectAssertion(function() {
+        obj2.someProp = 'foo-bar';
+      }, 'You must use Ember.set() to set the `someProp` property (of custom-object) to `foo-bar`.');
     });
   }
 } else {
