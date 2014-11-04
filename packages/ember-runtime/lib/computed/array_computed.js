2535cfc369b4d2a25f5c539d9b2ce3d20db5e176
patricklx <pa.trickjlp@gmail.com>
2014-11-04 18:44:05 +0000

    now done in ReduceComputedProperty
    This is now required in ReduceComputedProperty. Since its not always recomputed on each change.
    No need do be here anymore

--- packages/ember-runtime/lib/computed/array_computed.js
+++ packages/ember-runtime/lib/computed/array_computed.js
@@ -10,27 +10,7 @@ import EmberError from 'ember-metal/error';
 var a_slice = [].slice;
 
 function ArrayComputedProperty() {
-  var cp = this;
-
   ReduceComputedProperty.apply(this, arguments);
-
-  this.func = (function(reduceFunc) {
-    return function (propertyName) {
-      if (!cp._hasInstanceMeta(this, propertyName)) {
-        // When we recompute an array computed property, we need already
-        // retrieved arrays to be updated; we can't simply empty the cache and
-        // hope the array is re-retrieved.
-        forEach(cp._dependentKeys, function(dependentKey) {
-          addObserver(this, dependentKey, function() {
-            cp.recomputeOnce.call(this, propertyName);
-          });
-        }, this);
-      }
-
-      return reduceFunc.apply(this, arguments);
-    };
-  })(this.func);
-
   return this;
 }
 
