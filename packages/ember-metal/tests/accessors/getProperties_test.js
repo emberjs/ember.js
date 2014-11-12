b7073aa419dedabbaf1f218ffaf81768e02ee79c
Levi Thornton <levi.thornton@backops.co>
2014-11-12 11:01:27 -0800

    Initial failing tests

--- packages/ember-metal/tests/accessors/getProperties_test.js
+++ packages/ember-metal/tests/accessors/getProperties_test.js
@@ -17,3 +17,21 @@ test('can retrieve a hash of properties from an object via an argument list or a
   deepEqual(getProperties(obj, ["firstName"]), { firstName: 'Steve' });
   deepEqual(getProperties(obj, []), {});
 });
+
+if (Ember.FEATURES.isEnabled("ember-metal-get-properties-rename-keys")) {
+  test('can transform property names in returned object', function() {
+    expect(4);
+
+    var obj = {
+      firstName: "Steve",
+      lastName: "Jobs"
+    };
+
+    deepEqual(getProperties(obj, "firstName:first", "lastName:last"), { first: 'Steve', last: 'Jobs' });
+    deepEqual(getProperties(obj, ":"), {});
+
+    obj['application:main'] = 'app';
+    deepEqual(getProperties(obj, "application:main"), { 'application:main': 'app' });
+    deepEqual(getProperties(obj, "application:main:application"), { 'application': 'app' });
+  });
+}
