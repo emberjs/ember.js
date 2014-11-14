fa5ec975410b45b73cb19f916f137a993afe0ec0
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-11-14 11:44:49 -0200

    Improved hook tests

--- packages/htmlbars-runtime/tests/main-test.js
+++ packages/htmlbars-runtime/tests/main-test.js
@@ -2,9 +2,21 @@ import {hooks} from "../htmlbars-runtime";
 
 QUnit.module("htmlbars-runtime");
 
+function keys(obj) {
+  var ownKeys = [];
+  for(var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      ownKeys.push(key);
+    }
+  }
+  return ownKeys;
+}
+
 test("hooks are present", function () {
-  var hookNames = ['content', 'lookupHelper'];
+  var hookNames = ["content", "component", "componentFallback", "element", "attribute", "concat", "partial", "subexpr", "lookupHelper", "simple", "hydrationHooks"];
   for (var i=0;i<hookNames.length;i++) {
     ok(hooks[hookNames[i]], "hook "+hookNames[i]+" is present");
   }
+
+  equal(keys(hooks).length, hookNames.length, "Hooks length match");
 });
