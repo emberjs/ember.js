abcc63fb45b0ee2be18da610200352f2d5be67dd
William Riancho <wr.wllm@gmail.com>
2014-11-14 19:35:36 +0100

    add unquoted notation for the loc helper

--- packages/ember-handlebars/lib/helpers/loc.js
+++ packages/ember-handlebars/lib/helpers/loc.js
@@ -29,6 +29,16 @@ import { loc } from "ember-runtime/system/string";
   </div>
   ```
 
+  If you use the unquoted notation, Ember will look for the attribute in the
+  current context and localize the value of this attribute. As this helper
+  doesn’t create any view, the value cannot be bound.
+
+  ```handlebars
+  <div class='message'>
+    {{loc welcomeMessage}}
+  </div>
+  ```
+
   See [Ember.String.loc](/api/classes/Ember.String.html#method_loc) for how to
   set up localized string references.
 
@@ -37,4 +47,33 @@ import { loc } from "ember-runtime/system/string";
   @param {String} str The string to format
   @see {Ember.String#loc}
 */
-export default loc;
+
+var slice = [].slice;
+
+export default function() {
+  var types = slice.call(arguments, -1)[0].types;
+  var params = slice.call(arguments, 0, -1);
+
+  var str = params.shift();
+
+  if (types.shift() === 'ID') {
+    str = this.get(str);
+  }
+
+  var length = params.length;
+  if (length) {
+    var formats = [];
+
+    while (length--) {
+      if (types.shift() === 'ID') {
+        formats.push(this.get(params.shift()));
+      } else {
+        formats.push(params.shift());
+      }
+    }
+    return loc(str, formats);
+
+  } else {
+    return loc(str);
+  }
+}
