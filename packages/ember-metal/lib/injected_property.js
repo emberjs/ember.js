711e419c030ea18e8d05fc1eeb9a316e0628dded
Steven Lindberg <stevenjohnlindberg@gmail.com>
2014-11-15 17:28:52 -0800

    Small perf and debuggability tweaks

--- packages/ember-metal/lib/injected_property.js
+++ packages/ember-metal/lib/injected_property.js
@@ -2,7 +2,10 @@ import Ember from "ember-metal/core"; // Ember.assert
 import { ComputedProperty } from "ember-metal/computed";
 import { Descriptor } from "ember-metal/properties";
 import { create } from "ember-metal/platform";
-import { inspect } from "ember-metal/utils";
+import {
+  meta,
+  inspect
+} from "ember-metal/utils";
 import EmberError from "ember-metal/error";
 
 /**
@@ -20,14 +23,22 @@ function InjectedProperty(type, name) {
   this.type = type;
   this.name = name;
 
-  this._super$Constructor(function(keyName) {
-    Ember.assert("Attempting to lookup an injected property on an object " +
-                 "without a container, ensure that the object was " +
-                 "instantiated via a container.", this.container);
-
-    return this.container.lookup(type + ':' + (name || keyName));
-  });
+  this._super$Constructor(injectedPropertyGet);
   this.readOnly();
+}
+
+function injectedPropertyGet(keyName) {
+  var desc = meta(this).descs[keyName];
+
+  Ember.assert("Attempting to lookup an injected property on an object " +
+               "without a container, ensure that the object was " +
+               "instantiated via a container.", this.container);
+
+  return this.container.lookup(desc.type + ':' + (desc.name || keyName));
+}
+
+function injectedPropertySet(obj, keyName) {
+  throw new EmberError("Cannot set injected property '" + keyName + "' on object: " + inspect(obj));
 }
 
 InjectedProperty.prototype = create(Descriptor.prototype);
@@ -40,9 +51,7 @@ InjectedPropertyPrototype._super$Constructor = ComputedProperty;
 InjectedPropertyPrototype.get = ComputedPropertyPrototype.get;
 InjectedPropertyPrototype.readOnly = ComputedPropertyPrototype.readOnly;
 
-InjectedPropertyPrototype.set = function(obj, keyName) {
-  throw new EmberError("Cannot set injected property '" + keyName + "' on object: " + inspect(obj));
-};
+InjectedPropertyPrototype.set = injectedPropertySet;
 
 InjectedPropertyPrototype.teardown = ComputedPropertyPrototype.teardown;
 
