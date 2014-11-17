7e5e4cbd9a29ce6e1c1c877d39044531dc3d5dff
Alex Navasardyan <twokul@users.noreply.github.com>
2014-11-17 14:55:33 -0500

    Adds license and version to output build

--- packages/htmlbars/lib/main.js
+++ packages/htmlbars/lib/main.js
@@ -1,3 +1,11 @@
+/*
+ * @overview  HTMLBars
+ * @copyright Copyright 2011-2014 Tilde Inc. and contributors
+ * @license   Licensed under MIT license
+ *            See https://raw.githubusercontent.com/tildeio/htmlbars/master/LICENSE
+ * @version   VERSION_STRING_PLACEHOLDER
+ */
+
 import {compile, compileSpec} from "./htmlbars-compiler/compiler";
 import Morph from "./morph/morph";
 import DOMHelper from "./morph/dom-helper";
