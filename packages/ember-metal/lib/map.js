0f3abdd25f04c459c2d6ef4015251a49a3e38a8d
Thomas Sunde Nielsen <me@thomassnielsen.com>
2014-11-18 16:26:09 +0100

    [DOC release] Fix doc bug where forEach parameter order is wrong
    Seems the code was updated to match ES6 but not the docs.

--- packages/ember-metal/lib/map.js
+++ packages/ember-metal/lib/map.js
@@ -379,7 +379,7 @@ Map.prototype = {
 
   /**
     Iterate over all the keys and values. Calls the function once
-    for each key, passing in the key and value, in that order.
+    for each key, passing in the value and key, in that order.
 
     The keys are guaranteed to be iterated over in insertion order.
 
