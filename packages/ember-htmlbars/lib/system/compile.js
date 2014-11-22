066f858f3cd42344bd4a24f3afc9dec918bbebab
Robert Jackson <robert.w.jackson@me.com>
2014-11-22 13:34:55 -0500

    [FEATURE] ember-htmlbars-component-generation
    Disable HTMLBars `component` hook unless this feature is enabled.

--- packages/ember-htmlbars/lib/system/compile.js
+++ packages/ember-htmlbars/lib/system/compile.js
@@ -1,5 +1,11 @@
+import Ember from "ember-metal/core";
 import { compile } from "htmlbars-compiler/compiler";
 import template from "ember-htmlbars/system/template";
+
+var disableComponentGeneration = true;
+if (Ember.FEATURES.isEnabled('ember-htmlbars-component-generation')) {
+  disableComponentGeneration = false;
+}
 
 /**
   Uses HTMLBars `compile` function to process a string into a compiled template.
@@ -11,7 +17,9 @@ import template from "ember-htmlbars/system/template";
   @param {String} templateString This is the string to be compiled by HTMLBars.
 */
 export default function(templateString) {
-  var templateSpec = compile(templateString);
+  var templateSpec = compile(templateString, {
+    disableComponentGeneration: disableComponentGeneration
+  });
 
   return template(templateSpec);
 }
