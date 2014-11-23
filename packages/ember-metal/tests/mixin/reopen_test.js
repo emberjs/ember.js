e81f626a52be31dbe1be8614043a8e48c3e017bf
Stanley Stuart <stanley@stan.li>
2014-11-23 13:25:28 -0600

    failing test showing infinite recursion w/_super
    refs emberjs/ember.js PR #9651

--- packages/ember-metal/tests/mixin/reopen_test.js
+++ packages/ember-metal/tests/mixin/reopen_test.js
@@ -13,3 +13,33 @@ test('using reopen() to add more properties to a simple', function() {
   equal(Ember.get(obj, 'bar'), 'BAR', 'include MixinB props');
 });
 
+test('using reopen() and calling _super does not cause infinite recursion', function(){
+
+  var Taco = Ember.Object.extend({
+    createBreakfast: function(){
+      this._super.apply(this, arguments);
+      return "Breakfast";
+    }
+  });
+
+
+  Taco.reopen({
+    createBreakfast: function(){
+      return this._super.apply(this, arguments);
+    }
+  });
+
+  var taco = Taco.create();
+
+  var result;
+  Ember.run(function(){
+    try {
+    result = taco.createBreakfast();
+    } catch (e) {
+      result = "Oatmeal :( This should not error.";
+    }
+  });
+
+  equal(result, "Breakfast!");
+});
+
