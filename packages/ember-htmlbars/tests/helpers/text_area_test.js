1df292f064b3e2f38d6050ba264f7c80fc3ed100
Alex Navasardyan <twokul@users.noreply.github.com>
2014-11-26 09:49:33 -0500

    Tweaks for text-area tests

--- packages/ember-htmlbars/tests/helpers/text_area_test.js
+++ packages/ember-htmlbars/tests/helpers/text_area_test.js
@@ -1,9 +1,15 @@
 import run from "ember-metal/run_loop";
 import View from "ember-views/views/view";
 import EmberHandlebars from "ember-htmlbars/compat";
+import htmlbarsCompile from "ember-htmlbars/system/compile";
 import { set as o_set } from "ember-metal/property_set";
 
-var compile = EmberHandlebars.compile;
+var compile;
+if (Ember.FEATURES.isEnabled('ember-htmlbars')) {
+  compile = htmlbarsCompile;
+} else {
+  compile = EmberHandlebars.compile;
+}
 
 var textArea, controller;
 
