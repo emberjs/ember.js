a87de7ac38a48b3da362fcf5b2dd64daf2a3c8aa
Matthew Beale <matt.beale@madhatted.com>
2014-11-29 18:48:17 -0500

    Port attrs and classes via bind-attr to AttrNode

--- packages/ember-views/lib/views/view.js
+++ packages/ember-views/lib/views/view.js
@@ -2273,6 +2273,7 @@ View.views = {};
 // method.
 View.childViewsProperty = childViewsProperty;
 
+// Used by Handlebars helpers, view element attributes
 View.applyAttributeBindings = function(elem, name, value) {
   var type = typeOf(value);
 
