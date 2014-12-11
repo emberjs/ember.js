31d6c95bf22fed830ae6b0b9eb50582d835cb69d
Robert Jackson <robert.w.jackson@me.com>
2014-12-10 22:38:12 -0500

    Export parse from htmlbars-syntax package.

--- packages/htmlbars-syntax/lib/main.js
+++ packages/htmlbars-syntax/lib/main.js
@@ -1,7 +1,11 @@
 import Walker from "./htmlbars-syntax/walker";
 import builders from "./htmlbars-syntax/builders";
+import {
+  preprocess as parse
+} from "./htmlbars-syntax/parser";
 
 export {
   Walker,
-  builders
+  builders,
+  parse
 };
