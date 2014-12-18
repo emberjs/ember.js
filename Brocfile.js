2ce1ad35c974a80085baba002044dc0fe1b65c1c
Alex Navasardyan <twokul@users.noreply.github.com>
2014-12-18 12:45:09 -0500

    brings the comment back

--- Brocfile.js
+++ Brocfile.js
@@ -1,5 +1,10 @@
 /* jshint node: true */
 
+// To create fast production builds (without ES3 support, minification, derequire, or JSHint)
+// run the following:
+//
+// DISABLE_ES3=true DISABLE_JSHINT=true DISABLE_MIN=true DISABLE_DEREQUIRE=true ember serve --environment=production
+
 var EmberBuild = require('emberjs-build');
 var packages   = require('./lib/packages');
 
