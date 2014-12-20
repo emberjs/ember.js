96fd897289f8d8b30800e4e8fd12b02a695676f0
Gil Pedersen <git@gpost.dk>
2014-12-20 14:50:44 +0100

    [BUGFIX release] Fix hasThis lookup regression
    Also optimizes lookup using lastIndexOf(x, 0), resulting in a constant time lookup

--- packages/ember-metal/lib/path_cache.js
+++ packages/ember-metal/lib/path_cache.js
@@ -6,7 +6,7 @@ var HAS_THIS       = 'this.';
 
 var isGlobalCache       = new Cache(1000, function(key) { return IS_GLOBAL.test(key);          });
 var isGlobalPathCache   = new Cache(1000, function(key) { return IS_GLOBAL_PATH.test(key);     });
-var hasThisCache        = new Cache(1000, function(key) { return key.indexOf(HAS_THIS) !== -1; });
+var hasThisCache        = new Cache(1000, function(key) { return key.lastIndexOf(HAS_THIS, 0) === 0; });
 var firstDotIndexCache  = new Cache(1000, function(key) { return key.indexOf('.');             });
 
 var firstKeyCache = new Cache(1000, function(path) {
