ee12bb232002dc716adb5f2776455a38d35e73db
Erik Bryn <erik.bryn@gmail.com>
2014-12-20 18:41:15 -0800

    Break out view state related code into ViewStateSupport

--- packages/ember-views/lib/views/view.js
+++ packages/ember-views/lib/views/view.js
@@ -416,6 +416,22 @@ var ViewChildViewsSupport = Mixin.create({
   }
 });
 
+var ViewStateSupport = Mixin.create({
+  transitionTo: function(state, children) {
+    Ember.deprecate("Ember.View#transitionTo has been deprecated, it is for internal use only");
+    this._transitionTo(state, children);
+  },
+
+  _transitionTo: function(state, children) {
+    var priorState = this.currentState;
+    var currentState = this.currentState = this._states[state];
+    this._state = state;
+
+    if (priorState && priorState.exit) { priorState.exit(this); }
+    if (currentState.enter) { currentState.enter(this); }
+  }
+});
+
 /**
   `Ember.View` is the class in Ember responsible for encapsulating templates of
   HTML content, combining templates with data to render as sections of a page's
@@ -1003,7 +1019,7 @@ var ViewChildViewsSupport = Mixin.create({
   @namespace Ember
   @extends Ember.CoreView
 */
-var View = CoreView.extend(ViewStreamSupport, ViewKeywordSupport, ViewContextSupport, ViewChildViewsSupport, {
+var View = CoreView.extend(ViewStreamSupport, ViewKeywordSupport, ViewContextSupport, ViewChildViewsSupport, ViewStateSupport, {
 
   concatenatedProperties: ['classNames', 'classNameBindings', 'attributeBindings'],
 
@@ -2077,20 +2093,6 @@ var View = CoreView.extend(ViewStreamSupport, ViewKeywordSupport, ViewContextSup
     return false;
   },
 
-  transitionTo: function(state, children) {
-    Ember.deprecate("Ember.View#transitionTo has been deprecated, it is for internal use only");
-    this._transitionTo(state, children);
-  },
-
-  _transitionTo: function(state, children) {
-    var priorState = this.currentState;
-    var currentState = this.currentState = this._states[state];
-    this._state = state;
-
-    if (priorState && priorState.exit) { priorState.exit(this); }
-    if (currentState.enter) { currentState.enter(this); }
-  },
-
   // .......................................................
   // EVENT HANDLING
   //
@@ -2229,4 +2231,4 @@ View.applyAttributeBindings = function(dom, elem, name, initialValue) {
 
 export default View;
 
-export { ViewKeywordSupport, ViewStreamSupport, ViewContextSupport, ViewChildViewsSupport };
+export { ViewKeywordSupport, ViewStreamSupport, ViewContextSupport, ViewChildViewsSupport, ViewStateSupport };
