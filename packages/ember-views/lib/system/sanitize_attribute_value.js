38d035cee31a418ff30352ad4073d26d57caf467
Matthew Beale <matt.beale@madhatted.com>
2014-12-21 13:22:36 -0500

    [BUGFIX stable] Expand sanitization to img and link tags

--- packages/ember-views/lib/system/sanitize_attribute_value.js
+++ packages/ember-views/lib/system/sanitize_attribute_value.js
@@ -2,14 +2,31 @@
 
 import EmberHandlebars from "ember-handlebars-compiler";
 
-var parsingNode = document.createElement('a');
+var parsingNode;
 var badProtocols = {
   'javascript:': true,
   'vbscript:': true
 };
 
+var badTags = {
+  'A': true,
+  'BODY': true,
+  'LINK': true,
+  'IMG': true
+};
+
+export var badAttributes = {
+  'href': true,
+  'src': true,
+  'background': true
+};
+
 export default function sanitizeAttributeValue(element, attribute, value) {
   var tagName;
+
+  if (!parsingNode) {
+    parsingNode = document.createElement('a');
+  }
 
   if (!element) {
     tagName = null;
@@ -21,8 +38,7 @@ export default function sanitizeAttributeValue(element, attribute, value) {
     return value.toString();
   }
 
-
-  if ((tagName === null || tagName === 'A') && attribute === 'href') {
+  if ((tagName === null || badTags[tagName]) && badAttributes[attribute]) {
     parsingNode.href = value;
 
     if (badProtocols[parsingNode.protocol] === true) {
