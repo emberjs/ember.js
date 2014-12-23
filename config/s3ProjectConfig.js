b371cfa3ef978ffb302c0fbdf5799a5d668724b2
Robert Jackson <robert.w.jackson@me.com>
2014-12-23 10:41:46 -0500

    [BUGFIX beta] Publish ember-template-compiler.js to S3.

--- config/s3ProjectConfig.js
+++ config/s3ProjectConfig.js
@@ -5,6 +5,7 @@ function fileMap(revision,tag,date) {
     "ember-testing.js":           fileObject("ember-testing",           ".js",   "text/javascript",  revision, tag, date),
     "ember-tests.js":             fileObject("ember-tests",             ".js",   "text/javascript",  revision, tag, date),
     "ember-runtime.js":           fileObject("ember-runtime",           ".js",   "text/javascript",  revision, tag, date),
+    "ember-template-compiler.js": fileObject("ember-template-compiler", ".js",   "text/javascript",  revision, tag, date),
     "ember.min.js":               fileObject("ember.min",               ".js",   "text/javascript",  revision, tag, date),
     "ember.prod.js":              fileObject("ember.prod",              ".js",   "text/javascript",  revision, tag, date),
     "../docs/data.json":          fileObject("ember-docs",              ".json", "application/json", revision, tag, date),
