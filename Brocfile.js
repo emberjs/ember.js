61639712643aac53e888ed633e0ec43c8fa9d7ba
Robert Jackson <robert.w.jackson@me.com>
2014-12-24 14:32:00 -0500

    Add DISABLE_JSCS flag to "fast prod build" comment in Brocfile.js.

--- Brocfile.js
+++ Brocfile.js
@@ -3,7 +3,7 @@
 // To create fast production builds (without ES3 support, minification, derequire, or JSHint)
 // run the following:
 //
-// DISABLE_ES3=true DISABLE_JSHINT=true DISABLE_MIN=true DISABLE_DEREQUIRE=true ember serve --environment=production
+// DISABLE_ES3=true DISABLE_JSCS=true DISABLE_JSHINT=true DISABLE_MIN=true DISABLE_DEREQUIRE=true ember serve --environment=production
 
 var EmberBuild = require('emberjs-build');
 var packages   = require('./lib/packages');
