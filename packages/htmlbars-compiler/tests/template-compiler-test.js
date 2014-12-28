15f48db8d6067894191b4376e34be64e72b81100
Or Neeman <oneeman@gmail.com>
2014-12-27 19:38:46 -0600

    Simplify namespace handling
    -- Omit unnecessary namespace changes in fragment generation
    -- Consolidate namespace handling into one place
    -- Correct assertion messages in some SVG-related tests

--- packages/htmlbars-compiler/tests/template-compiler-test.js
+++ packages/htmlbars-compiler/tests/template-compiler-test.js
@@ -10,6 +10,14 @@ QUnit.module("TemplateCompiler");
 
 var dom, hooks, helpers;
 
+function countNamespaceChanges(template) {
+  var ast = preprocess(template);
+  var compiler = new TemplateCompiler();
+  var program = compiler.compile(ast);
+  var matches = program.match(/dom\.setNamespace/g);
+  return matches ? matches.length : 0;
+}
+
 test("it works", function testFunction() {
   /* jshint evil: true */
   var ast = preprocess('<div>{{#if working}}Hello {{firstName}} {{lastName}}!{{/if}}</div>');
@@ -40,5 +48,13 @@ test("it works", function testFunction() {
   };
   var frag = template.render(context, env, document.body);
   equalHTML(frag, '<div>Hello Kris Selden!</div>');
+});
+
+test("it omits unnecessary namespace changes", function () {
+  equal(countNamespaceChanges('<div></div>'), 0);  // sanity check
+  equal(countNamespaceChanges('<div><svg></svg></div><svg></svg>'), 1);
+  equal(countNamespaceChanges('<div><svg></svg></div><div></div>'), 2);
+  equal(countNamespaceChanges('<div><svg><title>foobar</title></svg></div><svg></svg>'), 1);
+  equal(countNamespaceChanges('<div><svg><title><h1>foobar</h1></title></svg></div><svg></svg>'), 3);
 });
 
