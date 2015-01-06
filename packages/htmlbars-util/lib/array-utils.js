a750d9559cd22ffb0ac3eec44b04205f56f0dbeb
Robert Jackson <robert.w.jackson@me.com>
2015-01-06 07:43:20 -0500

    Do not use Array.prototype.map.
    This causes a number of failures on IE8 in Ember.

--- packages/htmlbars-util/lib/array-utils.js
+++ packages/htmlbars-util/lib/array-utils.js
@@ -1,12 +1,23 @@
 export function forEach(array, callback, binding) {
-  var i;
+  var i, l;
   if (binding === undefined) {
-    for (i = 0; i < array.length; i++) {
+    for (i = 0, l = array.length; i < l; i++) {
       callback(array[i], i, array);
     }
   } else {
-    for (i = 0; i < array.length; i++) {
+    for (i = 0, l = array.length; i < l; i++) {
       callback.call(binding, array[i], i, array);
     }
   }
+}
+
+export function map(array, callback) {
+  var output = [];
+  var i, l;
+
+  for (i = 0, l = array.length; i < l; i++) {
+    output.push(callback(array[i], i, array));
+  }
+
+  return output;
 }
