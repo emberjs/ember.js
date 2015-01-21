67200d29842684df85e8cf00553ef2a390dbe04e
Robert Jackson <robert.w.jackson@me.com>
2015-01-21 07:23:23 -0500

    [BUGFIX beta] Remove HTMLBars from builds.

---
+++ packages/ember-template-compiler/lib/compat.js
@@ -0,0 +1,10 @@
+import Ember from "ember-metal/core";
+import precompile from "ember-template-compiler/compat/precompile";
+import compile from "ember-template-compiler/system/compile";
+import template from "ember-template-compiler/system/template";
+
+var EmberHandlebars = Ember.Handlebars = Ember.Handlebars || {};
+
+EmberHandlebars.precompile = precompile;
+EmberHandlebars.compile = compile;
+EmberHandlebars.template = template;
