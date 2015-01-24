e01cc46cafaacfbe0b6e21b6aa3ce15c4229bdcc
Tom Dale and Yehuda Katz <engineering+tomdale+wycats@tilde.io>
2015-01-23 17:41:55 -0800

    Legacy -> deprecated method naming
    Shout out to @rwjblue

--- packages/ember-testing/lib/helpers.js
+++ packages/ember-testing/lib/helpers.js
@@ -64,7 +64,7 @@ function visit(app, url) {
     run(app, 'advanceReadiness');
     delete router['initialURL'];
   } else {
-    run(app, app.legacyHandleURL, app.__legacyInstance__, url);
+    run(app.__deprecatedInstance__, 'handleURL', url);
   }
 
   return app.testHelpers.wait();
