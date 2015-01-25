d85e31d11ce94a5800afc5f52c3c1c04e8553dc0
Daniel <kuzirashi@interia.pl>
2015-01-25 21:50:06 +0100

    [DOCS]Mark some Em.CoreObject's members as static
    Seems like `eachComputedProperty` and `metaForProperty` are actually static members of Ember.CoreObject. I was confused when `console.log()` of `instanceOfEmObject.eachComputedProperty` returned `undefined`. However I could use it with `Foo.eachComputedProperty()` where `Foo = Em.Object.extend();`. [Demo here](http://emberjs.jsbin.com/powelibeve/4/edit). I hope it will save time and frustration of other developers.

--- packages/ember-runtime/lib/system/core_object.js
+++ packages/ember-runtime/lib/system/core_object.js
@@ -795,7 +795,8 @@ var ClassMixinProps = {
     ```
 
     This will return the original hash that was passed to `meta()`.
-
+    
+    @static
     @method metaForProperty
     @param key {String} property name
   */
@@ -830,7 +831,8 @@ var ClassMixinProps = {
   /**
     Iterate over each computed property for the class, passing its name
     and any associated metadata (see `metaForProperty`) to the callback.
-
+    
+    @static
     @method eachComputedProperty
     @param {Function} callback
     @param {Object} binding
