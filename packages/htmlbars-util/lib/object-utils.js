1b22e53087c970c4434e318cf6e66439fe977191
Yehuda Katz <wycats@gmail.com>
2015-02-12 15:17:27 -0800

    Try to make more of the hot path static

--- packages/htmlbars-util/lib/object-utils.js
+++ packages/htmlbars-util/lib/object-utils.js
@@ -5,3 +5,16 @@ export function merge(options, defaults) {
   }
   return options;
 }
+
+// IE8 does not have Object.create, so use a polyfill if needed.
+// Polyfill based on Mozilla's (MDN)
+export function createObject(obj) {
+  if (typeof Object.create === 'function') {
+    return Object.create(obj);
+  } else {
+    var Temp = function() {};
+    Temp.prototype = obj;
+    return new Temp();
+  }
+}
+
