ed5ccab089e2f0785d068cd8f743842b47dc1dab
Tom Dale and Yehuda Katz <engineering+tomdale+wycats@tilde.io>
2015-02-21 15:31:38 -0800

    Expose revalidation logic

--- packages/htmlbars-runtime/lib/main.js
+++ packages/htmlbars-runtime/lib/main.js
@@ -1,9 +1,11 @@
 import hooks from 'htmlbars-runtime/hooks';
 import render from 'htmlbars-runtime/render';
 import { visitChildren } from "../htmlbars-util/morph-utils";
+import { validateChildMorphs } from "htmlbars-runtime/expression-visitor";
 
 export {
   hooks,
   render,
-  visitChildren
+  visitChildren,
+  validateChildMorphs
 };
