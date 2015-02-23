03532141a9bc216ac4f1175b8fce8e36a5b6d076
mostafa <mostafa.ali@future-group.com>
2015-02-23 17:55:48 +0200

    fixed a check in render_buffer

--- packages/ember-views/lib/system/render_buffer.js
+++ packages/ember-views/lib/system/render_buffer.js
@@ -514,7 +514,7 @@ RenderBuffer.prototype = {
     var content = this.innerContent();
     // No content means a text node buffer, with the content
     // in _element. Ember._BoundView is an example.
-    if (content === null) {
+    if (content === null && typeof(content) === "undefined") {
       return this._element;
     }
 
