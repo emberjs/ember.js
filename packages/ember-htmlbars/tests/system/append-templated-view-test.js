d92c0b46f2f77cb9cb15cf8cd65f0f1869017454
Robert Jackson <robert.w.jackson@me.com>
2015-02-24 21:09:21 -0500

    [BUGFIX release] Ensure that Component context is not forced to parent context.
    (cherry picked from commit 83acee97fb18631f755c6d6760796c15e3deee7a)
    
    Conflicts:
    	packages/ember-htmlbars/lib/system/append-templated-view.js

---
+++ packages/ember-htmlbars/tests/system/append-templated-view-test.js
@@ -0,0 +1,106 @@
+import { runAppend, runDestroy } from "ember-runtime/tests/utils";
+import EmberView from 'ember-views/views/view';
+import EmberComponent from 'ember-views/views/component';
+import compile from "ember-template-compiler/system/compile";
+
+var view;
+QUnit.module('ember-htmlbars: appendTemplatedView', {
+  teardown: function() {
+    runDestroy(view);
+  }
+});
+
+QUnit.test('can accept a view instance', function() {
+  var controller = {
+    someProp: 'controller context',
+    someView: EmberView.create({
+      template: compile('{{someProp}}')
+    })
+  };
+
+  view = EmberView.create({
+    controller: controller,
+    template: compile('{{someProp}} - {{view someView}}')
+  });
+
+  runAppend(view);
+
+  equal(view.$().text(), 'controller context - controller context');
+});
+
+QUnit.test('can accept a view factory', function() {
+  var controller = {
+    someProp: 'controller context',
+    someView: EmberView.extend({
+      template: compile('{{someProp}}')
+    })
+  };
+
+  view = EmberView.create({
+    controller: controller,
+    template: compile('{{someProp}} - {{view someView}}')
+  });
+
+  runAppend(view);
+
+  equal(view.$().text(), 'controller context - controller context');
+});
+
+QUnit.test('does change the context if the view factory has a controller specified', function() {
+  var controller = {
+    someProp: 'controller context',
+    someView: EmberView.extend({
+      controller: {
+        someProp: 'view local controller context'
+      },
+      template: compile('{{someProp}}')
+    })
+  };
+
+  view = EmberView.create({
+    controller: controller,
+    template: compile('{{someProp}} - {{view someView}}')
+  });
+
+  runAppend(view);
+
+  equal(view.$().text(), 'controller context - view local controller context');
+});
+
+QUnit.test('does change the context if a component factory is used', function() {
+  var controller = {
+    someProp: 'controller context',
+    someView: EmberComponent.extend({
+      someProp: 'view local controller context',
+      layout: compile('{{someProp}}')
+    })
+  };
+
+  view = EmberView.create({
+    controller: controller,
+    template: compile('{{someProp}} - {{view someView}}')
+  });
+
+  runAppend(view);
+
+  equal(view.$().text(), 'controller context - view local controller context');
+});
+
+QUnit.test('does change the context if a component instanced is used', function() {
+  var controller = {
+    someProp: 'controller context',
+    someView: EmberComponent.create({
+      someProp: 'view local controller context',
+      layout: compile('{{someProp}}')
+    })
+  };
+
+  view = EmberView.create({
+    controller: controller,
+    template: compile('{{someProp}} - {{view someView}}')
+  });
+
+  runAppend(view);
+
+  equal(view.$().text(), 'controller context - view local controller context');
+});
