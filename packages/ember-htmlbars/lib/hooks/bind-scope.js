39a2a964cb6689511f83fb74e9a102727160e447
Tilde Engineering <engineering@tilde.io>
2015-03-01 13:33:49 -0800

    bindScope now takes an env

--- packages/ember-htmlbars/lib/hooks/bind-scope.js
+++ packages/ember-htmlbars/lib/hooks/bind-scope.js
@@ -1,6 +1,6 @@
 import updateScope from "ember-htmlbars/utils/update-scope";
 
-export default function bindScope(scope) {
+export default function bindScope(env, scope) {
   if (scope.attrs || scope.view) {
     updateScope(scope.locals, 'view', scope.view, null);
   }
