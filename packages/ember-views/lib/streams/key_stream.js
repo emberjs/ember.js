a8386c2eb2f902682880bb92a533ff010d27983e
raytiley <raytiley@gmail.com>
2015-03-04 19:42:00 -0800

    Do it the correct way
    Now a test fails. Is the test "correct" or am I missing something. /cc @mmum

--- packages/ember-views/lib/streams/key_stream.js
+++ packages/ember-views/lib/streams/key_stream.js
@@ -38,10 +38,10 @@ merge(KeyStream.prototype, {
     if (nextObj !== prevObj) {
       if (prevObj && typeof prevObj === 'object') {
         removeObserver(prevObj, this.key, this, this.notify);
+      }
 
-        if (nextObj && typeof nextObj === 'object') {
-          addObserver(nextObj, this.key, this, this.notify);
-        }
+      if (nextObj && typeof nextObj === 'object' && this.isActive) {
+        addObserver(nextObj, this.key, this, this.notify);
       }
 
       this.obj = nextObj;
