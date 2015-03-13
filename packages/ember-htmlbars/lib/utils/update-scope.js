7e49888306fcd708a2867ec31c4544585ff8d739
Tom Dale and Yehuda Katz <engineering+tomdale+wycats@tilde.io>
2015-03-12 18:24:13 -0700

    Compatibility with legacy Handlebars helpers
    This commit makes use of the new `invokeHelper` hook in Handlebars to
    invoke helpers created with `Handlebars.registerHelper` with the correct
    parameters that they were expecting.
    
    The TLDR is that legacy (original generation) Handlebars helpers expect
    to receive their parameters as strings + types, so we track the original
    paths through streams and pass those to the helper.

--- packages/ember-htmlbars/lib/utils/update-scope.js
+++ packages/ember-htmlbars/lib/utils/update-scope.js
@@ -1,13 +1,13 @@
 import SimpleStream from "ember-metal/streams/simple-stream";
 import subscribe from "ember-htmlbars/utils/subscribe";
 
-export default function updateScope(scope, key, newValue, renderNode) {
+export default function updateScope(scope, key, newValue, renderNode, isSelf) {
   var existing = scope[key];
 
   if (existing) {
     existing.setSource(newValue);
   } else {
-    var stream = new SimpleStream(newValue);
+    var stream = new SimpleStream(newValue, isSelf ? null : key);
     if (renderNode) { subscribe(renderNode, scope, stream); }
     scope[key] = stream;
   }
