907910fb29ea62b87adc2055e09df93691b853fd
Tom Dale and Yehuda Katz <engineering+tomdale+wycats@tilde.io>
2015-03-19 17:16:40 -0700

    Take machete to inBuffer state

--- packages/ember-views/lib/views/states.js
+++ packages/ember-views/lib/views/states.js
@@ -2,7 +2,6 @@ import create from 'ember-metal/platform/create';
 import merge from "ember-metal/merge";
 import _default from "ember-views/views/states/default";
 import preRender from "ember-views/views/states/pre_render";
-import inBuffer from "ember-views/views/states/in_buffer";
 import hasElement from "ember-views/views/states/has_element";
 import inDOM from "ember-views/views/states/in_dom";
 import destroying from "ember-views/views/states/destroying";
@@ -13,7 +12,6 @@ export function cloneStates(from) {
   into._default = {};
   into.preRender = create(into._default);
   into.destroying = create(into._default);
-  into.inBuffer = create(into._default);
   into.hasElement = create(into._default);
   into.inDOM = create(into.hasElement);
 
@@ -29,7 +27,6 @@ export var states = {
   _default: _default,
   preRender: preRender,
   inDOM: inDOM,
-  inBuffer: inBuffer,
   hasElement: hasElement,
   destroying: destroying
 };
