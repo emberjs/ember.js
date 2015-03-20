3ddcad2a72d796cb0e30bca15d176d95e087f417
Stefan Penner <stefan.penner@gmail.com>
2015-03-19 21:07:17 -0700

    make jshint happy

--- packages/ember-runtime/lib/main.js
+++ packages/ember-runtime/lib/main.js
@@ -21,10 +21,6 @@ import { Container, Registry } from "ember-runtime/system/container";
 import ArrayProxy from 'ember-runtime/system/array_proxy';
 import ObjectProxy from 'ember-runtime/system/object_proxy';
 import CoreObject from 'ember-runtime/system/core_object';
-import {
-  EachArray,
-  EachProxy
-} from 'ember-runtime/system/each_proxy';
 
 import NativeArray from 'ember-runtime/system/native_array';
 import Set from 'ember-runtime/system/set';
