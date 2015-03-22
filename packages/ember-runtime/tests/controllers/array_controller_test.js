3e394e9cf5f72c6531d0959cf1f0a1a3bb48b194
Robert Jackson <robert.w.jackson@me.com>
2015-03-22 14:35:58 -0400

    [BUGFIX beta] Assert if ArrayController's model is not an Ember.Array.
    (cherry picked from commit 9b52e8b97f184447166070235e9ead3a1c9e3a45)

--- packages/ember-runtime/tests/controllers/array_controller_test.js
+++ packages/ember-runtime/tests/controllers/array_controller_test.js
@@ -1,6 +1,8 @@
 import Ember from 'ember-metal/core';
 import MutableArrayTests from 'ember-runtime/tests/suites/mutable_array';
 import ArrayController from "ember-runtime/controllers/array_controller";
+import { set } from 'ember-metal/property_set';
+import { get } from 'ember-metal/property_get';
 
 QUnit.module("ember-runtime/controllers/array_controller_test");
 
@@ -23,6 +25,8 @@ MutableArrayTests.extend({
   }
 }).run();
 
+QUnit.module("ember-runtime: array_controller");
+
 QUnit.test("defaults its `model` to an empty array", function () {
   var Controller = ArrayController.extend();
   deepEqual(Controller.create().get("model"), [], "`ArrayController` defaults its model to an empty array");
@@ -30,9 +34,30 @@ QUnit.test("defaults its `model` to an empty array", function () {
   equal(Controller.create().get('lastObject'), undefined, 'can fetch lastObject');
 });
 
-
 QUnit.test("Ember.ArrayController length property works even if model was not set initially", function() {
   var controller = ArrayController.create();
   controller.pushObject('item');
   equal(controller.get('length'), 1);
+});
+
+QUnit.test('works properly when model is set to an Ember.A()', function() {
+  var controller = ArrayController.create();
+
+  set(controller, 'model', Ember.A(['red', 'green']));
+
+  deepEqual(get(controller, 'model'), ['red', 'green'], "can set model as an Ember.Array");
+});
+
+QUnit.test('works properly when model is set to a plain array', function() {
+  var controller = ArrayController.create();
+
+  if (Ember.EXTEND_PROTOTYPES) {
+    set(controller, 'model', ['red', 'green']);
+
+    deepEqual(get(controller, 'model'), ['red', 'green'], "can set model as a plain array");
+  } else {
+    expectAssertion(function() {
+      set(controller, 'model', ['red', 'green']);
+    }, /ArrayController expects `model` to implement the Ember.Array mixin. This can often be fixed by wrapping your model with `Ember\.A\(\)`./);
+  }
 });
