f2ad68c0c2ebd313ba2d520c2584a26961b85183
Craig Teegarden <craig.teegarden@gmail.com>
2015-03-26 19:00:38 -0400

    [BUGFIX beta] fix broken query params on ObjectController fixes #10733
    (cherry picked from commit 04980d7a1c388be952ed719faa5d93e59db27f18)

--- packages/ember-runtime/lib/controllers/object_controller.js
+++ packages/ember-runtime/lib/controllers/object_controller.js
@@ -26,6 +26,8 @@ export var objectControllerDeprecation = 'Ember.ObjectController is deprecated, 
 **/
 export default ObjectProxy.extend(ControllerMixin, {
   init: function() {
+    this._super();
+
     Ember.deprecate(objectControllerDeprecation, this.isGenerated, {
       url: 'http://emberjs.com/guides/deprecations/#toc_objectcontroller'
     });
