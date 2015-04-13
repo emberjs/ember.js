36225a2df71bdfab5de9580a332963793a294ee1
Robert Jackson <robert.w.jackson@me.com>
2015-04-13 18:17:18 -0400

    Add skipped test for compat mode attrs-proxy.

---
+++ packages/ember-views/tests/compat/attrs_proxy_test.js
@@ -0,0 +1,34 @@
+import View from "ember-views/views/view";
+import { runAppend, runDestroy } from "ember-runtime/tests/utils";
+import compile from "ember-template-compiler/system/compile";
+import Registry from "container/registry";
+
+var view, registry, container;
+
+QUnit.module("ember-views: attrs-proxy", {
+  setup() {
+    registry = new Registry();
+    container = registry.container();
+  },
+
+  teardown() {
+    runDestroy(view);
+  }
+});
+
+QUnit.skip('works with properties setup in root of view', function() {
+  registry.register('view:foo', View.extend({
+    bar: 'qux',
+
+    template: compile('{{bar}}')
+  }));
+
+  view = View.extend({
+    container: registry.container(),
+    template: compile('{{view "foo" bar="baz"}}')
+  }).create();
+
+  runAppend(view);
+
+  equal(view.$().text(), 'baz', 'value specified in the template is used');
+});
