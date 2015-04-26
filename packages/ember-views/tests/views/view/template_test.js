10d0861673f1ba5ef93672c0b5ae5f1a85ca8512
Matthew Beale <matt.beale@madhatted.com>
2015-04-26 17:20:30 -0400

    Update template_test to use compiled templates

--- packages/ember-views/tests/views/view/template_test.js
+++ packages/ember-views/tests/views/view/template_test.js
@@ -1,8 +1,8 @@
 import Registry from "container/registry";
 import { get } from "ember-metal/property_get";
 import run from "ember-metal/run_loop";
-import EmberObject from "ember-runtime/system/object";
 import EmberView from "ember-views/views/view";
+import { compile } from "ember-template-compiler";
 
 var registry, container, view;
 
@@ -32,25 +32,10 @@ QUnit.test("Template views return throw if their template cannot be found", func
   }, /cantBeFound/);
 });
 
-if (typeof Handlebars === "object") {
-  QUnit.skip("should allow standard Handlebars template usage", function() {
-    view = EmberView.create({
-      context: { name: "Erik" },
-      template: Handlebars.compile("Hello, {{name}}")
-    });
-
-    run(function() {
-      view.createElement();
-    });
-
-    equal(view.$().text(), "Hello, Erik");
-  });
-}
-
-QUnit.skip("should call the function of the associated template", function() {
-  registry.register('template:testTemplate', function() {
-    return "<h1 id='twas-called'>template was called</h1>";
-  });
+QUnit.test("should call the function of the associated template", function() {
+  registry.register('template:testTemplate', compile(
+    "<h1 id='twas-called'>template was called</h1>"
+  ));
 
   view = EmberView.create({
     container: container,
@@ -64,10 +49,10 @@ QUnit.skip("should call the function of the associated template", function() {
   ok(view.$('#twas-called').length, "the named template was called");
 });
 
-QUnit.skip("should call the function of the associated template with itself as the context", function() {
-  registry.register('template:testTemplate', function(dataSource) {
-    return "<h1 id='twas-called'>template was called for " + get(dataSource, 'personName') + "</h1>";
-  });
+QUnit.test("should call the function of the associated template with itself as the context", function() {
+  registry.register('template:testTemplate', compile(
+    "<h1 id='twas-called'>template was called for {{personName}}</h1>"
+  ));
 
   view = EmberView.create({
     container: container,
@@ -82,14 +67,17 @@ QUnit.skip("should call the function of the associated template with itself as t
     view.createElement();
   });
 
-  equal("template was called for Tom DAAAALE", view.$('#twas-called').text(), "the named template was called with the view as the data source");
+  equal("template was called for Tom DAAAALE", view.$('#twas-called').text(),
+        "the named template was called with the view as the data source");
 });
 
-QUnit.skip("should fall back to defaultTemplate if neither template nor templateName are provided", function() {
+QUnit.test("should fall back to defaultTemplate if neither template nor templateName are provided", function() {
   var View;
 
   View = EmberView.extend({
-    defaultTemplate(dataSource) { return "<h1 id='twas-called'>template was called for " + get(dataSource, 'personName') + "</h1>"; }
+    defaultTemplate: compile(
+      "<h1 id='twas-called'>template was called for {{personName}}</h1>"
+    )
   });
 
   view = View.create({
@@ -102,15 +90,16 @@ QUnit.skip("should fall back to defaultTemplate if neither template nor template
     view.createElement();
   });
 
-  equal("template was called for Tom DAAAALE", view.$('#twas-called').text(), "the named template was called with the view as the data source");
+  equal("template was called for Tom DAAAALE", view.$('#twas-called').text(),
+        "the named template was called with the view as the data source");
 });
 
-QUnit.skip("should not use defaultTemplate if template is provided", function() {
-  var View;
-
-  View = EmberView.extend({
-    template() { return "foo"; },
-    defaultTemplate(dataSource) { return "<h1 id='twas-called'>template was called for " + get(dataSource, 'personName') + "</h1>"; }
+QUnit.test("should not use defaultTemplate if template is provided", function() {
+  var View = EmberView.extend({
+    template: compile("foo"),
+    defaultTemplate: compile(
+      "<h1 id='twas-called'>template was called for {{personName}}</h1>"
+    )
   });
 
   view = View.create();
@@ -121,15 +110,15 @@ QUnit.skip("should not use defaultTemplate if template is provided", function() 
   equal("foo", view.$().text(), "default template was not printed");
 });
 
-QUnit.skip("should not use defaultTemplate if template is provided", function() {
-  var View;
+QUnit.test("should not use defaultTemplate if template is provided", function() {
+  registry.register('template:foobar', compile("foo"));
 
-  registry.register('template:foobar', function() { return 'foo'; });
-
-  View = EmberView.extend({
+  var View = EmberView.extend({
     container: container,
     templateName: 'foobar',
-    defaultTemplate(dataSource) { return "<h1 id='twas-called'>template was called for " + get(dataSource, 'personName') + "</h1>"; }
+    defaultTemplate: compile(
+      "<h1 id='twas-called'>template was called for {{personName}}</h1>"
+    )
   });
 
   view = View.create();
@@ -140,107 +129,16 @@ QUnit.skip("should not use defaultTemplate if template is provided", function() 
   equal("foo", view.$().text(), "default template was not printed");
 });
 
-QUnit.skip("should render an empty element if no template is specified", function() {
+QUnit.test("should render an empty element if no template is specified", function() {
   view = EmberView.create();
   run(function() {
     view.createElement();
   });
 
-  equal(view.$().html(), '', "view div should be empty");
+  equal(view.$().text(), '', "view div should be empty");
 });
 
-QUnit.skip("should provide a controller to the template if a controller is specified on the view", function() {
-  expect(7);
-
-  var Controller1 = EmberObject.extend({
-    toString() { return "Controller1"; }
-  });
-
-  var Controller2 = EmberObject.extend({
-    toString() { return "Controller2"; }
-  });
-
-  var controller1 = Controller1.create();
-  var controller2 = Controller2.create();
-  var optionsDataKeywordsControllerForView;
-  var optionsDataKeywordsControllerForChildView;
-  var contextForView;
-  var contextForControllerlessView;
-
-  view = EmberView.create({
-    controller: controller1,
-
-    template(buffer, options) {
-      optionsDataKeywordsControllerForView = options.data.view._keywords.controller.value();
-    }
-  });
-
-  run(function() {
-    view.appendTo('#qunit-fixture');
-  });
-
-  strictEqual(optionsDataKeywordsControllerForView, controller1, "passes the controller in the data");
-
-  run(function() {
-    view.destroy();
-  });
-
-  var parentView = EmberView.create({
-    controller: controller1,
-
-    template(buffer, options) {
-      options.data.view.appendChild(EmberView.create({
-        controller: controller2,
-        template(context, options) {
-          contextForView = context;
-          optionsDataKeywordsControllerForChildView = options.data.view._keywords.controller.value();
-        }
-      }));
-      optionsDataKeywordsControllerForView = options.data.view._keywords.controller.value();
-    }
-  });
-
-  run(function() {
-    parentView.appendTo('#qunit-fixture');
-  });
-
-  strictEqual(optionsDataKeywordsControllerForView, controller1, "passes the controller in the data");
-  strictEqual(optionsDataKeywordsControllerForChildView, controller2, "passes the child view's controller in the data");
-
-  run(function() {
-    parentView.destroy();
-  });
-
-  var parentViewWithControllerlessChild = EmberView.create({
-    controller: controller1,
-
-    template(buffer, options) {
-      options.data.view.appendChild(EmberView.create({
-        template(context, options) {
-          contextForControllerlessView = context;
-          optionsDataKeywordsControllerForChildView = options.data.view._keywords.controller.value();
-        }
-      }));
-      optionsDataKeywordsControllerForView = options.data.view._keywords.controller.value();
-    }
-  });
-
-  run(function() {
-    parentViewWithControllerlessChild.appendTo('#qunit-fixture');
-  });
-
-  strictEqual(optionsDataKeywordsControllerForView, controller1, "passes the original controller in the data");
-  strictEqual(optionsDataKeywordsControllerForChildView, controller1, "passes the controller in the data to child views");
-  strictEqual(contextForView, controller2, "passes the controller in as the main context of the parent view");
-  strictEqual(contextForControllerlessView, controller1, "passes the controller in as the main context of the child view");
-
-  run(function() {
-    parentView.destroy();
-    parentViewWithControllerlessChild.destroy();
-  });
-});
-
-QUnit.skip("should throw an assertion if no container has been set", function() {
+QUnit.test("should throw an assertion if no container has been set", function() {
   expect(1);
   var View;
 
@@ -254,4 +152,6 @@ QUnit.skip("should throw an assertion if no container has been set", function() 
       view.createElement();
     });
   }, /Container was not found when looking up a views template./);
+
+  view.renderNode = null;
 });
