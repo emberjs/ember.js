3e662449ff4a3bfb935eb0d4c34908120364df12
Tom Dale <engineering@tilde.io>
2015-05-02 09:45:06 -0700

    Add failing controller scope/action test

---
+++ packages/ember/tests/template_scope_test.js
@@ -0,0 +1,65 @@
+import "ember";
+import EmberHandlebars from "ember-htmlbars/compat";
+
+var compile = EmberHandlebars.compile;
+var App, $fixture, templates;
+
+QUnit.module("Template scoping examples", {
+  setup() {
+    Ember.run(function() {
+      templates = Ember.TEMPLATES;
+      App = Ember.Application.create({
+        name: "App",
+        rootElement: '#qunit-fixture'
+      });
+      App.deferReadiness();
+
+      App.Router.reopen({
+        location: 'none'
+      });
+
+      App.LoadingRoute = Ember.Route.extend();
+    });
+
+    $fixture = Ember.$('#qunit-fixture');
+  },
+
+  teardown() {
+    Ember.run(function() {
+      App.destroy();
+    });
+
+    App = null;
+
+    Ember.TEMPLATES = {};
+  }
+});
+
+QUnit.test("Actions inside an outlet go to the associated controller", function() {
+  expect(1);
+
+  templates.index = compile("{{component-with-action action='componentAction'}}");
+
+  App.IndexController = Ember.Controller.extend({
+    actions: {
+      componentAction() {
+        ok(true, "received the click");
+      }
+    }
+  });
+
+  App.ComponentWithActionComponent = Ember.Component.extend({
+    classNames: ['component-with-action'],
+    click() {
+      this.sendAction();
+    }
+  });
+
+  bootApp();
+
+  $fixture.find('.component-with-action').click();
+});
+
+function bootApp() {
+  Ember.run(App, 'advanceReadiness');
+}
