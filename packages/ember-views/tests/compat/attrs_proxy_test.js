ef926752426f99285e643d3286c034a2ccddcdb3
Robert Jackson <robert.w.jackson@me.com>
2015-05-05 11:38:02 -0400

    Prevent error when attributes are undefined/null.

--- packages/ember-views/tests/compat/attrs_proxy_test.js
+++ packages/ember-views/tests/compat/attrs_proxy_test.js
@@ -2,6 +2,9 @@ import View from "ember-views/views/view";
 import { runAppend, runDestroy } from "ember-runtime/tests/utils";
 import compile from "ember-template-compiler/system/compile";
 import Registry from "container/registry";
+import run from "ember-metal/run_loop";
+import { set } from "ember-metal/property_set";
+import { get } from "ember-metal/property_get";
 
 var view, registry, container;
 
@@ -31,4 +34,35 @@ QUnit.test('works with properties setup in root of view', function() {
   runAppend(view);
 
   equal(view.$().text(), 'baz', 'value specified in the template is used');
+});
+
+QUnit.test('works with undefined attributes', function() {
+  expectDeprecation();
+
+  var childView;
+  registry.register('view:foo', View.extend({
+    init: function() {
+      this._super(...arguments);
+
+      childView = this;
+    },
+
+    template: compile('{{bar}}')
+  }));
+
+  view = View.extend({
+    container: registry.container(),
+
+    template: compile('{{view "foo" bar=undefined}}')
+  }).create();
+
+  runAppend(view);
+
+  equal(view.$().text(), '', 'precond - value is used');
+
+  run(function() {
+    set(childView, 'bar', 'stuff');
+  });
+
+  equal(get(view, 'bar'), undefined, 'value is updated upstream');
 });
