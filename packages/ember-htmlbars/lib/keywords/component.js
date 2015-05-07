9885e9ac48cc2c8b22f63d8e439add860cfe0e80
Yehuda Katz <wycats@gmail.com>
2015-05-08 00:34:46 +0300

    Dynamic {{component}} re-renders when attrs change
    For some reason, the `{{component}}` keyword did not implement any
    re-rendering behavior, which means that it... didn't rerender. Go
    figure.
    
    Fixes #11044.

--- packages/ember-htmlbars/lib/keywords/component.js
+++ packages/ember-htmlbars/lib/keywords/component.js
@@ -1,28 +1,31 @@
+import { assign } from "ember-metal/merge";
+
 export default {
   setupState(lastState, env, scope, params, hash) {
-    let state = {
-      componentPath: env.hooks.getValue(params[0]),
-      manager: lastState && lastState.manager,
-      isComponentHelper: true
-    };
-
-    return state;
+    let componentPath = env.hooks.getValue(params[0]);
+    return assign({}, lastState, { componentPath, isComponentHelper: true });
   },
 
-  render(morph, env, scope, params, hash, template, inverse, visitor) {
+  render(morph, ...rest) {
     // Force the component hook to treat this as a first-time render,
     // because normal components (`<foo-bar>`) cannot change at runtime,
     // but the `{{component}}` helper can.
     morph.state.manager = null;
 
-    let componentPath = morph.state.componentPath;
+    render(morph, ...rest);
+  },
 
-    // If the value passed to the {{component}} helper is undefined or null,
-    // don't create a new ComponentNode.
-    if (componentPath === undefined || componentPath === null) {
-      return;
-    }
-
-    env.hooks.component(morph, env, scope, componentPath, params, hash, template, visitor);
-  }
+  rerender: render
 };
+
+function render(morph, env, scope, params, hash, template, inverse, visitor) {
+  let componentPath = morph.state.componentPath;
+
+  // If the value passed to the {{component}} helper is undefined or null,
+  // don't create a new ComponentNode.
+  if (componentPath === undefined || componentPath === null) {
+    return;
+  }
+
+  env.hooks.component(morph, env, scope, componentPath, params, hash, template, visitor);
+}
