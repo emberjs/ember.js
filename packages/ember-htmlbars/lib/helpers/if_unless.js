2e49773daf4762462ece350bd0534803b4adf4de
joe <joe@responsis.com.au>
2015-05-11 10:12:56 +1000

    [DOC canary] Document {{#if}} and {{#unless}} helpers
    (cherry picked from commit e75b209c69acf888ac1368390da3a259b950e940)

--- packages/ember-htmlbars/lib/helpers/if_unless.js
+++ packages/ember-htmlbars/lib/helpers/if_unless.js
@@ -7,6 +7,57 @@ import Ember from "ember-metal/core"; // Ember.assert
 import shouldDisplay from "ember-views/streams/should_display";
 
 /**
+  Use the `if` block helper to conditionally render a block depending on a
+  property. If the property is "falsey", for example: `false`, `undefined
+ `null`, `""`, `0` or an empty array, the block will not be rendered.
+
+ ```handlebars
+  {{! will not render if foo is falsey}}
+  {{#if foo}}
+    Welcome to the {{foo.bar}}
+  {{/if}}
+  ```
+
+  You can also specify a template to show if the property is falsey by using
+  the `else` helper.
+
+  ```handlebars
+  {{!Is it raining outside?}}
+  {{#if isRaining}}
+    Yes, grab an umbrella!
+  {{else}}
+    No, it's lovely outside!
+  {{/if}}
+  ```
+
+  You are also able to combine `else` and `if` helpers to create more complex
+  conditional logic.
+
+  ```handlebars
+  {{#if isMorning}}
+    Good morning
+  {{else if isAfternoon}}
+    Good afternoon
+  {{else}}
+    Good night
+  {{/if}}
+  ```
+
+  You can use `if` inline to conditionally render a single property or string.
+  This helper acts like a ternary operator. If the first property is truthy,
+  the second argument will be displayed, if not, the third argument will be
+  displayed
+
+  ```handlebars
+  {{if useLongGreeting "Hello" "Hi"}} Dave
+  ```
+
+  Finally, you can use the `if` helper inside another helper as a subexpression.
+
+  ```handlebars
+  {{some-component height=(if isBig "100" "10")}}
+  ```
+
   @method if
   @for Ember.Handlebars.helpers
 */
@@ -15,6 +66,10 @@ function ifHelper(params, hash, options) {
 }
 
 /**
+  The `unless` helper is the inverse of the `if` helper. Its block will be
+  rendered if the expression contains a falsey value.  All forms of the `if`
+  helper can also be used with `unless`.
+
   @method unless
   @for Ember.Handlebars.helpers
 */
