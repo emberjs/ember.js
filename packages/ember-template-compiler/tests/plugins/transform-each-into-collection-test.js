501373e7d9952ea95c1e91102272d212523af8ca
Robert Jackson <robert.w.jackson@me.com>
2015-05-11 21:26:23 -0400

    Deprecate {{each}} hash arguments that trigger usage of LegacyEachView.
    This includes:
    
    * `itemController`
    * `tagName`
    * `itemView`
    * `itemViewClass`

---
+++ packages/ember-template-compiler/tests/plugins/transform-each-into-collection-test.js
@@ -0,0 +1,36 @@
+import { compile } from "ember-template-compiler";
+
+QUnit.module('ember-template-compiler: transform-each-into-collection');
+
+let deprecatedAttrs = ['itemController', 'itemView', 'itemViewClass', 'tagName', 'emptyView', 'emptyViewClass'];
+
+function testBlockForm(attr) {
+  QUnit.test(`Using the '${attr}' hash argument with a block results in a deprecation`, function() {
+    expect(1);
+
+    expectDeprecation(function() {
+      compile(`\n\n    {{#each model ${attr}="foo" as |item|}}{{item}}{{/each}}`, {
+        moduleName: 'lol-wat-app/index/template'
+      });
+    }, `Using '${attr}' with '{{each}}' 'lol-wat-app/index/template' @L3:C18 is deprecated.  Please refactor to a component.`);
+  });
+}
+
+function testNonBlockForm(attr) {
+  QUnit.test(`Using the '${attr}' hash argument in non-block form results in a deprecation`, function() {
+    expect(1);
+
+    expectDeprecation(function() {
+      compile(`\n\n    {{each model ${attr}="foo"}}`, {
+        moduleName: 'lol-wat-app/index/template'
+      });
+    }, `Using '${attr}' with '{{each}}' 'lol-wat-app/index/template' @L3:C17 is deprecated.  Please refactor to a component.`);
+  });
+}
+
+for (let i = 0, l = deprecatedAttrs.length; i < l; i++) {
+  let attr = deprecatedAttrs[i];
+
+  testBlockForm(attr);
+  testNonBlockForm(attr);
+}
