826d33d2b6eaf178ac65a22d455b41e4a4c5b205
Yoran Brondsema <yoran.brondsema@gmail.com>
2015-05-13 13:14:23 +0200

    Fix documentation for {{with}} helper.

--- packages/ember-htmlbars/lib/helpers/with.js
+++ packages/ember-htmlbars/lib/helpers/with.js
@@ -10,35 +10,56 @@ import shouldDisplay from "ember-views/streams/should_display";
   Use the `{{with}}` helper when you want to alias a property to a new name. This is helpful
   for semantic clarity as it allows you to retain default scope or to reference a property from another
   `{{with}}` block.
+
   If the aliased property is "falsey", for example: `false`, `undefined` `null`, `""`, `0` or
   an empty array, the block will not be rendered.
+
   ```handlebars
-  // will only render if user.posts contains items
+  {{! Will only render if user.posts contains items}}
   {{#with user.posts as |blogPosts|}}
     <div class="notice">
       There are {{blogPosts.length}} blog posts written by {{user.name}}.
     </div>
-    {{#each post in blogPosts}}
+    {{#each blogPosts as |post|}}
       <li>{{post.title}}</li>
     {{/each}}
   {{/with}}
   ```
+
   Without the `as` operator, it would be impossible to reference `user.name` in the example above.
+
   NOTE: The alias should not reuse a name from the bound property path.
-  For example: `{{#with foo.bar as foo}}` is not supported because it attempts to alias using
-  the first part of the property path, `foo`. Instead, use `{{#with foo.bar as baz}}`.
+  For example: `{{#with foo.bar as |foo|}}` is not supported because it attempts to alias using
+  the first part of the property path, `foo`. Instead, use `{{#with foo.bar as |baz|}}`.
+
   ### `controller` option
-  Adding `controller='something'` instructs the `{{with}}` helper to create and use an instance of
-  the specified controller wrapping the aliased keyword.
-  This is very similar to using an `itemController` option with the `{{each}}` helper.
+
+  Adding `controller='someController'` instructs the `{{with}}` helper to create and use an instance of
+  the specified controller wrapping the aliased keyword.  This is very similar to using an
+  `itemController` option with the [{{each}}](/api/classes/Ember.Handlebars.helpers.html#method_each) helper.
+
+  ```javascript
+  App.UserBlogPostsController = Ember.Controller.extend({
+    isProlificBlogger: function() {
+      return this.get('model.length') > 5;
+    }.property('model.length')
+  })
+  ```
+
   ```handlebars
   {{#with users.posts controller='userBlogPosts' as |posts|}}
-    {{!- `posts` is wrapped in our controller instance }}
+    {{! `posts` is wrapped in our controller instance }}
+    {{#if isProlificBlogger}}
+      {{user.name}} has written more than {{posts.model.length}} blog posts!
+    {{else}}
+      {{user.name}} has only written {{posts.model.length}} blog posts. 
+    {{/if}}
   {{/with}}
   ```
-  In the above example, the `posts` keyword is now wrapped in the `userBlogPost` controller,
-  which provides an elegant way to decorate the context with custom
-  functions/properties.
+
+  In the above example, the `posts` keyword is now wrapped in the `userBlogPosts` controller,
+  which provides an elegant way to decorate the context with custom functions/properties.
+
   @method with
   @for Ember.Handlebars.helpers
   @param {Function} context
