9cf9cb9293240b39495d68aeb72dbaacb7c802a8
Samy Alzhrani <samy@sadeem.net>
2015-05-13 12:11:09 +0300

    manualElement handles SVG properly

--- packages/htmlbars-runtime/tests/main-test.js
+++ packages/htmlbars-runtime/tests/main-test.js
@@ -1,4 +1,9 @@
 import { hooks } from "../htmlbars-runtime";
+import { manualElement } from "../htmlbars-runtime/render";
+import { compile } from "../htmlbars-compiler/compiler";
+import { hostBlock } from "../htmlbars-runtime/hooks";
+import { equalTokens } from "../htmlbars-test-helpers";
+import DOMHelper from "../dom-helper";
 
 QUnit.module("htmlbars-runtime");
 
@@ -58,4 +63,37 @@ test("hooks are present", function () {
   }
 
   equal(keys(hooks).length, hookNames.length, "Hooks length match");
+});
+
+test("manualElement function honors namespaces", function() {
+  var env = {
+    dom: new DOMHelper(),
+    hooks: hooks,
+    helpers: {},
+    partials: {},
+    useFragmentCache: true
+  };
+  hooks.keywords['manual-element'] = {
+      render: function(morph, env, scope, params, hash, template, inverse, visitor) {
+        var attributes = {
+          version: '1.1'
+        };
+
+        var layout = manualElement('svg', attributes);
+
+        hostBlock(morph, env, scope, template, inverse, null, visitor, function(options) {
+          options.templates.template.yieldIn({ raw: layout }, hash);
+        });
+
+        manualElement(env, scope, 'span', attributes, morph);
+      },
+
+      isStable: function() { return true; }
+    };
+
+    var template = compile('{{#manual-element}}<linearGradient><stop offset="{{startOffset}}"></stop><stop offset="{{stopOffset}}"></stop></linearGradient>{{/manual-element}}');
+    var result = template.render({startOffset:0.1, stopOffset:0.6}, env);
+    ok(result.fragment.childNodes[1] instanceof SVGElement);
+    ok(result.fragment.childNodes[1].childNodes[0] instanceof SVGLinearGradientElement);
+    equalTokens(result.fragment, '<svg version="1.1"><linearGradient><stop offset="0.1"></stop><stop offset="0.6"></stop></linearGradient></svg>');
 });
