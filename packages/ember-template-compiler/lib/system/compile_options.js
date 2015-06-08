43c6a35f29fba4ad2f7d519f5109c2269042624e
Robert Jackson <robert.w.jackson@me.com>
2015-06-08 10:48:38 -0700

    [BUGFIX beta] Prevent AST plugins from being shared across compiles.
    (cherry picked from commit 6e4cbeb35cf86e4f5329353fabbfb2aff07da58c)

--- packages/ember-template-compiler/lib/system/compile_options.js
+++ packages/ember-template-compiler/lib/system/compile_options.js
@@ -4,7 +4,8 @@
 */
 
 import Ember from "ember-metal/core";
-import plugins from "ember-template-compiler/plugins";
+import { assign } from "ember-metal/merge";
+import defaultPlugins from "ember-template-compiler/plugins";
 
 /**
   @private
@@ -16,15 +17,25 @@ export default function(_options) {
     disableComponentGeneration = false;
   }
 
-  var options = _options || {};
+  let options;
   // When calling `Ember.Handlebars.compile()` a second argument of `true`
   // had a special meaning (long since lost), this just gaurds against
   // `options` being true, and causing an error during compilation.
-  if (options === true) {
+  if (_options === true) {
     options = {};
+  } else {
+    options = assign({}, _options);
   }
 
   options.disableComponentGeneration = disableComponentGeneration;
+
+  let plugins = {
+    ast: defaultPlugins.ast.slice()
+  };
+
+  if (options.plugins && options.plugins.ast) {
+    plugins.ast = plugins.ast.concat(options.plugins.ast);
+  }
   options.plugins = plugins;
 
   options.buildMeta = function buildMeta(program) {
