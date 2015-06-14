2fba9a7937f9052cd5b78c9f1f6b1cd352487617
Miguel Camba <miguel.camba@gmail.com>
2015-06-14 15:34:05 +0100

    [CLEANUP beta] Remove deprecated LinkView
    (cherry picked from commit 6c41761c1e031a617ddab510769206f39855bfbd)

--- packages/ember-routing-views/tests/main_test.js
+++ packages/ember-routing-views/tests/main_test.js
@@ -6,13 +6,3 @@ QUnit.test("exports correctly", function() {
   ok(Ember.LinkComponent, "LinkComponent is exported correctly");
   ok(Ember.OutletView, "OutletView is exported correctly");
 });
-
-QUnit.test("Ember.LinkView throws a deprecation warning when instantiated", function() {
-  expectDeprecation(/Ember.LinkView is deprecated. Please use Ember.LinkComponent/);
-  Ember.LinkView.create();
-});
-
-QUnit.test("Ember.LinkView throws a deprecation warning when reopened", function() {
-  expectDeprecation(/Ember.LinkView is deprecated. Please use Ember.LinkComponent/);
-  Ember.LinkView.reopen({});
-});
