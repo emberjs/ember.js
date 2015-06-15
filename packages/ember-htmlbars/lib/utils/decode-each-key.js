5b8b314620edb5a925bad9ce95c0aba7c52f9298
Robert Jackson <robert.w.jackson@me.com>
2015-06-15 01:03:08 -0400

    [BUGFIX release] Default {{each}} key to @guid or @item based on type.
    (cherry picked from commit 01a710336085930a030cbddccf73a3c0918b2cb0)

--- packages/ember-htmlbars/lib/utils/decode-each-key.js
+++ packages/ember-htmlbars/lib/utils/decode-each-key.js
@@ -1,32 +1,50 @@
-import Ember from "ember-metal/core";
+import Ember from 'ember-metal/core';
 import { get } from "ember-metal/property_get";
 import { guidFor } from "ember-metal/utils";
 
+function identity(item) {
+  let key;
+  let type = typeof item;
+
+  if (type === 'string' || type === 'number') {
+    key = item;
+  } else {
+    key = guidFor(item);
+  }
+
+  return key;
+}
 export default function decodeEachKey(item, keyPath, index) {
-  var key;
+  var key, deprecatedSpecialKey;
 
   switch (keyPath) {
   case '@index':
     key = index;
     break;
   case '@guid':
+    deprecatedSpecialKey = '@guid';
     key = guidFor(item);
     break;
   case '@item':
+    deprecatedSpecialKey = '@item';
     key = item;
+    break;
+  case '@identity':
+    key = identity(item);
     break;
   default:
     if (keyPath) {
       key = get(item, keyPath);
     } else {
-      Ember.warn('Using `{{each}}` without specifying a key can lead to unusual behavior.  Please specify a `key` that identifies a unique value on each item being iterated. E.g. `{{each model key="@guid" as |item|}}`.');
-      key = index;
+      key = identity(item);
     }
   }
 
   if (typeof key === 'number') {
     key = String(key);
   }
+
+  Ember.deprecate(`Using '${deprecatedSpecialKey}' with the {{each}} helper, is deprecated. Switch to '@identity' or remove 'key=' from your template.`, !deprecatedSpecialKey);
 
   return key;
 }
