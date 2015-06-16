cb34e615cf7f45a7d907b7e6ceef1c310f24e51d
Martin Muñoz <im.mmun@gmail.com>
2015-06-16 01:13:20 -0400

    Switch parsing strategy to simple-html-tokenizer's EventedTokenizer

--- packages/htmlbars-syntax/lib/parser.js
+++ packages/htmlbars-syntax/lib/parser.js
@@ -1,28 +1,14 @@
 import { parse } from "./handlebars/compiler/base";
-import { Tokenizer } from "./tokenizer";
+import * as syntax from "../htmlbars-syntax";
+import EventedTokenizer from "../simple-html-tokenizer/evented-tokenizer";
 import EntityParser from "../simple-html-tokenizer/entity-parser";
 import fullCharRefs from "../simple-html-tokenizer/char-refs/full";
-import nodeHandlers from "./node-handlers";
-import tokenHandlers from "./token-handlers";
-import * as syntax from "../htmlbars-syntax";
-
-var splitLines;
-// IE8 throws away blank pieces when splitting strings with a regex
-// So we split using a string instead as appropriate
-if ("foo\n\nbar".split(/\n/).length === 2) {
-  splitLines = function(str) {
-     var clean = str.replace(/\r\n?/g, '\n');
-     return clean.split('\n');
-  };
-} else {
-  splitLines = function(str) {
-    return str.split(/(?:\r\n?|\n)/g);
-  };
-}
+import handlebarsNodeVisitors from "./parser/handlebars-node-visitors";
+import tokenizerEventHandlers from "./parser/tokenizer-event-handlers";
 
 export function preprocess(html, options) {
   var ast = (typeof html === 'object') ? html : parse(html);
-  var combined = new HTMLProcessor(html, options).acceptNode(ast);
+  var combined = new Parser(html, options).acceptNode(ast);
 
   if (options && options.plugins && options.plugins.ast) {
     for (var i = 0, l = options.plugins.ast.length; i < l; i++) {
@@ -39,33 +25,52 @@ export function preprocess(html, options) {
 
 export default preprocess;
 
-function HTMLProcessor(source, options) {
+var splitLines;
+// IE8 throws away blank pieces when splitting strings with a regex
+// So we split using a string instead as appropriate
+if ("foo\n\nbar".split(/\n/).length === 2) {
+  splitLines = function(str) {
+     var clean = str.replace(/\r\n?/g, '\n');
+     return clean.split('\n');
+  };
+} else {
+  splitLines = function(str) {
+    return str.split(/(?:\r\n?|\n)/g);
+  };
+}
+
+const entityParser = new EntityParser(fullCharRefs);
+
+export function Parser(source, options) {
   this.options = options || {};
   this.elementStack = [];
-  this.tokenizer = new Tokenizer(new EntityParser(fullCharRefs));
-  this.nodeHandlers = nodeHandlers;
-  this.tokenHandlers = tokenHandlers;
+  this.tokenizer = new EventedTokenizer(this, entityParser);
+
+  this.currentNode = null;
+  this.currentAttribute = null;
 
   if (typeof source === 'string') {
     this.source = splitLines(source);
   }
 }
 
-HTMLProcessor.prototype.acceptNode = function(node) {
-  return this.nodeHandlers[node.type].call(this, node);
+for (let key in handlebarsNodeVisitors) {
+  Parser.prototype[key] = handlebarsNodeVisitors[key];
+}
+
+for (let key in tokenizerEventHandlers) {
+  Parser.prototype[key] = tokenizerEventHandlers[key];
+}
+
+Parser.prototype.acceptNode = function(node) {
+  return this[node.type](node);
 };
 
-HTMLProcessor.prototype.acceptToken = function(token) {
-  if (token) {
-    return this.tokenHandlers[token.type].call(this, token);
-  }
-};
-
-HTMLProcessor.prototype.currentElement = function() {
+Parser.prototype.currentElement = function() {
   return this.elementStack[this.elementStack.length - 1];
 };
 
-HTMLProcessor.prototype.sourceForMustache = function(mustache) {
+Parser.prototype.sourceForMustache = function(mustache) {
   var firstLine = mustache.loc.start.line - 1;
   var lastLine = mustache.loc.end.line - 1;
   var currentLine = firstLine - 1;
