f03fce59954adf2b7033e93410b66dfd0fc4fd74
joe <joe@responsis.com.au>
2015-06-29 11:10:56 +1000

    [DOC Release] Mark Route.disconnectOutlet() as public

--- packages/ember-routing/lib/system/route.js
+++ packages/ember-routing/lib/system/route.js
@@ -2007,7 +2007,7 @@ var Route = EmberObject.extend(ActionHandler, Evented, {
 
     @method disconnectOutlet
     @param {Object|String} options the options hash or outlet name
-    @private
+    @public
   */
   disconnectOutlet(options) {
     var outletName;
