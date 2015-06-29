43bceb11852075fb421373888177d3433ff8869f
joe <joe@responsis.com.au>
2015-06-29 11:04:16 +1000

    [DOC Release] Mark Route.replaceWith() as public

--- packages/ember-routing/lib/system/route.js
+++ packages/ember-routing/lib/system/route.js
@@ -1096,7 +1096,7 @@ var Route = EmberObject.extend(ActionHandler, Evented, {
       transitioning to the route.
     @return {Transition} the transition object associated with this
       attempted transition
-    @private
+    @public
   */
   replaceWith() {
     var router = this.router;
