f3c9b6187104155d218da1a4177495d15fb21d9e
Robert Jackson <robert.w.jackson@me.com>
2015-07-05 18:50:37 -0400

    Fix missing import of Object.create.

--- packages/ember-metal/tests/chains_test.js
+++ packages/ember-metal/tests/chains_test.js
@@ -13,7 +13,7 @@ QUnit.test("finishChains should properly copy chains from prototypes to instance
   var obj = {};
   addObserver(obj, 'foo.bar', null, didChange);
 
-  var childObj = create(obj);
+  var childObj = Object.create(obj);
   finishChains(childObj);
 
   ok(obj['__ember_meta__'].chains !== childObj['__ember_meta__'].chains, "The chains object is copied");
