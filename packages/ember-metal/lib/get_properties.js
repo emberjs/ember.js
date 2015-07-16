f1f176e14028a5d21f4625eca7eb92af3edddcbc
Doug Yun <yun.douglas@gmail.com>
2015-07-16 16:15:36 -0400

    [DOC release] Mark `Ember.getProperties` as `public`
    [ci skip]
    
    (cherry picked from commit 5e7fd8a8484d078552c3795a9445e7c844caead1)

--- packages/ember-metal/lib/get_properties.js
+++ packages/ember-metal/lib/get_properties.js
@@ -22,7 +22,7 @@ import { isArray } from "ember-metal/utils";
   @param {Object} obj
   @param {String...|Array} list of keys to get
   @return {Object}
-  @private
+  @public
 */
 export default function getProperties(obj) {
   var ret = {};
