e31205a687c1f4fc5d6c289138b1c73556f233bf
Robert Jackson <robert.w.jackson@me.com>
2015-07-21 16:34:37 -0400

    [CLEANUP beta] Remove `{{template}}` support.
    (cherry picked from commit e35eb2ccbfaf5df7f5d5fc5cb6682683cfd46a01)

--- packages/ember-htmlbars/lib/keywords/template.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember-metal/core';
-
-export let deprecation = 'The `template` helper has been deprecated in favor of the `partial` helper.';
-
-export default function templateKeyword(morph, env, scope, params, hash, template, inverse, visitor) {
-  Ember.deprecate(deprecation, false, { id: 'ember-htmlbars.template-keyword', until: '3.0.0' });
-  env.hooks.keyword('partial', morph, env, scope, params, hash, template, inverse, visitor);
-  return true;
-}
