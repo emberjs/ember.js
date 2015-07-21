ab51f45b7de34b8ffca84173fcd45598c7ddd2f3
Max Schoening <desk@max.wtf>
2015-07-20 22:16:12 -0700

    Refs #11835
    Include required options for occurences of `Ember.warn` in
    `packages/ember-testing`
    
    (cherry picked from commit ef5c762009cf995295b5f00ca12a5d492d4d691d)

--- packages/ember-testing/lib/support.js
+++ packages/ember-testing/lib/support.js
@@ -53,7 +53,11 @@ if (environment.hasDOM) {
 
     // Try again to verify that the patch took effect or blow up.
     testCheckboxClick(function() {
-      Ember.warn('clicked checkboxes should be checked! the jQuery patch didn\'t work', this.checked);
+      Ember.warn(
+        'clicked checkboxes should be checked! the jQuery patch didn\'t work',
+        this.checked,
+        { id: 'ember-testing.test-checkbox-click' }
+      );
     });
   });
 }
