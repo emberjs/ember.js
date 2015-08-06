c0ea6530774d5d1e6fcdd09424ad98d5f3714539
Robert Jackson <robert.w.jackson@me.com>
2015-08-06 14:10:06 -0400

    [CLEANUP beta] Remove {{with}} keyword's controller option.
    (cherry picked from commit 95823bc9a348522731e4fe46f177b9b947fef733)

--- packages/ember-htmlbars/lib/keywords/with.js
+++ packages/ember-htmlbars/lib/keywords/with.js
@@ -1,40 +1,7 @@
 import Ember from 'ember-metal/core';
-import { get } from 'ember-metal/property_get';
 import { internal } from 'htmlbars-runtime';
-import { read } from 'ember-metal/streams/utils';
 
 export default {
-  setupState(state, env, scope, params, hash) {
-    var controller = hash.controller;
-
-    if (controller) {
-      if (!state.controller) {
-        var context = params[0];
-        var controllerFactory = env.container.lookupFactory('controller:' + controller);
-        var parentController = null;
-
-        if (scope.locals.controller) {
-          parentController = read(scope.locals.controller);
-        } else if (scope.locals.view) {
-          parentController = get(read(scope.locals.view), 'context');
-        }
-
-        var controllerInstance = controllerFactory.create({
-          model: env.hooks.getValue(context),
-          parentController: parentController,
-          target: parentController
-        });
-
-        params[0] = controllerInstance;
-        return { controller: controllerInstance };
-      }
-
-      return state;
-    }
-
-    return { controller: null };
-  },
-
   isStable() {
     return true;
   },
@@ -44,11 +11,6 @@ export default {
   },
 
   render(morph, env, scope, params, hash, template, inverse, visitor) {
-    if (morph.state.controller) {
-      morph.addDestruction(morph.state.controller);
-      hash.controller = morph.state.controller;
-    }
-
     Ember.assert(
       '{{#with foo}} must be called with a single argument or the use the ' +
       '{{#with foo as |bar|}} syntax',
