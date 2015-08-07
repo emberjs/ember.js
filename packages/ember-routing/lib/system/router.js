5bb99c1c34a692e044afbd2bfa40fe52e34f1061
Bujorel Tecu <btecu@outlook.com>
2015-08-07 17:49:28 -0400

    [DOC release] Mark didTransition and willTransition as public
    (cherry picked from commit 9a0b064e9c2050a8efaad2efe190788fd8113ced)

--- packages/ember-routing/lib/system/router.js
+++ packages/ember-routing/lib/system/router.js
@@ -178,7 +178,7 @@ var EmberRouter = EmberObject.extend(Evented, {
     Triggers the router level `didTransition` hook.
 
     @method didTransition
-    @private
+    @public
     @since 1.2.0
   */
   didTransition(infos) {
@@ -240,7 +240,7 @@ var EmberRouter = EmberObject.extend(Evented, {
     Triggers the router level `willTransition` hook.
 
     @method willTransition
-    @private
+    @public
     @since 1.11.0
   */
   willTransition(oldInfos, newInfos, transition) {
