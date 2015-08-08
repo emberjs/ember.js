b07ad42556b8bf420a5a50a9a94f489684f6d722
Robert Jackson <robert.w.jackson@me.com>
2015-08-08 15:41:53 -0400

    [BUGFIX release] Deprecate specifying `.render` to views/components.
    (cherry picked from commit 32fb31a7fe1fd7e759c1e90677c389e0a1cc1665)

--- packages/ember-views/lib/views/view.js
+++ packages/ember-views/lib/views/view.js
@@ -1326,6 +1326,12 @@ var View = CoreView.extend(
     }
 
     this.renderer.componentInitAttrs(this, this.attrs || {});
+
+    Ember.deprecate(
+      'Using a custom `.render` function is deprecated and will be removed in Ember 2.0.0.',
+      !this.render,
+      { id: 'ember-views.render', until: '2.0.0' }
+    );
   },
 
   __defineNonEnumerable(property) {
