c10d14715c7f2da98c0ff9d28d46cbfbf0d95461
Matthew Beale <matt.beale@madhatted.com>
2015-08-08 18:17:40 -0400

    [DOC beta] Add docs for get helper

--- packages/ember-htmlbars/lib/keywords/get.js
+++ packages/ember-htmlbars/lib/keywords/get.js
@@ -1,3 +1,8 @@
+/**
+@module ember
+@submodule ember-templates
+*/
+
 import Ember from 'ember-metal/core';
 import isEnabled from 'ember-metal/features';
 import Stream from 'ember-metal/streams/stream';
@@ -30,6 +35,49 @@ if (isEnabled('ember-htmlbars-get-helper')) {
     return stream;
   };
 
+
+  /**
+    Dynamically look up a property on an object. The second argument to `{{get}}`
+    should have a string value, although it can be bound.
+
+    For example, these two usages are equivilent:
+
+    ```handlebars
+    {{person.height}}
+    {{get person "height"}}
+    ```
+
+    If there were several facts about a person, the `{{get}}` helper can dynamically
+    pick one:
+
+    ```handlebars
+    {{get person factName}}
+    ```
+
+    For a more complex example, this template would allow the user to switch
+    between showing the user's height and weight with a click:
+
+    ```handlebars
+    {{get person factName}}
+    <button {{action (mut factName) "height"}}>Show height</button>
+    <button {{action (mut factName) "weight"}}>Show weight</button>
+    ```
+
+    The `{{get}}` helper can also respect mutable values itself. For example:
+
+    ```handlebars
+    {{input value=(mut (get person factName)) type="text"}}
+    <button {{action (mut factName) "height"}}>Show height</button>
+    <button {{action (mut factName) "weight"}}>Show weight</button>
+    ```
+
+    Would allow the user to swap what fact is being displayed, and also edit
+    that fact via a two-way mutable binding.
+
+    @public
+    @method get
+    @for Ember.Templates.helpers
+  */
   var getKeyword = function getKeyword(morph, env, scope, params, hash, template, inverse, visitor) {
     if (morph === null) {
       return buildStream(params);
