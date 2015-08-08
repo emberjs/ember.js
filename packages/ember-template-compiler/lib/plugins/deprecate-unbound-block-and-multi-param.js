991f182129109674336f3e38937829c7435aec51
Matthew Beale <matt.beale@madhatted.com>
2015-08-08 18:39:19 -0400

    [DOC release] Add URL for unbound deprecation guide

--- packages/ember-template-compiler/lib/plugins/deprecate-unbound-block-and-multi-param.js
+++ packages/ember-template-compiler/lib/plugins/deprecate-unbound-block-and-multi-param.js
@@ -20,7 +20,11 @@ DeprecateUnboundBlockAndMultiParam.prototype.transform = function(ast) {
       Ember.deprecate(
         `Using the {{unbound}} helper with a block ${moduleInfo}is deprecated and will be removed in 2.0.0.`,
         false,
-        { id: 'ember-template-compiler.unbound-block', until: '2.0.0' }
+        {
+          id: 'ember-template-compiler.unbound-block',
+          until: '2.0.0',
+          url: 'http://emberjs.com/deprecations/v1.x/#toc_block-and-multi-argument-unbound-helper'
+        }
       );
     } else if (pluginContext.hasMultipleParams(node)) {
       let moduleInfo = calculateLocationDisplay(moduleName, node.loc);
@@ -28,7 +32,11 @@ DeprecateUnboundBlockAndMultiParam.prototype.transform = function(ast) {
       Ember.deprecate(
         `Using the {{unbound}} helper with multiple params ${moduleInfo}is deprecated and will be removed in 2.0.0. Please refactor to nested helper usage.`,
         false,
-        { id: 'ember-template-compiler.unbound-multiple-params', until: '2.0.0' }
+        {
+          id: 'ember-template-compiler.unbound-multiple-params',
+          until: '2.0.0',
+          url: 'http://emberjs.com/deprecations/v1.x/#toc_block-and-multi-argument-unbound-helper'
+        }
       );
     }
   });
