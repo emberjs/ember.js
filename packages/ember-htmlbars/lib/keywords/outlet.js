5b379d11007fae10730591aacdcc4858531fc6f6
Eric Schank <eric.schank@gmail.com>
2015-08-08 21:25:31 -0500

    [DOC release] document {{outlet}} helper
    (cherry picked from commit 02ed7516c7f9612ae8cb5d7d2aa3080f38396666)

--- packages/ember-htmlbars/lib/keywords/outlet.js
+++ packages/ember-htmlbars/lib/keywords/outlet.js
@@ -1,6 +1,6 @@
 /**
 @module ember
-@submodule ember-htmlbars
+@submodule ember-templates
 */
 
 import Ember from 'ember-metal/core';
@@ -9,6 +9,62 @@ import ViewNodeManager from 'ember-htmlbars/node-managers/view-node-manager';
 import topLevelViewTemplate from 'ember-htmlbars/templates/top-level-view';
 topLevelViewTemplate.meta.revision = 'Ember@VERSION_STRING_PLACEHOLDER';
 
+/**
+  The `{{outlet}}` helper lets you specify where a child routes will render in
+  your template. An important use of the `{{outlet}}` helper is in your
+  application's `application.hbs` file:
+
+  ```handlebars
+  {{! app/templates/application.hbs }}
+
+  <!-- header content goes here, and will always display -->
+  {{my-header}}
+
+  <div class="my-dynamic-content">
+    <!-- this content will change based on the current route, which depends on the current URL -->
+    {{outlet}}
+  </div>
+
+  <!-- footer content goes here, and will always display -->
+  {{my-footer}}
+  ```
+
+  See [templates guide](http://emberjs.com/guides/templates/the-application-template/) for
+  additional information on using `{{outlet}}` in `application.hbs`.
+
+  You may also specify a name for the `{{outlet}}`, which is useful when using more than one
+  `{{outlet}}` in a template:
+
+  ```handlebars
+  {{outlet "menu"}}
+  {{outlet "sidebar"}}
+  {{outlet "main"}}
+  ```
+
+  Your routes can then render into a specific one of these `outlet`s by specifying the `outlet`
+  attribute in your `renderTemplate` function:
+
+  ```javascript
+  // app/routes/menu.js
+
+  export default Ember.Route.extend({
+    renderTemplate() {
+      this.render({ outlet: 'menu' });
+    }
+  });
+  ```
+
+  See the [routing guide](http://emberjs.com/guides/routing/rendering-a-template/) for more
+  information on how your `route` interacts with the `{{outlet}}` helper.
+
+  Note: Your content __will not render__ if there isn't an `{{outlet}}` for it.
+
+  @public
+  @method outlet
+  @param {String} [name]
+  @for Ember.Templates.helpers
+  @public
+*/
 export default {
   willRender(renderNode, env) {
     env.view.ownerView._outlets.push(renderNode);
