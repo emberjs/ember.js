a2b5dc82099adef7da11a8a858f7ec28508420eb
Matthew Beale <matt.beale@madhatted.com>
2015-08-10 20:16:46 -0400

    [DOC beta] each-in docs
    (cherry picked from commit b5734429665f895583c3b553655544e415f9338a)

--- packages/ember-htmlbars/lib/helpers/each-in.js
+++ packages/ember-htmlbars/lib/helpers/each-in.js
@@ -1,7 +1,43 @@
+/**
+@module ember
+@submodule ember-templates
+*/
+
 import isEnabled from 'ember-metal/features';
 import shouldDisplay from 'ember-views/streams/should_display';
 
 if (isEnabled('ember-htmlbars-each-in')) {
+  /**
+    The `{{each-in}}` helper loops over properties on an object. It is unbound,
+    in that new (or removed) properties added to the target object will not be
+    rendered.
+
+    For example, given a `user` object that looks like:
+
+    ```javascript
+    {
+      "name": "Shelly Sails",
+      "age": 42
+    }
+    ```
+
+    This template would display all properties on the `user`
+    object in a list:
+
+    ```handlebars
+    <ul>
+    {{#each-in user as |key value|}}
+      <li>{{key}}: {{value}}</li>
+    {{/each-in}}
+    </ul>
+    ```
+
+    Outputting their name and age.
+
+    @method each-in
+    @for Ember.Templates.helpers
+    @public
+  */
   var eachInHelper = function([ object ], hash, blocks) {
     var objKeys, prop, i;
     objKeys = object ? Object.keys(object) : [];
