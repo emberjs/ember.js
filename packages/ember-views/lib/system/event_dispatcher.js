acbb0b2d2551e2032490ce56becccc08e8bf5e4d
Robert Jackson <robert.w.jackson@me.com>
2015-08-11 20:07:50 -0400

    [BUGFIX canary] Honor `customEvents` opting out of event listeners.
    Setting `customEvents` to a hash in an application is a way to add new
    listeners that the `EventDispatcher` knows about.
    
    Often, users see this and decide that they want to disable a given event
    (for example `mouseenter` and `mouseleave` events) and think they can do
    something like:
    
    ```javascript
    var App = Ember.Application.create({
      customEvents: {
        mouseenter: null,
        mouseleave: null
      }
    });
    ```
    
    Unfortunately, that just makes the `EventDispatcher` call a method named
    `null` on the view/component instances (LOL).
    
    This fixes that bug by allowing a `null` value to disable the event
    listener.

--- packages/ember-views/lib/system/event_dispatcher.js
+++ packages/ember-views/lib/system/event_dispatcher.js
@@ -12,7 +12,7 @@ import EmberObject from 'ember-runtime/system/object';
 import jQuery from 'ember-views/system/jquery';
 import ActionManager from 'ember-views/system/action_manager';
 import View from 'ember-views/views/view';
-import merge from 'ember-metal/merge';
+import assign from 'ember-metal/assign';
 
 /**
   `Ember.EventDispatcher` handles delegating browser events to their
@@ -29,12 +29,29 @@ export default EmberObject.extend({
 
   /**
     The set of events names (and associated handler function names) to be setup
-    and dispatched by the `EventDispatcher`. Custom events can added to this list at setup
-    time, generally via the `Ember.Application.customEvents` hash. Only override this
-    default set to prevent the EventDispatcher from listening on some events all together.
+    and dispatched by the `EventDispatcher`. Modifications to this list can be done
+    at setup time, generally via the `Ember.Application.customEvents` hash.
 
-    This set will be modified by `setup` to also include any events added at that time.
+    To add new events to be listened to:
 
+    ```javascript
+    var App = Ember.Application.create({
+      customEvents: {
+        paste: 'paste'
+      }
+    });
+    ```
+
+    To prevent default events from being listened to:
+
+    ```javascript
+    var App = Ember.Application.create({
+      customEvents: {
+        mouseenter: null,
+        mouseleave: null
+      }
+    });
+    ```
     @property events
     @type Object
     @private
@@ -129,9 +146,7 @@ export default EmberObject.extend({
   */
   setup(addedEvents, rootElement) {
     var event;
-    var events = get(this, 'events');
-
-    merge(events, addedEvents || {});
+    var events = assign({}, get(this, 'events'), addedEvents);
 
     if (!isNone(rootElement)) {
       set(this, 'rootElement', rootElement);
@@ -171,6 +186,10 @@ export default EmberObject.extend({
   setupHandler(rootElement, event, eventName) {
     var self = this;
     var viewRegistry = this.container && this.container.lookup('-view-registry:main') || View.views;
+
+    if (eventName === null) {
+      return;
+    }
 
     rootElement.on(event + '.ember', '.ember-view', function(evt, triggeringManager) {
       var view = viewRegistry[this.id];
