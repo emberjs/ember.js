d8a95b11d5776ca840b4c123d3b40d48db4eaf2f
Tom Dale <tom@tomdale.net>
2015-08-12 15:47:29 -0400

    Clean up server-side rendering tests
    The tests for running Ember applications inside a Node.js environment
    evolved organically and were in need of a cleanup. In particular, there
    was so much boilerplate code that was repeated for each test that it
    was hard for the layperson to follow along, and even experienced users
    had trouble understanding what the intent of the test was without
    careful parsing.
    
    This commit splits the test into two files:
    
    1. Simple component rendering tests that just verify that a single
       component can be rendered to a string of HTML
    2. Integration tests that boot and test an entire application.
    
    The integration tests have been cleaned up significantly. I implemented
    a small DSL for building up an application in the tests, as well as some
    helper methods that abstract the steps necessary to put Ember into
    server-side rendering mode, as well as the asynchrony that comes with
    it.
    
    That abstraction happens in a helper that wraps QUnit.module, giving any
    tests in that module a pre-built environment for registering classes
    in the application, then routing to a URL and testing the resulting
    HTML.

---
+++ tests/node/component-rendering-test.js
@@ -0,0 +1,62 @@
+/*globals global,__dirname*/
+
+var SimpleDOM = require('simple-dom');
+var path = require('path');
+
+var distPath = path.join(__dirname, '../../dist');
+var emberPath = path.join(distPath, 'ember.debug.cjs');
+var templateCompilerPath = path.join(distPath, 'ember-template-compiler');
+
+var compile = require(templateCompilerPath).compile;
+var Ember, DOMHelper, run;
+
+QUnit.module("Components can be rendered without a DOM dependency", {
+  beforeEach: function() {
+    Ember = require(emberPath);
+    DOMHelper = Ember.HTMLBars.DOMHelper;
+    run = Ember.run;
+  },
+
+  afterEach: function() {
+    delete global.Ember;
+
+    // clear the previously cached version of this module
+    delete require.cache[emberPath + '.js'];
+    delete require.cache[templateCompilerPath + '.js'];
+  }
+});
+
+QUnit.test("Simple component", function(assert) {
+  var component = buildComponent("<h1>Hello</h1>");
+  var html = renderComponent(component);
+
+  assert.ok(html.match(/<h1>Hello<\/h1>/));
+});
+
+QUnit.test("Component with dynamic value", function(assert) {
+  var component = buildComponent('<h1>Hello {{location}}</h1>', {
+    location: "World"
+  });
+
+  var html = renderComponent(component);
+
+  assert.ok(html.match(/<h1>Hello World<\/h1>/));
+});
+
+function buildComponent(template, props) {
+  var Component = Ember.Component.extend({
+    renderer: new Ember._Renderer(new DOMHelper(new SimpleDOM.Document())),
+    layout: compile(template)
+  }, props || {});
+
+  return Component.create({
+    _domHelper: new DOMHelper(new SimpleDOM.Document()),
+  });
+}
+
+function renderComponent(component) {
+  run(component, component.createElement);
+
+  var serializer = new SimpleDOM.HTMLSerializer(SimpleDOM.voidMap);
+  return serializer.serialize(component.element);
+}
