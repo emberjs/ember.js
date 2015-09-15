78e70558eec99b583687d6154a28c36f217cfe3a
Travis Hoover <thoov7@gmail.com>
2015-09-15 15:12:31 -0700

    [BUGFIX beta] Add private WeakMap (2/3)
    (cherry picked from commit 52792ff38c14d1a7677e61f2bd45f2607b77e1d2)

--- packages/ember-metal/lib/weak_map.js
+++ packages/ember-metal/lib/weak_map.js
@@ -1,10 +1,12 @@
+import { assert } from 'ember-metal/debug';
 import { GUID_KEY } from 'ember-metal/utils';
 import { meta } from 'ember-metal/meta';
 
 var id = 0;
+function UNDEFINED() {}
 
 /*
- * @private
+ * @public
  * @class Ember.WeakMap
  *
  * Weak relationship from Map -> Key, but not Key to Map.
@@ -23,6 +25,10 @@ export default function WeakMap() {
 WeakMap.prototype.get = function(obj) {
   var map = meta(obj).readableWeak();
   if (map) {
+    if (map[this._id] === UNDEFINED) {
+      return undefined;
+    }
+
     return map[this._id];
   }
 };
@@ -34,6 +40,35 @@ WeakMap.prototype.get = function(obj) {
  * @return {Any} stored value
  */
 WeakMap.prototype.set = function(obj, value) {
+  assert('Uncaught TypeError: Invalid value used as weak map key', obj && (typeof obj === 'object' || typeof obj === 'function'));
+
+  if (value === undefined) {
+    value = UNDEFINED;
+  }
+
   meta(obj).writableWeak()[this._id] = value;
+  return this;
+};
+
+/*
+ * @method has
+ * @param key {Object}
+ * @return {Boolean} if the key exists
+ */
+WeakMap.prototype.has = function(obj) {
+  var map = meta(obj).readableWeak();
+
+  return (map && map[this._id] !== undefined);
+};
+
+/*
+ * @method delete
+ * @param key {Object}
+ */
+WeakMap.prototype.delete = function(obj) {
+  if (this.has(obj)) {
+    delete meta(obj).writableWeak()[this._id];
+  }
+
   return this;
 };
