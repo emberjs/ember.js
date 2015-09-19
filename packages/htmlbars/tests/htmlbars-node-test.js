94e2885c03a7bf9facdcede51c3bf61689af2735
Godhuda <engineering+godhuda@tilde.io>
2015-09-19 13:36:56 -0700

    Get TS compilation working through Broccoli

--- packages/htmlbars/tests/htmlbars-node-test.js
+++
@@ -1,6 +0,0 @@
-import { compile } from "../htmlbars";
-QUnit.module('htmlbars');
-test("compile is exported", function () {
-    ok(typeof compile === 'function', 'compile is exported');
-});
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaHRtbGJhcnMtbm9kZS10ZXN0LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vc3JjL2h0bWxiYXJzL3Rlc3RzL2h0bWxiYXJzLW5vZGUtdGVzdC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiT0FBTyxFQUFDLE9BQU8sRUFBQyxNQUFNLGFBQWE7QUFFbkMsS0FBSyxDQUFDLE1BQU0sQ0FBQyxVQUFVLENBQUMsQ0FBQztBQUV6QixJQUFJLENBQUMscUJBQXFCLEVBQUU7SUFDMUIsRUFBRSxDQUFDLE9BQU8sT0FBTyxLQUFLLFVBQVUsRUFBRSxxQkFBcUIsQ0FBQyxDQUFDO0FBQzNELENBQUMsQ0FBQyxDQUFDIn0=
