85d725c61f3b43a630d1f806c7d0824b3c299d0e
Jonathan Skeate <skeate@gmail.com>
2015-09-24 14:51:14 -0400

    [BUGFIX release-1-13] Fix string classify normalizer
    Resolves #11326. If a piece of a name starts with dash (-), it will be
    changed into an underscore (_).

--- packages/ember-runtime/tests/system/string/classify_test.js
+++ packages/ember-runtime/tests/system/string/classify_test.js
@@ -57,3 +57,59 @@ QUnit.test('classify namespaced dasherized string', function() {
     deepEqual('private-docs/owner-invoice'.classify(), 'PrivateDocs/OwnerInvoice');
   }
 });
+
+QUnit.test('classify prefixed dasherized string', function() {
+  deepEqual(classify('-view-registry'), '_ViewRegistry');
+  if (Ember.EXTEND_PROTOTYPES) {
+    deepEqual('-view-registry'.classify(), '_ViewRegistry');
+  }
+});
+
+QUnit.test('classify namespaced prefixed dasherized string', function() {
+  deepEqual(classify('components/-text-field'), 'Components/_TextField');
+  if (Ember.EXTEND_PROTOTYPES) {
+    deepEqual('components/-text-field'.classify(), 'Components/_TextField');
+  }
+});
+
+QUnit.test('does nothing with classified prefixed string', function() {
+  deepEqual(classify('_FooBar'), '_FooBar');
+  if (Ember.EXTEND_PROTOTYPES) {
+    deepEqual('_FooBar'.classify(), '_FooBar');
+  }
+});
+
+QUnit.test('classify underscore-prefixed underscored string', function() {
+  deepEqual(classify('_Foo_Bar'), '_FooBar');
+  if (Ember.EXTEND_PROTOTYPES) {
+    deepEqual('_Foo_Bar'.classify(), '_FooBar');
+  }
+});
+
+QUnit.test('classify underscore-prefixed dasherized string', function() {
+  deepEqual(classify('_Foo-Bar'), '_FooBar');
+  if (Ember.EXTEND_PROTOTYPES) {
+    deepEqual('_Foo-Bar'.classify(), '_FooBar');
+  }
+});
+
+QUnit.test('classify underscore-prefixed-namespaced underscore-prefixed string', function() {
+  deepEqual(classify('_foo/_bar'), '_Foo/_Bar');
+  if (Ember.EXTEND_PROTOTYPES) {
+    deepEqual('_foo/_bar'.classify(), '_Foo/_Bar');
+  }
+});
+
+QUnit.test('classify dash-prefixed-namespaced underscore-prefixed string', function() {
+  deepEqual(classify('-foo/_bar'), '_Foo/_Bar');
+  if (Ember.EXTEND_PROTOTYPES) {
+    deepEqual('-foo/_bar'.classify(), '_Foo/_Bar');
+  }
+});
+
+QUnit.test('classify dash-prefixed-namespaced dash-prefixed string', function() {
+  deepEqual(classify('-foo/-bar'), '_Foo/_Bar');
+  if (Ember.EXTEND_PROTOTYPES) {
+    deepEqual('-foo/-bar'.classify(), '_Foo/_Bar');
+  }
+});
