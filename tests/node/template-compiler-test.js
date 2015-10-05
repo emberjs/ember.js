9dd644ff00c6c356ccaa316ca14e64a42cba6932
Robert Jackson <robert.w.jackson@me.com>
2015-10-05 16:05:24 -0400

    Cherry-picked @rwjblue's commit to fix the node tests.
    Fix node tests.
    
    QUnit 1.19 was released and changed the way the npm package exports its
    value (it no longer sets up `global.QUnit` and all the assertion methods
    automatically).
    
    This updates to a better pattern of importing `QUnit` and using the
    `assert` argument for assertions.

--- tests/node/template-compiler-test.js
+++ tests/node/template-compiler-test.js
@@ -1,13 +1,14 @@
 /*globals __dirname*/
 
 var path = require('path');
+var QUnit = require('qunitjs');
+
 var distPath = path.join(__dirname, '../../dist');
 var emberPath = path.join(distPath, 'ember.debug.cjs');
 var templateCompilerPath = path.join(distPath, 'ember-template-compiler');
 
 var module = QUnit.module;
-var ok = QUnit.ok;
-var equal = QUnit.equal;
+var test = QUnit.test;
 
 var distPath = path.join(__dirname, '../../dist');
 var templateCompiler = require(path.join(distPath, 'ember-template-compiler'));
@@ -25,25 +26,24 @@ module('ember-template-compiler.js', {
   }
 });
 
-test('can be required', function() {
-
-  ok(typeof templateCompiler.precompile === 'function', 'precompile function is present');
-  ok(typeof templateCompiler.compile === 'function', 'compile function is present');
-  ok(typeof templateCompiler.template === 'function', 'template function is present');
+QUnit.test('can be required', function(assert) {
+  assert.ok(typeof templateCompiler.precompile === 'function', 'precompile function is present');
+  assert.ok(typeof templateCompiler.compile === 'function', 'compile function is present');
+  assert.ok(typeof templateCompiler.template === 'function', 'template function is present');
 });
 
-test('uses plugins with precompile', function() {
+QUnit.test('uses plugins with precompile', function(assert) {
   var templateOutput;
   var templateCompiler = require(path.join(distPath, 'ember-template-compiler'));
 
   templateOutput = templateCompiler.precompile('{{#each foo in bar}}{{/each}}');
-  ok(templateOutput.match(/locals: \["foo"\]/), 'transform each in to block params');
+  assert.ok(templateOutput.match(/locals: \["foo"\]/), 'transform each in to block params');
 
   templateOutput = templateCompiler.precompile('{{#with foo as bar}}{{/with}}');
-  ok(templateOutput.match(/locals: \["bar"\]/), 'transform with as to block params');
+  assert.ok(templateOutput.match(/locals: \["bar"\]/), 'transform with as to block params');
 });
 
-test('allows enabling of features', function() {
+QUnit.test('allows enabling of features', function(assert) {
   var templateOutput;
   var templateCompiler = require(path.join(distPath, 'ember-template-compiler'));
 
@@ -53,8 +53,8 @@ test('allows enabling of features', function() {
     templateCompiler._Ember.FEATURES['ember-htmlbars-component-generation'] = true;
 
     templateOutput = templateCompiler.precompile('<some-thing></some-thing>');
-    ok(templateOutput.indexOf('["component","<some-thing>",[],0]') > -1, 'component generation can be enabled');
+    assert.ok(templateOutput.indexOf('["component","@<some-thing>",[],0]') > -1, 'component generation can be enabled');
   } else {
-    ok(true, 'cannot test features in feature stripped build');
+    assert.ok(true, 'cannot test features in feature stripped build');
   }
 });
