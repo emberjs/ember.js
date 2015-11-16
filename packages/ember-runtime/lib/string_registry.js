cbb58b4d5666f32ab8ac007df818cb1d231492b8
Stefan Penner <stefan.penner@gmail.com>
2015-11-15 17:51:32 -0800

    decouple internals from Ember.STRINGS global

---
+++ packages/ember-runtime/lib/string_registry.js
@@ -0,0 +1,16 @@
+// STATE within a module is frowned apon, this exists
+// to support Ember.STRINGS but shield ember internals from this legacy global
+// API.
+let STRINGS = {};
+
+export function setStrings(strings) {
+  STRINGS = strings;
+}
+
+export function getStrings() {
+  return STRINGS;
+}
+
+export function get(name) {
+  return STRINGS[name];
+}
