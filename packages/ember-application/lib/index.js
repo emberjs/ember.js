21e86e04291a60e984bae6d71c7ad0d1df9bae86
Dan Gebhardt <dan@cerebris.com>
2015-12-05 10:37:59 -0500

    [FEATURE ember-application-engines] Feature flag for Ember.Engine.
    Only expose `Ember.Engine` when feature flag is enabled.

--- packages/ember-application/lib/index.js
+++ packages/ember-application/lib/index.js
@@ -1,4 +1,5 @@
 import Ember from 'ember-metal/core';
+import isEnabled from 'ember-metal/features';
 import { runLoadHooks } from 'ember-runtime/system/lazy_load';
 
 /**
@@ -11,10 +12,14 @@ import {
   Resolver
 } from 'ember-application/system/resolver';
 import Application from 'ember-application/system/application';
+import Engine from 'ember-application/system/engine';
 
 Ember.Application = Application;
 Ember.Resolver = Resolver;
 Ember.DefaultResolver = DefaultResolver;
 
+if (isEnabled('ember-application-engines')) {
+  Ember.Engine = Engine;
+}
 
 runLoadHooks('Ember.Application', Application);
