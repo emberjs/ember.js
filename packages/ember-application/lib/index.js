18b0a61d8c792f16afe5c15571978eff73ed3d26
Dan Gebhardt <dan@cerebris.com>
2015-12-06 17:39:17 -0500

    [FEATURE ember-application-engines] Expose Ember.EngineInstance and Ember.ApplicationInstance.
    Expose Ember.EngineInstance and Ember.ApplicationInstance for
    easy overriding. Need to reanalyze whether to continue exposing
    them after feature flag is removed.

--- packages/ember-application/lib/index.js
+++ packages/ember-application/lib/index.js
@@ -12,7 +12,9 @@ import {
   Resolver
 } from 'ember-application/system/resolver';
 import Application from 'ember-application/system/application';
+import ApplicationInstance from 'ember-application/system/application-instance';
 import Engine from 'ember-application/system/engine';
+import EngineInstance from 'ember-application/system/engine-instance';
 
 Ember.Application = Application;
 Ember.Resolver = Resolver;
@@ -20,6 +22,11 @@ Ember.DefaultResolver = DefaultResolver;
 
 if (isEnabled('ember-application-engines')) {
   Ember.Engine = Engine;
+
+  // Expose `EngineInstance` and `ApplicationInstance` for easy overriding.
+  // Reanalyze whether to continue exposing these after feature flag is removed.
+  Ember.EngineInstance = EngineInstance;
+  Ember.ApplicationInstance = ApplicationInstance;
 }
 
 runLoadHooks('Ember.Application', Application);
