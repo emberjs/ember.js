2f38e75a5f000dc74a18f77afb84a91bbfe600f5
Jake Worth <jakeworth82@gmail.com>
2015-12-20 12:04:11 -0600

    [DOC release] Fix typo

--- packages/ember-views/lib/system/ext.js
+++ packages/ember-views/lib/system/ext.js
@@ -7,6 +7,6 @@ import run from 'ember-metal/run_loop';
 
 // Add a new named queue for rendering views that happens
 // after bindings have synced, and a queue for scheduling actions
-// that that should occur after view rendering.
+// that should occur after view rendering.
 run._addQueue('render', 'actions');
 run._addQueue('afterRender', 'render');
