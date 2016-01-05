0f09498b56556be6c1f315f163cf29394ce2e273
Robert Jackson <robert.w.jackson@me.com>
2016-01-05 18:25:09 -0500

    Do not override `OWNER` if already present on child view.
    Previously, `linkChild` would always set the new child view's `OWNER` to
    its own `OWNER`.  Unfortunately, when dealing with Engines mounted at a
    given outlet this means that any components and whatnot rendered in that
    engines template have their `OWNER` changed after init (just before
    render).
    
    The intent of the original code was to ensure that all child views had
    an `OWNER` (actually it was `.container` at the time). The
    `ViewNodeManager` and `ComponentNodeManager`'s now handle properly
    creating the views/components with the right `OWNER` so this code is
    mostly no longer needed (except for edge case legacy support). The guard
    added here ensures that we are only setting `OWNER` if it wasn't
    previously set (which is that one legacy edge case).

--- packages/ember-views/lib/mixins/legacy_child_views_support.js
+++ packages/ember-views/lib/mixins/legacy_child_views_support.js
@@ -1,11 +1,14 @@
 import { Mixin } from 'ember-metal/mixin';
 import { get } from 'ember-metal/property_get';
 import { set } from 'ember-metal/property_set';
-import { getOwner, setOwner } from 'container/owner';
+import { getOwner, setOwner, OWNER } from 'container/owner';
 
 export default Mixin.create({
   linkChild(instance) {
-    setOwner(instance, getOwner(this));
+    if (!instance[OWNER]) {
+      setOwner(instance, getOwner(this));
+    }
+
     if (get(instance, 'parentView') !== this) {
       // linkChild should be idempotent
       set(instance, 'parentView', this);
