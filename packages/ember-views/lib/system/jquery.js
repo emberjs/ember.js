40d3eee65fa916e36a5ebe298d1fe90e355a54f4
Thijs van den Anker <thijs@bitsnbolts.nl>
2016-01-08 22:54:38 +0100

    Added jQuery 2.2 to regex

--- packages/ember-views/lib/system/jquery.js
+++ packages/ember-views/lib/system/jquery.js
@@ -15,7 +15,7 @@ if (environment.hasDOM) {
 
   assert(
     'Ember Views require jQuery between 1.7 and 2.1',
-    jQuery && (jQuery().jquery.match(/^((1\.(7|8|9|10|11|12))|(2\.(0|1)))(\.\d+)?(pre|rc\d?)?/) || Ember.ENV.FORCE_JQUERY)
+    jQuery && (jQuery().jquery.match(/^((1\.(7|8|9|10|11|12))|(2\.(0|1|2)))(\.\d+)?(pre|rc\d?)?/) || Ember.ENV.FORCE_JQUERY)
   );
 
   if (jQuery) {
