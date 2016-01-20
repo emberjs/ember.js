fd9805438bfc34c2b9e33bd056d57d2d36dde53e
Stefan Penner <stefan.penner@gmail.com>
2016-01-20 11:30:22 -0800

    Add Addition CP + Chains tests
    scenarios with:
    
    * object proxy
    * array proxy

--- packages/ember-runtime/tests/computed/chains_test.js
+++ packages/ember-runtime/tests/computed/chains_test.js
@@ -4,6 +4,8 @@ import { get } from 'ember-metal/property_get';
 import { set } from 'ember-metal/property_set';
 import { isWatching } from 'ember-metal/watching';
 import { A as emberA } from 'ember-runtime/system/native_array';
+import ObjectProxy from 'ember-runtime/system/object_proxy';
+import ArrayProxy from 'ember-runtime/system/array_proxy';
 
 let a1, a2, a3, a4, obj;
 
@@ -46,7 +48,7 @@ QUnit.test('replace array (overlap)', function() {
   ok(isWatching(a3, 'foo'), 'BEFORE: a3.foo is watched');
   ok(!isWatching(a4, 'foo'), 'BEFORE: a4.foo is NOT watched');
 
-  obj.set('array', [a2, a3, a4]);
+  set(obj, 'array', [a2, a3, a4]);
 
   ok(!isWatching(a1, 'foo'), 'AFTER: a1.foo is NOT watched');
   ok(isWatching(a2, 'foo'), 'AFTER: a2.foo is watched');
@@ -64,11 +66,91 @@ QUnit.test('responds to change of property value on element after replacing arra
   set(obj, 'array', emberA([a1, a2]));
 
   deepEqual(get(obj, 'a'), 3, 'value is correct initially');
+
   set(a1, 'foo', false);
+
   deepEqual(get(obj, 'a'), 2, 'responds to change of property on element');
+
   set(obj, 'array', emberA([a1, a2, a3]));
-  deepEqual(get(obj, 'a'), 6, 'responds to content array change');
+
+  deepEqual(get(obj, 'a'), 2, 'responds to content array change');
+
   set(a1, 'foo', true);
-  deepEqual(get(obj, 'a'), 7, 'still responds to change of property on element');
+
+  deepEqual(get(obj, 'a'), 3, 'still responds to change of property on element');
+  set(a3, 'foo', true);
+
+  deepEqual(get(obj, 'a'), 6, 'still responds to change of property on element');
 });
 
+
+QUnit.test('responds to change of property value on element after replacing array (object proxy)', function() {
+  let obj = { };
+
+  defineProperty(obj, 'a', computed('array.@each.foo', function() {
+    return get(this, 'array').filter(elt => get(elt, 'foo')).reduce((a, b) => a + get(b, 'id'), 0);
+  }));
+
+  set(obj, 'array', emberA([
+    ObjectProxy.create({ content: a1 }),
+    ObjectProxy.create({ content: a2 })
+  ]));
+
+  deepEqual(get(obj, 'a'), 3, 'value is correct initially');
+
+  set(a1, 'foo', false);
+
+  deepEqual(get(obj, 'a'), 2, 'responds to change of property on element');
+  set(obj, 'array', emberA([
+    ObjectProxy.create({ content: a1 }),
+    ObjectProxy.create({ content: a2 }),
+    ObjectProxy.create({ content: a3 })
+  ]));
+
+  deepEqual(get(obj, 'a'), 2, 'responds to content array change');
+
+  set(a1, 'foo', true);
+
+  deepEqual(get(obj, 'a'), 3, 'still responds to change of property on element');
+  set(a3, 'foo', true);
+
+  deepEqual(get(obj, 'a'), 6, 'still responds to change of property on element');
+});
+
+
+QUnit.test('responds to change of property value on element after replacing array (array proxy)', function() {
+  let obj = { };
+
+  defineProperty(obj, 'a', computed('array.@each.foo', function() {
+    return get(this, 'array').filter(elt => get(elt, 'foo')).reduce((a, b) => a + get(b, 'id'), 0);
+  }));
+
+  set(obj, 'array', ArrayProxy.create({
+    content: emberA([
+      ObjectProxy.create({ content: a1 }),
+      ObjectProxy.create({ content: a2 })
+    ])
+  }));
+
+  deepEqual(get(obj, 'a'), 3, 'value is correct initially');
+
+  set(a1, 'foo', false);
+
+  deepEqual(get(obj, 'a'), 2, 'responds to change of property on element');
+  set(obj, 'array', ArrayProxy.create({
+    content: emberA([
+      ObjectProxy.create({ content: a1 }),
+      ObjectProxy.create({ content: a2 }),
+      ObjectProxy.create({ content: a3 })
+    ])
+  }));
+
+  deepEqual(get(obj, 'a'), 2, 'responds to content array change');
+
+  set(a1, 'foo', true);
+
+  deepEqual(get(obj, 'a'), 3, 'still responds to change of property on element');
+  set(a3, 'foo', true);
+
+  deepEqual(get(obj, 'a'), 6, 'still responds to change of property on element');
+});
