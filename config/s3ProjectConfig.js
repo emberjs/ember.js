2fd66b406e1f99934d9b2982c26bac0f9d6199d8
Sylvain MINA <sylvain.mina@gmail.com>
2016-02-12 17:57:41 +0100

    publish dist bower.json & co to s3
    (cherry picked from commit bfe959e72879f38adde992a3e46ea4cd6fabef3d)

--- config/s3ProjectConfig.js
+++ config/s3ProjectConfig.js
@@ -9,7 +9,11 @@ function fileMap(revision,tag,date) {
     "ember.min.js":               fileObject("ember.min",               ".js",   "text/javascript",  revision, tag, date),
     "ember.prod.js":              fileObject("ember.prod",              ".js",   "text/javascript",  revision, tag, date),
     "../docs/data.json":          fileObject("ember-docs",              ".json", "application/json", revision, tag, date),
-    "ember-tests/index.html":     fileObject("ember-tests-index",       ".html", "text/html",        revision, tag, date)
+    "ember-tests/index.html":     fileObject("ember-tests-index",       ".json", "application/json", revision, tag, date),
+    "bower.json":                 fileObject("bower",                   ".json", "application/json", revision, tag, date),
+    "component.json":             fileObject("component",               ".json", "application/json", revision, tag, date),
+    "composer.json":              fileObject("composer",                ".json", "application/json", revision, tag, date),
+    "package.json":               fileObject("package",                 ".json", "application/json", revision, tag, date),
   };
 };
 
@@ -17,40 +21,40 @@ function fileObject(baseName, extension, contentType, currentRevision, tag, date
   var fullName = "/" + baseName + extension;
   var obj =  {
     contentType: contentType,
-      destinations: {
-        canary: [
-          "latest" + fullName,
-          "canary" + fullName,
-          "canary/daily/" + date + fullName,
-          "canary/shas/" + currentRevision + fullName
-        ],
-        release: [
-          "stable" + fullName,
-          "release" + fullName,
-          "release/daily/" + date + fullName,
-          "release/shas/" + currentRevision + fullName
-        ],
-        'release-1-13': [
-          "release-1-13" + fullName,
-          "release-1-13/daily/" + date + fullName,
-          "release-1-13/shas/" + currentRevision + fullName
-        ],
-        beta: [
-          "beta" + fullName,
-          "beta/daily/" + date + fullName,
-          "beta/shas/" + currentRevision + fullName
-        ],
-        wildcard: []
-      }
-   };
+    destinations: {
+      canary: [
+        "latest" + fullName,
+        "canary" + fullName,
+        "canary/daily/" + date + fullName,
+        "canary/shas/" + currentRevision + fullName
+      ],
+      release: [
+        "stable" + fullName,
+        "release" + fullName,
+        "release/daily/" + date + fullName,
+        "release/shas/" + currentRevision + fullName
+      ],
+      'release-1-13': [
+        "release-1-13" + fullName,
+        "release-1-13/daily/" + date + fullName,
+        "release-1-13/shas/" + currentRevision + fullName
+      ],
+      beta: [
+        "beta" + fullName,
+        "beta/daily/" + date + fullName,
+        "beta/shas/" + currentRevision + fullName
+      ],
+      wildcard: []
+    }
+  };
 
-   if (tag) {
-     for (var key in obj.destinations) {
-       obj.destinations[key].push("tags/" + tag + fullName);
-     }
-   }
+  if (tag) {
+    for (var key in obj.destinations) {
+      obj.destinations[key].push("tags/" + tag + fullName);
+    }
+  }
 
-   return obj;
+  return obj;
 }
 
 module.exports = fileMap;
