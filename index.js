3329d74b9b0943e25b577e29e3f7f175ca139894
Stefan Penner <stefan.penner@gmail.com>
2016-02-27 16:01:39 -0800

    provide ember-source from NPM
    * obviate the need to `bower install ember.js`
    * provide an api for addons to retrieve paths to
      ember.debug.js, ember.prod.js and
      ember-template-compiler.js

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 /* jshint node: true */
 'use strict';
+var stew = require('broccoli-stew');
+
+var paths = {};
+var absolutePaths = {};
+
+function add(paths, name, path) {
+  Object.defineProperty(paths, name, {
+    configurable: false,
+    get: function() { return path; }
+  });
+}
+
+add(paths, 'prod',  'vendor/ember/ember.prod.js');
+add(paths, 'debug', 'vendor/ember/ember.debug.js');
+
+add(absolutePaths, 'templateCompiler', __dirname + '/dist/ember-template-compiler.js');
 
 module.exports = {
-  name: 'ember'
+  name: 'ember-source',
+  paths: paths,
+  absolutePaths: absolutePaths,
+  treeForVendor: function() {
+    return stew.find(__dirname + '/dist', {
+      destDir: 'ember',
+      files: [
+        'ember-runtime.js',
+        'ember-runtime.map',
+        'ember-template-compiler.js',
+        'ember-template-compiler.map',
+        'ember-testing.js',
+        'ember.debug.cjs.js',
+        'ember.debug.cjs.map',
+        'ember.debug.js',
+        'ember.min.js',
+        'ember.prod.js',
+        // 'shims/shims.js'
+      ]
+    });
+  }
 };
