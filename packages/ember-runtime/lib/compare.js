99d9cbc30605df89bd9a6b575c273b4ec4a7ebbe
Kris Selden <kris.selden@gmail.com>
2016-03-01 22:49:24 -0800

    Revert "[BUGFIX canary] Update Ember.compare to use operators"
    (cherry picked from commit ef8622c7e7f89b398ae6a090af873550270f886b)

--- packages/ember-runtime/lib/compare.js
+++ packages/ember-runtime/lib/compare.js
@@ -109,18 +109,7 @@ export default function compare(v, w) {
       return spaceship(v, w);
 
     case 'string':
-      // We are comparing Strings using operators instead of `String#localeCompare`
-      // because of unexpected behavior for certain edge cases.
-      // For example `'Z'.localeCompare('a')` returns `1`.
-      //
-      // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare#Description
-      if (v < w) {
-        return -1;
-      } else if (v === w) {
-        return 0;
-      }
-
-      return 1;
+      return spaceship(v.localeCompare(w), 0);
 
     case 'array':
       var vLen = v.length;
