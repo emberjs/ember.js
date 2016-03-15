e0a98eb91d258ae58ca4e185c835d1edab4724c5
Todd Jordan <todd.jordan@inin.com>
2016-03-14 23:41:26 -0400

    [DOC release] Initial pass at filtering out api docs behind FFs
    (cherry picked from commit 7723a941cf5982fd82d4f8aa4d93934a24642a87)

---
+++ bin/feature-flag-yuidoc-filter.js
@@ -0,0 +1,57 @@
+var fs = require('fs');
+
+function isClassToBeIncluded(item, featuresToFilter) {
+  if (item.category) {
+    for (var j = 0; j < featuresToFilter.length; j++) {
+      for (var k = 0; k < item.category.length; k++) {
+        if (featuresToFilter[j] === item.category[k]) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+};
+
+function gatherFeatures() {
+    var featuresJson = JSON.parse(fs.readFileSync('features.json'));
+    var featuresObj = featuresJson.features;
+    var featuresToFilter = [];
+    for (var feature in featuresObj) {
+        if (featuresObj[feature] === null || featuresObj[feature] === false) {
+            featuresToFilter.push(feature);
+        }
+    }
+    return featuresToFilter;
+};
+
+function gatherClassesToDocument(data, featuresToFilter) {
+  var classesToDocument = {};
+
+  for (var c in data.classes) {
+    if (isClassToBeIncluded(data.classes[c], featuresToFilter)) {
+      classesToDocument[c] = data.classes[c];
+    }
+  }
+  return classesToDocument;
+};
+
+function updateClassReferencesInNamespaces(data) {
+  for (var namespace in data.modules) {
+    var namespaceClasses = {};
+    var originalClasses = data.modules[namespace].classes;
+    for(var className in originalClasses) {
+      if (data.classes.hasOwnProperty(className)) {
+        namespaceClasses[className] = originalClasses[className]
+      }
+    }
+    data.modules[namespace].classes = namespaceClasses;
+  }
+
+};
+
+module.exports = function (data, options) {
+    var featuresToFilter = gatherFeatures();
+    data.classes = gatherClassesToDocument(data, featuresToFilter);
+    updateClassReferencesInNamespaces(data);
+};
