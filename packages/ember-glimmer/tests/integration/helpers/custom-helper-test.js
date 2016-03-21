00e51576a337cac58361a5963b87d94e28d89a25
Chad Hietala <chadhietala@gmail.com>
2016-03-20 22:12:26 -0700

    [Glimmer2] Move Helper Lookup
    While this is indirectly tested in multiple places probably the best to
    have an actually test for it.
    
    /cc @chancancode

--- packages/ember-glimmer/tests/integration/helpers/custom-helper-test.js
+++ packages/ember-glimmer/tests/integration/helpers/custom-helper-test.js
@@ -2,6 +2,14 @@ import { RenderingTest, moduleFor } from '../../utils/test-case';
 
 moduleFor('Helpers test: custom helpers', class extends RenderingTest {
 
+  ['@test non-dashed helpers are found']() {
+    this.registerHelper('fullname', ([first, last]) => `${first} ${last}`);
+
+    this.render('{{fullname "Robert" "Jackson"}}');
+
+    this.assertText('Robert Jackson');
+  }
+
   ['@test it can resolve custom helpers']() {
     this.registerHelper('hello-world', () => 'hello world');
 
