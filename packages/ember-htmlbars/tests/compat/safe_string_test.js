8c341bc1d3f311cadcddd61d021c1908e75e86e4
Robert Jackson <robert.w.jackson@me.com>
2016-03-31 19:52:25 -0400

    Fix deprecation for Ember.Handlebars.SafeString.
    We have been trolled by exactly the same issue that has caused us to
    deprecate `Ember.Handlebars.SafeString` to begin with. Invoking the
    function doesn't actually work, we must instantiate a class (which is
    not done by `deprecateFunc`).
    
    :troll:

--- packages/ember-htmlbars/tests/compat/safe_string_test.js
+++ packages/ember-htmlbars/tests/compat/safe_string_test.js
@@ -3,7 +3,11 @@ import EmberHandlebars from 'ember-htmlbars/compat';
 QUnit.module('ember-htmlbars: compat - SafeString');
 
 QUnit.test('using new results in a deprecation', function(assert) {
+  let result;
+
   expectDeprecation(function() {
-    new EmberHandlebars.SafeString('test');
+    result = new EmberHandlebars.SafeString('<b>test</b>');
   }, 'Ember.Handlebars.SafeString is deprecated in favor of Ember.String.htmlSafe');
+
+  assert.equal(result.toHTML(), '<b>test</b>');
 });
