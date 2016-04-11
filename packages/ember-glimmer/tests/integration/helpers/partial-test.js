d0a9bb7ccf057f3bba6b4292cedadae1818f6202
Chad Hietala <chadhietala@gmail.com>
2016-04-11 15:20:51 -0700

    [Glimmer 2] Port partial tests
    This supersedes #13199

--- packages/ember-glimmer/tests/integration/helpers/partial-test.js
+++ packages/ember-glimmer/tests/integration/helpers/partial-test.js
@@ -8,9 +8,9 @@ moduleFor('Helpers test: {{partial}}', class extends RenderingTest {
 
     this.render(`This {{partial "subTemplateFromContainer"}} is pretty great.`);
 
-    equal(this.$().text(), 'This sub-template is pretty great.');
-
     this.assertStableRerender();
+
+    this.assertText('This sub-template is pretty great.');
   }
 
   ['@htmlbars should render other slash-separated templates registered with the container']() {
@@ -18,35 +18,52 @@ moduleFor('Helpers test: {{partial}}', class extends RenderingTest {
 
     this.render(`This {{partial "child/subTemplateFromContainer"}} is pretty great.`);
 
-    equal(this.$().text(), 'This sub-template is pretty great.');
-
     this.assertStableRerender();
+
+    this.assertText('This sub-template is pretty great.');
   }
 
   ['@htmlbars should use the current context']() {
     this.registerPartial('_person_name', '{{firstName}} {{lastName}}');
 
-    this.render('Who is {{partial "person_name"}}?', { firstName: 'Kris', lastName: 'Selden' });
-
-    equal(this.$().text(), 'Who is Kris Selden?');
+    this.render('Who is {{partial "person_name"}}?', {
+      firstName: 'Kris',
+      lastName: 'Selden'
+    });
 
     this.assertStableRerender();
+
+    this.assertText('Who is Kris Selden?');
+
+    this.runTask(() => set(this.context, 'firstName', 'Kelly'));
+
+    this.assertText('Who is Kelly Selden?');
+
+    this.runTask(() => set(this.context, 'firstName', 'Kris'));
+
+    this.assertText('Who is Kris Selden?');
   }
 
-  ['@htmlbars Quoteless parameters passed to {{template}} perform a bound property lookup of the partial name']() {
+  ['@htmlbars Quoteless parameters passed to {{partial}} perform a bound property lookup of the partial name']() {
     this.registerPartial('_subTemplate', 'sub-template');
     this.registerPartial('_otherTemplate', 'other-template');
 
     this.render('This {{partial partialName}} is pretty {{partial nonexistent}}great.', { partialName: 'subTemplate' });
 
-    equal(this.$().text(), 'This sub-template is pretty great.');
+    this.assertStableRerender();
+
+    this.assertText('This sub-template is pretty great.');
 
     this.runTask(() => set(this.context, 'partialName', 'otherTemplate'));
 
-    equal(this.$().text(), 'This other-template is pretty great.');
+    this.assertText('This other-template is pretty great.');
 
     this.runTask(() => set(this.context, 'partialName', null));
 
-    equal(this.$().text(), 'This  is pretty great.');
+    this.assertText('This  is pretty great.');
+
+    this.runTask(() => set(this.context, 'partialName', 'subTemplate'));
+
+    this.assertText('This sub-template is pretty great.');
   }
 });
