bc03bb59eca7ad98af750164bb58e9709eb2ad15
Gaurav Munjal <gmunjal@hedgeserv.com>
2016-04-14 15:23:54 -0400

    Create helper integration tests
    Follow new pattern after merge
    
    Get it working and passing tests
    
    Fix tests again

--- blueprints/helper-test/index.js
+++ blueprints/helper-test/index.js
@@ -1,15 +1,52 @@
 /*jshint node:true*/
 
-var testInfo = require('ember-cli-test-info');
-var stringUtils = require('ember-cli-string-utils');
+var testInfo                 = require('ember-cli-test-info');
+var stringUtils              = require('ember-cli-string-utils');
+var isPackageMissing         = require('ember-cli-is-package-missing');
 var useTestFrameworkDetector = require('../test-framework-detector');
 
 module.exports = useTestFrameworkDetector({
-  description: 'Generates a helper unit test.',
-  locals: function(options) {
+  description: 'Generates a helper integration test or a unit test.',
+
+  availableOptions: [
+    {
+      name: 'test-type',
+      type: ['integration', 'unit'],
+      default: 'unit',
+      aliases:[
+        { 'i': 'integration'},
+        { 'u': 'unit'},
+        { 'integration': 'integration' },
+        { 'unit': 'unit' }
+      ]
+    }
+  ],
+
+  fileMapTokens: function() {
     return {
-      friendlyTestName: testInfo.name(options.entity.name, 'Unit', 'Helper'),
-      dasherizedModulePrefix: stringUtils.dasherize(options.project.config().modulePrefix)
+      __testType__: function(options) {
+        return options.locals.testType || 'unit';
+      }
     };
+  },
+
+  locals: function(options) {
+    var testType = options.testType || 'unit';
+    var testName = testType === 'integration' ? 'Integration' : 'Unit';
+    var friendlyTestName = testInfo.name(options.entity.name, testName, 'Helper');
+
+    return {
+      friendlyTestName: friendlyTestName,
+      dasherizedModulePrefix: stringUtils.dasherize(options.project.config().modulePrefix),
+      testType:testType
+    };
+  },
+
+  afterInstall: function(options) {
+    if (!options.dryRun && options.testType === 'integration' && isPackageMissing(this, 'ember-cli-htmlbars-inline-precompile')) {
+      return this.addPackagesToProject([
+        { name: 'ember-cli-htmlbars-inline-precompile', target: '^0.3.1' }
+      ]);
+    }
   }
 });
