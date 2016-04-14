8c152265a6b1994f03adaf88257e0d9e58317781
Robert Jackson <robert.w.jackson@me.com>
2016-04-14 15:57:23 -0400

    [BUGFIX release] Ensure injected property assertion checks `container`.
    During the Ember 2.3.0 cycle, we introduced the `getOwner` / `setOwner`
    concepts and deprecated using the injected `.container` property on
    objects. Unfortunately, if you are using an older library to create
    objects that hasn't been updated to use `setOwner` you would only have
    the `container` specified which causes this injection assertion to be
    thrown (since `getOwner(this)` returns undefined).
    
    Specifically, using Ember Data 1.13.<latest> with Ember 2.4.5 and having
    a model with an injected property will trigger an assertion.

--- packages/ember-metal/tests/injected_property_test.js
+++ packages/ember-metal/tests/injected_property_test.js
@@ -22,13 +22,28 @@ QUnit.test('injected properties should be overridable', function() {
   equal(get(obj, 'foo'), 'bar', 'should return the overriden value');
 });
 
-QUnit.test('getting on an object without a container should fail assertion', function() {
+QUnit.test('getting on an object without an owner or container should fail assertion', function() {
   var obj = {};
   defineProperty(obj, 'foo', new InjectedProperty('type', 'name'));
 
   expectAssertion(function() {
     get(obj, 'foo');
   }, /Attempting to lookup an injected property on an object without a container, ensure that the object was instantiated via a container./);
+});
+
+QUnit.test('getting on an object without an owner but with a container should not fail', function() {
+  var obj = {
+    container: {
+      lookup(key) {
+        ok(true, 'should call container.lookup');
+        return key;
+      }
+    }
+  };
+
+  defineProperty(obj, 'foo', new InjectedProperty('type', 'name'));
+
+  equal(get(obj, 'foo'), 'type:name', 'should return the value of container.lookup');
 });
 
 QUnit.test('getting should return a lookup on the container', function() {
