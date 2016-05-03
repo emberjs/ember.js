b33b8a0c38044edcd3bbda2a588f43ef55665d36
Robert Jackson <robert.w.jackson@me.com>
2016-05-03 07:56:56 -0400

    Revert "Deprecate Ember.Handlebars.SafeString in favor of Ember.String.htmlSafe"
    This reverts commit 0e95d7cc1325efa03d19732ebb89034279b5deeb.

--- packages/ember-htmlbars/tests/compat/safe_string_test.js
+++
@@ -1,9 +0,0 @@
-import EmberHandlebars from 'ember-htmlbars/compat';
-
-QUnit.module('ember-htmlbars: compat - SafeString');
-
-QUnit.test('using new results in a deprecation', function(assert) {
-  expectDeprecation(function() {
-    new EmberHandlebars.SafeString('test');
-  }, 'Ember.Handlebars.SafeString is deprecated in favor of Ember.String.htmlSafe');
-});
