38408138c431dad843d6eb0329d5096e1268c7a7
Robert Jackson <robert.w.jackson@me.com>
2016-05-03 07:56:25 -0400

    Revert "Fix deprecation for Ember.Handlebars.SafeString."
    This reverts commit 8c341bc1d3f311cadcddd61d021c1908e75e86e4.
    
    (cherry picked from commit 1e439e88498718e332df834c8da24174e4c4c460)

--- packages/ember-htmlbars/lib/compat.js
+++ packages/ember-htmlbars/lib/compat.js
@@ -1,25 +1,21 @@
-import Ember from 'ember-metal/core';
-import { htmlSafe } from 'ember-htmlbars/utils/string';
-import { deprecate } from 'ember-metal/debug';
+import Ember from 'ember-metal/core'; // for Handlebars export
+import { deprecateFunc } from 'ember-metal/debug';
+
 import {
+  SafeString,
   escapeExpression
 } from 'ember-htmlbars/utils/string';
 
 var EmberHandlebars = Ember.Handlebars = Ember.Handlebars || {};
 
-EmberHandlebars.SafeString = function(value) {
-  deprecate(
-    'Ember.Handlebars.SafeString is deprecated in favor of Ember.String.htmlSafe',
-    false,
-    {
-      id: 'ember-htmlbars.ember-handlebars-safestring',
-      until: '3.0.0',
-      url: 'http://emberjs.com/deprecations/v2.x#toc_use-ember-string-htmlsafe-over-ember-handlebars-safestring'
-    }
-  );
-
-  return htmlSafe(value);
-};
+EmberHandlebars.SafeString = deprecateFunc(
+  'Ember.Handlebars.SafeString is deprecated in favor of Ember.String.htmlSafe',
+  {
+    id: 'ember-htmlbars.ember-handlebars-safestring',
+    until: '3.0.0',
+    url: 'http://emberjs.com/deprecations/v2.x#toc_use-ember-string-htmlsafe-over-ember-handlebars-safestring'
+  },
+  SafeString);
 
 EmberHandlebars.Utils =  {
   escapeExpression: escapeExpression
--- packages/ember-htmlbars/tests/compat/safe_string_test.js
+++ packages/ember-htmlbars/tests/compat/safe_string_test.js
@@ -3,11 +3,7 @@ import EmberHandlebars from 'ember-htmlbars/compat';
 QUnit.module('ember-htmlbars: compat - SafeString');
 
 QUnit.test('using new results in a deprecation', function(assert) {
-  let result;
-
   expectDeprecation(function() {
-    result = new EmberHandlebars.SafeString('<b>test</b>');
+    new EmberHandlebars.SafeString('test');
   }, 'Ember.Handlebars.SafeString is deprecated in favor of Ember.String.htmlSafe');
-
-  assert.equal(result.toHTML(), '<b>test</b>');
 });
