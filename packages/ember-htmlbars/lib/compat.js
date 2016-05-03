fe80d19e48fb47544ec78f8120457d3b50ac0c99
Robert Jackson <robert.w.jackson@me.com>
2016-05-03 07:56:56 -0400

    Revert "Deprecate Ember.Handlebars.SafeString in favor of Ember.String.htmlSafe"
    This reverts commit 0e95d7cc1325efa03d19732ebb89034279b5deeb.
    
    (cherry picked from commit b33b8a0c38044edcd3bbda2a588f43ef55665d36)

--- packages/ember-htmlbars/lib/compat.js
+++ packages/ember-htmlbars/lib/compat.js
@@ -1,6 +1,4 @@
 import Ember from 'ember-metal/core'; // for Handlebars export
-import { deprecateFunc } from 'ember-metal/debug';
-
 import {
   SafeString,
   escapeExpression
@@ -8,15 +6,7 @@ import {
 
 var EmberHandlebars = Ember.Handlebars = Ember.Handlebars || {};
 
-EmberHandlebars.SafeString = deprecateFunc(
-  'Ember.Handlebars.SafeString is deprecated in favor of Ember.String.htmlSafe',
-  {
-    id: 'ember-htmlbars.ember-handlebars-safestring',
-    until: '3.0.0',
-    url: 'http://emberjs.com/deprecations/v2.x#toc_use-ember-string-htmlsafe-over-ember-handlebars-safestring'
-  },
-  SafeString);
-
+EmberHandlebars.SafeString = SafeString;
 EmberHandlebars.Utils =  {
   escapeExpression: escapeExpression
 };
