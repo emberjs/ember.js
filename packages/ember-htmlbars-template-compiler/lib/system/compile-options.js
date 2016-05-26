2f9388040c6e664fe87678a3bfe06f59a19d8a9a
Kris Selden <kris.selden@gmail.com>
2016-05-25 21:34:40 -0700

    remove unused fragmentReason meta that was for the removed angleBracket stuff (#13562)

--- packages/ember-htmlbars-template-compiler/lib/system/compile-options.js
+++ packages/ember-htmlbars-template-compiler/lib/system/compile-options.js
@@ -11,7 +11,6 @@ import TransformComponentCurlyToReadonly from '../plugins/transform-component-cu
 import TransformOldClassBindingSyntax from '../plugins/transform-old-class-binding-syntax';
 
 let compileOptions;
-let fragmentReason;
 
 export let PLUGINS = [
   TransformClosureComponentAttrsIntoMut,
@@ -81,7 +80,6 @@ compileOptions = function(_options) {
 
   options.buildMeta = function buildMeta(program) {
     return {
-      fragmentReason: fragmentReason(program),
       revision: 'Ember@' + VERSION,
       loc: program.loc,
       moduleName: options.moduleName
@@ -90,52 +88,5 @@ compileOptions = function(_options) {
 
   return options;
 };
-
-fragmentReason = function(program) {
-  let { loc, body } = program;
-  if (!loc || loc.start.line !== 1 || loc.start.column !== 0) { return false; }
-
-  let candidate;
-  let nodeCount = 0;
-
-  let problems = {};
-
-  for (let i = 0; i < body.length; i++) {
-    let curr = body[i];
-
-    // text node with whitespace only
-    if (curr.type === 'TextNode' && /^[\s]*$/.test(curr.chars)) { continue; }
-
-    // has multiple root elements if we've been here before
-    if (nodeCount++ > 0) { problems['multiple-nodes'] = true; }
-
-    if (curr.type === 'ComponentNode' || curr.type === 'ElementNode') {
-      candidate = curr;
-    } else {
-      problems['wrong-type'] = true;
-    }
-  }
-
-  if (nodeCount === 0) {
-    return { name: 'missing-wrapper', problems: ['empty-body'] };
-  }
-
-  let problemList = Object.keys(problems);
-  if (problemList.length) {
-    return { name: 'missing-wrapper', problems: problemList };
-  }
-
-  if (candidate.type === 'ComponentNode') {
-    return false;
-  } else if (candidate.modifiers.length) {
-    return { name: 'modifiers', modifiers: candidate.modifiers.map(m => m.path.original) };
-  } else if (candidate.attributes.some(attr => !attr.value.escaped)) {
-    return { name: 'triple-curlies' };
-  } else {
-    return false;
-  }
-};
-
-
 
 export default compileOptions;
