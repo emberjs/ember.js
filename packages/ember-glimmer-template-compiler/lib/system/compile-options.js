a11b9c207ddd81b785b328ee72b1d0e19f73cba9
Joel Kang <jkang@linkedin.com>
2016-05-26 09:58:22 -0700

    Move the transform to the new location

--- packages/ember-glimmer-template-compiler/lib/system/compile-options.js
+++ packages/ember-glimmer-template-compiler/lib/system/compile-options.js
@@ -1,10 +1,12 @@
 import TransformHasBlockSyntax from '../plugins/transform-has-block-syntax';
 import TransformActionSyntax from '../plugins/transform-action-syntax';
+import TransformEachInIntoEach from '../plugins/transform-each-in-into-each';
 import assign from 'ember-metal/assign';
 
 export const PLUGINS = [
   TransformHasBlockSyntax,
-  TransformActionSyntax
+  TransformActionSyntax,
+  TransformEachInIntoEach
 ];
 
 let USER_PLUGINS = [];
