348185618dc49e102b8ad2a86a6cd8e6915fee5e
Trent Willis <trentmwillis@gmail.com>
2016-06-04 16:56:04 -0700

    [BUGFIX beta] Ensure pauseTest runs after other async helpers

--- packages/ember-testing/lib/helpers/pause_test.js
+++ packages/ember-testing/lib/helpers/pause_test.js
@@ -1,7 +1,5 @@
 import RSVP from 'ember-runtime/ext/rsvp';
-import Test from '../test';
 
 export default function pauseTest() {
-  Test.adapter.asyncStart();
   return new RSVP.Promise(function() { }, 'TestAdapter paused promise');
 }
