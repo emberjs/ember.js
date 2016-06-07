ee1e5ba0cf660d8d1426fb24f912d19f44cf0bcd
David J. Hamilton <dhamilton@linkedin.com>
2016-06-07 13:00:27 -0700

    [Glimmer2] Remove unnecessary test
    This test appears to have been intended to ensure that glimmer compiled
    templates in a way compatible with HTMLBars.  Presumably it worked initially but
    now the output of `compile` produces an object without a useful `toString`.

--- packages/ember-template-compiler/tests/system/compile_test.js
+++ packages/ember-template-compiler/tests/system/compile_test.js
@@ -8,15 +8,6 @@ import { test, testModule } from 'ember-glimmer/tests/utils/skip-if-glimmer';
 
 testModule('ember-template-compiler: compile');
 
-test('compiles the provided template with htmlbars', function() {
-  var templateString = '{{foo}} -- {{some-bar blah=\'foo\'}}';
-
-  var actual = compile(templateString);
-  var expected = htmlbarsCompile(templateString);
-
-  equal(actual.toString(), expected.toString(), 'compile function matches content with htmlbars compile');
-});
-
 test('includes the current revision in the compiled template', function() {
   var templateString = '{{foo}} -- {{some-bar blah=\'foo\'}}';
 
