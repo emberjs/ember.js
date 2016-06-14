9c79789e35b4072090320e046dde8eba7dfc6efc
Ricardo Mendes <rokusu@gmail.com>
2016-06-14 13:15:42 +0100

    [DOC release] Fixes `Ember.computed.uniqBy`'s code sample

--- packages/ember-runtime/lib/computed/reduce_computed_macros.js
+++ packages/ember-runtime/lib/computed/reduce_computed_macros.js
@@ -379,7 +379,9 @@ export function uniq(...args) {
 /**
   A computed property which returns a new array with all the unique
   elements from an array, with uniqueness determined by specific key.
+  
   Example
+  
   ```javascript
   let Hamster = Ember.Object.extend({
     uniqueFruits: Ember.computed.uniqBy('fruits', 'id')
@@ -394,6 +396,7 @@ export function uniq(...args) {
   });
   hamster.get('uniqueFruits'); // [ { id: 1, 'banana' }, { id: 2, 'grape' }, { id: 3, 'peach' }]
   ```
+  
   @method uniqBy
   @for Ember.computed
   @param {String} dependentKey
