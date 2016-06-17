b50256e2604ca9820505cf2421e7cf16cd5ce4f0
Matt Gardner <wmattgardner@gmail.com>
2016-06-16 22:37:42 -0400

    [BUGFIX beta] Adding a failing test to start to address #13686

--- packages/ember/tests/routing/query_params_test.js
+++ packages/ember/tests/routing/query_params_test.js
@@ -3222,3 +3222,33 @@ QUnit.test('handle routes names that clash with Object.prototype properties', fu
   let controller = container.lookup('controller:constructor');
   equal(get(controller, 'foo'), '999');
 });
+
+QUnit.test('boolean toggle of null controller query parameter yields logical values', function() {
+  expect(3);
+
+  App.register('template:application', compile(
+    '<button id="flag" {{action "flag"}}>Flag</button> ' +
+    '{{outlet}}'
+  ));
+
+  App.ApplicationController = Controller.extend({
+    queryParams: ['nullProperty'],
+    nullProperty: null,
+    actions: {
+      flag() {
+        this.toggleProperty('nullProperty')
+      }
+    }
+  });
+
+  bootApplication();
+
+  let controller = container.lookup('controller:application');
+
+  run(jQuery('#flag'), 'click');
+  equal(controller.get('nullProperty'), true);
+  run(jQuery('#flag'), 'click');
+  equal(controller.get('nullProperty'), false);
+  run(jQuery('#flag'), 'click');
+  equal(controller.get('nullProperty'), true);
+});
