0dedcce0a7f79d1968445c17dc962eda9cc31999
Igor Kvasnička <igor.kvasnicka@annotation.sk>
2016-06-23 07:12:44 +0200

    fixed code style

--- packages/ember-htmlbars/lib/morphs/attr-morph.js
+++ packages/ember-htmlbars/lib/morphs/attr-morph.js
@@ -32,9 +32,9 @@ function deprecateEscapedStyle(morph, value) {
 }
 
 export function createStyleWarning(value) {
-  return 'Binding style attributes may introduce cross-site scripting vulnerabilities;' +
-  'please ensure that values being bound are properly escaped. For more information,' +
-  'including how to disable this warning, see' +
+  return 'Binding style attributes may introduce cross-site scripting vulnerabilities; ' +
+  'please ensure that values being bound are properly escaped. For more information, ' +
+  'including how to disable this warning, see ' +
   'http://emberjs.com/deprecations/v1.x/#toc_binding-style-attributes. Affected style: "' + value + '"';
 }
 
