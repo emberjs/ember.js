acfe07ab5d09bbfa8aeaab793f2ffec6566f4f60
Gavin Joyce <gavinjoyce@gmail.com>
2016-07-12 16:18:50 +0100

    extract benchmarks into glimmer-benchmarks package

--- packages/glimmer-demos/index.ts
+++ packages/glimmer-demos/index.ts
@@ -3,15 +3,9 @@ import * as dbmon from "./lib/dbmon";
 import * as conways from "./lib/conways";
 import * as ripples from "./lib/ripples";
 import * as visualizer from "./lib/visualizer";
-import * as stats from "./lib/stats";
-import * as benchmark from "./lib/bench-init";
-import suites from './lib/bench-suites';
 
 export const UptimeDemo = uptime;
 export const Dbmon = dbmon;
 export const RipplesDemo = ripples;
 export const ConwaysDemo = conways;
 export const Visualizer = visualizer;
-export const Benchmark = benchmark;
-export const Stats = stats; // TODO does this belong in demos?
-export const Suites = suites;
