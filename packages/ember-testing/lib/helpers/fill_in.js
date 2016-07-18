e4cb9f48b8af0f60af20f78dbd268a6c980d79a4
Chad Hietala <chadhietala@gmail.com>
2016-07-18 11:01:42 -0700

    [Glimmer2] Fix some tests related input

--- packages/ember-testing/lib/helpers/fill_in.js
+++ packages/ember-testing/lib/helpers/fill_in.js
@@ -1,4 +1,3 @@
-import run from 'ember-metal/run_loop';
 import { focus, fireEvent } from '../events';
 
 export default function fillIn(app, selector, contextOrText, text) {
@@ -11,10 +10,10 @@ export default function fillIn(app, selector, contextOrText, text) {
   $el = app.testHelpers.findWithAssert(selector, context);
   el = $el[0];
   focus(el);
-  run(() => {
-    $el.val(text);
-    fireEvent(el, 'input');
-    fireEvent(el, 'change');
-  });
+
+  $el.val(text);
+  fireEvent(el, 'input');
+  fireEvent(el, 'change');
+
   return app.testHelpers.wait();
 }
