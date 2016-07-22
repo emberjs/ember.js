0a56e68653cd9751eb6437f9a7ee5d86a8df644d
Robert Jackson <rwjblue@twitch.tv>
2016-07-22 10:01:20 -0400

    Fix bootstrapping of script tag templates.
    There were two main issues with this:
    
    * When the `ember-template-compiler/system/bootstrap` module was
      present, the initializers `initialize` function was being set to an
      object (not a function). The fix is "simple", grab the `.default`
      export from the module.
    * Depending on timing, the
      `ember-application/initializers/dom-templates` module is evaluated
      before the second script tag including `ember-template-compiler.js`,
      which means that the `bootstrap` function was never updated (since it
      was only checking for the boostrap module at eval time). The fix for
      this is to lazily check for the module during the initializer itself.

--- packages/ember-application/lib/initializers/dom-templates.js
+++ packages/ember-application/lib/initializers/dom-templates.js
@@ -4,13 +4,14 @@ import Application from '../system/application';
 
 let bootstrap = function() { };
 
-let bootstrapModuleId = 'ember-template-compiler/system/bootstrap';
-if (environment.hasDOM && has(bootstrapModuleId)) {
-  bootstrap = require(bootstrapModuleId);
-}
-
-
 Application.initializer({
   name: 'domTemplates',
-  initialize: bootstrap
+  initialize() {
+    let bootstrapModuleId = 'ember-template-compiler/system/bootstrap';
+    if (environment.hasDOM && has(bootstrapModuleId)) {
+      bootstrap = require(bootstrapModuleId).default;
+    }
+
+    bootstrap();
+  }
 });
