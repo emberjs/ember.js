fffeed1662028c3fcd611f6c9f94abeb7eb88f1e
Purvi Tiwari <purvitiwari@users.noreply.github.com>
2016-07-24 14:55:22 +0530

    [DOC Canary]
    Modified comments #13862 (+4 squashed commits)
    Squashed commits:
    [d717b27] [DOC Canary]
    
    Modified comments #13862
    [c6cca4b] [DOC Canary]
    
    Modified comments #13862
    [d64458c] [DOC Canary]
    
    Modified comments #13862
    [9a95acb]  [DOC Canary]
    
    Documentation added according to #13862 (+1 squashed commit)
    Squashed commits:
    [8a89468] [DOC Canary]
    
    Documentation added according to #13862

--- packages/ember-testing/lib/helpers/find_with_assert.js
+++ packages/ember-testing/lib/helpers/find_with_assert.js
@@ -1,3 +1,20 @@
+/**
+@module ember
+@submodule ember-testing
+*/
+/**
+  Like `find`, but throws an error if the element selector returns no results.
+  Example:
+  ```javascript
+  var $el = findWithAssert('.doesnt-exist'); // throws error
+  ```
+  @method findWithAssert
+  @param {String} selector jQuery selector string for finding an element within
+  the DOM
+  @return {Object} jQuery object representing the results of the query
+  @throws {Error} throws error if jQuery object returned has a length of 0
+  @public
+*/
 export default function findWithAssert(app, selector, context) {
   let $el = app.testHelpers.find(selector, context);
   if ($el.length === 0) {
