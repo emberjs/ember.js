ca438f595e40b90eecc18c4607eb851c7e5f24dd
Chad Hietala <chadhietala@gmail.com>
2016-08-09 12:15:36 -0700

    Refactor js compiler to be typed

--- packages/glimmer-wire-format/index.ts
+++ packages/glimmer-wire-format/index.ts
@@ -36,17 +36,14 @@ export namespace Expressions {
   type Params = Core.Params;
   type Hash = Core.Hash;
 
-  type UndefinedStringLiteral = 'undefined';
-  type UndefinedLiteral = ['undefinedLiteral', 'undefined'];
-
   export type Unknown        = ['unknown', Path];
   export type Arg            = ['arg', Path];
   export type Get            = ['get', Path];
   export type SelfGet        = ['self-get', Path];
   export type Value          = str | number | boolean | null; // tslint:disable-line
-  export type HasBlock       = ['hasBlock', str];
-  export type HasBlockParams = ['hasBlockParams', str];
-  export type Undefined      = UndefinedStringLiteral | UndefinedLiteral;
+  export type HasBlock       = ['has-block', str];
+  export type HasBlockParams = ['has-block-params', str];
+  export type Undefined      = ['undefined'];
 
   export type Expression =
       Unknown
@@ -79,16 +76,9 @@ export namespace Expressions {
   export const isSelfGet        = is<SelfGet>('self-get');
   export const isConcat         = is<Concat>('concat');
   export const isHelper         = is<Helper>('helper');
-  export const isHasBlock       = is<HasBlock>('hasBlock');
-  export const isHasBlockParams = is<HasBlockParams>('hasBlockParams');
-
-  export function isUndefined(value: any): value is Undefined {
-    if (typeof value === 'object' && value !== null && value[0] === 'undefinedLiteral') {
-      return true;
-    }
-
-    return false;
-  }
+  export const isHasBlock       = is<HasBlock>('has-block');
+  export const isHasBlockParams = is<HasBlockParams>('has-block-params');
+  export const isUndefined      = is<Undefined>('undefined');
 
   export function isPrimitiveValue(value: any): value is Value {
     if (value === null) {
@@ -111,28 +101,28 @@ export namespace Statements {
   export type Comment       = ['comment', str];
   export type Modifier      = ['modifier', Path, Params, Hash];
   export type Block         = ['block', Path, Params, Hash, TemplateReference, TemplateReference];
-  export type OpenElement   = ['openElement', str, str[]];
-  export type CloseElement  = ['closeElement'];
-  export type StaticAttr    = ['staticAttr', str, Expression, str];
-  export type DynamicAttr   = ['dynamicAttr', str, Expression, str];
+  export type OpenElement   = ['open-element', str, str[]];
+  export type CloseElement  = ['close-element'];
+  export type StaticAttr    = ['static-attr', str, Expression, str];
+  export type DynamicAttr   = ['dynamic-attr', str, Expression, str];
   export type Yield         = ['yield', YieldTo, Params];
-  export type DynamicArg    = ['dynamicArg', str, Expression];
-  export type StaticArg     = ['staticArg', str, Expression];
-  export type TrustingAttr  = ['trustingAttr', str, Expression, str];
+  export type DynamicArg    = ['dynamic-arg', str, Expression];
+  export type StaticArg     = ['static-arg', str, Expression];
+  export type TrustingAttr  = ['trusting-attr', str, Expression, str];
 
   export const isText         = is<Text>('text');
   export const isAppend       = is<Append>('append');
   export const isComment      = is<Comment>('comment');
   export const isModifier     = is<Modifier>('modifier');
   export const isBlock        = is<Block>('block');
-  export const isOpenElement  = is<OpenElement>('openElement');
-  export const isCloseElement = is<CloseElement>('closeElement');
-  export const isStaticAttr   = is<StaticAttr>('staticAttr');
-  export const isDynamicAttr  = is<DynamicAttr>('dynamicAttr');
+  export const isOpenElement  = is<OpenElement>('open-element');
+  export const isCloseElement = is<CloseElement>('close-element');
+  export const isStaticAttr   = is<StaticAttr>('static-attr');
+  export const isDynamicAttr  = is<DynamicAttr>('dynamic-attr');
   export const isYield        = is<Yield>('yield');
-  export const isDynamicArg   = is<DynamicArg>('dynamicArg');
-  export const isStaticArg    = is<StaticArg>('staticArg');
-  export const isTrustingAttr = is<TrustingAttr>('trustingAttr');
+  export const isDynamicArg   = is<DynamicArg>('dynamic-arg');
+  export const isStaticArg    = is<StaticArg>('static-arg');
+  export const isTrustingAttr = is<TrustingAttr>('trusting-attr');
 
   export type Statement =
       Text
