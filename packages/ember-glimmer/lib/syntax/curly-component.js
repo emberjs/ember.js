941e400f902fb178b4c8e70a66f27b9f5e9f9c6a
Robert Jackson <rwjblue@twitch.tv>
2016-08-27 15:59:07 -0400

    [BUGFIX beta] Ensure injections happen in engine instances.
    Prior to this `_environment` could not be relied upon in routes or
    views (which is used when `shouldRender: false` is used with `visit`).
    
    The refactor uses the environment to determine what injections to setup
    in the engine-instance, and has the `application-instance` defer to the
    `engine-instance` to avoid duplication.
    
    (cherry picked from commit 3de2360772f1f542c06f46e62d096b269e77361b)

--- packages/ember-glimmer/lib/syntax/curly-component.js
+++ packages/ember-glimmer/lib/syntax/curly-component.js
@@ -72,7 +72,6 @@ class CurlyComponentManager {
     aliasIdToElementId(args, props);
 
     props.parentView = parentView;
-    props.renderer = parentView.renderer;
     props[HAS_BLOCK] = hasBlock;
 
     // dynamicScope here is inherited from the parent dynamicScope,
