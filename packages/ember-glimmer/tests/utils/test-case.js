dfba3d8c5ee3434c0f70afbb83944c67b43774e5
Godfrey Chan <godfreykfc@gmail.com>
2016-08-31 12:00:48 -0700

    Remove unused [GLIMMER] flag

--- packages/ember-glimmer/tests/utils/test-case.js
+++ packages/ember-glimmer/tests/utils/test-case.js
@@ -3,14 +3,9 @@ import {
   AbstractApplicationTest,
   AbstractRenderingTest
 } from './abstract-test-case';
-import assign from 'ember-metal/assign';
-import { GLIMMER } from 'ember-application/system/engine';
 import ComponentLookup from 'ember-views/component_lookup';
 
 export class ApplicationTest extends AbstractApplicationTest {
-  get applicationOptions() {
-    return assign(super.applicationOptions, { [GLIMMER]: true });
-  }
 }
 
 export class RenderingTest extends AbstractRenderingTest {
