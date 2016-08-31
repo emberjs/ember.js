a9aa06d3d7003f2105f874471e13739456432c89
Robert Jackson <rwjblue@twitch.tv>
2016-08-30 21:47:19 -0400

    Remove ember-htmlbars-template-compiler package.

--- packages/ember-htmlbars-template-compiler/lib/index.js
+++
@@ -1,4 +0,0 @@
-export { default as compile } from './system/compile';
-export { default as precompile } from './system/precompile';
-export { default as defaultCompileOptions } from './system/compile-options';
-export { registerPlugin } from './system/compile-options';
