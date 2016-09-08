1a4ebc02226240a2f296fae694c10400f9839fab
Robert Jackson <rwjblue@twitch.tv>
2016-09-08 09:35:57 -0400

    Fix `assign` utility to skip `null` / `undefined`.
    `Object.assign` ignores `null` / `undefined` arguments, but our internal
    `assign` does not (prior to these changes it errored if it received
    `null` / `undefined`).

---
+++ packages/glimmer-util/tests/object-utils-test.ts
@@ -0,0 +1,9 @@
+import { assign } from '../lib/object-utils';
+
+QUnit.module('object-utils tests');
+
+QUnit.test('assign should ignore null/undefined arguments', function(assert) {
+  let result = assign({}, { foo: 'bar' }, null, undefined, { derp: "herk" });
+
+  assert.deepEqual(result, { foo: 'bar', derp: 'herk' }, 'has correct result');
+});
