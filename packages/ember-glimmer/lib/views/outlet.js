d17f88368882eced47463fa8919a6aa23f3e9082
Robert Jackson <rwjblue@twitch.tv>
2016-09-19 10:32:43 -0700

    [BUGFIX beta] Use custom root template for top level outlet.
    (cherry picked from commit 2c77c30d818cd48f3d51f3a67c762916c84dd633)

--- packages/ember-glimmer/lib/views/outlet.js
+++ packages/ember-glimmer/lib/views/outlet.js
@@ -1,6 +1,7 @@
 import { assign, EmptyObject } from 'ember-utils';
 import { DirtyableTag } from 'glimmer-reference';
 import { environment } from 'ember-environment';
+import { OWNER } from 'ember-utils';
 
 /**
 @module ember
@@ -19,10 +20,6 @@ class OutletStateReference {
 
   value() {
     return this.outletView.outletState;
-  }
-
-  get isTopLevel() {
-    return true;
   }
 
   getOrphan(name) {
@@ -47,7 +44,7 @@ class OrphanedOutletStateReference extends OutletStateReference {
   value() {
     let rootState = this.root.value();
 
-    let orphans = rootState.outlets.__ember_orphans__;
+    let orphans = rootState.outlets.main.outlets.__ember_orphans__;
 
     if (!orphans) {
       return null;
@@ -80,10 +77,6 @@ class ChildOutletStateReference {
   value() {
     return this.parent.value()[this.key];
   }
-
-  get isTopLevel() {
-    return false;
-  }
 }
 
 export default class OutletView {
@@ -103,13 +96,16 @@ export default class OutletView {
     assign(this, injections);
   }
 
-  static create({ _environment, renderer, template }) {
-    return new OutletView(_environment, renderer, template);
+  static create(options) {
+    let { _environment, renderer, template } = options;
+    let owner = options[OWNER];
+    return new OutletView(_environment, renderer, owner, template);
   }
 
-  constructor(_environment, renderer, template) {
+  constructor(_environment, renderer, owner, template) {
     this._environment = _environment;
     this.renderer = renderer;
+    this.owner = owner;
     this.template = template;
     this.outletState = null;
     this._renderResult = null;
@@ -134,7 +130,20 @@ export default class OutletView {
   }
 
   setOutletState(state) {
-    this.outletState = state;
+    this.outletState = {
+      outlets: {
+        main: state
+      },
+      render: {
+        owner: undefined,
+        into: undefined,
+        outlet: 'main',
+        name: '-top-level',
+        controller: undefined,
+        ViewClass: undefined,
+        template: undefined
+      }
+    };
     this._tag.dirty();
   }
 
