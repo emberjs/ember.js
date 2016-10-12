0de4a96698426e9ee586b6ce994479c1b8bdfb2a
Trent Willis <twillis@linkedin.com>
2016-10-12 11:26:45 -0700

    [CLEANUP] Remove qp.prefix and use qp.controllerName
    Prefix was only ever set to the ctrl property, so removing that helps
    simplify the code. Renaming ctrl to controllerName for added clarity.

--- packages/ember-routing/lib/system/router.js
+++ packages/ember-routing/lib/system/router.js
@@ -824,7 +824,7 @@ const EmberRouter = EmberObject.extend(Evented, {
             delete queryParams[presentProp];
           }
         } else {
-          let cacheKey = calculateCacheKey(qp.ctrl, qp.parts, state.params);
+          let cacheKey = calculateCacheKey(qp.controllerName, qp.parts, state.params);
           queryParams[qp.scopedPropertyName] = appCache.lookup(cacheKey, qp.prop, qp.defaultValue);
         }
       }
