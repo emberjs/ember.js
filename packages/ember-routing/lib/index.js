70df216be2896d57884a8daeeb44ea512625f864
Ricardo Mendes <rokusu@gmail.com>
2016-10-27 03:45:16 +0100

    fix

--- packages/ember-routing/lib/index.js
+++ packages/ember-routing/lib/index.js
@@ -7,35 +7,11 @@
 import './ext/run_loop';
 import './ext/controller';
 
-import EmberLocation from './location/api';
-import NoneLocation from './location/none_location';
-import HashLocation from './location/hash_location';
-import HistoryLocation from './location/history_location';
-import AutoLocation from './location/auto_location';
-
-import generateController from './system/generate_controller';
-import {
-  generateControllerFactory
-} from './system/generate_controller';
-import controllerFor from './system/controller_for';
-import RouterDSL from './system/dsl';
-import Router from './system/router';
-import RouterService from './services/router';
-import Route from './system/route';
-
-Ember.Location = EmberLocation;
-Ember.AutoLocation = AutoLocation;
-Ember.HashLocation = HashLocation;
-Ember.HistoryLocation = HistoryLocation;
-Ember.NoneLocation = NoneLocation;
-
-Ember.controllerFor = controllerFor;
-Ember.generateControllerFactory = generateControllerFactory;
-Ember.generateController = generateController;
-Ember.RouterDSL = RouterDSL;
-Ember.Router = RouterService;
-Ember.Route = Route;
->>>>>>> Assign RouterService to Ember.Router instead of internal Router
+export { default as Location } from './location/api';
+export { default as NoneLocation } from './location/none_location';
+export { default as HashLocation } from './location/hash_location';
+export { default as HistoryLocation } from './location/history_location';
+export { default as AutoLocation } from './location/auto_location';
 
 export {
   default as generateController,
