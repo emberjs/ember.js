5b8fb11bcdeffea4455f7217828e66c02c04c87a
simon <ihmig@kaliber5.de>
2016-11-27 02:29:18 +0100

    Add a failing test for #14642
    (cherry picked from commit 8ec69ba1cb086faa21d5d66fd09aa0f386c1c943)

--- packages/ember-glimmer/tests/integration/components/curly-components-test.js
+++ packages/ember-glimmer/tests/integration/components/curly-components-test.js
@@ -595,6 +595,20 @@ moduleFor('Components test: curly components', class extends RenderingTest {
     this.assertComponentElement(this.firstChild, { content: 'yielded: [hello] - In component' });
   }
 
+  ['@test it can render a basic component with a block param when the yield is in a partial']() {
+    this.registerPartial('_partialWithYield', 'yielded: [{{yield "hello"}}]');
+
+    this.registerComponent('foo-bar', { template: '{{partial "partialWithYield"}} - In component' });
+
+    this.render('{{#foo-bar as |value|}}{{value}}{{/foo-bar}}');
+
+    this.assertComponentElement(this.firstChild, { content: 'yielded: [hello] - In component' });
+
+    this.runTask(() => this.rerender());
+
+    this.assertComponentElement(this.firstChild, { content: 'yielded: [hello] - In component' });
+  }
+
   ['@test it renders the layout with the component instance as the context']() {
     let instance;
 
