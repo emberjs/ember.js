d35ef2c434f2a23a7343f26091b9dfed0c4888ad
Chad Hietala <chadhietala@gmail.com>
2017-01-11 15:31:08 -0800

    Fix node tests due to hashing of template leads to new short-SHA

--- packages/@glimmer/node/tests/node-test.ts
+++ packages/@glimmer/node/tests/node-test.ts
@@ -274,8 +274,8 @@ module('default template id');
 QUnit.test('generates id in node', function (assert) {
   let template = precompile('hello');
   let obj = JSON.parse(template);
-  assert.equal(obj.id, 'tjhOuBK+', 'short sha of template source');
+  assert.equal(obj.id, 'ZDHnvwb1', 'short sha of template source');
   template = precompile('hello', { meta: {moduleName: 'template/hello'} });
   obj = JSON.parse(template);
-  assert.equal(obj.id, '/0QJKO+y', 'short sha of template source and meta');
+  assert.equal(obj.id, 'mQsxG3uR', 'short sha of template source and meta');
 });
