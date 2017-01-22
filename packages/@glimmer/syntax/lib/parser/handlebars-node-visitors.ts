9f0a905cbc304991d92a6eda5bc80ecbafcfb1d7
Ricardo Mendes <rokusu@gmail.com>
2017-01-22 16:26:23 +0000

    Throws error for decorator and decorator block

--- packages/@glimmer/syntax/lib/parser/handlebars-node-visitors.ts
+++ packages/@glimmer/syntax/lib/parser/handlebars-node-visitors.ts
@@ -146,6 +146,21 @@ export default {
     throw new Error(`Handlebars partial blocks are not supported: "{{#> ${name.original}" at L${loc.start.line}:C${loc.start.column}`);
   },
 
+  Decorator: function(decorator) {
+    let { loc, path } = decorator;
+
+    let source = this.sourceForMustache(decorator);
+
+    throw new Error(`Handlebars decorators are not supported: "{{* ${path.original}" at L${loc.start.line}:C${loc.start.column}`);
+  },
+
+  DecoratorBlock: function(decoratorBlock) {
+    let { loc, path } = decoratorBlock;
+    let source = this.sourceForMustache(decoratorBlock);
+
+    throw new Error(`Handlebars decorator blocks are not supported: "{{#* ${path.original}" at L${loc.start.line}:C${loc.start.column}`);
+  },
+
   SubExpression: function(sexpr) {
     return acceptCommonNodes(this, sexpr);
   },
