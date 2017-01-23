540ec5e7f6c69085e3a5877dd7bf748d9c880285
Robert Jackson <rjackson@linkedin.com>
2017-01-23 10:05:08 -0500

    Use eslint plugin for custom eslint rules.
    When emberjs-build is linked, the eslint resolution rules do not work
    properly so we must fallback to manually providing the rules paths (as
    we did before).
    
    When emberjs-build is not linked, this follows the standard/simple eslint
    pattern (and avoids requiring editor specific hacks to get linting working).

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,23 @@
-module.exports = {
+var fs = require('fs');
+var path = require('path');
+
+function isEmberJSBuildLinked() {
+  var emberjsBuildPath = path.dirname(require.resolve('emberjs-build'));
+  var emberjsBuildLinked = emberjsBuildPath.indexOf(__dirname + '/node_modules') === -1;
+
+  return emberjsBuildLinked;
+}
+
+var options = {
   root: true,
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module',
   },
   extends: 'eslint:recommended',
+  plugins: [
+    "ember-internal"
+  ],
   env: {
     qunit: true,
   },
@@ -29,11 +42,26 @@ module.exports = {
     'WeakMap': true,
   },
   rules: {
-    'require-yuidoc-access': 'error',
-    'no-const-outside-module-scope': 'error',
+    'ember-internal/require-yuidoc-access': 'error',
+    'ember-internal/no-const-outside-module-scope': 'error',
 
     // temporarily disabled
     'no-unused-vars': 'off',
     'comma-dangle': 'off',
   },
 };
+
+if (isEmberJSBuildLinked()) {
+  delete options.plugins;
+
+  for (var ruleName in options.rules) {
+    var ruleParts = ruleName.split('ember-internal/');
+
+    if (ruleParts.length > 1) {
+      options.rules[ruleParts[1]] = options.rules[ruleName];
+      delete options.rules[ruleName];
+    }
+  }
+}
+
+module.exports = options;
