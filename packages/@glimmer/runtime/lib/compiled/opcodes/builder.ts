fhrbek/glimmer

Initial pass at dynamic invocation

packages/glimmer-runtime/lib/compiled/opcodes/builder.ts
@@ -211,12 +211,20 @@ export abstract class BasicOpcodeBuilder implements SymbolLookup {
     this.opcode(Op.BeginComponentTransaction);
   }
 
+  commitComponentTransaction() {
+    this.opcode(Op.CommitComponentTransaction);
+  }
+
   pushComponentOperations() {
     this.opcode(Op.PushComponentOperations);
   }
 
   putDynamicComponentDefinition() {
     this.opcode(Op.PushDynamicComponent);
+  }
+
+  getComponentSelf(state: number) {
+    this.opcode(Op.GetComponentSelf, state);
   }
 
   getComponentLayout(state: number ) {
@@ -644,8 +652,8 @@ export abstract class BasicOpcodeBuilder implements SymbolLookup {
     this.opcode(Op.Exit);
   }
 
-  invokeVirtual(): void {
-    this.opcode(Op.InvokeVirtual);
+  invokeDynamic(invoker: vm.LayoutInvoker): void {
+    this.opcode(Op.InvokeDynamic, this.other(invoker));
   }
 
   invokeStatic(_block: InlineBlock, args: Option<AppendOpcode[]>): void;

emberjs/ember.js

packages/@glimmer/runtime/lib/compiled/opcodes/builder.ts
@@ -194,12 +194,20 @@ export abstract class BasicOpcodeBuilder implements SymbolLookup {
     this.push(Op.BeginComponentTransaction);
   }
 
+  commitComponentTransaction() {
+    this.push(Op.CommitComponentTransaction);
+  }
+
   pushComponentOperations() {
     this.push(Op.PushComponentOperations);
   }
 
   putDynamicComponentDefinition() {
     this.push(Op.PushDynamicComponent);
+  }
+
+  getComponentSelf(state: number) {
+    this.push(Op.GetComponentSelf, state);
   }
 
   getComponentLayout(state: number ) {
@@ -423,12 +431,8 @@ export abstract class BasicOpcodeBuilder implements SymbolLookup {
     this.push(Op.RootScope, symbols, <any>bindCallerScope|0);
   }
 
-  pushVirtualRootScope(bindCallerScope: boolean) {
-    this.push(Op.VirtualRootScope, <any>bindCallerScope|0);
-  }
-
   pushChildScope() {
-    this.push(Op.PushChildScope);
+    this.push(Op.ChildScope);
   }
 
   popScope() {
@@ -555,8 +559,8 @@ export abstract class BasicOpcodeBuilder implements SymbolLookup {
     this.push(Op.Exit);
   }
 
-  invokeVirtual(): void {
-    this.push(Op.InvokeVirtual);
+  invokeDynamic(invoker: vm.LayoutInvoker): void {
+    this.push(Op.InvokeDynamic, this.other(invoker));
   }
 
   // invokeStatic(_block: InlineBlock, args: Option<AppendOpcode[]>): void;