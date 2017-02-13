7b825a4fd06f7339a513698485feb5c3409fef61
Godfrey Chan <godfreykfc@gmail.com>
2017-02-13 13:47:26 -0800

    Convert `TemplateVisitor` to a class, add types
    Closes #329

--- packages/@glimmer/syntax/index.ts
+++ packages/@glimmer/syntax/index.ts
@@ -6,3 +6,7 @@ export { default as builders } from './lib/builders';
 export { default as traverse } from './lib/traversal/traverse';
 export { default as Walker } from './lib/traversal/walker';
 export { default as print } from './lib/generation/print';
+
+// AST
+import * as AST from './lib/types/nodes';
+export { AST };
