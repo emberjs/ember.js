81ced8c41a9fd32b6cf1f447e3ab1848fc6852eb
Kelly Selden <kellyselden@gmail.com>
2017-03-04 21:30:04 -0800

    observe typo (cherry picked from commit 1d0c4653abc28fdf6fd31356e1eb7556c933506e)

--- packages/ember-runtime/lib/mixins/observable.js
+++ packages/ember-runtime/lib/mixins/observable.js
@@ -373,9 +373,9 @@ export default Mixin.create({
     any of these values, to write an observer that has no parameters at all.
 
     @method addObserver
-    @param {String} key The key to observer
+    @param {String} key The key to observe
     @param {Object} target The target object to invoke
-    @param {String|Function} method The method to invoke.
+    @param {String|Function} method The method to invoke
     @public
   */
   addObserver(key, target, method) {
@@ -388,9 +388,9 @@ export default Mixin.create({
     target will no longer receive notifications.
 
     @method removeObserver
-    @param {String} key The key to observer
+    @param {String} key The key to observe
     @param {Object} target The target object to invoke
-    @param {String|Function} method The method to invoke.
+    @param {String|Function} method The method to invoke
     @public
   */
   removeObserver(key, target, method) {
