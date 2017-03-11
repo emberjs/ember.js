7a4d25ab87194f9e7cdd6f5a4e7d3fd198d004b8
Ryosuke Nakayama <catto.je1mji@gmail.com>
2017-03-12 02:28:50 +0900

    Fix typo

--- packages/ember-routing/lib/system/route.js
+++ packages/ember-routing/lib/system/route.js
@@ -1701,7 +1701,7 @@ let Route = EmberObject.extend(ActionHandler, Evented, {
     `_super`:
 
     ```app/routes/photos.js
-    import Ember from 'ebmer';
+    import Ember from 'ember';
 
     export default Ember.Route.extend({
       model() {
