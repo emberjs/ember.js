f4287e4ba14ba550f642c4fa239845be47f8db6a
Gabor Babicz <gabor.babicz@gmail.com>
2017-04-05 15:07:56 +0200

    Update repository URLs

--- packages/glimmer/index.ts
+++ packages/glimmer/index.ts
@@ -2,7 +2,7 @@
  * @overview  Glimmer
  * @copyright Copyright 2011-2015 Tilde Inc. and contributors
  * @license   Licensed under MIT license
- *            See https://raw.githubusercontent.com/tildeio/glimmer/master/LICENSE
+ *            See https://raw.githubusercontent.com/glimmerjs/glimmer-vm/master/LICENSE
  * @version   VERSION_STRING_PLACEHOLDER
  */
 export { precompile } from '@glimmer/compiler';
