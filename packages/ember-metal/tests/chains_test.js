651d57028da916ffc9c89e05681e20aede9c063c
Stefan Penner <stefan.penner@gmail.com>
2017-04-16 13:27:45 -0700

    [BUGFIX beta] finishChains should operate on metas
    Previously, finishChains (run in every EmberObject creation) would have to lookup its own meta, even though the caller already has access to this meta.
    
    * also fix an invalid test (I introduced the test bug here: https://github.com/emberjs/ember.js/commit/e46e056f1d5ed3136efbdf2136febebacd0461c6#commitcomment-21779571)

--- packages/ember-metal/tests/chains_test.js
+++ packages/ember-metal/tests/chains_test.js
@@ -6,7 +6,8 @@ import {
   defineProperty,
   computed,
   propertyDidChange,
-  peekMeta
+  peekMeta,
+  meta
 } from '..';
 
 QUnit.module('Chains');
@@ -18,8 +19,13 @@ QUnit.test('finishChains should properly copy chains from prototypes to instance
   addObserver(obj, 'foo.bar', null, didChange);
 
   let childObj = Object.create(obj);
-  finishChains(childObj);
-  ok(peekMeta(obj) !== peekMeta(childObj).readableChains(), 'The chains object is copied');
+
+  let parentMeta = meta(obj);
+  let childMeta = meta(childObj);
+
+  finishChains(childMeta);
+
+  ok(parentMeta.readableChains() !== childMeta.readableChains(), 'The chains object is copied');
 });
 
 QUnit.test('does not observe primitive values', function(assert) {
