097607a712ab7a64cdcb4f9b658f910ac2436fcf
Kris Selden <kris.selden@gmail.com>
2017-05-25 11:57:14 -0700

    fix if missing paren and let / const conflict.

--- packages/@glimmer/local-debug-flags/index.ts
+++ packages/@glimmer/local-debug-flags/index.ts
@@ -1,10 +1,10 @@
-let location = typeof window !== undefined && window.location;
-let DEBUG = false;
-if (location && /[?&]glimmer_logging/.test(window.location.search) {
-  DEBUG = true;
-}
-
-export const DEBUG = DEBUG;
+export const DEBUG = (() => {
+  let location = typeof window !== undefined && window.location;
+  if (location && /[?&]glimmer_logging/.test(window.location.search)) {
+    return true;
+  }
+  return false;
+})();
 
 // TODO this is hacky but requires unifying the build
 export const CI = !!window['Testem'];
