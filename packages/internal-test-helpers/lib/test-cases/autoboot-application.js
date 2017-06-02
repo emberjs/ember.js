9e7b57181d6b81ebb500e4c873d9d7fb13bbedc0
Matthew Beale <matt.beale@madhatted.com>
2017-06-01 18:08:49 -0700

    Decouple reset_test from globals resolver

--- packages/internal-test-helpers/lib/test-cases/autoboot-application.js
+++ packages/internal-test-helpers/lib/test-cases/autoboot-application.js
@@ -3,6 +3,8 @@ import TestResolver from '../test-resolver';
 import { Application } from 'ember-application';
 import { assign } from 'ember-utils';
 import { runDestroy } from '../run';
+import { run } from 'ember-metal';
+import { compile } from 'ember-template-compiler';
 
 export default class AutobootApplicationTestCase extends AbstractTestCase {
 
@@ -25,9 +27,26 @@ export default class AutobootApplicationTestCase extends AbstractTestCase {
     this.resolver.add(specifier, factory);
   }
 
+  get router() {
+    return this.application.resolveRegistration('router:main');
+  }
+
+  visit(url, options) {
+    return run(this.applicationInstance, 'visit', url, options);
+  }
+
+  get applicationInstance() {
+    return this.application.__deprecatedInstance__;
+  }
+
+  compile(string, options) {
+    return compile(...arguments);
+  }
+
   addTemplate(templateName, templateString) {
-    this.resolver.addTemplate(templateName, templateString);
+    this.resolver.add(`template:${templateName}`, this.compile(templateString, {
+      moduleName: templateName
+    }));
   }
 
 }
-
