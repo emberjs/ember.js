e66261dfe126b08d292a7e76665a820f36ac0920
Robert Jackson <rjackson@linkedin.com>
2017-06-03 21:43:55 -0400

    [FEATURE ember-engines-mount-params] Constrain to `model` hash arg.
    After discussion during a recent core team meeting, the original
    implementation (and RFC) were determined to be a bit future hostile.
    
    Specifically, with the implementation prior to this PR it would have
    been impossible to add any additional hash arguments that were
    targeting the `{{mount` syntax directly (e.g. namespacing for use
    with tree shaking) without breaking changes (since all hash arg names
    were allowed to be passed through before).
    
    This change updates things a bit to limit the pass through to the `model`
    hash argument, which actually makes things a tad bit more teachable
    anyways. The ability to pass down arbitrary named values is absolutely
    still present through the usage of the `(hash` helper.

--- packages/ember-glimmer/tests/integration/mount-test.js
+++ packages/ember-glimmer/tests/integration/mount-test.js
@@ -208,7 +208,7 @@ if (EMBER_ENGINES_MOUNT_PARAMS) {
       this.router.map(function() {
         this.route('engine-params-static');
       });
-      this.addTemplate('engine-params-static', '{{mount "paramEngine" foo="bar"}}');
+      this.addTemplate('engine-params-static', '{{mount "paramEngine" model=(hash foo="bar")}}');
 
       return this.visit('/engine-params-static').then(() => {
         this.assertComponentElement(this.firstChild, { content: '<h2>Param Engine: bar</h2>' });
@@ -227,7 +227,7 @@ if (EMBER_ENGINES_MOUNT_PARAMS) {
           controller = this;
         }
       }));
-      this.addTemplate('engine-params-bound', '{{mount "paramEngine" foo=boundParamValue}}');
+      this.addTemplate('engine-params-bound', '{{mount "paramEngine" model=(hash foo=boundParamValue)}}');
 
       return this.visit('/engine-params-bound').then(() => {
         this.assertComponentElement(this.firstChild, { content: '<h2>Param Engine: </h2>' });
@@ -277,7 +277,7 @@ if (EMBER_ENGINES_MOUNT_PARAMS) {
           this.register('template:application', compile('{{model.foo}}', { moduleName: 'application' }));
         }
       }));
-      this.addTemplate('engine-params-contextual-component', '{{mount "componentParamEngine" foo=(component "foo-component")}}');
+      this.addTemplate('engine-params-contextual-component', '{{mount "componentParamEngine" model=(hash foo=(component "foo-component"))}}');
 
       return this.visit('/engine-params-contextual-component').then(() => {
         this.assertComponentElement(this.firstChild.firstChild, { content: 'foo-component rendered! - rendered app-bar-component from the app' });
