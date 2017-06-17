beff6a7671cc9d96ddfca93d05d83f465c1800ec
Robbie Pitts <me@robbie-pitts.com>
2017-06-17 10:53:20 -0400

    Convert SyntaxError class to ES5 syntax
    The `prototype` property on classes is readonly.

--- packages/@glimmer/syntax/lib/errors/syntax-error.ts
+++ packages/@glimmer/syntax/lib/errors/syntax-error.ts
@@ -1,23 +1,32 @@
 import * as AST from '../types/nodes';
 
-/*
+export interface SyntaxError extends Error {
+  location: AST.SourceLocation;
+  constructor: SyntaxErrorConstructor;
+}
+
+export interface SyntaxErrorConstructor {
+  new (message: string, location: AST.SourceLocation): SyntaxError;
+  readonly prototype: SyntaxError;
+}
+
+/**
  * Subclass of `Error` with additional information
  * about location of incorrect markup.
  */
-class SyntaxError {
-  message: string;
-  stack: string;
-  location: AST.SourceLocation;
+const SyntaxError: SyntaxErrorConstructor = (function () {
+  SyntaxError.prototype = Object.create(Error.prototype);
+  SyntaxError.prototype.constructor = SyntaxError;
 
-  constructor(message: string, location: AST.SourceLocation) {
+  function SyntaxError(this: SyntaxError, message: string, location: AST.SourceLocation) {
     let error = Error.call(this, message);
 
     this.message = message;
     this.stack = error.stack;
     this.location = location;
   }
-}
 
-SyntaxError.prototype = Object.create(Error.prototype);
+  return SyntaxError as any;
+}());
 
 export default SyntaxError;
