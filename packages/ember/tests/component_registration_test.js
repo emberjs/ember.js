98cc07f7efeeabed7524511966a510e0470b1000
karthiick <karthicksivakrr@gmail.com>
2017-06-21 00:01:56 +0530

    Decouple test from the default resolver for component_registration_test
    updated test cases
    
    Remove extra visit calls

--- packages/ember/tests/component_registration_test.js
+++ packages/ember/tests/component_registration_test.js
@@ -1,200 +1,198 @@
 import { Controller } from 'ember-runtime';
-import { run } from 'ember-metal';
+import { Component } from 'ember-glimmer';
+import { moduleFor, AutobootApplicationTestCase } from 'internal-test-helpers';
 
-import { Application } from 'ember-application';
-import { Router } from 'ember-routing';
-import { compile } from 'ember-template-compiler';
-import {
-  Component,
-  setTemplates,
-  setTemplate
-} from 'ember-glimmer';
-import { jQuery } from 'ember-views';
+moduleFor('Application Lifecycle - Component Registration', class extends AutobootApplicationTestCase {
 
-let App, appInstance;
+  ['@test The helper becomes the body of the component'](assert) {
+    this.runTask(() => {
+      this.createApplication();
 
-function prepare() {
-  setTemplate('components/expand-it', compile('<p>hello {{yield}}</p>'));
-  setTemplate('application', compile('Hello world {{#expand-it}}world{{/expand-it}}'));
-}
-
-function cleanup() {
-  run(() => {
-    try {
-      if (App) {
-        App.destroy();
-      }
-      App = appInstance = null;
-    } finally {
-      setTemplates({});
-    }
-  });
-}
-
-QUnit.module('Application Lifecycle - Component Registration', {
-  setup: prepare,
-  teardown: cleanup
-});
-
-function boot(callback, startURL = '/') {
-  run(() => {
-    App = Application.create({
-      name: 'App',
-      rootElement: '#qunit-fixture'
+      this.addTemplate('components/expand-it', '<p>hello {{yield}}</p>');
+      this.addTemplate('application', 'Hello world {{#expand-it}}world{{/expand-it}}');
     });
 
-    App.deferReadiness();
+    let text = this.$('div.ember-view > div.ember-view').text().trim();
+    assert.equal(text, 'hello world', 'The component is composed correctly');
+  }
 
-    App.Router = Router.extend({
-      location: 'none'
-    });
+  ['@test If a component is registered, it is used'](assert) {
+    this.runTask(() => {
+      this.createApplication();
 
-    appInstance = App.__deprecatedInstance__;
+      this.addTemplate('components/expand-it', '<p>hello {{yield}}</p>');
+      this.addTemplate('application', `Hello world {{#expand-it}}world{{/expand-it}}`);
 
-    if (callback) { callback(); }
-  });
-
-  let router = appInstance.lookup('router:main');
-
-  run(App, 'advanceReadiness');
-  run(() => router.handleURL(startURL));
-}
-
-QUnit.test('The helper becomes the body of the component', function() {
-  boot();
-  equal(jQuery('div.ember-view > div.ember-view', '#qunit-fixture').text(), 'hello world', 'The component is composed correctly');
-});
-
-QUnit.test('If a component is registered, it is used', function() {
-  boot(() => {
-    appInstance.register('component:expand-it', Component.extend({
-      classNames: 'testing123'
-    }));
-  });
-
-  equal(jQuery('div.testing123', '#qunit-fixture').text(), 'hello world', 'The component is composed correctly');
-});
-
-QUnit.test('Late-registered components can be rendered with custom `layout` property', function() {
-  setTemplate('application', compile('<div id=\'wrapper\'>there goes {{my-hero}}</div>'));
-
-  boot(() => {
-    appInstance.register('component:my-hero', Component.extend({
-      classNames: 'testing123',
-      layout: compile('watch him as he GOES')
-    }));
-  });
-
-  equal(jQuery('#wrapper').text(), 'there goes watch him as he GOES', 'The component is composed correctly');
-});
-
-QUnit.test('Late-registered components can be rendered with template registered on the container', function() {
-  setTemplate('application', compile('<div id=\'wrapper\'>hello world {{sally-rutherford}}-{{#sally-rutherford}}!!!{{/sally-rutherford}}</div>'));
-
-  boot(() => {
-    appInstance.register('template:components/sally-rutherford', compile('funkytowny{{yield}}'));
-    appInstance.register('component:sally-rutherford', Component);
-  });
-
-  equal(jQuery('#wrapper').text(), 'hello world funkytowny-funkytowny!!!', 'The component is composed correctly');
-});
-
-QUnit.test('Late-registered components can be rendered with ONLY the template registered on the container', function() {
-  setTemplate('application', compile('<div id=\'wrapper\'>hello world {{borf-snorlax}}-{{#borf-snorlax}}!!!{{/borf-snorlax}}</div>'));
-
-  boot(() => {
-    appInstance.register('template:components/borf-snorlax', compile('goodfreakingTIMES{{yield}}'));
-  });
-
-  equal(jQuery('#wrapper').text(), 'hello world goodfreakingTIMES-goodfreakingTIMES!!!', 'The component is composed correctly');
-});
-
-QUnit.test('Assigning layoutName to a component should setup the template as a layout', function() {
-  expect(1);
-
-  setTemplate('application', compile('<div id=\'wrapper\'>{{#my-component}}{{text}}{{/my-component}}</div>'));
-  setTemplate('foo-bar-baz', compile('{{text}}-{{yield}}'));
-
-  boot(() => {
-    appInstance.register('controller:application', Controller.extend({
-      'text': 'outer'
-    }));
-
-    appInstance.register('component:my-component', Component.extend({
-      text: 'inner',
-      layoutName: 'foo-bar-baz'
-    }));
-  });
-
-  equal(jQuery('#wrapper').text(), 'inner-outer', 'The component is composed correctly');
-});
-
-QUnit.test('Assigning layoutName and layout to a component should use the `layout` value', function() {
-  expect(1);
-
-  setTemplate('application', compile('<div id=\'wrapper\'>{{#my-component}}{{text}}{{/my-component}}</div>'));
-  setTemplate('foo-bar-baz', compile('No way!'));
-
-  boot(() => {
-    appInstance.register('controller:application', Controller.extend({
-      'text': 'outer'
-    }));
-
-    appInstance.register('component:my-component', Component.extend({
-      text: 'inner',
-      layoutName: 'foo-bar-baz',
-      layout: compile('{{text}}-{{yield}}')
-    }));
-  });
-
-  equal(jQuery('#wrapper').text(), 'inner-outer', 'The component is composed correctly');
-});
-
-QUnit.test('Assigning defaultLayout to a component should set it up as a layout if no layout was found [DEPRECATED]', function() {
-  expect(2);
-
-  setTemplate('application', compile('<div id=\'wrapper\'>{{#my-component}}{{text}}{{/my-component}}</div>'));
-
-  expectDeprecation(() => {
-    boot(() => {
-      appInstance.register('controller:application', Controller.extend({
-        'text': 'outer'
+      this.applicationInstance.register('component:expand-it', Component.extend({
+        classNames: 'testing123'
       }));
+    });
 
-      appInstance.register('component:my-component', Component.extend({
+    let text = this.$('div.testing123').text().trim();
+    assert.equal(text, 'hello world', 'The component is composed correctly');
+  }
+
+  ['@test Late-registered components can be rendered with custom `layout` property'](assert) {
+    this.runTask(() => {
+      this.createApplication();
+
+      this.addTemplate('application', `<div id='wrapper'>there goes {{my-hero}}</div>`);
+
+      this.applicationInstance.register('component:my-hero', Component.extend({
+        classNames: 'testing123',
+        layout: this.compile('watch him as he GOES')
+      }));
+    });
+
+    let text = this.$('#wrapper').text().trim();
+    assert.equal(text, 'there goes watch him as he GOES', 'The component is composed correctly');
+  }
+
+  ['@test Late-registered components can be rendered with template registered on the container'](assert) {
+    this.runTask(() => {
+      this.createApplication();
+
+      this.addTemplate('application', `<div id='wrapper'>hello world {{sally-rutherford}}-{{#sally-rutherford}}!!!{{/sally-rutherford}}</div>`);
+
+      this.applicationInstance.register('template:components/sally-rutherford', this.compile('funkytowny{{yield}}'));
+      this.applicationInstance.register('component:sally-rutherford', Component);
+    });
+
+    let text = this.$('#wrapper').text().trim();
+    assert.equal(text, 'hello world funkytowny-funkytowny!!!', 'The component is composed correctly');
+  }
+
+  ['@test Late-registered components can be rendered with ONLY the template registered on the container'](assert) {
+    this.runTask(() => {
+      this.createApplication();
+
+      this.addTemplate('application', `<div id='wrapper'>hello world {{borf-snorlax}}-{{#borf-snorlax}}!!!{{/borf-snorlax}}</div>`);
+
+      this.applicationInstance.register('template:components/borf-snorlax', this.compile('goodfreakingTIMES{{yield}}'));
+    });
+
+    let text = this.$('#wrapper').text().trim();
+    assert.equal(text, 'hello world goodfreakingTIMES-goodfreakingTIMES!!!', 'The component is composed correctly');
+  }
+
+  ['@test Assigning layoutName to a component should setup the template as a layout'](assert) {
+    assert.expect(1);
+
+    this.runTask(() => {
+      this.createApplication();
+
+      this.addTemplate('application', `<div id='wrapper'>{{#my-component}}{{text}}{{/my-component}}</div>`);
+      this.addTemplate('foo-bar-baz', '{{text}}-{{yield}}');
+
+      this.applicationInstance.register('controller:application', Controller.extend({
+        text: 'outer'
+      }));
+      this.applicationInstance.register('component:my-component', Component.extend({
         text: 'inner',
-        defaultLayout: compile('{{text}}-{{yield}}')
+        layoutName: 'foo-bar-baz'
       }));
     });
-  }, /Specifying `defaultLayout` to .+ is deprecated\./);
 
-  equal(jQuery('#wrapper').text(), 'inner-outer', 'The component is composed correctly');
-});
+    let text = this.$('#wrapper').text().trim();
+    assert.equal(text, 'inner-outer', 'The component is composed correctly');
+  }
 
-QUnit.test('Assigning defaultLayout to a component should set it up as a layout if layout was found [DEPRECATED]', function() {
-  expect(2);
+  ['@test Assigning layoutName and layout to a component should use the `layout` value'](assert) {
+    assert.expect(1);
 
-  setTemplate('application', compile('<div id=\'wrapper\'>{{#my-component}}{{text}}{{/my-component}}</div>'));
-  setTemplate('components/my-component', compile('{{text}}-{{yield}}'));
+    this.runTask(() => {
+      this.createApplication();
 
-  expectDeprecation(() => {
-    boot(() => {
-      appInstance.register('controller:application', Controller.extend({
-        'text': 'outer'
+      this.addTemplate('application', `<div id='wrapper'>{{#my-component}}{{text}}{{/my-component}}</div>`);
+      this.addTemplate('foo-bar-baz', 'No way!');
+
+      this.applicationInstance.register('controller:application', Controller.extend({
+        text: 'outer'
       }));
-
-      appInstance.register('component:my-component', Component.extend({
+      this.applicationInstance.register('component:my-component', Component.extend({
         text: 'inner',
-        defaultLayout: compile('should not see this!')
+        layoutName: 'foo-bar-baz',
+        layout: this.compile('{{text}}-{{yield}}')
       }));
     });
-  }, /Specifying `defaultLayout` to .+ is deprecated\./);
 
-  equal(jQuery('#wrapper').text(), 'inner-outer', 'The component is composed correctly');
-});
+    let text = this.$('#wrapper').text().trim();
+    assert.equal(text, 'inner-outer', 'The component is composed correctly');
+  }
 
-QUnit.test('Using name of component that does not exist', function () {
-  setTemplate('application', compile('<div id=\'wrapper\'>{{#no-good}} {{/no-good}}</div>'));
+  ['@test Assigning defaultLayout to a component should set it up as a layout if no layout was found [DEPRECATED]'](assert) {
+    assert.expect(2);
 
-  expectAssertion(() => boot(), /.* named "no-good" .*/);
+    expectDeprecation(() => {
+      this.runTask(() => {
+        this.createApplication();
+
+        this.addTemplate('application', `<div id='wrapper'>{{#my-component}}{{text}}{{/my-component}}</div>`);
+
+        this.applicationInstance.register('controller:application', Controller.extend({
+          text: 'outer'
+        }));
+        this.applicationInstance.register('component:my-component', Component.extend({
+          text: 'inner',
+          defaultLayout: this.compile('{{text}}-{{yield}}')
+        }));
+      });
+    });
+
+    let text = this.$('#wrapper').text().trim();
+    assert.equal(text, 'inner-outer', 'The component is composed correctly');
+  }
+
+  ['@test Assigning defaultLayout to a component should set it up as a layout if layout was found [DEPRECATED]'](assert) {
+    assert.expect(2);
+
+    expectDeprecation(() => {
+      this.runTask(() => {
+        this.createApplication();
+
+        this.addTemplate('application', `<div id='wrapper'>{{#my-component}}{{text}}{{/my-component}}</div>`);
+        this.addTemplate('components/my-component', '{{text}}-{{yield}}');
+
+        this.applicationInstance.register('controller:application', Controller.extend({
+          text: 'outer'
+        }));
+        this.applicationInstance.register('component:my-component', Component.extend({
+          text: 'inner',
+          defaultLayout: this.compile('should not see this!')
+        }));
+      });
+    }, /Specifying `defaultLayout` to .+ is deprecated\./);
+
+    let text = this.$('#wrapper').text().trim();
+    assert.equal(text, 'inner-outer', 'The component is composed correctly');
+  }
+
+  /*
+   * When an exception is thrown during the initial rendering phase, the
+   * `visit` promise is not resolved or rejected. This means the `applicationInstance`
+   * is never torn down and tests running after this one will fail.
+   *
+   * It is ugly, but since this test intentionally causes an initial render
+   * error, it requires globals mode to access the `applicationInstance`
+   * for teardown after test completion.
+   *
+   * Application "globals mode" is trigged by `autoboot: true`. It doesn't
+   * have anything to do with the resolver.
+   *
+   * We should be able to fix this by having the application eagerly stash a
+   * copy of each application instance it creates. When the application is
+   * destroyed, it can also destroy the instances (this is how the globals
+   * mode avoid the problem).
+   *
+   * See: https://github.com/emberjs/ember.js/issues/15327
+   */
+  ['@test Using name of component that does not exist']() {
+    expectAssertion(() => {
+      this.runTask(() => {
+        this.createApplication();
+
+        this.addTemplate('application', `<div id='wrapper'>{{#no-good}} {{/no-good}}</div>`);
+      });
+    }, /.* named "no-good" .*/);
+  }
 });
