0960519155795e9bd7cf2ea0cf6bbc298cb9503f
Ricardo Mendes <rokusu@gmail.com>
2017-06-30 08:59:42 +0100

    [DOC beta] Fix wording on private Routing service

--- packages/ember-routing/lib/services/routing.js
+++ packages/ember-routing/lib/services/routing.js
@@ -16,9 +16,8 @@ import { routeArgs } from '../utils';
   The Routing service is used by LinkComponent, and provides facilities for
   the component/view layer to interact with the router.
 
-  While still private, this service can eventually be opened up, and provides
-  the set of API needed for components to control routing without interacting
-  with router internals.
+  This is a private service for internal usage only. For public usage,
+  refer to the `Router` service.
 
   @private
   @class RoutingService
