fb2ab987cbc72ed1640a3e310e1489a9f91fd5f9
bekzod <bekzod@me.com>
2017-07-03 09:32:19 +0500

    removed `isGlobalCache`
    since its not being used

--- packages/ember-metal/lib/path_cache.js
+++ packages/ember-metal/lib/path_cache.js
@@ -1,42 +1,29 @@
 import Cache from './cache';
 
-const IS_GLOBAL      = /^[A-Z$]/;
 const IS_GLOBAL_PATH = /^[A-Z$].*[\.]/;
 const HAS_THIS       = 'this.';
 
-const isGlobalCache      = new Cache(1000, key => IS_GLOBAL.test(key));
 const isGlobalPathCache  = new Cache(1000, key => IS_GLOBAL_PATH.test(key));
 const hasThisCache       = new Cache(1000, key => key.lastIndexOf(HAS_THIS, 0) === 0);
 const firstDotIndexCache = new Cache(1000, key => key.indexOf('.'));
 
 const firstKeyCache = new Cache(1000, (path) => {
   let index = firstDotIndexCache.get(path);
-  if (index === -1) {
-    return path;
-  } else {
-    return path.slice(0, index);
-  }
+  return index === -1 ? path : path.slice(0, index);
 });
 
 const tailPathCache = new Cache(1000, (path) => {
   let index = firstDotIndexCache.get(path);
-  if (index !== -1) {
-    return path.slice(index + 1);
-  }
+  return index === -1 ? undefined : path.slice(index + 1);
 });
 
 export const caches = {
-  isGlobalCache,
   isGlobalPathCache,
   hasThisCache,
   firstDotIndexCache,
   firstKeyCache,
   tailPathCache
 };
-
-export function isGlobal(path) {
-  return isGlobalCache.get(path);
-}
 
 export function isGlobalPath(path) {
   return isGlobalPathCache.get(path);
