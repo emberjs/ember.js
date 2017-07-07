c5ed0f68ba2a08c2cc96d6c8a786d16ae644a975
Michael Donaldson <mike@mike-donaldson.com>
2017-07-07 10:33:12 +0100

    [BUGFIX lts] Ensure event dispatcher is a singleton

--- packages/ember-application/tests/system/application_instance_test.js
+++ packages/ember-application/tests/system/application_instance_test.js
@@ -156,7 +156,6 @@ QUnit.test('can build and boot a registered engine', function(assert) {
 
       let registrations = [
         'route:basic',
-        'event_dispatcher:main',
         'service:-routing',
         'service:-glimmer-environment'
       ];
@@ -173,7 +172,8 @@ QUnit.test('can build and boot a registered engine', function(assert) {
         P`-bucket-cache:main`,
         '-view-registry:main',
         '-environment:main',
-        'service:-document'
+        'service:-document',
+        'event_dispatcher:main'
       ];
 
       let env = appInstance.lookup('-environment:main');
