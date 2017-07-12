33155b50c2725695a452ed29f01032b264a22107
bekzod <bekzod@me.com>
2017-07-12 14:12:44 +0500

    reuse `shallowEqual`

--- packages/ember-routing/lib/system/router_state.js
+++ packages/ember-routing/lib/system/router_state.js
@@ -1,5 +1,5 @@
 import { assign } from 'ember-utils';
-import { isEmpty } from 'ember-metal';
+import { shallowEqual } from '../utils';
 import { Object as EmberObject } from 'ember-runtime';
 
 export default EmberObject.extend({
@@ -11,11 +11,8 @@ export default EmberObject.extend({
     let state = this.routerJsState;
     if (!this.routerJs.isActiveIntent(routeName, models, null, state)) { return false; }
 
-    let emptyQueryParams = isEmpty(Object.keys(queryParams));
-
-    if (queryParamsMustMatch && !emptyQueryParams) {
-      let visibleQueryParams = {};
-      assign(visibleQueryParams, queryParams);
+    if (queryParamsMustMatch && Object.keys(queryParams).length > 0) {
+      let visibleQueryParams = assign({}, queryParams);
 
       this.emberRouter._prepareQueryParams(routeName, models, visibleQueryParams);
       return shallowEqual(visibleQueryParams, state.queryParams);
@@ -24,14 +21,3 @@ export default EmberObject.extend({
     return true;
   }
 });
-
-function shallowEqual(a, b) {
-  let k;
-  for (k in a) {
-    if (a.hasOwnProperty(k) && a[k] !== b[k]) { return false; }
-  }
-  for (k in b) {
-    if (b.hasOwnProperty(k) && a[k] !== b[k]) { return false; }
-  }
-  return true;
-}
