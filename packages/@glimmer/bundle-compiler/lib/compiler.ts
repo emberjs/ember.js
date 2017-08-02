f0befccd2e951b4e20aea1b0ca05d743c0151c13
Yehuda Katz <wycats@gmail.com>
2017-08-02 14:28:51 -0700

    WIP

---
+++ packages/@glimmer/bundle-compiler/lib/compiler.ts
@@ -0,0 +1,27 @@
+import { CompilationOptions, Program } from '@glimmer/runtime';
+import { Resolver } from "@glimmer/interfaces";
+import { TemplateMeta } from "@glimmer/wire-format";
+import { preprocess, ASTPluginBuilder } from "@glimmer/syntax";
+import { PrecompileOptions, TemplateCompiler } from "@glimmer/compiler";
+
+export interface EagerCompilerOptions<T extends TemplateMeta> {
+  meta: T;
+  plugins: ASTPluginBuilder[];
+  id: string;
+}
+
+export class EagerCompiler<T extends TemplateMeta, Specifier, R extends Resolver<Specifier, T>> {
+  private program: Program;
+  private resolver: R;
+
+  constructor(private options: CompilationOptions<T, Specifier, R>) {
+    this.program = options.program;
+    this.resolver = options.resolver;
+  }
+
+  compile(input: string, options: EagerCompilerOptions<T>) {
+    let ast = preprocess(input, options);
+    let { block, meta } = TemplateCompiler.compile(options, ast);
+    let { id } = options;
+  }
+}
--- packages/@glimmer/compiler/index.ts
+++ packages/@glimmer/compiler/index.ts
@@ -2,5 +2,9 @@ export {
   precompile, PrecompileOptions
 } from "./lib/compiler";
 
+export {
+  default as TemplateCompiler
+} from "./lib/template-compiler";
+
 // exported only for tests
 export { default as TemplateVisitor } from './lib/template-visitor';
