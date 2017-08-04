6b0c4f69f5422c9fd40a96109681f306a5663af8
Tom Dale <tom@tomdale.net>
2017-08-04 13:52:25 -0400

    Unify Broccoli build, so that "production" assets are used in tests

--- packages/@glimmer/syntax/index.ts
+++ packages/@glimmer/syntax/index.ts
@@ -10,6 +10,12 @@ export {
 
 // needed for tests only
 export { default as builders } from './lib/builders';
+export {
+  default as TraversalError,
+  cannotRemoveNode,
+  cannotReplaceNode,
+  cannotReplaceOrRemoveInKeyHandlerYet
+} from './lib/traversal/errors';
 export { default as traverse, NodeHandler, NodeVisitor, NodeHandlerFunction, EnterExitNodeHandler } from './lib/traversal/traverse';
 export { default as Walker } from './lib/traversal/walker';
 export { default as print } from './lib/generation/print';
