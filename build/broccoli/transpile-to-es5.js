631fb58f3e13623bae209d7400978564cb9e15e2
Tom Dale <tom@tomdale.net>
2017-08-07 09:58:01 -0400

    Fix IE9 and IE10 compatibility

--- build/broccoli/transpile-to-es5.js
+++ build/broccoli/transpile-to-es5.js
@@ -15,9 +15,17 @@ module.exports = function transpileToES5(inputNode, modules = false) {
         loose: true,
         modules,
         exclude: [
+          // Transforms all `typeof` checks to account for Symbol, which we don't
+          // rely on, so can safely skip.
           'transform-es2015-typeof-symbol'
         ]
       }]
+    ],
+    plugins: [
+      // Required for tests to pass in IE <=10, which rely on inheritance of
+      // static class methods. This doesn't work in IE 10 and below so we
+      // statically copy properties off __proto__ at class definition time.
+      'transform-proto-to-assign'
     ]
   });
 }
