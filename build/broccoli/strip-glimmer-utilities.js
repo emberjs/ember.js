ce62384a0d644533063c2a9b6295a41c17a9cb4a
Chad Hietala <chadhietala@gmail.com>
2017-09-01 10:02:40 -0400

    Fix dependencies and add ability to compile with flags

--- build/broccoli/strip-glimmer-utilities.js
+++ build/broccoli/strip-glimmer-utilities.js
@@ -9,25 +9,30 @@ const debugMacros = require('babel-plugin-debug-macros').default;
  * Babel plugins.
  */
 module.exports = function(jsTree) {
+  let RETAIN_FLAGS = process.env.RETAIN_FLAGS;
+  let glimmerUtils = [];
+  if (!RETAIN_FLAGS) {
+    glimmerUtils.push([debugMacros, {
+      envFlags: {
+        source: '@glimmer/local-debug-flags',
+        flags: {
+          DEBUG: process.env.EMBER_ENV !== 'production'
+        }
+      },
+      debugTools: {
+        source: '@glimmer/debug'
+      },
+      externalizeHelpers: {
+        module: true
+      }
+    }])
+  }
+
   return babel(jsTree, {
     annotation: 'Babel - Strip Glimmer Utilities',
     sourceMaps: 'inline',
     plugins: [
-      [debugMacros, {
-        envFlags: {
-          source: '@glimmer/local-debug-flags',
-          flags: {
-            DEBUG: process.env.EMBER_ENV !== 'production',
-            CI: !!process.env.CI
-          }
-        },
-        debugTools: {
-          source: '@glimmer/debug'
-        },
-        externalizeHelpers: {
-          module: true
-        }
-      }],
+      ...glimmerUtils,
       [stripGlimmerUtils, { bindings: ['expect', 'unwrap'], source: '@glimmer/util' }]
     ]
   });
