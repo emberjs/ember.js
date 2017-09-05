bbf792b7b46789d0dc063c59f5e5edf54b7a562a
Sergio Arbeo <serabe@gmail.com>
2017-09-05 17:56:32 +0200

    [DOC beta] Improve run.scheduleOnce documentation
    Improve the documentation about how scheduleOnce prevents a function
    being called more than once.
    
    Fixes #15620
    
    (cherry picked from commit e7a5c1f244e8b0258c986c52c51e052c98af710a)

--- packages/ember-metal/lib/run_loop.js
+++ packages/ember-metal/lib/run_loop.js
@@ -389,9 +389,23 @@ run.once = function(...args) {
   });
   ```
 
-  Also note that passing an anonymous function to `run.scheduleOnce` will
-  not prevent additional calls with an identical anonymous function from
-  scheduling the items multiple times, e.g.:
+  Also note that for `run.scheduleOnce` to prevent additional calls, you need to
+  pass the same function instance. The following case works as expected:
+
+  ```javascript
+  function log() {
+    console.log('Logging only once');
+  }
+
+  function scheduleIt() {
+    run.scheduleOnce('actions', myContext, log);
+  }
+
+  scheduleIt();
+  scheduleIt();
+  ```
+
+  But this other case will schedule the function multiple times:
 
   ```javascript
   function scheduleIt() {
@@ -404,7 +418,7 @@ run.once = function(...args) {
   scheduleIt();
 
   // "Closure" will print twice, even though we're using `run.scheduleOnce`,
-  // because the function we pass to it is anonymous and won't match the
+  // because the function we pass to it won't match the
   // previously scheduled operation.
   ```
 
