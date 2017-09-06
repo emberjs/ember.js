210505dfb908f8a91183193975f4410cf0f06532
Tom Dale <tom@tomdale.net>
2017-09-06 12:30:37 -0400

    Bundle Compiler refactoring pass
    Previously, all of the bundle compiler implementation was in a single file called `templates.ts`. This commit breaks that file into logical units and also adds some additional inline documentation.

--- packages/@glimmer/bundle-compiler/index.ts
+++ packages/@glimmer/bundle-compiler/index.ts
@@ -1,1 +1,20 @@
-export * from './lib/templates';
+export {
+  BundleCompiler,
+  BundleCompilerOptions,
+  BundleCompileOptions,
+  BundleCompilationResult
+} from './lib/bundle-compiler';
+
+export {
+  CompilerDelegate
+} from './lib/compiler-delegate';
+
+export {
+  Specifier,
+  specifierFor
+} from './lib/specifiers';
+
+export {
+  SpecifierMap,
+  LookupMap
+} from './lib/specifier-map';
