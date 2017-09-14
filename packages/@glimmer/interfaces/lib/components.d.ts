20aebe71ef2b923802f0ac2a6dba9072356d85bf
Tom Dale <tom@tomdale.net>
2017-09-14 12:54:08 -0400

    StaticComponentState -> ComponentDefinitionState

--- packages/@glimmer/interfaces/lib/components.d.ts
+++ packages/@glimmer/interfaces/lib/components.d.ts
@@ -3,8 +3,8 @@ import { Option, Opaque, Unique } from './core';
 export type ComponentDefinitionState = Unique<'ComponentDefinitionState'>;
 export type ComponentInstanceState = Unique<'ComponentInstanceState'>;
 
-export interface ComponentDefinition<ComponentManager = {}, StaticComponentState = {}> {
-  state: StaticComponentState;
+export interface ComponentDefinition<ComponentManager = {}, ComponentDefinitionState = {}> {
+  state: ComponentDefinitionState;
   manager: ComponentManager;
 }
 
