c032e7a932ae8972ace4350c5c4a0e53a2bcb017
Todd Jordan <todd.jordan@inin.com>
2017-10-17 15:04:55 -0400

    Make observer static

--- packages/ember-metal/lib/mixin.js
+++ packages/ember-metal/lib/mixin.js
@@ -748,6 +748,7 @@ export function aliasMethod(methodName) {
   @param {Function} func
   @return func
   @public
+  @static
 */
 export function observer(...args) {
   let _paths, func;
@@ -781,7 +782,7 @@ export function observer(...args) {
 
   ```javascript
   import EmberObject from '@ember/object';
-  
+
   EmberObject.extend({
     valueObserver: Ember.immediateObserver('value', function() {
       // Executes whenever the "value" property changes
