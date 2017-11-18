9f61acdde04719b84b9bbe654fc43418055ee3ad
Kris Selden <kris.selden@gmail.com>
2017-11-17 17:54:21 -0800

    bump glimmer-vm because Tom's done cleanup in the resolver area.

--- packages/ember-utils/lib/index.d.ts
+++ packages/ember-utils/lib/index.d.ts
@@ -1,7 +1,18 @@
 import { Opaque } from '@glimmer/interfaces';
 
+export interface Factory<T> {
+  create(options: any): T;
+}
+
+export interface Owner {
+  lookup<T>(fullName: string, options?: { source: string }): T;
+  lookup(fullName: string, options?: { source: string }): any;
+  factoryFor<T>(fullName: string, options?: { source: string }): Factory<T>;
+  factoryFor(fullName: string, options?: { source: string }): Factory<any>;
+}
+
 export const NAME_KEY: string;
-export function getOwner(obj: any): any;
+export function getOwner(obj: {}): Owner;
 export function symbol(debugName: string): string;
 export function assign(original: any, ...args: any[]): any;
 export const OWNER: string;
--- packages/ember-views/lib/index.d.ts
+++ packages/ember-views/lib/index.d.ts
@@ -1,5 +1,6 @@
 import { Opaque } from '@glimmer/util';
 import { Simple } from '@glimmer/interfaces';
+import { Owner } from 'ember-utils';
 
 export const ActionSupport: any;
 export const ChildViewsSupport: any;
@@ -18,7 +19,7 @@ export function constructStyleDeprecationMessage(affectedStyle: any): string;
 
 export function hasPartial(name: string, owner: any): boolean;
 
-export function lookupComponent(owner: any, name: string, options: any): any;
+export function lookupComponent(owner: Owner, name: string, options?: { source?: string }): any;
 
 export function lookupPartial(templateName: string, owner: any): any;
 
