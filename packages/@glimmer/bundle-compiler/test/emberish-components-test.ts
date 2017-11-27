e10ce7a8b3af440b76f9da0200bdf868e4e73ee5
Chad Hietala <chadhietala@gmail.com>
2017-11-27 14:39:42 -0500

    Remove serializables array

--- packages/@glimmer/bundle-compiler/test/emberish-components-test.ts
+++ packages/@glimmer/bundle-compiler/test/emberish-components-test.ts
@@ -1,4 +1,4 @@
-import { EmberishGlimmerComponent rawModule, EmberishComponentTests, EagerRenderDelegate, test } from "@glimmer/test-helpers";
+import { EmberishGlimmerComponent, rawModule, EmberishComponentTests, EagerRenderDelegate, test } from "@glimmer/test-helpers";
 
 class BundleCompilerEmberTests extends EmberishComponentTests {
   @test({ kind: 'glimmer' })
@@ -6,8 +6,19 @@ class BundleCompilerEmberTests extends EmberishComponentTests {
     this.registerComponent('Glimmer', 'A', '{{component "B" foo=@bar}} {{component "B" foo=2}} {{component "B" foo=3}}');
     this.registerComponent('Glimmer', 'B', 'B {{@foo}}');
     this.render('<A @bar={{1}} /> {{component "B" foo=4}}');
-    this.assert.equal(this.delegate.constants!.toPool().serializables.length, 1);
-    this.assert.deepEqual(this.delegate.constants!.toPool().serializables[0], JSON.stringify({ locator: { module: 'ui/components/B', name: 'default' } }));
+    let locator = JSON.stringify({ locator: { module: 'ui/components/B', name: 'default' } });
+    let { strings } = this.delegate.constants!.toPool();
+    this.assert.ok(strings.indexOf(locator) > 0);
+
+    let uniq: string[] = [];
+    strings.forEach((str) => {
+      if (str === locator) {
+        uniq.push(str);
+      }
+    });
+
+    this.assert.equal(uniq.length, 1);
+
     this.assertHTML('B 1 B 2 B 3 B 4');
     this.assertStableRerender();
   }
@@ -20,7 +31,9 @@ class BundleCompilerEmberTests extends EmberishComponentTests {
     this.registerComponent('Glimmer', 'A', '{{component "B"}}');
     this.registerComponent('Glimmer', 'B', 'B {{bar}}', B);
     this.render('<A /> {{component "B"}}');
-    this.assert.equal(this.delegate.constants!.toPool().serializables.length, 0);
+    let locator = JSON.stringify({ locator: { module: 'ui/components/B', name: 'default' } });
+    let { strings } = this.delegate.constants!.toPool();
+    this.assert.equal(strings.indexOf(locator), -1);
     this.assertHTML('B 1 B 1');
     this.assertStableRerender();
   }
