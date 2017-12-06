59749b773cfed3c1f785c0b1cb979056a319f120
simonihmig <ihmig@kaliber5.de>
2017-12-06 21:58:47 +0100

    [BUGFIX lts] blueprints: fix framework detection to work with prerelease versions of ember-cli-mocha
    `semver.satisfies` does not cover prerelease versions like `*.*.*-beta` as required, so use `gte`
    
    (cherry picked from commit eb45abe381bcab53d2bb82fb1796d102627c1a3d)

--- blueprints/test-framework-detector.js
+++ blueprints/test-framework-detector.js
@@ -19,7 +19,7 @@ module.exports = function(blueprint) {
 
     } else if ('ember-cli-mocha' in dependencies) {
       var checker = new VersionChecker({ project: this.project });
-      if (fs.existsSync(this.path + '/mocha-0.12-files') && checker.for('ember-cli-mocha', 'npm').satisfies('>=0.12.0')) {
+      if (fs.existsSync(this.path + '/mocha-0.12-files') && checker.for('ember-cli-mocha', 'npm').gte('0.12.0')) {
         type = 'mocha-0.12';
       } else {
         type = 'mocha';
