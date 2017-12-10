4879d49d675c487ad4de8eebce777de7ec6b6492
snewcomer24 <snewcomer24@gmail.com>
2017-12-09 18:31:45 -0800

    fixup based on comments

--- node-tests/fixtures/instance-initializer-test/rfc232.js
+++ node-tests/fixtures/instance-initializer-test/rfc232.js
@@ -1,7 +1,6 @@
 import Application from '@ember/application';
-import { run } from '@ember/runloop';
 
-import { initialize } from 'my-app/initializers/foo';
+import { initialize } from 'my-app/instance-initializers/foo';
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import destroyApp from '../../helpers/destroy-app';
@@ -10,20 +9,23 @@ module('Unit | Instance Initializer | foo', function(hooks) {
   setupTest(hooks);
 
   hooks.beforeEach(function() {
-    run(() => {
-      this.application = Application.create();
-      this.application.deferReadiness();
+    this.TestApplication = Application.extend();
+    this.TestApplication.instanceInitializer({
+      name: 'initializer under test',
+      initialize
     });
+    this.application = this.TestApplication.create({ autoboot: false });
+    this.instance = this.application.buildInstance();
   });
   hooks.afterEach(function() {
     destroyApp(this.application);
+    destroyApp(this.instance);
   });
 
   // Replace this with your real tests.
-  test('it works', function(assert) {
-    initialize(this.application);
+  test('it works', async function(assert) {
+    await this.instance.boot();
 
-    // you would normally confirm the results of the initializer here
     assert.ok(true);
   });
 });
