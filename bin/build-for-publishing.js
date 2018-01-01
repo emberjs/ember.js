c5b253913bfa3edcff8bff7b54a4d431ad8ddf8e
Robert Jackson <rjackson@linkedin.com>
2018-01-01 09:39:08 -0500

    Add `bin/build-for-publishing.js` script.
    This is responsible for updating the `package.json` version number (so
    that the packed tarball has the same version string as `Ember.VERSION`)
    and running `npm pack` to build a tarball suitable for referencing in an
    applications `package.json` as:
    
    ```js
    {
      devDependencies: {
        "ember-source": "http://builds.emberjs.com/canary.tgz",
      }
    }
    ```

---
+++ bin/build-for-publishing.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const execSync = require('child_process').execSync;
+
+/*
+  Updates the `package.json`'s `version` string to be the same value that
+  the built assets will have as `Ember.VERSION`.
+*/
+function updatePackageJSONVersion() {
+  let packageJSONPath = path.join(__dirname, '..', 'package.json');
+  let VERSION = require('../broccoli/version').VERSION;
+
+  let pkgContents = fs.readFileSync(packageJSONPath, { encoding: 'utf-8' });
+  let pkg = JSON.parse(pkgContents);
+  pkg._versionPreviouslyCalculated = true;
+  pkg._originalVersion = pkg.version;
+  pkg.version = VERSION;
+  fs.writeFileSync(packageJSONPath, JSON.stringify(pkg, null, 2), { encoding: 'utf-8' });
+}
+
+
+updatePackageJSONVersion();
+
+// using npm pack here because `yarn pack` does not honor the `package.json`'s `files`
+// property properly, and therefore the tarball generated is quite large (~7MB).
+execSync('npm pack');
