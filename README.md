8adb793ef3578d68592ffa2f6fab39be77c6a290
Katie Gengler <katie@kmg.io>
2018-01-03 03:16:59 -0500

    Remove Saucelabs and use Browserstack for cross-browser testing

--- README.md
+++ README.md
@@ -8,10 +8,6 @@
   <a href="https://ember-community-slackin.herokuapp.com"><img src="https://ember-community-slackin.herokuapp.com/badge.svg" alt="Build Status"></a>
 </p>
 
-<p align="center">
-  <a href="https://saucelabs.com/u/ember-ci"><img src="https://saucelabs.com/browser-matrix/ember-ci.svg" alt="Sauce Test Status"></a>
-</p>
-
 Ember.js is a JavaScript framework that greatly reduces the time, effort and resources needed
 to build any web application. It is focused on making you, the developer, as productive as possible by doing all the common, repetitive, yet essential, tasks involved in most web development projects.
 
---
+++ testem.dist.js
@@ -0,0 +1,108 @@
+var TapReporter = require('testem/lib/reporters/tap_reporter');
+
+function FailureOnlyPerBrowserReporter() {
+  TapReporter.apply(this, arguments);
+  this._reportCount = 0;
+  this._resultsByBrowser = {};
+}
+
+FailureOnlyPerBrowserReporter.prototype = Object.create(TapReporter.prototype);
+FailureOnlyPerBrowserReporter.prototype.constructor = FailureOnlyPerBrowserReporter;
+
+FailureOnlyPerBrowserReporter.prototype.display = function(prefix, result) {
+  this._reportCount++;
+
+  if (!result.passed) {
+    TapReporter.prototype.display.apply(this, arguments);
+  }
+
+  if (this._reportCount > 100) {
+    this.out.write('pass count: ' + this.pass + '\n');
+    this._reportCount = 0;
+  }
+};
+
+FailureOnlyPerBrowserReporter.prototype.report = function(prefix, data) {
+  if (!this._resultsByBrowser[prefix]) {
+    this._resultsByBrowser[prefix] = {
+      total: 0,
+      pass: 0,
+      skipped: 0
+    };
+  }
+
+  this._resultsByBrowser[prefix].total++;
+  if (data.skipped) {
+    this._resultsByBrowser[prefix].skipped++;
+  } else if (data.passed) {
+    this._resultsByBrowser[prefix].pass++;
+  }
+
+  TapReporter.prototype.report.apply(this, arguments);
+};
+
+FailureOnlyPerBrowserReporter.prototype.summaryDisplay = function() {
+  var originalSummary = TapReporter.prototype.summaryDisplay.apply(this, arguments);
+  var lines = [];
+  var resultsByBrowser = this._resultsByBrowser;
+  Object.keys(resultsByBrowser).forEach(function(browser) {
+    var results = resultsByBrowser[browser];
+
+    lines.push('#');
+    lines.push('# Browser: ' + browser);
+    lines.push('# tests ' + results.total);
+    lines.push('# pass  ' + results.pass);
+    lines.push('# skip  ' + results.skipped);
+    lines.push('# fail  ' + (results.total - results.pass - results.skipped));
+  });
+  lines.push('#');
+  return lines.join('\n') + '\n' + originalSummary;
+};
+
+module.exports = {
+  framework: "qunit",
+  test_page: "dist/tests/index.html?hidepassed&hideskipped&timeout=60000",
+  timeout: 540,
+  reporter: FailureOnlyPerBrowserReporter,
+  browser_start_timeout: 600,
+  parallel: 4,
+  disable_watching: true,
+  launchers: {
+    BS_Chrome_Current: {
+      exe: "node_modules/.bin/browserstack-launch",
+      args: ["--os", "Windows", "--osv", "10", "--b", "chrome", "--bv", "latest", "-t", "600", "--u", "<url>"],
+      protocol: "browser"
+    },
+    BS_Firefox_Current: {
+      exe: "node_modules/.bin/browserstack-launch",
+      args: ["--os", "Windows", "--osv", "10", "--b", "firefox", "--bv", "latest", "-t", "600", "--u", "<url>"],
+      protocol: "browser"
+    },
+    BS_Safari_Current: {
+      exe: "node_modules/.bin/browserstack-launch",
+      args: ["--os", "OS X", "--osv", "High Sierra", "--b", "safari", "--bv", "11", "-t", "600", "--u", "<url>"],
+      protocol: "browser"
+    },
+    BS_Safari_Last: {
+      exe: "node_modules/.bin/browserstack-launch",
+      args: ["--os", "OS X", "--osv", "Sierra", "--b", "safari", "--bv", "10.1", "-t", "600", "--u", "<url>"],
+      protocol: "browser"
+    },
+    BS_MS_Edge: {
+      exe: "node_modules/.bin/browserstack-launch",
+      args: ["--os", "Windows", "--osv", "10", "--b", "edge", "--bv", "latest", "-t", "600", "--u", "<url>"],
+      protocol: "browser"
+    },
+    BS_IE_11: {
+      exe: "node_modules/.bin/browserstack-launch",
+      args: ["--os", "Windows", "--osv", "10", "--b", "ie", "--bv", "11.0", "-t", "600", "--u", "<url>"],
+      protocol: "browser"
+    }
+  },
+  launch_in_dev: [],
+  launch_in_ci: [
+    "BS_Safari_Current",
+    "BS_MS_Edge",
+    "BS_IE_11"
+  ]
+};
