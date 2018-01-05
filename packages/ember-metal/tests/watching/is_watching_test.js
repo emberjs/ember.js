b36da1962d346833e1a22a6567fd6b51f2b07ab6
Travis Hoover <thoov7@gmail.com>
2018-01-05 13:59:31 -0800

    [CLEANUP] Convert ember-metal watching tests to new style

--- packages/ember-metal/tests/watching/is_watching_test.js
+++ packages/ember-metal/tests/watching/is_watching_test.js
@@ -8,8 +8,7 @@ import {
   removeObserver,
   isWatching
 } from '../..';
-
-QUnit.module('isWatching');
+import { moduleFor, AbstractTestCase } from 'internal-test-helpers';
 
 function testObserver(assert, setup, teardown, key = 'key') {
   let obj = {};
@@ -22,47 +21,50 @@ function testObserver(assert, setup, teardown, key = 'key') {
   assert.equal(isWatching(obj, key), false, 'isWatching is false after observers are removed');
 }
 
-QUnit.test('isWatching is true for regular local observers', function(assert) {
-  testObserver(assert, (obj, key, fn) => {
-    Mixin.create({
-      didChange: observer(key, fn)
-    }).apply(obj);
-  }, (obj, key, fn) => removeObserver(obj, key, obj, fn));
+moduleFor('isWatching', class extends AbstractTestCase {
+  ['@test isWatching is true for regular local observers'](assert) {
+    testObserver(assert, (obj, key, fn) => {
+      Mixin.create({
+        didChange: observer(key, fn)
+      }).apply(obj);
+    }, (obj, key, fn) => removeObserver(obj, key, obj, fn));
+  }
+
+  ['@test isWatching is true for nonlocal observers'](assert) {
+    testObserver(assert, (obj, key, fn) => {
+      addObserver(obj, key, obj, fn);
+    }, (obj, key, fn) => removeObserver(obj, key, obj, fn));
+  }
+
+  ['@test isWatching is true for chained observers'](assert) {
+    testObserver(assert, function(obj, key, fn) {
+      addObserver(obj, key + '.bar', obj, fn);
+    }, function(obj, key, fn) {
+      removeObserver(obj, key + '.bar', obj, fn);
+    });
+  }
+
+  ['@test isWatching is true for computed properties'](assert) {
+    testObserver(assert, (obj, key, fn) => {
+      defineProperty(obj, 'computed', computed(fn).property(key));
+      get(obj, 'computed');
+    }, obj => defineProperty(obj, 'computed', null));
+  }
+
+  ['@test isWatching is true for chained computed properties'](assert) {
+    testObserver(assert, (obj, key, fn) => {
+      defineProperty(obj, 'computed', computed(fn).property(key + '.bar'));
+      get(obj, 'computed');
+    }, obj => defineProperty(obj, 'computed', null));
+  }
+
+  // can't watch length on Array - it is special...
+  // But you should be able to watch a length property of an object
+  ['@test isWatching is true for \'length\' property on object'](assert) {
+    testObserver(assert, (obj, key, fn) => {
+      defineProperty(obj, 'length', null, '26.2 miles');
+      addObserver(obj, 'length', obj, fn);
+    }, (obj, key, fn) => removeObserver(obj, 'length', obj, fn), 'length');
+  }
 });
 
-QUnit.test('isWatching is true for nonlocal observers', function(assert) {
-  testObserver(assert, (obj, key, fn) => {
-    addObserver(obj, key, obj, fn);
-  }, (obj, key, fn) => removeObserver(obj, key, obj, fn));
-});
-
-QUnit.test('isWatching is true for chained observers', function(assert) {
-  testObserver(assert, function(obj, key, fn) {
-    addObserver(obj, key + '.bar', obj, fn);
-  }, function(obj, key, fn) {
-    removeObserver(obj, key + '.bar', obj, fn);
-  });
-});
-
-QUnit.test('isWatching is true for computed properties', function(assert) {
-  testObserver(assert, (obj, key, fn) => {
-    defineProperty(obj, 'computed', computed(fn).property(key));
-    get(obj, 'computed');
-  }, obj => defineProperty(obj, 'computed', null));
-});
-
-QUnit.test('isWatching is true for chained computed properties', function(assert) {
-  testObserver(assert, (obj, key, fn) => {
-    defineProperty(obj, 'computed', computed(fn).property(key + '.bar'));
-    get(obj, 'computed');
-  }, obj => defineProperty(obj, 'computed', null));
-});
-
-// can't watch length on Array - it is special...
-// But you should be able to watch a length property of an object
-QUnit.test('isWatching is true for \'length\' property on object', function(assert) {
-  testObserver(assert, (obj, key, fn) => {
-    defineProperty(obj, 'length', null, '26.2 miles');
-    addObserver(obj, 'length', obj, fn);
-  }, (obj, key, fn) => removeObserver(obj, 'length', obj, fn), 'length');
-});
