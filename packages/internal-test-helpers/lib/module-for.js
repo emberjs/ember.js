8350298a1aa23fd5ea23d0c74f0d3a25ff8ea3b4
Robert Jackson <rjackson@linkedin.com>
2018-01-05 00:12:20 -0500

    Conditionally return a promise.
    This is 🍌s and I don't like it one bit, but I also don't want to wait
    3 times as long for the test suite to finish.

--- packages/internal-test-helpers/lib/module-for.js
+++ packages/internal-test-helpers/lib/module-for.js
@@ -22,7 +22,19 @@ export default function moduleFor(description, TestClass, ...mixins) {
         promises.push(context.afterEach());
       }
 
-      return all(promises);
+      // this seems odd, but actually saves significant time
+      // in the test suite
+      //
+      // returning a promise from a QUnit test always adds a 13ms
+      // delay to the test, this filtering prevents returning a
+      // promise when it is not needed
+      //
+      // Remove after we can update to QUnit that includes
+      // https://github.com/qunitjs/qunit/pull/1246
+      let filteredPromises = promises.filter(Boolean);
+      if (filteredPromises.length > 0) {
+        return all(filteredPromises);
+      }
     }
   });
 
