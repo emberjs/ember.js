db305cfc45b84c5b9b1c2b8c8b13b11821e74658
Robert Jackson <rjackson@linkedin.com>
2018-01-05 11:47:34 -0500

    [BUGFIX beta] Add simple Ember.run.debounce tests.
    Some very basic tests around common usage patterns to help prevent
    regressions in the future.

--- packages/ember-metal/tests/run_loop/debounce_test.js
+++ packages/ember-metal/tests/run_loop/debounce_test.js
@@ -1,22 +1,80 @@
 import { run } from '../..';
 import { moduleFor, AbstractTestCase } from 'internal-test-helpers';
 
-const originalDebounce = run.backburner.debounce;
-let wasCalled = false;
-
 moduleFor('Ember.run.debounce', class extends AbstractTestCase {
-  constructor() {
-    super();
+  ['@test Ember.run.debounce - with target, with method, without args'](assert) {
+    let done = assert.async();
 
-    run.backburner.debounce = function() { wasCalled = true; };
+    let calledWith = [];
+    let target = {
+      someFunc(...args) {
+        calledWith.push(args);
+      }
+    };
+
+    run.debounce(target, target.someFunc, 10);
+    run.debounce(target, target.someFunc, 10);
+    run.debounce(target, target.someFunc, 10);
+
+    setTimeout(() => {
+      assert.deepEqual(calledWith, [ [] ], 'someFunc called once with correct arguments');
+      done();
+    }, 20);
   }
 
-  teardown() {
-    run.backburner.debounce = originalDebounce;
+  ['@test Ember.run.debounce - with target, with method name, without args'](assert) {
+    let done = assert.async();
+
+    let calledWith = [];
+    let target = {
+      someFunc(...args) {
+        calledWith.push(args);
+      }
+    };
+
+    run.debounce(target, 'someFunc', 10);
+    run.debounce(target, 'someFunc', 10);
+    run.debounce(target, 'someFunc', 10);
+
+    setTimeout(() => {
+      assert.deepEqual(calledWith, [ [] ], 'someFunc called once with correct arguments');
+      done();
+    }, 20);
   }
 
-  ['@test Ember.run.debounce uses Backburner.debounce'](assert) {
-    run.debounce(() => {});
-    assert.ok(wasCalled, 'Ember.run.debounce used');
+  ['@test Ember.run.debounce - without target, without args'](assert) {
+    let done = assert.async();
+
+    let calledWith = [];
+    function someFunc(...args) {
+      calledWith.push(args);
+    }
+
+    run.debounce(someFunc, 10);
+    run.debounce(someFunc, 10);
+    run.debounce(someFunc, 10);
+
+    setTimeout(() => {
+      assert.deepEqual(calledWith, [ [] ], 'someFunc called once with correct arguments');
+      done();
+    }, 20);
+  }
+
+  ['@test Ember.run.debounce - without target, with args'](assert) {
+    let done = assert.async();
+
+    let calledWith = [];
+    function someFunc(...args) {
+      calledWith.push(args);
+    }
+
+    run.debounce(someFunc, { isFoo: true }, 10);
+    run.debounce(someFunc, { isBar: true }, 10);
+    run.debounce(someFunc, { isBaz: true }, 10);
+
+    setTimeout(() => {
+      assert.deepEqual(calledWith, [ [ { isBaz: true } ] ], 'someFunc called once with correct arguments');
+      done();
+    }, 20);
   }
 });
