c0517ee2c434b3fd1561b874b6dde575fbfa03d0
Ruslan Grabovoy <kudgo.test@gmail.com>
2018-01-06 02:55:23 +0200

    do not use jQuery in ember-template-compiler tests

--- packages/ember-template-compiler/tests/system/bootstrap-test.js
+++ packages/ember-template-compiler/tests/system/bootstrap-test.js
@@ -1,5 +1,4 @@
 import { run } from 'ember-metal';
-import { jQuery } from 'ember-views';
 import {
   Component,
   getTemplate,
@@ -22,9 +21,10 @@ function checkTemplate(templateName, assert) {
   run(() => bootstrap({ context: fixture, hasTemplate, setTemplate }));
 
   let template = getTemplate(templateName);
+  let qunitFixture = document.querySelector('#qunit-fixture');
 
   assert.ok(template, 'template is available on Ember.TEMPLATES');
-  assert.equal(jQuery('#qunit-fixture script').length, 0, 'script removed');
+  assert.notOk(qunitFixture.querySelector('script'), 'script removed');
 
   let owner = buildOwner();
   owner.register('template:-top-level', template);
@@ -37,7 +37,7 @@ function checkTemplate(templateName, assert) {
   component = owner.lookup('component:-top-level');
   runAppend(component);
 
-  assert.equal(jQuery('#qunit-fixture').text().trim(), 'Tobias takes teamocil', 'template works');
+  assert.equal(qunitFixture.textContent.trim(), 'Tobias takes teamocil', 'template works');
   runDestroy(component);
 }
 
@@ -54,26 +54,26 @@ moduleFor('ember-templates: bootstrap', class extends AbstractTestCase {
   }
 
   ['@test template with data-template-name should add a new template to Ember.TEMPLATES'](assert) {
-    jQuery('#qunit-fixture').html('<script type="text/x-handlebars" data-template-name="funkyTemplate">{{firstName}} takes {{drug}}</script>');
+    fixture.innerHTML = '<script type="text/x-handlebars" data-template-name="funkyTemplate">{{firstName}} takes {{drug}}</script>';
 
     checkTemplate('funkyTemplate', assert);
   }
 
   ['@test template with id instead of data-template-name should add a new template to Ember.TEMPLATES'](assert) {
-    jQuery('#qunit-fixture').html('<script type="text/x-handlebars" id="funkyTemplate" >{{firstName}} takes {{drug}}</script>');
+    fixture.innerHTML = '<script type="text/x-handlebars" id="funkyTemplate" >{{firstName}} takes {{drug}}</script>';
 
     checkTemplate('funkyTemplate', assert);
   }
 
   ['@test template without data-template-name or id should default to application'](assert) {
-    jQuery('#qunit-fixture').html('<script type="text/x-handlebars">{{firstName}} takes {{drug}}</script>');
+    fixture.innerHTML = '<script type="text/x-handlebars">{{firstName}} takes {{drug}}</script>';
 
     checkTemplate('application', assert);
   }
 
   // Add this test case, only for typeof Handlebars === 'object';
   [`${typeof Handlebars === 'object' ? '@test' : '@skip'} template with type text/x-raw-handlebars should be parsed`](assert) {
-    jQuery('#qunit-fixture').html('<script type="text/x-raw-handlebars" data-template-name="funkyTemplate">{{name}}</script>');
+    fixture.innerHTML = '<script type="text/x-raw-handlebars" data-template-name="funkyTemplate">{{name}}</script>';
 
     run(() => bootstrap({ context: fixture, hasTemplate, setTemplate }));
 
@@ -86,7 +86,7 @@ moduleFor('ember-templates: bootstrap', class extends AbstractTestCase {
   }
 
   ['@test duplicated default application templates should throw exception'](assert) {
-    jQuery('#qunit-fixture').html('<script type="text/x-handlebars">first</script><script type="text/x-handlebars">second</script>');
+    fixture.innerHTML = '<script type="text/x-handlebars">first</script><script type="text/x-handlebars">second</script>';
 
     assert.throws(() => bootstrap({ context: fixture, hasTemplate, setTemplate }),
            /Template named "[^"]+" already exists\./,
@@ -94,7 +94,7 @@ moduleFor('ember-templates: bootstrap', class extends AbstractTestCase {
   }
 
   ['@test default default application template and id application template present should throw exception'](assert) {
-    jQuery('#qunit-fixture').html('<script type="text/x-handlebars">first</script><script type="text/x-handlebars" id="application">second</script>');
+    fixture.innerHTML = '<script type="text/x-handlebars">first</script><script type="text/x-handlebars" id="application">second</script>';
 
     assert.throws(() => bootstrap({ context: fixture, hasTemplate, setTemplate }),
            /Template named "[^"]+" already exists\./,
@@ -102,7 +102,7 @@ moduleFor('ember-templates: bootstrap', class extends AbstractTestCase {
   }
 
   ['@test default application template and data-template-name application template present should throw exception'](assert) {
-    jQuery('#qunit-fixture').html('<script type="text/x-handlebars">first</script><script type="text/x-handlebars" data-template-name="application">second</script>');
+    fixture.innerHTML = '<script type="text/x-handlebars">first</script><script type="text/x-handlebars" data-template-name="application">second</script>';
 
     assert.throws(() => bootstrap({ context: fixture, hasTemplate, setTemplate }),
            /Template named "[^"]+" already exists\./,
@@ -110,7 +110,7 @@ moduleFor('ember-templates: bootstrap', class extends AbstractTestCase {
   }
 
   ['@test duplicated template id should throw exception'](assert) {
-    jQuery('#qunit-fixture').html('<script type="text/x-handlebars" id="funkyTemplate">first</script><script type="text/x-handlebars" id="funkyTemplate">second</script>');
+    fixture.innerHTML = '<script type="text/x-handlebars" id="funkyTemplate">first</script><script type="text/x-handlebars" id="funkyTemplate">second</script>';
 
     assert.throws(() => bootstrap({ context: fixture, hasTemplate, setTemplate }),
            /Template named "[^"]+" already exists\./,
@@ -118,7 +118,7 @@ moduleFor('ember-templates: bootstrap', class extends AbstractTestCase {
   }
 
   ['@test duplicated template data-template-name should throw exception'](assert) {
-    jQuery('#qunit-fixture').html('<script type="text/x-handlebars" data-template-name="funkyTemplate">first</script><script type="text/x-handlebars" data-template-name="funkyTemplate">second</script>');
+    fixture.innerHTML = '<script type="text/x-handlebars" data-template-name="funkyTemplate">first</script><script type="text/x-handlebars" data-template-name="funkyTemplate">second</script>';
 
     assert.throws(() => bootstrap({ context: fixture, hasTemplate, setTemplate }),
            /Template named "[^"]+" already exists\./,
