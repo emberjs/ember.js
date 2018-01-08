44f52006414f11a49ff5efcc5f6088093f4e3ab0
Steven Footea <sfoote@linkedin.com>
2018-01-08 11:25:52 -0700

    Fix textarea helper tests

--- packages/ember-glimmer/lib/syntax/-text-area.ts
+++ packages/ember-glimmer/lib/syntax/-text-area.ts
@@ -1,8 +1,12 @@
+import {
+  LazyOpcodeBuilder,
+  TemplateMeta
+} from '@glimmer/opcode-compiler';
 import { wrapComponentClassAttribute } from '../utils/bindings';
 import { hashToArgs } from './utils';
 
-export function textAreaMacro(_name: string, params: any[], hash: any, builder: any) {
-  let definition = builder.env.getComponentDefinition('-text-area', builder.meta.templateMeta);
+export function textAreaMacro(_name: string, params: any[], hash: any, builder: LazyOpcodeBuilder<TemplateMeta>) {
+  let definition = builder.resolver.lookupComponentDefinition('-text-area', builder.referrer);
   wrapComponentClassAttribute(hash);
   builder.component.static(definition, [params, hashToArgs(hash), null, null]);
   return true;
