8e779f65e47e95845e3295d0b6deb847608f9331
Kris Selden <kris.selden@gmail.com>
2018-01-16 14:41:24 -0800

    cleanup test case class
    this stuff is already setup by base class

--- packages/internal-test-helpers/lib/test-cases/rendering.js
+++ packages/internal-test-helpers/lib/test-cases/rendering.js
@@ -1,5 +1,3 @@
-import { ComponentLookup } from 'ember-views';
-
 import AbstractRenderingTestCase from './abstract-rendering';
 
 export default class RenderingTestCase extends AbstractRenderingTestCase {
@@ -8,8 +6,5 @@ export default class RenderingTestCase extends AbstractRenderingTestCase {
     let { owner } = this;
 
     this.env = owner.lookup('service:-glimmer-environment');
-    owner.register('component-lookup:main', ComponentLookup);
-    owner.registerOptionsForType('helper', { instantiate: false });
-    owner.registerOptionsForType('component', { singleton: false });
   }
 }
