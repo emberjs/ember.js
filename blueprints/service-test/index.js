27c215d3f15ae97312a0209833e4eab5687c2913
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-01-21 09:47:50 +0100

    blueprints/service-test: Use `options.entity-name` for test description
    (cherry picked from commit 4d5fdc0ad473866fd5d4f0f6acdddc53233a3ecd)

--- blueprints/service-test/index.js
+++ blueprints/service-test/index.js
@@ -1,14 +1,12 @@
 'use strict';
 
-const testInfo = require('ember-cli-test-info');
-
 const useTestFrameworkDetector = require('../test-framework-detector');
 
 module.exports = useTestFrameworkDetector({
   description: 'Generates a service unit test.',
   locals: function(options) {
     return {
-      friendlyTestDescription: testInfo.description(options.entity.name, 'Unit', 'Service')
+      friendlyTestDescription: ['Unit', 'Service', options.entity.name].join(' | ')
     };
   },
 });
