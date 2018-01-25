eeecb9b2c88fb5ec7943bae52950c1e2fa439b42
Robert Jackson <rjackson@linkedin.com>
2018-01-25 09:26:54 -0800

    Fix backwards compat shim for custom AST plugins.

--- packages/ember-template-compiler/lib/system/compile-options.js
+++ packages/ember-template-compiler/lib/system/compile-options.js
@@ -16,27 +16,24 @@ export default function compileOptions(_options) {
     options.plugins = { ast: [...USER_PLUGINS, ...PLUGINS] };
   } else {
     let potententialPugins = [...USER_PLUGINS, ...PLUGINS];
+    let providedPlugins = options.plugins.ast.map(plugin => wrapLegacyPluginIfNeeded(plugin));
     let pluginsToAdd = potententialPugins.filter((plugin) => {
       return options.plugins.ast.indexOf(plugin) === -1;
     });
-    options.plugins.ast = options.plugins.ast.slice().concat(pluginsToAdd);
+    options.plugins.ast = providedPlugins.concat(pluginsToAdd);
   }
 
   return options;
 }
 
-export function registerPlugin(type, _plugin) {
-  if (type !== 'ast') {
-    throw new Error(`Attempting to register ${_plugin} as "${type}" which is not a valid Glimmer plugin type.`);
-  }
-
-  let plugin;
+function wrapLegacyPluginIfNeeded(_plugin) {
+  let plugin = _plugin;
   if (_plugin.prototype && _plugin.prototype.transform) {
     plugin = (env) => {
       return {
         name: _plugin.constructor && _plugin.constructor.name,
 
-        visitors: {
+        visitor: {
           Program(node) {
             let plugin = new _plugin(env);
 
@@ -45,16 +42,24 @@ export function registerPlugin(type, _plugin) {
             return plugin.transform(node);
           }
         }
-        };
+      };
     };
-  } else {
-    plugin = _plugin;
   }
+
+  return plugin;
+}
+
+export function registerPlugin(type, _plugin) {
+  if (type !== 'ast') {
+    throw new Error(`Attempting to register ${_plugin} as "${type}" which is not a valid Glimmer plugin type.`);
+  }
+
+  let plugin = wrapLegacyPluginIfNeeded(_plugin);
 
   USER_PLUGINS = [plugin, ...USER_PLUGINS];
 }
 
-export function removePlugin(type, PluginClass) {
+export function unregisterPlugin(type, PluginClass) {
   if (type !== 'ast') {
     throw new Error(`Attempting to unregister ${PluginClass} as "${type}" which is not a valid Glimmer plugin type.`);
   }
