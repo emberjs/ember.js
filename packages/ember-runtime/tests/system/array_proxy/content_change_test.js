b5b21172e831631bcf5d4f1eef62f19c015d1c93
Martin Muñoz <im.mmun@gmail.com>
2018-01-27 02:39:03 -0500

    [BUGFIX] Fix a few bugs in the caching ArrayProxy implementation
    (cherry picked from commit 9b68ff9219f8a935d1e95bd98f94d2a2217b6f3a)

--- packages/ember-runtime/tests/system/array_proxy/content_change_test.js
+++ packages/ember-runtime/tests/system/array_proxy/content_change_test.js
@@ -1,4 +1,4 @@
-import { set, run } from 'ember-metal';
+import { get, set, run, changeProperties } from 'ember-metal';
 import { not } from '../../../computed/computed_macros';
 import ArrayProxy from '../../../system/array_proxy';
 import { A as emberA } from '../../../system/native_array';
@@ -44,4 +44,94 @@ QUnit.test('The ArrayProxy doesn\'t explode when assigned a destroyed object', f
   set(proxy2, 'content', proxy1);
 
   ok(true, 'No exception was raised');
+});
+
+QUnit.test('should update if content changes while change events are deferred', function(assert) {
+  let proxy = ArrayProxy.create();
+
+  assert.deepEqual(proxy.toArray(), []);
+
+  changeProperties(() => {
+    proxy.set('content', emberA([1, 2, 3]));
+    assert.deepEqual(proxy.toArray(), [1, 2, 3]);
+  });
+});
+
+QUnit.test('objectAt recomputes the object cache correctly', function(assert) {
+  let indexes = [];
+
+  let proxy = ArrayProxy.extend({
+    objectAtContent(index) {
+      indexes.push(index);
+      return this.content[index];
+    }
+  }).create({
+    content: emberA([1, 2, 3, 4, 5])
+  });
+
+  assert.deepEqual(indexes, []);
+  assert.deepEqual(proxy.objectAt(0), 1);
+  assert.deepEqual(indexes, [0, 1, 2, 3, 4]);
+
+  indexes.length = 0;
+  proxy.set('content', emberA([1, 2, 3]));
+  assert.deepEqual(proxy.objectAt(0), 1);
+  assert.deepEqual(indexes, [0, 1, 2]);
+
+  indexes.length = 0;
+  proxy.content.replace(2, 0, [4, 5]);
+  assert.deepEqual(proxy.objectAt(0), 1);
+  assert.deepEqual(proxy.objectAt(1), 2);
+  assert.deepEqual(indexes, []);
+  assert.deepEqual(proxy.objectAt(2), 4);
+  assert.deepEqual(indexes, [2, 3, 4]);
+});
+
+QUnit.test('getting length does not recompute the object cache', function(assert) {
+  let indexes = [];
+
+  let proxy = ArrayProxy.extend({
+    objectAtContent(index) {
+      indexes.push(index);
+      return this.content[index];
+    }
+  }).create({
+    content: emberA([1, 2, 3, 4, 5])
+  });
+
+  assert.equal(get(proxy, 'length'), 5);
+  assert.deepEqual(indexes, []);
+
+  indexes.length = 0;
+  proxy.set('content', emberA([6, 7, 8]));
+  assert.equal(get(proxy, 'length'), 3);
+  assert.deepEqual(indexes, []);
+
+  indexes.length = 0;
+  proxy.content.replace(1, 0, [1, 2, 3]);
+  assert.equal(get(proxy, 'length'), 6);
+  assert.deepEqual(indexes, []);
+});
+
+QUnit.test('negative indexes are handled correctly', function(assert) {
+  let indexes = [];
+
+  let proxy = ArrayProxy.extend({
+    objectAtContent(index) {
+      indexes.push(index);
+      return this.content[index];
+    }
+  }).create({
+    content: emberA([1, 2, 3, 4, 5])
+  });
+
+  assert.deepEqual(proxy.toArray(), [1, 2, 3, 4, 5]);
+
+  indexes.length = 0;
+
+  proxy.content.replace(-1, 0, [7]);
+  proxy.content.replace(-2, 0, [6]);
+
+  assert.deepEqual(proxy.toArray(), [1, 2, 3, 4, 6, 7, 5]);
+  assert.deepEqual(indexes, [4, 5, 6]);
 });
