4fb3ba47e8fa1e53b35c0ad8627ff40dcb03df81
Kris Selden <kris.selden@gmail.com>
2018-01-31 21:19:40 -0800

    fix performance issue with babel new Array() for operands
    remove unused compact method

--- packages/@glimmer/encoder/lib/encoder.ts
+++ packages/@glimmer/encoder/lib/encoder.ts
@@ -13,27 +13,26 @@ export class InstructionEncoder {
   constructor(public buffer: Operand[]) {}
   typePos = 0;
   size = 0;
-  encode(type: Op, machine: 0 | typeof MACHINE_MASK, ...operands: Operand[]) {
+
+  encode(type: Op, machine: 0 | typeof MACHINE_MASK, ...operands: Operand[]): void;
+  encode(type: Op, machine: 0 | typeof MACHINE_MASK) {
     if (type > TYPE_SIZE) {
       throw new Error(`Opcode type over 8-bits. Got ${type}.`);
     }
 
-    this.buffer.push((type | machine | (operands.length << ARG_SHIFT)));
+    this.buffer.push((type | machine | (arguments.length - 2 << ARG_SHIFT)));
 
     this.typePos = this.buffer.length - 1;
 
-    operands.forEach(op => {
+    for (let i = 2; i < arguments.length; i++) {
+      let op = arguments[i];
       if (typeof op === 'number' && op > MAX_SIZE) {
         throw new Error(`Operand over 16-bits. Got ${op}.`);
       }
       this.buffer.push(op);
-    });
+    };
 
     this.size = this.buffer.length;
-  }
-
-  compact(program: number[]) {
-    return String.fromCharCode(...program);
   }
 
   patch(position: number, operand: number) {
