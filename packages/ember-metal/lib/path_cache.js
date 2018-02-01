0c81a324d99cefb525bf316c8708e5c1e7f435eb
bekzod <bekzod@me.com>
2018-02-01 16:04:20 +0500

    remove unused `caches` export

--- packages/ember-metal/lib/path_cache.js
+++ packages/ember-metal/lib/path_cache.js
@@ -2,10 +2,6 @@ import Cache from './cache';
 
 const firstDotIndexCache = new Cache(1000, key => key.indexOf('.'));
 
-export const caches = {
-  firstDotIndexCache
-};
-
 export function isPath(path) {
   return firstDotIndexCache.get(path) !== -1;
 }
