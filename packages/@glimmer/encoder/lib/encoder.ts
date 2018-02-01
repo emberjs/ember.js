9f5b579de9e2ff5e3531fcf0df94a82ef318070c
Kris Selden <kris.selden@gmail.com>
2018-01-31 22:49:14 -0800

    make sure constant get built

--- packages/@glimmer/encoder/lib/encoder.ts
+++ packages/@glimmer/encoder/lib/encoder.ts
@@ -1,11 +1,13 @@
 import { Op } from "@glimmer/vm";
 
-export const ARG_SHIFT = 8;
-const MAX_SIZE                = 0b1111111111111111;
-export const TYPE_SIZE        = 0b11111111;
-export const TYPE_MASK        = 0b0000000011111111;
-export const OPERAND_LEN_MASK = 0b0000001100000000;
-export const MACHINE_MASK     = 0b0000010000000000;
+export const enum OpcodeSize {
+  ARG_SHIFT = 8,
+  MAX_SIZE  = 0b1111111111111111,
+  TYPE_SIZE = 0b11111111,
+  TYPE_MASK        = 0b0000000011111111,
+  OPERAND_LEN_MASK = 0b0000001100000000,
+  MACHINE_MASK     = 0b0000010000000000,
+}
 
 export type Operand = number | (() => number);
 
@@ -14,19 +16,19 @@ export class InstructionEncoder {
   typePos = 0;
   size = 0;
 
-  encode(type: Op, machine: 0 | typeof MACHINE_MASK, ...operands: Operand[]): void;
-  encode(type: Op, machine: 0 | typeof MACHINE_MASK) {
-    if (type > TYPE_SIZE) {
+  encode(type: Op, machine: 0 | OpcodeSize.MACHINE_MASK, ...operands: Operand[]): void;
+  encode(type: Op, machine: 0 | OpcodeSize.MACHINE_MASK) {
+    if (type as number > OpcodeSize.TYPE_SIZE) {
       throw new Error(`Opcode type over 8-bits. Got ${type}.`);
     }
 
-    this.buffer.push((type | machine | (arguments.length - 2 << ARG_SHIFT)));
+    this.buffer.push((type | machine | (arguments.length - 2 << OpcodeSize.ARG_SHIFT)));
 
     this.typePos = this.buffer.length - 1;
 
     for (let i = 2; i < arguments.length; i++) {
       let op = arguments[i];
-      if (typeof op === 'number' && op > MAX_SIZE) {
+      if (typeof op === 'number' && op > OpcodeSize.MAX_SIZE) {
         throw new Error(`Operand over 16-bits. Got ${op}.`);
       }
       this.buffer.push(op);
