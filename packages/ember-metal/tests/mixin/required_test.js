30d70bd3438ecd01e5f9b15eca9a504b1bee5499
Travis Hoover <thoov7@gmail.com>
2018-02-08 15:34:58 -0800

    Cleaning up hack

--- packages/ember-metal/tests/mixin/required_test.js
+++ packages/ember-metal/tests/mixin/required_test.js
@@ -11,9 +11,16 @@ let PartialMixin, FinalMixin, obj;
 let originalEnvVal;
 
 moduleFor('Module.required', class extends AbstractTestCase {
-  beforeEach() {
+  beforeEach(assert) {
     originalEnvVal = ENV._ENABLE_PROPERTY_REQUIRED_SUPPORT;
     ENV._ENABLE_PROPERTY_REQUIRED_SUPPORT = true;
+
+    assert.expectDeprecation(() => {
+      PartialMixin = Mixin.create({
+        foo: required(),
+        bar: 'BAR'
+      });
+    }, 'Ember.required is deprecated as its behavior is inconsistent and unreliable.');
 
     FinalMixin = Mixin.create({
       foo: 'FOO'
@@ -28,59 +35,28 @@ moduleFor('Module.required', class extends AbstractTestCase {
   }
 
   ['@test applying a mixin to meet requirement'](assert) {
-    assert.expectDeprecation(() => {
-      PartialMixin = Mixin.create({
-        foo: required(),
-        bar: 'BAR'
-      });
-    }, 'Ember.required is deprecated as its behavior is inconsistent and unreliable.');
-
     FinalMixin.apply(obj);
     PartialMixin.apply(obj);
     assert.equal(get(obj, 'foo'), 'FOO', 'should now be defined');
   }
 
   ['@test combined mixins to meet requirement'](assert) {
-    assert.expectDeprecation(() => {
-      PartialMixin = Mixin.create({
-        foo: required(),
-        bar: 'BAR'
-      });
-    }, 'Ember.required is deprecated as its behavior is inconsistent and unreliable.');
     Mixin.create(PartialMixin, FinalMixin).apply(obj);
     assert.equal(get(obj, 'foo'), 'FOO', 'should now be defined');
   }
 
   ['@test merged mixin'](assert) {
-    assert.expectDeprecation(() => {
-      PartialMixin = Mixin.create({
-        foo: required(),
-        bar: 'BAR'
-      });
-    }, 'Ember.required is deprecated as its behavior is inconsistent and unreliable.');
     Mixin.create(PartialMixin, { foo: 'FOO' }).apply(obj);
     assert.equal(get(obj, 'foo'), 'FOO', 'should now be defined');
   }
 
   ['@test define property on source object'](assert) {
-    assert.expectDeprecation(() => {
-      PartialMixin = Mixin.create({
-        foo: required(),
-        bar: 'BAR'
-      });
-    }, 'Ember.required is deprecated as its behavior is inconsistent and unreliable.');
     obj.foo = 'FOO';
     PartialMixin.apply(obj);
     assert.equal(get(obj, 'foo'), 'FOO', 'should now be defined');
   }
 
   ['@test using apply'](assert) {
-    assert.expectDeprecation(() => {
-      PartialMixin = Mixin.create({
-        foo: required(),
-        bar: 'BAR'
-      });
-    }, 'Ember.required is deprecated as its behavior is inconsistent and unreliable.');
     mixin(obj, PartialMixin, { foo: 'FOO' });
     assert.equal(get(obj, 'foo'), 'FOO', 'should now be defined');
   }
