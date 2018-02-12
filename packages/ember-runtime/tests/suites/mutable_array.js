151616c94643701776358a79221c7d11096978c9
Chad Hietala <chadhietala@gmail.com>
2018-02-11 21:59:49 -0500

    Migrate removeAt, removeObject, removeObjects

--- packages/ember-runtime/tests/suites/mutable_array.js
+++ packages/ember-runtime/tests/suites/mutable_array.js
@@ -1,13 +1,11 @@
 import { ArrayTests } from './array';
 
-import removeAtTests from './mutable_array/removeAt';
 import replaceTests from './mutable_array/replace';
 import shiftObjectTests from './mutable_array/shiftObject';
 import unshiftObjectTests from './mutable_array/unshiftObject';
 import reverseObjectsTests from './mutable_array/reverseObjects';
 
 const MutableArrayTests = ArrayTests.extend();
-MutableArrayTests.importModuleTests(removeAtTests);
 MutableArrayTests.importModuleTests(replaceTests);
 MutableArrayTests.importModuleTests(shiftObjectTests);
 MutableArrayTests.importModuleTests(unshiftObjectTests);
