9dc66ab2c065d972892a7f53f4a2e68ef32d3d1b
Matthew Beale <matt.beale@madhatted.com>
2018-02-12 21:07:26 -0800

    Add referrer capability to the test resolver

--- packages/internal-test-helpers/lib/test-resolver.js
+++ packages/internal-test-helpers/lib/test-resolver.js
@@ -1,25 +1,43 @@
 import { compile } from 'ember-template-compiler';
 
+const DELIMITER = '\0';
+
+function serializeKey(specifier, source) {
+  return [specifier, source].join(DELIMITER);
+}
+
 class Resolver {
   constructor() {
     this._registered = {};
     this.constructor.lastInstance = this;
   }
-  resolve(specifier) {
-    return this._registered[specifier];
+  resolve(specifier, source) {
+    return this._registered[serializeKey(specifier, source)];
   }
-  add(specifier, factory) {
-    if (specifier.indexOf(':') === -1) {
-      throw new Error('Specifiers added to the resolver must be in the format of type:name');
+  add(lookup, factory) {
+    let key;
+    switch (typeof lookup) {
+      case 'string':
+        if (lookup.indexOf(':') === -1) {
+          throw new Error('Specifiers added to the resolver must be in the format of type:name');
+        }
+        key = serializeKey(lookup);
+        break;
+      case 'object':
+        key = serializeKey(lookup.specifier, lookup.source);
+        break;
+      default:
+        throw new Error('Specifier string has an unknown type');
     }
-    return this._registered[specifier] = factory;
+
+    return this._registered[key] = factory;
   }
   addTemplate(templateName, template) {
     let templateType = typeof template;
     if (templateType !== 'string') {
       throw new Error(`You called addTemplate for "${templateName}" with a template argument of type of '${templateType}'. addTemplate expects an argument of an uncompiled template as a string.`);
     }
-    return this._registered[`template:${templateName}`] = compile(template, {
+    return this._registered[serializeKey(`template:${templateName}`)] = compile(template, {
       moduleName: templateName
     });
   }
