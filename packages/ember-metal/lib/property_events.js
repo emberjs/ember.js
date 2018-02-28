1053798c59ab718b193c197aacacd0d3b50126ff
Bryan Crotaz <bgcrotaz@hotmail.com>
2018-02-28 13:15:41 +0000

    ad public wrapper for changeProperties in Ember.Observable

--- packages/ember-metal/lib/property_events.js
+++ packages/ember-metal/lib/property_events.js
@@ -206,7 +206,7 @@ function endPropertyChanges() {
 
   @method changeProperties
   @param {Function} callback
-  @public
+  @private
 */
 function changeProperties(callback) {
   beginPropertyChanges();
