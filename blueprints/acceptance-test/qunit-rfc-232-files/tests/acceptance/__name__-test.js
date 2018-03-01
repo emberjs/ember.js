304fbe591f0030247e356570d8e89855cfed08ef
Ryan Toronto <ryanto@gmail.com>
2018-03-01 16:08:54 -0500

    Acceptance testing blueprint will use the app's setup-application-test blueprint.

--- blueprints/acceptance-test/qunit-rfc-232-files/tests/acceptance/__name__-test.js
+++ blueprints/acceptance-test/qunit-rfc-232-files/tests/acceptance/__name__-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
-import { setupApplicationTest } from 'ember-qunit';
+import setupApplicationTest from '<%= dasherizedPackageName %>/tests/helpers/setup-application-test';
 
 module('<%= friendlyTestName %>', function(hooks) {
   setupApplicationTest(hooks);
