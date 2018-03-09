821a34ede19ee4a57de3d8273ad3fb3a902c1344
Sergio Arbeo <serabe@gmail.com>
2018-03-09 18:36:11 +0100

    [BUGFIX] Fix initialiters tests blueprints
    Fixes #16344
    
    (cherry picked from commit dc63a2449001eca92b2b3933799b073775372226)

--- blueprints/instance-initializer-test/index.js
+++ blueprints/instance-initializer-test/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const path = require('path');
+const existsSync = require('exists-sync');
 const stringUtils = require('ember-cli-string-utils');
 
 const useTestFrameworkDetector = require('../test-framework-detector');
@@ -9,7 +11,8 @@ module.exports = useTestFrameworkDetector({
   locals: function(options) {
     return {
       friendlyTestName: ['Unit', 'Instance Initializer', options.entity.name].join(' | '),
-      dasherizedModulePrefix: stringUtils.dasherize(options.project.config().modulePrefix)
+      dasherizedModulePrefix: stringUtils.dasherize(options.project.config().modulePrefix),
+      destroyAppExists: existsSync(path.join(this.project.root, '/tests/helpers/destroy-app.js'))
     };
   }
 });
