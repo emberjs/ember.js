dc63a2449001eca92b2b3933799b073775372226
Sergio Arbeo <serabe@gmail.com>
2018-03-09 18:36:11 +0100

    [BUGFIX] Fix initialiters tests blueprints
    Fixes #16344

--- blueprints/initializer-test/qunit-rfc-232-files/tests/unit/initializers/__name__-test.js
+++ blueprints/initializer-test/qunit-rfc-232-files/tests/unit/initializers/__name__-test.js
@@ -3,7 +3,7 @@ import Application from '@ember/application';
 import { initialize } from '<%= dasherizedModulePrefix %>/initializers/<%= dasherizedModuleName %>';
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
-import destroyApp from '../../helpers/destroy-app';
+<% if (destroyAppExists) { %>import destroyApp from '../../helpers/destroy-app';<% } else { %>import { run } from '@ember/runloop';<% } %>
 
 module('<%= friendlyTestName %>', function(hooks) {
   setupTest(hooks);
@@ -19,7 +19,7 @@ module('<%= friendlyTestName %>', function(hooks) {
   });
 
   hooks.afterEach(function() {
-    destroyApp(this.application);
+    <% if (destroyAppExists) { %>destroyApp(this.application);<% } else { %>run(this.application, 'destroy');<% } %>
   });
 
   // Replace this with your real tests.
