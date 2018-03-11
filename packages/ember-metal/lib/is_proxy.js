06c40663a22e1a225d3308fd35487b27bdeef83d
bekzod <bekzod@me.com>
2018-03-12 00:49:13 +0500

    add weakset polyfill

--- packages/ember-metal/lib/is_proxy.js
+++ packages/ember-metal/lib/is_proxy.js
@@ -1,9 +1,11 @@
-const PROXIES = new WeakMap();
+import WeakSet from './weak_set';
+
+const PROXIES = new WeakSet();
 
 export function isProxy(object) {
   return PROXIES.has(object);
 }
 
 export function setProxy(object) {
-  return PROXIES.set(object, true);
+  return PROXIES.add(object);
 }
