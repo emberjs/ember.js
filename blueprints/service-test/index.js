9b1999873f7654b86ce18c4b689b82a7e4cbe891
Gavin Joyce <gavinjoyce@gmail.com>
2018-03-23 15:08:17 +0000

    [Feature] module unification service blueprint

--- blueprints/service-test/index.js
+++ blueprints/service-test/index.js
@@ -1,10 +1,38 @@
 'use strict';
 
 const useTestFrameworkDetector = require('../test-framework-detector');
+const isModuleUnificationProject = require('../module-unification').isModuleUnificationProject;
 
 module.exports = useTestFrameworkDetector({
   description: 'Generates a service unit test.',
-  locals: function(options) {
+
+  fileMapTokens() {
+    if (isModuleUnificationProject(this.project)) {
+      return {
+        __root__(options) {
+          if (options.pod) {
+            throw "Pods aren't supported within a module unification app";
+          }
+
+          return 'src';
+        },
+        __testType__() {
+          return '';
+        },
+      };
+    } else {
+      return {
+        __root__() {
+          return 'tests';
+        },
+        __testType__() {
+          return 'unit';
+        },
+      };
+    }
+  },
+
+  locals(options) {
     return {
       friendlyTestDescription: ['Unit', 'Service', options.entity.name].join(' | '),
     };
