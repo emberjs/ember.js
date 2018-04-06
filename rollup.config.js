fae7d703614555bab465d7ea8941b06830ab224a
Robert Jackson <rjackson@linkedin.com>
2018-04-06 17:22:37 -0400

    Begin experimenting with rollup API's...

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,20 @@
 
 const fs = require('fs');
 const path = require('path');
+const walkSync = require('walk-sync');
+
+let entryPoints = walkSync('dist/es', { directories: false })
+  .filter(path => path.startsWith('@ember') && path.endsWith('.js'))
+  .map(relativePath => relativePath.slice(0, -3));
 
 module.exports = {
-  input: 'dist/es/ember/index.js',
+  input: [...entryPoints, 'dist/es/ember-template-compiler/index', 'dist/es/ember/index'],
   plugins: [emberPackage()],
+
+  experimentalCodeSplitting: true,
   output: {
-    file: 'rollup-bundle.es.js',
-    format: 'iife',
-    name: '__Ember',
-    named: true,
+    dir: 'rollup-dist',
+    format: 'es',
     sourcemap: true,
   },
 };
