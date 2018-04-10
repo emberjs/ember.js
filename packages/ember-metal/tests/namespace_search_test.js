71192b2e86796e1f23ada1156ef970edc137a6ac
Alex Kanunnikov <lifeart92@gmail.com>
2018-04-10 20:00:23 +0300

    test comments update

--- packages/ember-metal/tests/namespace_search_test.js
+++ packages/ember-metal/tests/namespace_search_test.js
@@ -6,8 +6,8 @@ moduleFor(
   class extends AbstractTestCase {
     ['@test classToString: null as this inside class must not throw error'](assert) {
       let mixin = Mixin.create();
-      assert.equal(mixin.toString(), '(unknown)', 'this = null should be handled');
-      assert.equal(classToString(mixin), '(unknown)', 'this = null should be handled');
+      assert.equal(mixin.toString(), '(unknown)', 'this = null should be handled on Mixin.toString() call');
+      assert.equal(classToString(mixin), '(unknown)', 'this = null should be handled on classToString over mixin');
     }
   }
 );
