caea2ac78d4d496366ff07716dbb9db1842d474e
Alex Kanunnikov <lifeart92@gmail.com>
2018-04-10 15:47:12 +0300

    null should be null

--- packages/ember-metal/tests/namespace_search_test.js
+++ packages/ember-metal/tests/namespace_search_test.js
@@ -7,7 +7,7 @@ moduleFor(
     ['@test classToString: null as this inside class must not throw error'](assert) {
       function F() {"use strict"; return this };
       result = classToString(F.bind(null));
-      assert.equal(result, undefined, 'this = null should be handled');
+      assert.equal(result, null, 'this = null should be handled');
     }
   }
 );
