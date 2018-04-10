1a10a30abb58575ab73bf90a972d8611096590ea
Alex Kanunnikov <lifeart92@gmail.com>
2018-04-10 14:26:29 +0300

    regression test

--- packages/ember-metal/tests/namespace_search_test.js
+++ packages/ember-metal/tests/namespace_search_test.js
@@ -1,1 +1,13 @@
+import { classToString } from '..';
+import { moduleFor, AbstractTestCase } from 'internal-test-helpers';
 
+moduleFor(
+  'NamespaceSearch',
+  class extends AbstractTestCase {
+    ['@test classToString: null as this inside class must not throw error'](assert) {
+      function F() {"use strict"; return this };
+      result = classToString(F.bind(null);
+      assert.equal(result, null, 'this = null should be handled');
+    }
+  }
+);
