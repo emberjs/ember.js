875d19fe6d6d2f3d0870e281280848c2ef8afe14
Ruairi Kenny <ruairi.kenny@intercom.io>
2018-04-11 19:31:24 +0100

    [WIP] [Feature] helper blueprint for module unification

--- blueprints/helper/index.js
+++ blueprints/helper/index.js
@@ -1,9 +1,38 @@
 'use strict';
 
+const isModuleUnificationProject = require('../module-unification').isModuleUnificationProject;
 const normalizeEntityName = require('ember-cli-normalize-entity-name');
 
 module.exports = {
   description: 'Generates a helper function.',
+
+  fileMapTokens() {
+    if (isModuleUnificationProject(this.project)) {
+      return {
+        __root__(options) {
+          if (options.pod) {
+            throw "Pods aren't supported within a module unification app";
+          }
+
+          return 'src';
+        },
+        __collection__(options) {
+          if (options.pod) {
+            throw "Pods aren't supported within a module unification app";
+          }
+
+          return 'ui/components';
+        },
+      };
+    } else {
+      return {
+        __collection__() {
+          return 'helpers';
+        },
+      };
+    }
+  },
+
   normalizeEntityName: function(entityName) {
     return normalizeEntityName(entityName);
   },
