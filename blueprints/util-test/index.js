48ee8e958cf9a9567ea1c066965b4c31a2ed03b7
David <david.newell@intercom.io>
2018-04-11 19:28:19 +0100

    [Feature] utils blueprint for module unification

--- blueprints/util-test/index.js
+++ blueprints/util-test/index.js
@@ -1,11 +1,40 @@
 'use strict';
 
 const stringUtils = require('ember-cli-string-utils');
+const path = require('path');
 
 const useTestFrameworkDetector = require('../test-framework-detector');
+const isModuleUnificationProject = require('../module-unification').isModuleUnificationProject;
 
 module.exports = useTestFrameworkDetector({
   description: 'Generates a util unit test.',
+
+  fileMapTokens() {
+    if (isModuleUnificationProject(this.project)) {
+      return {
+        __root__(options) {
+          if (options.pod) {
+            throw "Pods aren't supported within a module unification app";
+          }
+
+          return 'src';
+        },
+        __testType__() {
+          return 'utils';
+        },
+      };
+    } else {
+      return {
+        __root__() {
+          return 'tests';
+        },
+        __testType__() {
+          return path.join('unit', 'utils');
+        },
+      };
+    }
+  },
+
   locals: function(options) {
     return {
       friendlyTestName: ['Unit', 'Utility', options.entity.name].join(' | '),
--- node-tests/blueprints/util-test.js
+++ node-tests/blueprints/util-test.js
@@ -5,9 +5,11 @@ const setupTestHooks = blueprintHelpers.setupTestHooks;
 const emberNew = blueprintHelpers.emberNew;
 const emberGenerateDestroy = blueprintHelpers.emberGenerateDestroy;
 const setupPodConfig = blueprintHelpers.setupPodConfig;
+const expectError = require('../helpers/expect-error');
 
 const chai = require('ember-cli-blueprint-test-helpers/chai');
 const expect = chai.expect;
+const fs = require('fs-extra');
 
 describe('Blueprint: util', function() {
   setupTestHooks(this);
@@ -84,6 +86,43 @@ describe('Blueprint: util', function() {
     });
   });
 
+  describe('in app - module unification', function() {
+    beforeEach(function() {
+      return emberNew().then(() => fs.ensureDirSync('src'));
+    });
+
+    it('util foo-bar', function() {
+      return emberGenerateDestroy(['util', 'foo-bar'], _file => {
+        expect(_file('src/utils/foo-bar.js')).to.contain(
+          'export default function fooBar() {\n' + '  return true;\n' + '}'
+        );
+
+        expect(_file('src/utils/foo-bar-test.js')).to.contain(
+          "import fooBar from 'my-app/utils/foo-bar';"
+        );
+      });
+    });
+
+    it('util foo-bar/baz', function() {
+      return emberGenerateDestroy(['util', 'foo/bar-baz'], _file => {
+        expect(_file('src/utils/foo/bar-baz.js')).to.contain(
+          'export default function fooBarBaz() {\n' + '  return true;\n' + '}'
+        );
+
+        expect(_file('src/utils/foo/bar-baz-test.js')).to.contain(
+          "import fooBarBaz from 'my-app/utils/foo/bar-baz';"
+        );
+      });
+    });
+
+    it('util foo-bar --pod', function() {
+      return expectError(
+        emberGenerateDestroy(['util', 'foo-bar', '--pod']),
+        "Pods aren't supported within a module unification app"
+      );
+    });
+  });
+
   describe('in addon', function() {
     beforeEach(function() {
       return emberNew({ target: 'addon' });
@@ -118,6 +157,80 @@ describe('Blueprint: util', function() {
         expect(_file('tests/unit/utils/foo/bar-baz-test.js')).to.contain(
           "import fooBarBaz from 'dummy/utils/foo/bar-baz';"
         );
+      });
+    });
+  });
+
+  describe('in addon - module unification', function() {
+    beforeEach(function() {
+      return emberNew({ target: 'addon' }).then(() => fs.ensureDirSync('src'));
+    });
+
+    it('util foo-bar', function() {
+      return emberGenerateDestroy(['util', 'foo-bar'], _file => {
+        expect(_file('src/utils/foo-bar.js')).to.contain(
+          'export default function fooBar() {\n' + '  return true;\n' + '}'
+        );
+
+        expect(_file('src/utils/foo-bar-test.js')).to.contain(
+          "import fooBar from 'dummy/utils/foo-bar';"
+        );
+
+        expect(_file('app/utils/foo-bar.js')).to.not.exist;
+      });
+    });
+
+    it('util foo-bar/baz', function() {
+      return emberGenerateDestroy(['util', 'foo/bar-baz'], _file => {
+        expect(_file('src/utils/foo/bar-baz.js')).to.contain(
+          'export default function fooBarBaz() {\n' + '  return true;\n' + '}'
+        );
+
+        expect(_file('src/utils/foo/bar-baz-test.js')).to.contain(
+          "import fooBarBaz from 'dummy/utils/foo/bar-baz';"
+        );
+
+        expect(_file('app/utils/foo/bar-baz.js')).to.not.exist;
+      });
+    });
+  });
+
+  describe('in in-repo-addon', function() {
+    beforeEach(function() {
+      return emberNew({ target: 'in-repo-addon' });
+    });
+
+    it('util foo --in-repo-addon=my-addon', function() {
+      return emberGenerateDestroy(['util', 'foo', '--in-repo-addon=my-addon'], _file => {
+        expect(_file('lib/my-addon/addon/utils/foo.js')).to.contain(
+          'export default function foo() {\n' + '  return true;\n' + '}'
+        );
+
+        expect(_file('lib/my-addon/app/utils/foo.js')).to.contain(
+          "export { default } from 'my-addon/utils/foo';"
+        );
+
+        expect(_file('tests/unit/utils/foo-test.js'))
+          .to.contain("import foo from 'my-app/utils/foo';")
+          .to.contain("import { module, test } from 'qunit';")
+          .to.contain("module('Unit | Utility | foo');");
+      });
+    });
+
+    it('util foo/bar --in-repo-addon=my-addon', function() {
+      return emberGenerateDestroy(['util', 'foo/bar', '--in-repo-addon=my-addon'], _file => {
+        expect(_file('lib/my-addon/addon/utils/foo/bar.js')).to.contain(
+          'export default function fooBar() {\n' + '  return true;\n' + '}'
+        );
+
+        expect(_file('lib/my-addon/app/utils/foo/bar.js')).to.contain(
+          "export { default } from 'my-addon/utils/foo/bar';"
+        );
+
+        expect(_file('tests/unit/utils/foo/bar-test.js'))
+          .to.contain("import fooBar from 'my-app/utils/foo/bar';")
+          .to.contain("import { module, test } from 'qunit';")
+          .to.contain("module('Unit | Utility | foo/bar');");
       });
     });
   });
