ef25f43bf84a33e0866854f412f7da11b5ae94e5
Gavin Joyce <gavinjoyce@gmail.com>
2018-04-11 19:24:57 +0100

    [WIP] [Feature] route blueprint for module unification

--- node-tests/blueprints/route-test.js
+++ node-tests/blueprints/route-test.js
@@ -223,6 +223,213 @@ describe('Blueprint: route', function() {
     });
   });
 
+  describe.only('in app - module unification', function() {
+    beforeEach(function() {
+      return emberNew().then(() => fs.ensureDirSync('src'));
+    });
+
+    it('route foo', function() {
+      return emberGenerateDestroy(['route', 'foo'], _file => {
+        expect(_file('src/ui/routes/foo/route.js'))
+          .to.contain("import Route from '@ember/routing/route';")
+          .to.contain('export default Route.extend({\n});');
+
+        // expect(_file('src/ui/routes/foo/template.hbs')).to.equal('{{outlet}}');
+        //
+        // expect(_file('tests/unit/routes/foo-test.js'))
+        //   .to.contain("import { moduleFor, test } from 'ember-qunit';")
+        //   .to.contain("moduleFor('route:foo'");
+        //
+        // expect(file('app/router.js')).to.contain("this.route('foo')");
+      }).then(() => {
+        // expect(file('app/router.js')).to.not.contain("this.route('foo')");
+      });
+    });
+
+    // it('route foo --skip-router', function() {
+    //   return emberGenerateDestroy(['route', 'foo', '--skip-router'], _file => {
+    //     expect(_file('app/routes/foo.js')).to.exist;
+    //     expect(_file('app/templates/foo.hbs')).to.exist;
+    //     expect(_file('tests/unit/routes/foo-test.js')).to.exist;
+    //     expect(file('app/router.js')).to.not.contain("this.route('foo')");
+    //   }).then(() => {
+    //     expect(file('app/router.js')).to.not.contain("this.route('foo')");
+    //   });
+    // });
+    //
+    // it('route foo --path=:foo_id/show', function() {
+    //   return emberGenerateDestroy(['route', 'foo', '--path=:foo_id/show'], _file => {
+    //     expect(_file('app/routes/foo.js'))
+    //       .to.contain("import Route from '@ember/routing/route';")
+    //       .to.contain('export default Route.extend({\n});');
+    //
+    //     expect(_file('app/templates/foo.hbs')).to.equal('{{outlet}}');
+    //
+    //     expect(_file('tests/unit/routes/foo-test.js'))
+    //       .to.contain("import { moduleFor, test } from 'ember-qunit';")
+    //       .to.contain("moduleFor('route:foo'");
+    //
+    //     expect(file('app/router.js'))
+    //       .to.contain("this.route('foo', {")
+    //       .to.contain("path: ':foo_id/show'")
+    //       .to.contain('});');
+    //   }).then(() => {
+    //     expect(file('app/router.js'))
+    //       .to.not.contain("this.route('foo'")
+    //       .to.not.contain("path: ':foo_id/show'");
+    //   });
+    // });
+    //
+    // it('route parent/child --reset-namespace', function() {
+    //   return emberGenerateDestroy(['route', 'parent/child', '--reset-namespace'], _file => {
+    //     expect(_file('app/routes/child.js'))
+    //       .to.contain("import Route from '@ember/routing/route';")
+    //       .to.contain('export default Route.extend({\n});');
+    //
+    //     expect(_file('app/templates/child.hbs')).to.equal('{{outlet}}');
+    //
+    //     expect(_file('tests/unit/routes/child-test.js'))
+    //       .to.contain("import { moduleFor, test } from 'ember-qunit';")
+    //       .to.contain("moduleFor('route:child'");
+    //
+    //     expect(file('app/router.js'))
+    //       .to.contain("this.route('parent', {")
+    //       .to.contain("this.route('child', {")
+    //       .to.contain('resetNamespace: true')
+    //       .to.contain('});');
+    //   });
+    // });
+    //
+    // it('route parent/child --reset-namespace --pod', function() {
+    //   return emberGenerateDestroy(
+    //     ['route', 'parent/child', '--reset-namespace', '--pod'],
+    //     _file => {
+    //       expect(_file('app/child/route.js'))
+    //         .to.contain("import Route from '@ember/routing/route';")
+    //         .to.contain('export default Route.extend({\n});');
+    //
+    //       expect(_file('app/child/template.hbs')).to.equal('{{outlet}}');
+    //
+    //       expect(_file('tests/unit/child/route-test.js'))
+    //         .to.contain("import { moduleFor, test } from 'ember-qunit';")
+    //         .to.contain("moduleFor('route:child'");
+    //
+    //       expect(file('app/router.js'))
+    //         .to.contain("this.route('parent', {")
+    //         .to.contain("this.route('child', {")
+    //         .to.contain('resetNamespace: true')
+    //         .to.contain('});');
+    //     }
+    //   );
+    // });
+    //
+    // it('route index', function() {
+    //   return emberGenerateDestroy(['route', 'index'], _file => {
+    //     expect(_file('app/routes/index.js')).to.exist;
+    //     expect(_file('app/templates/index.hbs')).to.exist;
+    //     expect(_file('tests/unit/routes/index-test.js')).to.exist;
+    //     expect(file('app/router.js')).to.not.contain("this.route('index')");
+    //   }).then(() => {
+    //     expect(file('app/router.js')).to.not.contain("this.route('index')");
+    //   });
+    // });
+    //
+    // it('route application', function() {
+    //   fs.removeSync('app/templates/application.hbs');
+    //   return emberGenerate(['route', 'application']).then(() => {
+    //     expect(file('app/router.js')).to.not.contain("this.route('application')");
+    //   });
+    // });
+    //
+    // it('route basic', function() {
+    //   return emberGenerateDestroy(['route', 'basic'], _file => {
+    //     expect(_file('app/routes/basic.js')).to.exist;
+    //     expect(file('app/router.js')).to.not.contain("this.route('basic')");
+    //   }).then(() => {
+    //     expect(file('app/router.js')).to.not.contain("this.route('basic')");
+    //   });
+    // });
+    //
+    // it('route foo --pod', function() {
+    //   return emberGenerateDestroy(['route', 'foo', '--pod'], _file => {
+    //     expect(_file('app/foo/route.js'))
+    //       .to.contain("import Route from '@ember/routing/route';")
+    //       .to.contain('export default Route.extend({\n});');
+    //
+    //     expect(_file('app/foo/template.hbs')).to.equal('{{outlet}}');
+    //
+    //     expect(_file('tests/unit/foo/route-test.js'))
+    //       .to.contain("import { moduleFor, test } from 'ember-qunit';")
+    //       .to.contain("moduleFor('route:foo'");
+    //
+    //     expect(file('app/router.js')).to.contain("this.route('foo')");
+    //   }).then(() => {
+    //     expect(file('app/router.js')).to.not.contain("this.route('foo')");
+    //   });
+    // });
+    //
+    // it('route foo --pod with --path', function() {
+    //   return emberGenerate(['route', 'foo', '--pod', '--path=:foo_id/show'])
+    //     .then(() =>
+    //       expect(file('app/router.js'))
+    //         .to.contain("this.route('foo', {")
+    //         .to.contain("path: ':foo_id/show'")
+    //         .to.contain('});')
+    //     )
+    //
+    //     .then(() => emberDestroy(['route', 'foo', '--pod', '--path=:foo_id/show']))
+    //     .then(() =>
+    //       expect(file('app/router.js'))
+    //         .to.not.contain("this.route('foo', {")
+    //         .to.not.contain("path: ':foo_id/show'")
+    //     );
+    // });
+    //
+    // it('route index --pod', function() {
+    //   return emberGenerate(['route', 'index', '--pod']).then(() =>
+    //     expect(file('app/router.js')).to.not.contain("this.route('index')")
+    //   );
+    // });
+    //
+    // it('route application --pod', function() {
+    //   return emberGenerate(['route', 'application', '--pod'])
+    //     .then(() => expect(file('app/application/route.js')).to.exist)
+    //     .then(() => expect(file('app/application/template.hbs')).to.exist)
+    //     .then(() => expect(file('app/router.js')).to.not.contain("this.route('application')"));
+    // });
+    //
+    // it('route basic --pod', function() {
+    //   return emberGenerateDestroy(['route', 'basic', '--pod'], _file => {
+    //     expect(_file('app/basic/route.js')).to.exist;
+    //     expect(file('app/router.js')).to.not.contain("this.route('index')");
+    //   });
+    // });
+    //
+    // describe('with podModulePrefix', function() {
+    //   beforeEach(function() {
+    //     setupPodConfig({ podModulePrefix: true });
+    //   });
+    //
+    //   it('route foo --pod', function() {
+    //     return emberGenerateDestroy(['route', 'foo', '--pod'], _file => {
+    //       expect(_file('app/pods/foo/route.js'))
+    //         .to.contain("import Route from '@ember/routing/route';")
+    //         .to.contain('export default Route.extend({\n});');
+    //
+    //       expect(_file('app/pods/foo/template.hbs')).to.equal('{{outlet}}');
+    //
+    //       expect(_file('tests/unit/pods/foo/route-test.js'))
+    //         .to.contain("import { moduleFor, test } from 'ember-qunit';")
+    //         .to.contain("moduleFor('route:foo'");
+    //
+    //       expect(file('app/router.js')).to.contain("this.route('foo')");
+    //     }).then(() => {
+    //       expect(file('app/router.js')).to.not.contain("this.route('foo')");
+    //     });
+    //   });
+    // });
+  });
+
   describe('in addon', function() {
     beforeEach(function() {
       return emberNew({ target: 'addon' });
