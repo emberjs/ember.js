6b8fdedb702ecdf19a1b9d0e2ba69fd740a4925b
Chad Hietala <chadhietala@gmail.com>
2018-04-12 09:35:28 -0400

    Add test

---
+++ packages/@glimmer/program/test/heap-test.ts
@@ -0,0 +1,32 @@
+import { Heap } from '@glimmer/program';
+
+QUnit.module('Heap');
+
+QUnit.test('Can grow', (assert) => {
+  let size = 0x100000;
+  let heap = new Heap();
+
+  let i = 0;
+
+  while (i !== (size - 1)) {
+    heap.push(1);
+    i++;
+  }
+
+  // Should grow here
+  heap.push(10);
+
+  // Slices the buffer. Passing MAX_SAFE_INTEGER ensures
+  // we get the whole thing out
+  let serialized = heap.capture(Number.MAX_SAFE_INTEGER);
+  let serializedHeap = new Uint16Array(serialized.buffer);
+  assert.equal(serializedHeap.length, size);
+  assert.equal(serializedHeap[size - 1], 10);
+
+  heap.push(11);
+
+  serialized = heap.capture(Number.MAX_SAFE_INTEGER);
+  serializedHeap = new Uint16Array(serialized.buffer);
+  assert.equal(serializedHeap.length, size * 2);
+  assert.equal(serializedHeap[size], 11);
+});
