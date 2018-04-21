ed4307207f8a933271b4b696e26f6e514bcc4b9a
Robert Jackson <rjackson@linkedin.com>
2018-04-21 13:20:27 -0400

    [BUGFIX release] Ensure `ariaRole` can be initially false.
    It should be possible to start without an `ariaRole` and then only add
    one as needed. The previous logic prevented this from working properly
    because we would only setup the required attribute binding when the
    initial `ariaRole` was truthy.

--- packages/ember-glimmer/lib/component-managers/curly.ts
+++ packages/ember-glimmer/lib/component-managers/curly.ts
@@ -351,8 +351,7 @@ export default class CurlyComponentManager
     }
     operations.setAttribute('class', PrimitiveReference.create('ember-view'), false, null);
 
-    const ariaRole = get(component, 'ariaRole');
-    if (ariaRole) {
+    if ('ariaRole' in component) {
       operations.setAttribute('role', referenceForKey(component, 'ariaRole'), false, null);
     }
 
