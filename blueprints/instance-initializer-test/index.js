47fb43d7187f2f351483e945f89f3236e47a52ae
Lily Beauvilliers <lily.beauvilliers@gmail.com>
2018-04-30 17:28:42 +0100

    rename files, tests for in app

--- blueprints/instance-initializer-test/index.js
+++ blueprints/instance-initializer-test/index.js
@@ -5,9 +5,35 @@ const path = require('path');
 const stringUtils = require('ember-cli-string-utils');
 
 const useTestFrameworkDetector = require('../test-framework-detector');
+const isModuleUnificationProject = require('../module-unification').isModuleUnificationProject;
 
 module.exports = useTestFrameworkDetector({
   description: 'Generates an instance initializer unit test.',
+  fileMapTokens() {
+    if (isModuleUnificationProject(this.project)) {
+      return {
+        __root__(options) {
+          if (options.pod) {
+            throw "Pods aren't supported within a module unification app";
+          }
+
+          return 'src';
+        },
+        __testType__() {
+          return '';
+        },
+      };
+    } else {
+      return {
+        __root__() {
+          return 'tests';
+        },
+        __testType__() {
+          return 'unit';
+        },
+      };
+    }
+  },
   locals: function(options) {
     return {
       friendlyTestName: ['Unit', 'Instance Initializer', options.entity.name].join(' | '),
