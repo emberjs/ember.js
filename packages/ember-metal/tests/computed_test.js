19c6d7a577cd4312724dd49094e3af4307af76cd
bekzod <bekzod@me.com>
2018-05-02 10:49:50 +0500

    Add test for issue #16427
    Test that a CP descriptor still works with notifyPropertyChange
    when it is not setup correctly via Ember.defineProperty.
    
    (cherry picked from commit c486d7a87f6015d7aac9ce59463f1a9927043ef8)

--- packages/ember-metal/tests/computed_test.js
+++ packages/ember-metal/tests/computed_test.js
@@ -10,6 +10,7 @@ import {
   isWatching,
   addObserver,
   meta as metaFor,
+  notifyPropertyChange,
 } from '..';
 import { moduleFor, AbstractTestCase } from 'internal-test-helpers';
 
@@ -58,6 +59,26 @@ moduleFor(
 
       assert.equal(obj.foo, 'computed foo', 'should return value');
       assert.equal(count, 1, 'should have invoked computed property');
+    }
+
+    ['@test `notifyPropertyChange` works for a computed property not setup using Ember.defineProperty #GH16427'](
+      assert
+    ) {
+      let obj = {
+        a: 50,
+        b: computed(function() {
+          return this.a / 5;
+        }),
+      };
+
+      expectDeprecation(function() {
+        assert.equal(get(obj, 'b'), 10);
+      });
+
+      obj.a = 10;
+      notifyPropertyChange(obj, 'b');
+
+      assert.equal(get(obj, 'b'), 2);
     }
 
     ['@test defining computed property should invoke property on get'](assert) {
