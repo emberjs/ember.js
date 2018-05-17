011e6e4c0977d68dbfce0faf62819c84de462f94
Pablo Brasero <pablo@thoughtbot.com>
2018-05-17 21:01:26 +0100

    [Feature] Module Unification support for the template blueprint

--- blueprints/template/index.js
+++ blueprints/template/index.js
@@ -1,5 +1,31 @@
 'use strict';
 
+const isModuleUnificationProject = require('../module-unification').isModuleUnificationProject;
+
 module.exports = {
   description: 'Generates a template.',
+
+  fileMapTokens() {
+    if (isModuleUnificationProject(this.project)) {
+      return {
+        __root__(options) {
+          if (options.pod) {
+            throw "Pods aren't supported within a module unification app";
+          } else if (options.inDummy) {
+            return 'tests/dummy/src/ui/routes';
+          } else {
+            return 'src/ui/routes';
+          }
+        },
+
+        __path__(options) {
+          return options.dasherizedModuleName;
+        },
+
+        __name__() {
+          return 'template';
+        },
+      };
+    }
+  },
 };
