6aa3c83fb1cb6dec8916a46b3009cdf71a7a7c61
Chris Santero <chris@simply360.org>
2018-05-19 16:11:27 -0400

    Loosen path type in buildSexpr

--- packages/@glimmer/syntax/lib/builders.ts
+++ packages/@glimmer/syntax/lib/builders.ts
@@ -176,7 +176,7 @@ function buildText(chars?: string, loc?: AST.SourceLocation): AST.TextNode {
 // Expressions
 
 function buildSexpr(
-  path: AST.PathExpression,
+  path: BuilderPath,
   params?: AST.Expression[],
   hash?: AST.Hash,
   loc?: AST.SourceLocation
