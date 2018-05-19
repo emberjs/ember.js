9918c2c2a579b3729e4a787d84e38eeaaa08cbb2
Chris Santero <chris@simply360.org>
2018-05-19 08:24:56 -0400

    simplify exported type def

--- packages/ember-metal/index.d.ts
+++ packages/ember-metal/index.d.ts
@@ -6,17 +6,7 @@ export const PROPERTY_DID_CHANGE: symbol;
 
 export function setHasViews(fn: () => boolean): null;
 
-export type MethodKey<T> = { [K in keyof T]: T[K] extends (() => void) ? K : never }[keyof T];
-export type RunInTransactionFunc = <T extends object, K extends MethodKey<T>>(
-  context: T,
-  methodName: K
-) => boolean;
-export type DidRenderFunc = (object: any, key: string, reference: any) => void;
-export type AssertNotRenderedFunc = (obj: object, keyName: string) => void;
-
-export const runInTransaction: RunInTransactionFunc;
-export const didRender: DidRenderFunc;
-export const assertNotRendered: AssertNotRenderedFunc;
+export { default as runInTransaction, didRender, assertNotRendered } from './lib/transaction';
 
 export function get(obj: any, keyName: string): any;
 
