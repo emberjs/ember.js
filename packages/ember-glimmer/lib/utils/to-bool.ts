b0488cc45962c3ed0980269fd052ad18145de995
bekzod <bekzod@me.com>
2018-06-04 12:44:13 +0500

    cleanup `toBool`

--- packages/ember-glimmer/lib/utils/to-bool.ts
+++ packages/ember-glimmer/lib/utils/to-bool.ts
@@ -1,19 +1,10 @@
 import { Opaque } from '@glimmer/interfaces';
-import { get } from 'ember-metal';
 import { isArray } from 'ember-runtime';
 
 export default function toBool(predicate: Opaque): boolean {
-  if (!predicate) {
-    return false;
-  }
-
-  if (predicate === true) {
-    return true;
-  }
-
   if (isArray(predicate)) {
-    return get(predicate, 'length') !== 0;
+    return (predicate as { length: number }).length !== 0;
+  } else {
+    return !!predicate;
   }
-
-  return true;
 }
