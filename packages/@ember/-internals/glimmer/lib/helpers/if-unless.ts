310620f34667010f14cab1b10353ce1ceb32bc21
Laura Kajpust <lkajpust@gmail.com>
2018-07-07 10:25:29 -0400

    Update unless documentation with block example and better clarification

--- packages/@ember/-internals/glimmer/lib/helpers/if-unless.ts
+++ packages/@ember/-internals/glimmer/lib/helpers/if-unless.ts
@@ -145,18 +145,34 @@ export function inlineIf(_vm: VM, { positional }: Arguments) {
 }
 
 /**
+  The `unless` helper is the inverse of the `if` helper. It displays if a value
+  is falsey (`not true` or `is false`). Example values that will display with
+  `unless`: `false`, `undefined`, `null`, `""`, `0`, `NaN` or an empty array.
+
+  ## Inline form
+
   The inline `unless` helper conditionally renders a single property or string.
   This helper acts like a ternary operator. If the first property is falsy,
   the second argument will be displayed, otherwise, the third argument will be
   displayed
 
+  For example, if `useLongGreeting` is false below:
+
   ```handlebars
   {{unless useLongGreeting "Hi" "Hello"}} Ben
   ```
 
+  Then it will display:
+
+  ```html
+  Hi
+  ```
+
   You can use the `unless` helper inside another helper as a subexpression.
+  If isBig is not true, it will set the height to 10:
 
   ```handlebars
+  {{! If isBig is not true, it will set the height to 10.}}
   <SomeComponent @height={{unless isBig "10" "100"}} />
   ```
 
@@ -164,6 +180,36 @@ export function inlineIf(_vm: VM, { positional }: Arguments) {
 
   ```handlebars
   {{some-component height=(unless isBig "10" "100")}}
+  ```
+
+  ## Block form
+
+  Like the `if` helper, `unless` helper also has a block form.
+
+  ```handlebars
+  {{! If greetings are found, the text below will not render.}}
+  {{#unless greeting}}
+    No greetings were found. Why not set one?
+  {{/unless}}
+  ```
+
+  You can also use an `else` helper with the `unless` block. The
+  `else` will display if the value is truthy.
+
+  ```handlebars
+  {{! Is the user logged in?}}
+  {{#unless userData}}
+    Please login.
+  {{else}}
+    Welcome back!
+  {{/unless}}
+  ```
+
+  If `userData` is false, undefined, null, or empty in the above example,
+  then it will render:
+
+  ```html
+  Please login.
   ```
 
   @method unless
