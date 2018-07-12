45f64c8524fd48a3f37a45ba604747e9cac26c7b
Kris Selden <kris.selden@gmail.com>
2018-07-11 17:18:01 -0700

    [BUGFIX] Centralize build versioning info
    Make BUILD_TYPE only affect published buildType if on master so we don't
    accidentally publish from lts/release branches over the current beta or
    release.
    
    Only use a tag as version if it is parseable by semver.
    
    The build-for-publishing script uses git info instead of travis env.

--- broccoli/version.js
+++ broccoli/version.js
@@ -1,36 +1,5 @@
 'use strict';
 
-const getGitInfo = require('git-repo-info');
-const path = require('path');
+const buildInfo = require('./build-info')();
 
-module.exports.VERSION = (() => {
-  let info = getGitInfo(path.resolve(__dirname, '..'));
-  // if the current commit _is_ a tagged commit, use the tag as the version
-  // number
-  if (info.tag) {
-    return info.tag.replace(/^v/, '');
-  }
-
-  let pkg = require('../package');
-  // if `_versionPreviouslyCalculated` is set the `package.json` version string
-  // _already_ includes the branch, sha, etc (from bin/build-for-publishing.js)
-  // so just use it directly
-  if (pkg._versionPreviouslyCalculated) {
-    return pkg.version;
-  }
-
-  // otherwise build the version number up of:
-  //
-  // * actual package.json version string
-  // * current "build type" or branch name (in CI this is generally not
-  //   present, but it is very useful for local / testing builds)
-  // * the sha for the commit
-  let packageVersion = pkg.version;
-  let sha = info.sha || '';
-  let suffix = process.env.BUILD_TYPE || info.branch;
-  // * remove illegal non-alphanumeric characters from branch name.
-  suffix = suffix && suffix.replace(/[^a-zA-Z\d\s-]/g, '-');
-  let metadata = sha.slice(0, 8);
-
-  return `${packageVersion}${suffix ? '-' + suffix : ''}+${metadata}`;
-})();
+module.exports.VERSION = buildInfo.version;
