edd05e00f1c790cd344303ff7e99f9028ddbaf80
Kris Selden <kris.selden@gmail.com>
2018-07-19 09:59:59 -0700

    Improve visitor typing

--- packages/@glimmer/syntax/lib/types/visitor-keys.ts
+++ packages/@glimmer/syntax/lib/types/visitor-keys.ts
@@ -1,6 +1,9 @@
-export default {
-  Program: ['body'],
+import { VisitorKeysMap } from './nodes';
 
+// ensure stays in sync with typing
+// ParentNode and ChildKey types are derived from VisitorKeysMap
+const visitorKeys: VisitorKeysMap = {
+  Program: ['body'],
   MustacheStatement: ['path', 'params', 'hash'],
   BlockStatement: ['path', 'params', 'hash', 'program', 'inverse'],
   ElementModifierStatement: ['path', 'params', 'hash'],
@@ -24,3 +27,5 @@ export default {
   Hash: ['pairs'],
   HashPair: ['value'],
 };
+
+export default visitorKeys;
