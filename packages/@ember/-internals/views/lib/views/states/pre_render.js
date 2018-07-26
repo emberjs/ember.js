0d48559cbc8bcccae15e7aa97fc8b10ba34a7369
bekzod <bekzod@me.com>
2018-07-27 00:53:32 +0500

    cleanup view states

--- packages/@ember/-internals/views/lib/views/states/pre_render.js
+++ packages/@ember/-internals/views/lib/views/states/pre_render.js
@@ -1,3 +1,6 @@
 import _default from './default';
+import { assign } from '@ember/polyfills';
 
-export default Object.create(_default);
+const preRender = assign({}, _default);
+
+export default Object.freeze(preRender);
