1916380db7ecc929de3c763196a77112e87f3135
Steven Foote <sfoote@linkedin.com>
2018-08-20 14:50:45 -0600

    TypeScript-ify computed_test

--- packages/ember-metal/tests/tracked/computed_test.ts
+++ packages/ember-metal/tests/tracked/computed_test.ts
@@ -1,24 +1,22 @@
-import { createWithDescriptors } from './support';
 import { get, set, tracked } from '../..';
 
-import { moduleFor, AbstractTestCase } from 'internal-test-helpers';
 import { EMBER_METAL_TRACKED_PROPERTIES } from '@ember/canary-features';
+import { AbstractTestCase, moduleFor } from 'internal-test-helpers';
 
 if (EMBER_METAL_TRACKED_PROPERTIES) {
   moduleFor(
-    'tracked getters',
+    '@tracked getters',
     class extends AbstractTestCase {
       ['@test works without get'](assert) {
         let count = 0;
 
         class Count {
+          @tracked
           get foo() {
             count++;
             return `computed foo`;
           }
         }
-
-        tracked(Count.prototype, 'foo', Object.getOwnPropertyDescriptor(Count.prototype, 'foo'));
 
         let obj = new Count();
 
@@ -30,13 +28,12 @@ if (EMBER_METAL_TRACKED_PROPERTIES) {
         let count = 0;
 
         class Count {
+          @tracked
           get foo() {
             count++;
             return `computed foo`;
           }
         }
-
-        tracked(Count.prototype, 'foo', Object.getOwnPropertyDescriptor(Count.prototype, 'foo'));
 
         let obj = new Count();
 
@@ -47,16 +44,19 @@ if (EMBER_METAL_TRACKED_PROPERTIES) {
       ['@test defining computed property should invoke property on set'](assert) {
         let count = 0;
 
-        let obj = createWithDescriptors({
+        let obj = new class {
+          __foo = '';
+
+          @tracked
           get foo() {
             return this.__foo;
-          },
+          }
 
           set foo(value) {
             count++;
             this.__foo = `computed ${value}`;
-          },
-        });
+          }
+        }();
 
         assert.equal(set(obj, 'foo', 'bar'), 'bar', 'should return set value');
         assert.equal(count, 1, 'should have invoked computed property');
