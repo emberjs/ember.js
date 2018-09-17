dffb160d5c9b8c6209d6b55237da7563c33bb5d3
Robert Jackson <rjackson@linkedin.com>
2018-09-17 13:55:24 -0400

    Fix custom helperGenerator for babel 7

--- broccoli/transforms/inject-babel-helpers.js
+++ broccoli/transforms/inject-babel-helpers.js
@@ -5,13 +5,12 @@ const { addNamed } = require('@babel/helper-module-imports');
 function injectBabelHelpers() {
   return {
     pre(file) {
-      // TODO: FIX THIS BEFORE LANDING
-      //file.set('helperGenerator', function(name) {
-      //  if (name === 'extends') {
-      //    return file.addImport('@ember/polyfills', 'assign', name);
-      //  }
-      //  return file.addImport('ember-babel', name, name);
-      //});
+      file.set('helperGenerator', function(name) {
+        if (name === 'extends') {
+          return addNamed(file.path, 'assign', '@ember/polyfills');
+        }
+        return addNamed(file.path, name, 'ember-babel');
+      });
     },
   };
 }
