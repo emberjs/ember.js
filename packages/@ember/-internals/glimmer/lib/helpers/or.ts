b6f5f7ff5f1865d24b18e3754803977177e101e1
Ricardo Mendes <rokusu@gmail.com>
2018-11-06 14:13:14 +0100

    Update packages/@ember/-internals/glimmer/lib/helpers/or.ts
    Co-Authored-By: cibernox <miguel.camba@gmail.com>

--- packages/@ember/-internals/glimmer/lib/helpers/or.ts
+++ packages/@ember/-internals/glimmer/lib/helpers/or.ts
@@ -19,7 +19,7 @@ import { InternalHelperReference } from '../utils/references';
   @public
   @method or
   @for Ember.Templates.helpers
-  @since 2.7.0
+  @since 3.7.0
 */
 function or({ positional: { references } }: CapturedArguments) {
   let last: any = false;
