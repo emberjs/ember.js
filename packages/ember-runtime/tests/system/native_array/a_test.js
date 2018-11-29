f251ea1c8ad3384cbd09d9949eae4ae74e784eea
Christopher Garrett <me@pzuraq.com>
2018-11-28 19:39:56 -0800

    [BUGFIX beta] Ensures Ember.A is a constructor
    The recent change to transpilation has made `new A()` break, because
    A is an arrow function and arrow functions cannot be constructors. This
    is a pretty common pattern in Ember apps and likely constitutes a
    breaking change, so this PR fixes it by changing the arrow function to
    a normal function, and deprecates this behavior.

--- packages/ember-runtime/tests/system/native_array/a_test.js
+++ packages/ember-runtime/tests/system/native_array/a_test.js
@@ -12,5 +12,15 @@ moduleFor(
       assert.ok(EmberArray.detect(A()), 'returned an ember array');
       assert.ok(EmberArray.detect(A([1, 2])), 'returned an ember array');
     }
+
+    ['@test new Ember.A'](assert) {
+      expectDeprecation(() => {
+        assert.deepEqual(new A([1, 2]), [1, 2], 'array values were not be modified');
+        assert.deepEqual(new A(), [], 'returned an array with no arguments');
+        assert.deepEqual(new A(null), [], 'returned an array with a null argument');
+        assert.ok(EmberArray.detect(new A()), 'returned an ember array');
+        assert.ok(EmberArray.detect(new A([1, 2])), 'returned an ember array');
+      });
+    }
   }
 );
