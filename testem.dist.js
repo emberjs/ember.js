355ec3061f12348c3d4ebaaab20c3c692a80908d
Katie Gengler <katie@kmg.io>
2018-12-06 16:12:14 -0500

    Disable 'node/no-unsupported-features' rule for some testem related files.
    Avoids backporting #17254 for now and is OK because CI only runs on >=
    Node 6

--- testem.dist.js
+++ testem.dist.js
@@ -1,3 +1,5 @@
+/* eslint-disable node/no-unsupported-features */
+
 const FailureOnlyReporter = require('./lib/failure-only-testem-reporter');
 
 class FailureOnlyPerBrowserReporter extends FailureOnlyReporter {
