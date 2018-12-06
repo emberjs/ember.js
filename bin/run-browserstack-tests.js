ee59a72cc0a28ca1755f689458a6fddb68d79871
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-12-06 01:22:42 +0100

    bin/run-browserstack-tests: Use `execa`

--- bin/run-browserstack-tests.js
+++ bin/run-browserstack-tests.js
@@ -1,63 +1,34 @@
 /* eslint-disable no-console, node/no-unsupported-features */
 
-var RSVP = require('rsvp');
-var spawn = require('child_process').spawn;
+var execa = require('execa');
 
-function run(command, _args) {
-  var args = _args || [];
+function run(command, args = []) {
+  console.log('Running: ' + command + ' ' + args.join(' '));
 
-  return new RSVP.Promise(function(resolve, reject) {
-    console.log('Running: ' + command + ' ' + args.join(' '));
+  let p = execa(command, args);
+  p.stdout.pipe(process.stdout);
+  p.stderr.pipe(process.stderr);
 
-    var child = spawn(command, args);
-
-    child.stdout.on('data', function(data) {
-      console.log(data.toString());
-    });
-
-    child.stderr.on('data', function(data) {
-      console.error(data.toString());
-    });
-
-    child.on('error', function(err) {
-      reject(err);
-    });
-
-    child.on('exit', function(code) {
-      if (code === 0) {
-        resolve();
-      } else {
-        reject(code);
-      }
-    });
-  });
+  return p;
 }
 
 (async function() {
-  await run('./node_modules/.bin/ember', ['browserstack:connect']);
+  await run('ember', ['browserstack:connect']);
 
   try {
     try {
       // Calling testem directly here instead of `ember test` so that
       // we do not have to do a double build (by the time this is run
       // we have already ran `ember build`).
-      await run('./node_modules/.bin/testem', [
-        'ci',
-        '-f',
-        'testem.dist.js',
-        '--host',
-        '127.0.0.1',
-        '--port',
-        '7774',
-      ]);
+      await run('testem', ['ci', '-f', 'testem.dist.js', '--host', '127.0.0.1', '--port', '7774']);
 
       console.log('success');
       process.exit(0);
     } finally {
       if (process.env.TRAVIS_JOB_NUMBER) {
-        await run('./node_modules/.bin/ember', ['browserstack:results']);
+        await run('ember', ['browserstack:results']);
       }
-      await run('./node_modules/.bin/ember', ['browserstack:disconnect']);
+      await run('ember', ['browserstack:disconnect']);
     }
   } catch (error) {
     console.log('error');
