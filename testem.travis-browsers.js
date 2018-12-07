23d5bbb710d61dafbe6a98b3c9e779d29b118ba8
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-12-07 10:50:58 +0100

    testem: Increase `browser_disconnect_timeout` setting (for `travis-browsers`)

--- testem.travis-browsers.js
+++ testem.travis-browsers.js
@@ -3,6 +3,7 @@ const FailureOnlyReporter = require('./lib/failure-only-testem-reporter');
 module.exports = {
   framework: 'qunit',
   test_page: 'dist/tests/index.html?hidepassed&hideskipped&timeout=60000',
+  browser_disconnect_timeout: 120,
   timeout: 540,
   parallel: 1,
   disable_watching: true,
