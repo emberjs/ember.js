804128a1d58c7082b1c58a6e0884cd40c44ab9eb
Yehuda Katz <wycats@gmail.com>
2018-12-06 21:35:45 -1000

    Start generating enum

---
+++ build/debug.js
@@ -0,0 +1,15 @@
+const { normalizeAll, buildEnum } = require('../dist/@glimmer/debug');
+const fs = require('fs');
+const toml = require('toml');
+
+function parse(file) {
+  let opcodes = fs.readFileSync(file, { encoding: 'utf8' });
+  let raw = toml.parse(opcodes);
+  return normalizeAll(raw);
+}
+
+let parsed = parse('./packages/@glimmer/vm/lib/opcodes.toml');
+
+console.log(buildEnum('MachineOp', parsed.machine));
+console.log('');
+console.log(buildEnum('Op', parsed.syscall));
