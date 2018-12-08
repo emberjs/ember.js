33ea8ebdfa31e4b61352a59b7f00d0d21c3306dd
Yehuda Katz <wycats@gmail.com>
2018-12-07 16:35:45 -1000

    Generate metadata

--- build/debug.js
+++ build/debug.js
@@ -16,4 +16,21 @@ let parsed = parse('./packages/@glimmer/vm/lib/opcodes.toml');
 // console.log('');
 // console.log(buildEnum('Op', parsed.syscall));
 
+console.log(`
+import { Op, MachineOp } from './opcodes';
+import { Option } from '@glimmer/interfaces';
+import { fillNulls } from '@glimmer/util';
+import { NormalizedMetadata } from '@glimmer/debug';
+
+export function opcodeMetadata(op: MachineOp | Op, isMachine: 0 | 1): Option<NormalizedMetadata> {
+  let value = isMachine ? MACHINE_METADATA[op] : METADATA[op];
+
+  return value || null;
+}
+
+const METADATA: Option<NormalizedMetadata>[] = fillNulls(Op.Size);
+const MACHINE_METADATA: Option<NormalizedMetadata>[] = fillNulls(MachineOp.Size);
+`);
+
 console.log(buildMetas('MACHINE_METADATA', parsed.machine));
+console.log(buildMetas('METADATA', parsed.syscall));
