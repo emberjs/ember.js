dcdff0a6c51a168de226a02c6f3eacbe11a621ad
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-12-09 15:14:31 +0100

    internal-test-helpers: Implement `HooksCompat` class

--- packages/internal-test-helpers/lib/ember-dev/setup-qunit.ts
+++ packages/internal-test-helpers/lib/ember-dev/setup-qunit.ts
@@ -10,6 +10,51 @@ export interface Assertion {
   restore(): void;
 }
 
+type HookFunction = (assert: Assert) => any;
+
+/**
+ * This class can be used to make `setupTest(hooks)` style functions
+ * compatible with the non-nested QUnit API.
+ */
+class HooksCompat {
+  _before: HookFunction[] = [];
+  _beforeEach: HookFunction[] = [];
+  _afterEach: HookFunction[] = [];
+  _after: HookFunction[] = [];
+
+  before(fn: HookFunction) {
+    this._before.push(fn);
+  }
+
+  beforeEach(fn: HookFunction) {
+    this._beforeEach.push(fn);
+  }
+
+  afterEach(fn: HookFunction) {
+    this._afterEach.push(fn);
+  }
+
+  after(fn: HookFunction) {
+    this._after.push(fn);
+  }
+
+  runBefore(assert: Assert) {
+    this._before.forEach(fn => fn(assert));
+  }
+
+  runBeforeEach(assert: Assert) {
+    this._beforeEach.forEach(fn => fn(assert));
+  }
+
+  runAfterEach(assert: Assert) {
+    this._afterEach.forEach(fn => fn(assert));
+  }
+
+  runAfter(assert: Assert) {
+    this._after.forEach(fn => fn(assert));
+  }
+}
+
 export default function setupQUnit({ runningProdBuild }: { runningProdBuild: boolean }) {
   let assertion = new EmberDevTestHelperAssert({
     runningProdBuild,
@@ -45,7 +90,10 @@ export default function setupQUnit({ runningProdBuild }: { runningProdBuild: boo
     delete options.setup;
     delete options.teardown;
 
+    let hooks = new HooksCompat();
     options.beforeEach = function() {
+      hooks.runBeforeEach(QUnit.config.current.assert);
+
       assertion.reset();
       assertion.inject();
 
@@ -54,6 +102,8 @@ export default function setupQUnit({ runningProdBuild }: { runningProdBuild: boo
 
     options.afterEach = function() {
       let result = originalTeardown.apply(this, arguments);
+
+      hooks.runAfterEach(QUnit.config.current.assert);
 
       assertion.assert();
       assertion.restore();
