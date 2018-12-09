f38989a3ca3cb6642dc483bac17ec3ab497a30a1
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-12-09 15:42:18 +0100

    internal-test-helpers: Move `HooksCompat` into dedicated file

--- packages/internal-test-helpers/lib/ember-dev/setup-qunit.ts
+++ packages/internal-test-helpers/lib/ember-dev/setup-qunit.ts
@@ -2,6 +2,7 @@ import { getDebugFunction, setDebugFunction } from '@ember/debug';
 
 // @ts-ignore
 import { setupContainersCheck } from './containers';
+import HooksCompat from './hooks-compat';
 // @ts-ignore
 import EmberDevTestHelperAssert from './index';
 // @ts-ignore
@@ -14,51 +15,6 @@ export interface Assertion {
   inject(): void;
   assert(): void;
   restore(): void;
-}
-
-type HookFunction = (assert: Assert) => any;
-
-/**
- * This class can be used to make `setupTest(hooks)` style functions
- * compatible with the non-nested QUnit API.
- */
-class HooksCompat {
-  _before: HookFunction[] = [];
-  _beforeEach: HookFunction[] = [];
-  _afterEach: HookFunction[] = [];
-  _after: HookFunction[] = [];
-
-  before(fn: HookFunction) {
-    this._before.push(fn);
-  }
-
-  beforeEach(fn: HookFunction) {
-    this._beforeEach.push(fn);
-  }
-
-  afterEach(fn: HookFunction) {
-    this._afterEach.push(fn);
-  }
-
-  after(fn: HookFunction) {
-    this._after.push(fn);
-  }
-
-  runBefore(assert: Assert) {
-    this._before.forEach(fn => fn(assert));
-  }
-
-  runBeforeEach(assert: Assert) {
-    this._beforeEach.forEach(fn => fn(assert));
-  }
-
-  runAfterEach(assert: Assert) {
-    this._afterEach.forEach(fn => fn(assert));
-  }
-
-  runAfter(assert: Assert) {
-    this._after.forEach(fn => fn(assert));
-  }
 }
 
 export default function setupQUnit({ runningProdBuild }: { runningProdBuild: boolean }) {
