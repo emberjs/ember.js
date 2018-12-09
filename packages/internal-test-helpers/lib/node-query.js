0a2fe79acf4c5d1067c303a9bf1f8fe7e39e12cf
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-12-09 21:36:53 +0100

    internal-test-helpers: Move `NodeQuery` class out of the `test-cases` folder

--- packages/internal-test-helpers/lib/node-query.js
+++ packages/internal-test-helpers/lib/node-query.js
@@ -1,7 +1,7 @@
 /* global Node */
 
 import { assert } from '@ember/debug';
-import { fireEvent, focus, matches } from '../system/synthetic-events';
+import { fireEvent, focus, matches } from './system/synthetic-events';
 
 export default class NodeQuery {
   static query(selector, context = document) {
