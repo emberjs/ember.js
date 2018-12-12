da07e832cdc67ba4a087d99d4e0a01382c1612ac
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-12-12 15:05:54 +0100

    internal-test-helpers/run: Convert to TypeScript

--- packages/internal-test-helpers/lib/run.ts
+++ packages/internal-test-helpers/lib/run.ts
@@ -1,10 +1,10 @@
 import { run } from '@ember/runloop';
 
-export function runAppend(view) {
+export function runAppend(view: any) {
   run(view, 'appendTo', document.getElementById('qunit-fixture'));
 }
 
-export function runDestroy(toDestroy) {
+export function runDestroy(toDestroy: any) {
   if (toDestroy) {
     run(toDestroy, 'destroy');
   }
