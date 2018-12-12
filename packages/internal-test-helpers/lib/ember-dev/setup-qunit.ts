ba01da35cb5ebb3623f48a3457fb743f9a7f464f
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-12-12 12:01:23 +0100

    internal-test-helpers: Simplify `setupQUnit()` function

--- packages/internal-test-helpers/lib/ember-dev/setup-qunit.ts
+++ packages/internal-test-helpers/lib/ember-dev/setup-qunit.ts
@@ -3,7 +3,6 @@ import { getDebugFunction, setDebugFunction } from '@ember/debug';
 import { setupAssertionHelpers } from './assertion';
 import { setupContainersCheck } from './containers';
 import { setupDeprecationHelpers } from './deprecation';
-import HooksCompat from './hooks-compat';
 import { setupNamespacesCheck } from './namespaces';
 import { setupRunLoopCheck } from './run-loop';
 import { DebugEnv } from './utils';
@@ -27,41 +26,11 @@ export default function setupQUnit({ runningProdBuild }: { runningProdBuild: boo
 
   let originalModule = QUnit.module;
 
-  QUnit.module = function(name: string, _options: any) {
-    if (typeof _options === 'function') {
-      let callback = _options;
+  QUnit.module = function(name: string, callback: any) {
+    return originalModule(name, function(hooks) {
+      setupAssert(hooks);
 
-      return originalModule(name, function(hooks) {
-        setupAssert(hooks);
-
-        callback(hooks);
-      });
-    }
-
-    let options = _options || {};
-    let originalSetup = options.setup || options.beforeEach || function() {};
-    let originalTeardown = options.teardown || options.afterEach || function() {};
-
-    delete options.setup;
-    delete options.teardown;
-
-    let hooks = new HooksCompat();
-    setupAssert(hooks);
-
-    options.beforeEach = function() {
-      hooks.runBeforeEach(QUnit.config.current.assert);
-
-      return originalSetup.apply(this, arguments);
-    };
-
-    options.afterEach = function() {
-      let result = originalTeardown.apply(this, arguments);
-
-      hooks.runAfterEach(QUnit.config.current.assert);
-
-      return result;
-    };
-
-    return originalModule(name, options);
+      callback(hooks);
+    });
   };
 }
