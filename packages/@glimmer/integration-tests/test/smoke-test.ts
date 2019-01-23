4c5b85b35752b5360853c8884cbeea88504b98df
Yehuda Katz <wycats@gmail.com>
2019-01-23 12:53:00 -0800

    Add the smoke tests

---
+++ packages/@glimmer/integration-tests/test/smoke-test.ts
@@ -0,0 +1,22 @@
+import { RenderTest, test, jitSuite, aotSuite } from '@glimmer/integration-tests';
+
+class SmokeTests extends RenderTest {
+  static suiteName = 'Smoke Tests';
+
+  @test
+  'can create many blocks without overflowing'() {
+    let input = '';
+    let output = '';
+    for (let i = 0; i < 10000; i++) {
+      input +=
+        '<div id="test">{{#if true}}Hello{{else}}Bye{{/if}}{{#if false}}Hello{{else}}Bye{{/if}}</div>';
+      output += '<div id="test">HelloBye</div>';
+    }
+
+    this.render(input);
+    this.assertHTML(output);
+  }
+}
+
+jitSuite(SmokeTests);
+aotSuite(SmokeTests);
