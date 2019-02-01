1f5438bd37af9f38d077e260a9c3cb7e5a56b09c
Chris Garrett <cgarrett@linkedin.com>
2019-02-01 13:23:32 -0800

    update svelte version

--- packages/@ember/deprecated-features/index.ts
+++ packages/@ember/deprecated-features/index.ts
@@ -1,5 +1,8 @@
 /* eslint-disable no-implicit-coercion */
 
+// These versions should be the version that the deprecation was _introduced_,
+// not the version that the feature will be removed.
+
 export const SEND_ACTION = !!'3.4.0';
 export const EMBER_EXTEND_PROTOTYPES = !!'3.2.0-beta.5';
 export const RUN_SYNC = !!'3.0.0-beta.4';
@@ -10,4 +13,4 @@ export const ROUTER_EVENTS = !!'3.9.0';
 export const TRANSITION_STATE = !!'3.9.0';
 export const COMPONENT_MANAGER_STRING_LOOKUP = !!'4.0.0';
 export const JQUERY_INTEGRATION = !!'3.9.0';
-export const ALIAS_METHOD = !!'4.0.0';
+export const ALIAS_METHOD = !!'3.9.0';
