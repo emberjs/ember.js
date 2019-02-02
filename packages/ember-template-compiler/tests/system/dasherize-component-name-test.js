6cd0258fb62a00272c909d3628dfbdfd720a9f67
Jen Weber <j@jenweber.me>
2019-02-02 12:24:53 -0500

    [BUGFIX lts] support numbers in component names for Angle Brackets
    Previously, c3-plot would not work when invoked as <C3Plot />.
    Now it will resolve correctly.
    Closes #17549

--- packages/ember-template-compiler/tests/system/dasherize-component-name-test.js
+++ packages/ember-template-compiler/tests/system/dasherize-component-name-test.js
@@ -8,6 +8,10 @@ moduleFor(
       assert.equal(COMPONENT_NAME_SIMPLE_DASHERIZE_CACHE.get('Foo'), 'foo');
       assert.equal(COMPONENT_NAME_SIMPLE_DASHERIZE_CACHE.get('foo-bar'), 'foo-bar');
       assert.equal(COMPONENT_NAME_SIMPLE_DASHERIZE_CACHE.get('FooBar'), 'foo-bar');
+      assert.equal(COMPONENT_NAME_SIMPLE_DASHERIZE_CACHE.get('F3Bar'), 'f3-bar');
+      assert.equal(COMPONENT_NAME_SIMPLE_DASHERIZE_CACHE.get('Foo3Bar'), 'foo3-bar');
+      assert.equal(COMPONENT_NAME_SIMPLE_DASHERIZE_CACHE.get('Foo3barBaz'), 'foo3bar-baz');
+      assert.equal(COMPONENT_NAME_SIMPLE_DASHERIZE_CACHE.get('FooB3ar'), 'foo-b3ar');
       assert.equal(COMPONENT_NAME_SIMPLE_DASHERIZE_CACHE.get('XBlah'), 'x-blah');
       assert.equal(COMPONENT_NAME_SIMPLE_DASHERIZE_CACHE.get('X-Blah'), 'x-blah');
       assert.equal(COMPONENT_NAME_SIMPLE_DASHERIZE_CACHE.get('Foo::BarBaz'), 'foo::bar-baz');
