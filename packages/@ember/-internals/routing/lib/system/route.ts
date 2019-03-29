bfe670c51d633aa1482c302940247a0b9008cf69
Chad Hietala <chadhietala@gmail.com>
2019-03-29 09:35:39 -0400

    Bump router_js
    (cherry picked from commit 77677b9311c24092e27f52b523064e2df57dd0a8)

--- packages/@ember/-internals/routing/lib/system/route.ts
+++ packages/@ember/-internals/routing/lib/system/route.ts
@@ -85,6 +85,7 @@ export function hasDefaultSerialize(route: Route) {
 
 class Route extends EmberObject implements IRoute {
   routeName!: string;
+  _internalName!: string;
   context: {} = {};
   serialize!: (model: {}, params: string[]) => object | undefined;
 
