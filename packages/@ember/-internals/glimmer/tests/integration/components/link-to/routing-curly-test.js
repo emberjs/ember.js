087dc5dcf0331b848408de8b875b18eedac6f0a6
simonihmig <simon.ihmig@kaliber5.de>
2019-04-06 19:56:02 +0200

    Revert commit cb3b041 from #17847
    (cherry picked from commit 561759e4d3cd2f978deb057e4e1f52d03098f40d)

--- packages/@ember/-internals/glimmer/tests/integration/components/link-to/routing-curly-test.js
+++ packages/@ember/-internals/glimmer/tests/integration/components/link-to/routing-curly-test.js
@@ -6,7 +6,6 @@ import { alias } from '@ember/-internals/metal';
 import { subscribe, reset } from '@ember/instrumentation';
 import { Route, NoneLocation } from '@ember/-internals/routing';
 import { EMBER_IMPROVED_INSTRUMENTATION } from '@ember/canary-features';
-import { jQueryDisabled } from '@ember/-internals/views';
 
 // IE includes the host name
 function normalizeUrl(url) {
@@ -862,12 +861,7 @@ moduleFor(
         .then(() => {
           assert.equal(this.$('#contact').text(), 'Contact', 'precond - the link worked');
 
-          if (jQueryDisabled) {
-            // BUG: https://github.com/emberjs/ember.js/issues/17840
-            assert.equal(hidden, 1, 'The link bubbled anyway');
-          } else {
-            assert.strictEqual(hidden, 0, "The link didn't bubble");
-          }
+          assert.equal(hidden, 0, "The link didn't bubble");
         });
     }
 
@@ -918,13 +912,7 @@ moduleFor(
         })
         .then(() => {
           assert.equal(this.$('#contact').text(), 'Contact', 'precond - the link worked');
-
-          if (jQueryDisabled) {
-            // BUG: https://github.com/emberjs/ember.js/issues/17840
-            assert.equal(hidden, 1, 'The link bubbled anyway');
-          } else {
-            assert.strictEqual(hidden, 0, "The link didn't bubble");
-          }
+          assert.equal(hidden, 0, "The link didn't bubble");
         });
     }
 
