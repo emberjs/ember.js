6ed071de16e139ceca78e52e979b78b7fb29bfc1
Robert Jackson <rjackson@linkedin.com>
2019-04-22 10:41:02 -0400

    [FEATURE] Implement `on` modifier.
    See https://emberjs.github.io/rfcs/0471-on-modifier.html for details.
    
    Special thanks to [buschtoens](https://github.com/buschtoens)'s work on
    [ember-on-modifier](https://github.com/buschtoens/ember-on-modifier)
    which heavily inspired this implementation.

---
+++ packages/@ember/-internals/glimmer/lib/modifiers/on.ts
@@ -0,0 +1,241 @@
+import { assert } from '@ember/debug';
+import { DEBUG } from '@glimmer/env';
+import { Opaque, Simple } from '@glimmer/interfaces';
+import { Tag } from '@glimmer/reference';
+import { Arguments, CapturedArguments, ModifierManager } from '@glimmer/runtime';
+import { Destroyable } from '@glimmer/util';
+
+/*
+  Internet Explorer 11 does not support `once` and also does not support
+  passing `eventOptions`. In some situations it then throws a weird script
+  error, like:
+
+  ```
+  Could not complete the operation due to error 80020101
+  ```
+
+  This flag determines, whether `{ once: true }` and thus also event options in
+  general are supported.
+*/
+const SUPPORTS_EVENT_OPTIONS = (() => {
+  try {
+    const div = document.createElement('div');
+    let counter = 0;
+    div.addEventListener('click', () => counter++, { once: true });
+
+    let event;
+    if (typeof Event === 'function') {
+      event = new Event('click');
+    } else {
+      event = document.createEvent('Event');
+      event.initEvent('click', true, true);
+    }
+
+    div.dispatchEvent(event);
+    div.dispatchEvent(event);
+
+    return counter === 1;
+  } catch (error) {
+    return false;
+  }
+})();
+
+export class OnModifierState {
+  public tag: Tag;
+  public element: Element;
+  public args: CapturedArguments;
+  public eventName!: string;
+  public callback!: EventListener;
+  private userProvidedCallback!: EventListener;
+  public once?: boolean;
+  public passive?: boolean;
+  public capture?: boolean;
+  public options?: AddEventListenerOptions;
+  public shouldUpdate = true;
+
+  constructor(element: Element, args: CapturedArguments) {
+    this.element = element;
+    this.args = args;
+    this.tag = args.tag;
+  }
+
+  updateFromArgs() {
+    let { args } = this;
+
+    let { once, passive, capture }: AddEventListenerOptions = args.named.value();
+    if (once !== this.once) {
+      this.once = once;
+      this.shouldUpdate = true;
+    }
+
+    if (passive !== this.passive) {
+      this.passive = passive;
+      this.shouldUpdate = true;
+    }
+
+    if (capture !== this.capture) {
+      this.capture = capture;
+      this.shouldUpdate = true;
+    }
+
+    let options: AddEventListenerOptions;
+    if (once || passive || capture) {
+      options = this.options = { once, passive, capture };
+    } else {
+      this.options = undefined;
+    }
+
+    assert(
+      'You must pass a valid DOM event name as the first argument to the `on` modifier',
+      args.positional.at(0) !== undefined && typeof args.positional.at(0).value() === 'string'
+    );
+    let eventName = args.positional.at(0).value() as string;
+    if (eventName !== this.eventName) {
+      this.eventName = eventName;
+      this.shouldUpdate = true;
+    }
+
+    assert(
+      'You must pass a function as the second argument to the `on` modifier',
+      args.positional.at(1) !== undefined && typeof args.positional.at(1).value() === 'function'
+    );
+    let userProvidedCallback = args.positional.at(1).value() as EventListener;
+    if (userProvidedCallback !== this.userProvidedCallback) {
+      this.userProvidedCallback = userProvidedCallback;
+      this.shouldUpdate = true;
+    }
+
+    if (this.shouldUpdate) {
+      let callback = (this.callback = function(this: Element, event) {
+        if (DEBUG && passive) {
+          event.preventDefault = () => {
+            assert(
+              `You marked this listener as 'passive', meaning that you must not call 'event.preventDefault()': \n\n${userProvidedCallback}`
+            );
+          };
+        }
+
+        if (!SUPPORTS_EVENT_OPTIONS && once) {
+          removeEventListener(this, eventName, callback, options);
+        }
+        return userProvidedCallback.call(null, event);
+      });
+    }
+  }
+
+  destroy() {
+    let { element, eventName, callback, options } = this;
+
+    removeEventListener(element, eventName, callback, options);
+  }
+}
+
+let adds = 0;
+let removes = 0;
+
+function removeEventListener(
+  element: Element,
+  eventName: string,
+  callback: EventListener,
+  options?: AddEventListenerOptions
+): void {
+  removes++;
+
+  if (SUPPORTS_EVENT_OPTIONS) {
+    // when options are supported, use them across the board
+    element.removeEventListener(eventName, callback, options);
+  } else if (options !== undefined && options.capture) {
+    // used only in the following case:
+    //
+    // `{ once: true | false, passive: true | false, capture: true }
+    //
+    // `once` is handled via a custom callback that removes after first
+    // invocation so we only care about capture here as a boolean
+    element.removeEventListener(eventName, callback, true);
+  } else {
+    // used only in the following cases:
+    //
+    // * where there is no options
+    // * `{ once: true | false, passive: true, capture: false }
+    element.removeEventListener(eventName, callback);
+  }
+}
+
+function addEventListener(
+  element: Element,
+  eventName: string,
+  callback: EventListener,
+  options?: AddEventListenerOptions
+): void {
+  adds++;
+
+  if (SUPPORTS_EVENT_OPTIONS) {
+    // when options are supported, use them across the board
+    element.addEventListener(eventName, callback, options);
+  } else if (options !== undefined && options.capture) {
+    // used only in the following case:
+    //
+    // `{ once: true | false, passive: true | false, capture: true }
+    //
+    // `once` is handled via a custom callback that removes after first
+    // invocation so we only care about capture here as a boolean
+    element.addEventListener(eventName, callback, true);
+  } else {
+    // used only in the following cases:
+    //
+    // * where there is no options
+    // * `{ once: true | false, passive: true, capture: false }
+    element.addEventListener(eventName, callback);
+  }
+}
+
+export default class OnModifierManager implements ModifierManager<OnModifierState, Opaque> {
+  public SUPPORTS_EVENT_OPTIONS: boolean = SUPPORTS_EVENT_OPTIONS;
+
+  get counters() {
+    return { adds, removes };
+  }
+
+  create(element: Simple.Element | Element, _state: Opaque, args: Arguments) {
+    const capturedArgs = args.capture();
+
+    return new OnModifierState(<Element>element, capturedArgs);
+  }
+
+  getTag({ tag }: OnModifierState): Tag {
+    return tag;
+  }
+
+  install(state: OnModifierState) {
+    state.updateFromArgs();
+
+    let { element, eventName, callback, options } = state;
+
+    addEventListener(element, eventName, callback, options);
+
+    state.shouldUpdate = false;
+  }
+
+  update(state: OnModifierState) {
+    // stash prior state for el.removeEventListener
+    let { element, eventName, callback, options } = state;
+
+    state.updateFromArgs();
+
+    if (!state.shouldUpdate) {
+      return;
+    }
+
+    // use prior state values for removal
+    removeEventListener(element, eventName, callback, options);
+
+    // read updated values from the state object
+    addEventListener(state.element, state.eventName, state.callback, state.options);
+
+    state.shouldUpdate = false;
+  }
+
+  getDestructor(state: Destroyable) {
+    return state;
+  }
+}
---
+++ packages/@ember/-internals/glimmer/tests/integration/modifiers/on-test.js
@@ -0,0 +1,324 @@
+import { EMBER_GLIMMER_ON_MODIFIER } from '@ember/canary-features';
+import { moduleFor, RenderingTestCase, runTask } from 'internal-test-helpers';
+import { isChrome, isFirefox } from '@ember/-internals/browser-environment';
+import { privatize as P } from '@ember/-internals/container';
+
+const isIE11 = !window.ActiveXObject && 'ActiveXObject' in window;
+
+if (EMBER_GLIMMER_ON_MODIFIER) {
+  moduleFor(
+    '{{on}} Modifier',
+    class extends RenderingTestCase {
+      beforeEach() {
+        // might error if getOnManagerInstance fails
+        this.startingCounters = this.getOnManagerInstance().counters;
+      }
+
+      getOnManagerInstance() {
+        // leveraging private APIs, this can be deleted if these APIs change
+        // but it has been useful to verify some internal details
+        let templateCompiler = this.owner.lookup(P`template-compiler:main`);
+
+        return templateCompiler.resolver.resolver.builtInModifiers.on.manager;
+      }
+
+      assertCounts(expected) {
+        let { counters } = this.getOnManagerInstance();
+
+        this.assert.deepEqual(
+          counters,
+          {
+            adds: expected.adds + this.startingCounters.adds,
+            removes: expected.removes + this.startingCounters.removes,
+          },
+          `counters have incremented by ${JSON.stringify(expected)}`
+        );
+      }
+
+      '@test SUPPORTS_EVENT_OPTIONS is correct (private API usage)'(assert) {
+        let manager = this.getOnManagerInstance();
+
+        let { SUPPORTS_EVENT_OPTIONS } = manager;
+
+        if (isChrome || isFirefox) {
+          assert.strictEqual(SUPPORTS_EVENT_OPTIONS, true, 'is true in chrome and firefox');
+        } else if (isIE11) {
+          assert.strictEqual(SUPPORTS_EVENT_OPTIONS, false, 'is true in chrome and firefox');
+        } else {
+          assert.expect(0);
+        }
+      }
+
+      ['@test it adds an event listener'](assert) {
+        let count = 0;
+
+        this.render('<button {{on "click" this.callback}}>Click Me</button>', {
+          callback() {
+            count++;
+          },
+        });
+
+        assert.equal(count, 0, 'not called on initial render');
+
+        this.assertStableRerender();
+        this.assertCounts({ adds: 1, removes: 0 });
+        assert.equal(count, 0, 'not called on a rerender');
+
+        runTask(() => this.$('button').click());
+        assert.equal(count, 1, 'has been called 1 time');
+
+        runTask(() => this.$('button').click());
+        assert.equal(count, 2, 'has been called 2 times');
+
+        this.assertCounts({ adds: 1, removes: 0 });
+      }
+
+      '@test passes the event to the listener'(assert) {
+        let event;
+        this.render('<button {{on "click" this.callback}}>Click Me</button>', {
+          callback(evt) {
+            event = evt;
+          },
+        });
+
+        runTask(() => this.$('button').click());
+        assert.strictEqual(
+          event.target,
+          this.element.querySelector('button'),
+          'has a valid event with a target'
+        );
+
+        this.assertCounts({ adds: 1, removes: 0 });
+      }
+
+      '@test the listener callback is bound'(assert) {
+        let first = 0;
+        let second = 0;
+        let firstCallback = () => first++;
+        let secondCallback = () => second++;
+
+        this.render('<button {{on "click" this.callback}}>Click Me</button>', {
+          callback: firstCallback,
+        });
+
+        assert.equal(first, 0, 'precond - first not called on initial render');
+        assert.equal(second, 0, 'precond - second not called on initial render');
+
+        runTask(() => this.$('button').click());
+        assert.equal(first, 1, 'first has been called 1 time');
+        assert.equal(second, 0, 'second not called on initial render');
+
+        runTask(() => this.context.set('callback', secondCallback));
+        runTask(() => this.$('button').click());
+
+        assert.equal(first, 1, 'first has been called 1 time');
+        assert.equal(second, 1, 'second has been called 1 times');
+
+        this.assertCounts({ adds: 2, removes: 1 });
+      }
+
+      '@test setting once named argument ensures the callback is only called once'(assert) {
+        let count = 0;
+
+        this.render('<button {{on "click" this.callback once=true}}>Click Me</button>', {
+          callback() {
+            count++;
+          },
+        });
+
+        assert.equal(count, 0, 'not called on initial render');
+
+        this.assertStableRerender();
+        assert.equal(count, 0, 'not called on a rerender');
+
+        runTask(() => this.$('button').click());
+        assert.equal(count, 1, 'has been called 1 time');
+
+        runTask(() => this.$('button').click());
+        assert.equal(count, 1, 'has been called 1 times');
+
+        if (isIE11) {
+          this.assertCounts({ adds: 1, removes: 1 });
+        } else {
+          this.assertCounts({ adds: 1, removes: 0 });
+        }
+      }
+
+      '@test changing from `once=false` to `once=true` ensures the callback can only be called once'(
+        assert
+      ) {
+        let count = 0;
+
+        this.render('<button {{on "click" this.callback once=once}}>Click Me</button>', {
+          callback() {
+            count++;
+          },
+
+          once: false,
+        });
+
+        runTask(() => this.$('button').click());
+        assert.equal(count, 1, 'has been called 1 time');
+
+        runTask(() => this.$('button').click());
+        assert.equal(count, 2, 'has been called 2 times');
+
+        runTask(() => this.context.set('once', true));
+        runTask(() => this.$('button').click());
+        assert.equal(count, 3, 'has been called 3 time');
+
+        runTask(() => this.$('button').click());
+        assert.equal(count, 3, 'is not called again');
+
+        if (isIE11) {
+          this.assertCounts({ adds: 2, removes: 2 });
+        } else {
+          this.assertCounts({ adds: 2, removes: 1 });
+        }
+      }
+
+      '@test setting passive named argument prevents calling preventDefault'() {
+        let matcher = /You marked this listener as 'passive', meaning that you must not call 'event.preventDefault\(\)'/;
+        this.render('<button {{on "click" this.callback passive=true}}>Click Me</button>', {
+          callback(event) {
+            expectAssertion(() => {
+              event.preventDefault();
+            }, matcher);
+          },
+        });
+
+        runTask(() => this.$('button').click());
+      }
+
+      '@test by default bubbling is used (capture: false)'(assert) {
+        this.render(
+          `
+            <div class="outer" {{on 'click' this.handleOuterClick}}>
+              <div class="inner" {{on 'click' this.handleInnerClick}}></div>
+            </div>
+          `,
+          {
+            handleOuterClick() {
+              assert.step('outer clicked');
+            },
+            handleInnerClick() {
+              assert.step('inner clicked');
+            },
+          }
+        );
+
+        runTask(() => this.$('.inner').click());
+
+        assert.verifySteps(['inner clicked', 'outer clicked'], 'uses capture: false by default');
+      }
+
+      '@test specifying capture named argument uses capture semantics'(assert) {
+        this.render(
+          `
+            <div class="outer" {{on 'click' this.handleOuterClick capture=true}}>
+              <div class="inner" {{on 'click' this.handleInnerClick}}></div>
+            </div>
+          `,
+          {
+            handleOuterClick() {
+              assert.step('outer clicked');
+            },
+            handleInnerClick() {
+              assert.step('inner clicked');
+            },
+          }
+        );
+
+        runTask(() => this.$('.inner').click());
+
+        assert.verifySteps(['outer clicked', 'inner clicked'], 'capture works');
+      }
+
+      '@test can use capture and once together'(assert) {
+        this.render(
+          `
+            <div class="outer" {{on 'click' this.handleOuterClick once=true capture=true}}>
+              <div class="inner" {{on 'click' this.handleInnerClick}}></div>
+            </div>
+          `,
+          {
+            handleOuterClick() {
+              assert.step('outer clicked');
+            },
+            handleInnerClick() {
+              assert.step('inner clicked');
+            },
+          }
+        );
+
+        runTask(() => this.$('.inner').click());
+
+        assert.verifySteps(['outer clicked', 'inner clicked'], 'capture works');
+
+        runTask(() => this.$('.inner').click());
+        assert.verifySteps(['inner clicked'], 'once works');
+      }
+
+      '@test unrelated updates to `this` context does not result in removing + re-adding'(assert) {
+        let called = false;
+
+        this.render('<button {{on "click" this.callback}}>Click Me</button>', {
+          callback() {
+            called = true;
+          },
+          otherThing: 0,
+        });
+
+        this.assertCounts({ adds: 1, removes: 0 });
+
+        runTask(() => this.$('button').click());
+        assert.equal(called, 1, 'callback is being invoked');
+
+        runTask(() => this.context.set('otherThing', 1));
+        this.assertCounts({ adds: 1, removes: 0 });
+      }
+
+      '@test asserts when eventName is missing'() {
+        expectAssertion(() => {
+          this.render(`<button {{on undefined this.callback}}>Click Me</button>`, {
+            callback() {},
+          });
+        }, /You must pass a valid DOM event name as the first argument to the `on` modifier/);
+      }
+
+      '@test asserts when eventName is a bound undefined value'() {
+        expectAssertion(() => {
+          this.render(`<button {{on this.someUndefinedThing this.callback}}>Click Me</button>`, {
+            callback() {},
+          });
+        }, /You must pass a valid DOM event name as the first argument to the `on` modifier/);
+      }
+
+      '@test asserts when eventName is a function'() {
+        expectAssertion(() => {
+          this.render(`<button {{on this.callback}}>Click Me</button>`, {
+            callback() {},
+          });
+        }, /You must pass a valid DOM event name as the first argument to the `on` modifier/);
+      }
+
+      '@test asserts when callback is missing'() {
+        expectAssertion(() => {
+          this.render(`<button {{on 'click'}}>Click Me</button>`);
+        }, /You must pass a function as the second argument to the `on` modifier/);
+      }
+
+      '@test asserts when callback is undefined'() {
+        expectAssertion(() => {
+          this.render(`<button {{on 'click' this.foo}}>Click Me</button>`);
+        }, /You must pass a function as the second argument to the `on` modifier/);
+      }
+
+      '@test asserts when callback is null'() {
+        expectAssertion(() => {
+          this.render(`<button {{on 'click' this.foo}}>Click Me</button>`, { foo: null });
+        }, /You must pass a function as the second argument to the `on` modifier/);
+      }
+    }
+  );
+}
