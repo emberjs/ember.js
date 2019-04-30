458fe54899c942f69f26cf789fa5339627f2fa81
Robert Jackson <rjackson@linkedin.com>
2019-04-30 13:56:34 -0400

    Add `codemod` mode for @glimmer/syntax preprocess.
    Currently, setting `mode` to `'codemod'` will:
    
    * Disable `entity` parsing in simple-html-tokenizer (ensures that
      parsing + printing is not lossy)
    * Enable `ignoreStandalone` mode in handlebars parser (ensures that
      standalone whitespace after a block opening or closing is not
      stripped).
    
    In the future we can do other things to make the codemod world even
    better, but this is a really good first step (IMHO).

--- packages/@glimmer/syntax/test/generation/print-test.ts
+++ packages/@glimmer/syntax/test/generation/print-test.ts
@@ -1,162 +1,104 @@
-import { preprocess as parse, print, builders as b } from '@glimmer/syntax';
+import { preprocess as parse, print } from '@glimmer/syntax';
 
 const { test } = QUnit;
 
-function printTransform(template: string) {
-  return print(parse(template));
-}
+let templates = [
+  '<h1></h1>',
+  '<h1 class="foo" id="title"></h1>',
+  '<h1>Test</h1>',
+  '<h1>{{model.title}}</h1>',
+  '<h1>{{link-to "Foo" class="bar"}}</h1>',
+  '<h1 class={{if foo "foo" "bar"}}>Test</h1>',
+  '<h1 class={{color}}>Test</h1>',
+  '<h1 class="{{if active "active" "inactive"}} foo">Test</h1>',
+  '<p {{action "activate"}} {{someting foo="bar"}}>Test</p>',
+  '<p>{{my-component submit=(action (mut model.name) (full-name model.firstName "Smith"))}}</p>',
+  '<ul>{{#each foos as |foo index|}}\n  <li>{{foo}}: {{index}}</li>\n{{/each}}</ul>',
+  '{{#if foo}}<p>{{foo}}</p>{{/if}}',
+  '<Foo>{{bar}}</Foo>',
+  '<Foo></Foo>',
+  '<Foo />',
+  '<Foo as |bar|>{{bar}}</Foo>',
 
-function printEqual(template: string) {
-  QUnit.assert.equal(printTransform(template), template);
-}
+  '<input disabled />',
 
-QUnit.module('[glimmer-syntax] Code generation');
+  // void elements
+  '<br>',
+  '<br />',
 
-test('ElementNode: tag', function() {
-  printEqual('<h1></h1>');
+  // comments
+  '<!-- foo -->',
+  '<div {{!-- foo --}}></div>',
+  '<div>{{!-- foo bar --}}<b></b></div>',
+  '{{!-- {{foo-bar}} --}}',
+
+  // literals
+  '<Panel @arg={{"Foo"}}></Panel>',
+  '<Panel @arg={{true}}></Panel>',
+  '<Panel @arg={{5}}></Panel>',
+  '{{panel arg="Foo"}}',
+  '{{panel arg=true}}',
+  '{{panel arg=5}}',
+
+  // nested tags with indent
+  '<div>\n  <p>Test</p>\n</div>',
+
+  // attributes escaping
+  '<h1 class="foo" id="title" data-a="&quot;Foo&nbsp;&amp;&nbsp;Bar&quot;"></h1>',
+  '<h1 class="< &nbsp; {{if x "&" "<"}} &amp; &quot;">Test</h1>',
+
+  // slash in path
+  '{{namespace/foo "bar" baz="qux"}}',
+];
+
+QUnit.module('[glimmer-syntax] Code generation', function() {
+  function printTransform(template: string) {
+    return print(parse(template));
+  }
+
+  templates.forEach(template => {
+    test(`${template} is stable when printed`, function(assert) {
+      assert.equal(printTransform(template), template);
+    });
+  });
+
+  test('TextNode: chars escape - but do not match', assert => {
+    assert.equal(
+      printTransform('&lt; &amp; &nbsp; &gt; &copy;2018'),
+      '&lt; &amp; &nbsp; &gt; ©2018'
+    );
+  });
+
+  test('Handlebars comment', assert => {
+    assert.equal(printTransform('{{! foo }}'), '{{!-- foo --}}');
+  });
 });
 
-test('ElementNode: nested tags with indent', function() {
-  printEqual('<div>\n  <p>Test</p>\n</div>');
-});
+QUnit.module('[glimmer-syntax] Code generation - source -> source', function() {
+  function printTransform(template: string) {
+    let ast = parse(template, {
+      mode: 'codemod',
+      parseOptions: { ignoreStandalone: true },
+    });
 
-test('ElementNode: attributes', function() {
-  printEqual('<h1 class="foo" id="title"></h1>');
-});
+    return print(ast, { entityEncoding: 'raw' });
+  }
 
-test('ElementNode: attributes escaping', function() {
-  printEqual('<h1 class="foo" id="title" data-a="&quot;Foo&nbsp;&amp;&nbsp;Bar&quot;"></h1>');
-});
+  function buildTest(template: string) {
+    test(`${template} is stable when printed`, function(assert) {
+      assert.equal(printTransform(template), template);
+    });
+  }
 
-test('TextNode: chars escape', assert => {
-  assert.equal(printTransform('&lt; &amp; &nbsp; &gt; &copy;2018'), '&lt; &amp; &nbsp; &gt; ©2018');
-});
+  templates.forEach(buildTest);
 
-test('TextNode: chars', function() {
-  printEqual('<h1>Test</h1>');
-});
+  [
+    '&lt; &amp; &nbsp; &gt; &copy;2018',
 
-test('MustacheStatement: slash in path', function() {
-  printEqual('{{namespace/foo "bar" baz="qux"}}');
-});
+    // newlines after opening block
+    '{{#each}}\n  <li> foo </li>\n{{/each}}',
 
-test('MustacheStatement: path', function() {
-  printEqual('<h1>{{model.title}}</h1>');
-});
-
-test('MustacheStatement: StringLiteral param', function() {
-  printEqual('<h1>{{link-to "Foo"}}</h1>');
-});
-
-test('MustacheStatement: StringLiteral path', function() {
-  printEqual('<Panel @arg={{"Foo"}}></Panel>');
-});
-
-test('MustacheStatement: BooleanLiteral path', function() {
-  printEqual('<Panel @arg={{true}}></Panel>');
-});
-
-test('MustacheStatement: NumberLiteral path', function() {
-  printEqual('<Panel @arg={{5}}></Panel>');
-});
-
-test('MustacheStatement: hash', function() {
-  printEqual('<h1>{{link-to "Foo" class="bar"}}</h1>');
-});
-
-test('MustacheStatement: as element attribute', function() {
-  printEqual('<h1 class={{if foo "foo" "bar"}}>Test</h1>');
-});
-
-test('MustacheStatement: as element attribute with path', function() {
-  printEqual('<h1 class={{color}}>Test</h1>');
-});
-
-test('ConcatStatement: in element attribute string', function() {
-  printEqual('<h1 class="{{if active "active" "inactive"}} foo">Test</h1>');
-});
-
-test('ConcatStatement: in element attribute string escaping', function() {
-  printEqual('<h1 class="< &nbsp; {{if x "&" "<"}} &amp; &quot;">Test</h1>');
-});
-
-test('ElementModifierStatement', function() {
-  printEqual('<p {{action "activate"}} {{someting foo="bar"}}>Test</p>');
-});
-
-test('SubExpression', function() {
-  printEqual(
-    '<p>{{my-component submit=(action (mut model.name) (full-name model.firstName "Smith"))}}</p>'
-  );
-});
-
-test('BlockStatement: multiline', function() {
-  printEqual('<ul>{{#each foos as |foo index|}}\n  <li>{{foo}}: {{index}}</li>\n{{/each}}</ul>');
-});
-
-test('BlockStatement: inline', function() {
-  printEqual('{{#if foo}}<p>{{foo}}</p>{{/if}}');
-});
-
-test('UndefinedLiteral', assert => {
-  const ast = b.program([b.mustache(b.undefined())]);
-  assert.equal(print(ast), '{{undefined}}');
-});
-
-test('NumberLiteral', assert => {
-  const ast = b.program([b.mustache('foo', undefined, b.hash([b.pair('bar', b.number(5))]))]);
-  assert.equal(print(ast), '{{foo bar=5}}');
-});
-
-test('BooleanLiteral', assert => {
-  const ast = b.program([b.mustache('foo', undefined, b.hash([b.pair('bar', b.boolean(true))]))]);
-  assert.equal(print(ast), '{{foo bar=true}}');
-});
-
-test('HTML comment', function() {
-  printEqual('<!-- foo -->');
-});
-
-test('Handlebars comment', assert => {
-  assert.equal(printTransform('{{! foo }}'), '{{!-- foo --}}');
-});
-
-test('Handlebars comment: in ElementNode', function() {
-  printEqual('<div {{!-- foo --}}></div>');
-});
-
-test('Handlebars comment: in ElementNode children', function() {
-  printEqual('<div>{{!-- foo bar --}}<b></b></div>');
-});
-
-test('Handlebars in handlebar comment', function() {
-  printEqual('{{!-- {{foo-bar}} --}}');
-});
-
-test('Void elements', function() {
-  printEqual('<br>');
-});
-
-test('Void elements self closing', function() {
-  printEqual('<br />');
-});
-
-test('Angle bracket component', function() {
-  printEqual('<Foo>{{bar}}</Foo>');
-});
-
-test('Angle bracket component without content', function() {
-  printEqual('<Foo></Foo>');
-});
-
-test('Self-closing angle bracket component', function() {
-  printEqual('<Foo />');
-});
-
-test('Block params', function() {
-  printEqual('<Foo as |bar|>{{bar}}</Foo>');
-});
-
-test('Attributes without value', function() {
-  printEqual('<input disabled />');
+    // TODO: fix whitespace control in codemod mode
+    // '\n{{~#foo-bar~}} {{~/foo-bar~}}  ',
+  ].forEach(buildTest);
 });
