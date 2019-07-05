84ab4ea5a6ce6780f5166efaef648308609b76ee
Samanta de Barros <samanta.dbarros@gmail.com>
2019-07-05 16:12:53 +0200

    Add code example for replaceWith

--- packages/@ember/-internals/routing/lib/services/router.ts
+++ packages/@ember/-internals/routing/lib/services/router.ts
@@ -141,6 +141,20 @@ export default class RouterService extends Service {
      This behavior is different from calling `replaceWith` on a route.
      See the [Router Service RFC](https://github.com/emberjs/rfcs/blob/master/text/0095-router-service.md#query-parameter-semantics) for more info.
 
+     Usage example:
+
+     ```app/routes/application.js
+     import Route from '@ember/routing/route';
+
+     export default Route.extend({
+       beforeModel() {
+         if (!authorized()){
+           this.replaceWith('unauthorized');
+         }
+       }
+     });
+     ```
+
      @method replaceWith
      @param {String} routeNameOrUrl the name of the route or a URL
      @param {...Object} models the model(s) or identifier(s) to be used while
