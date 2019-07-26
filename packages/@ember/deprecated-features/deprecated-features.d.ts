b3d35e62f660ad331037b6f2d6e0ac68013f6dcf
Eli Flanagan <eflanagan@innovu.com>
2019-07-26 11:30:55 -0400

    use esm for consuming deprecated features

---
+++ packages/@ember/deprecated-features/deprecated-features.d.ts
@@ -0,0 +1,10 @@
+export declare const SEND_ACTION: boolean;
+export declare const EMBER_EXTEND_PROTOTYPES: boolean;
+export declare const LOGGER: boolean;
+export declare const MERGE: boolean;
+export declare const ROUTER_EVENTS: boolean;
+export declare const COMPONENT_MANAGER_STRING_LOOKUP: boolean;
+export declare const JQUERY_INTEGRATION: boolean;
+export declare const ALIAS_METHOD: boolean;
+export declare const APP_CTRL_ROUTER_PROPS: boolean;
+export declare const FUNCTION_PROTOTYPE_EXTENSIONS: boolean;
