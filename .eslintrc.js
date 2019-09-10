08297eddea1d5b2d04e18e635d1165e880b3d381
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-09-10 18:41:18 +0200

    ESLint: Disable `no-ternary` rule
    We use the ternary operator in a lot of places in this project, so having that rule enabled seems wrong.

--- .eslintrc.js
+++ .eslintrc.js
@@ -107,7 +107,7 @@ module.exports = {
     'no-shadow': 'error',
     'no-sync': 'off',
     'no-template-curly-in-string': 'error',
-    'no-ternary': 'error',
+    'no-ternary': 'off',
     'no-throw-literal': 'error',
     'no-undef-init': 'error',
     'no-undefined': 'error',
