64a6421615596f4b11c0ba346fa29e0bd8382a11
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-09-11 00:17:02 +0200

    ESLint: Enable `no-var` rule
    Similar to `no-var-keyword` in TSLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -122,7 +122,7 @@ module.exports = {
     'no-useless-constructor': 'error',
     'no-useless-rename': 'error',
     'no-useless-return': 'error',
-    'no-var': 'off',
+    'no-var': 'error',
     'no-void': 'error',
     'no-warning-comments': 'error',
     'object-shorthand': 'error',
