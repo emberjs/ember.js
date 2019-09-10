c635d2340cb469c3b00e88deefebbd833cbd0c86
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-09-10 18:34:03 +0200

    ESLint: Use `ecmaVersion: 6` parser option for all files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
   root: true,
+  parserOptions: {
+    ecmaVersion: 2019,
+  },
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   plugins: ['prettier'],
   rules: {
