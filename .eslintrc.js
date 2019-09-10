f085c3f6f178d7ae9f65271706a45e06d066c6d4
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-09-10 18:49:59 +0200

    ESLint: Disable `max-statements` rules
    We break these rules quite a few times in this project so they are probably not intended to be turned on here

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,8 +41,6 @@ module.exports = {
     'max-lines': 'error',
     'max-nested-callbacks': 'error',
     'max-params': 'error',
-    'max-statements': 'error',
-    'max-statements-per-line': 'off',
     'new-cap': 'error',
     'newline-after-var': 'off',
     'newline-before-return': 'off',
