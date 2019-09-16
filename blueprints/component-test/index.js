4427edd2db29b4fda0a246ac695315f10e23f0eb
Godfrey Chan <godfreykfc@gmail.com>
2019-09-16 15:43:17 -0700

    Remove MU support in component blueprints

--- blueprints/component-test/index.js
+++ blueprints/component-test/index.js
@@ -6,7 +6,6 @@ const isPackageMissing = require('ember-cli-is-package-missing');
 const getPathOption = require('ember-cli-get-component-path-option');
 
 const useTestFrameworkDetector = require('../test-framework-detector');
-const isModuleUnificationProject = require('../module-unification').isModuleUnificationProject;
 
 function needsCurlyBracketInvocation(options) {
   let path = options.path || '';
@@ -34,47 +33,20 @@ module.exports = useTestFrameworkDetector({
   ],
 
   fileMapTokens: function() {
-    if (isModuleUnificationProject(this.project)) {
-      return {
-        __test__() {
-          return 'component-test';
-        },
-        __root__(options) {
-          if (options.inRepoAddon) {
-            return path.join('packages', options.inRepoAddon, 'src');
-          }
-          return 'src';
-        },
-        __testType__(options) {
-          if (options.locals.testType === 'unit') {
-            throw new Error("The --unit flag isn't supported within a module unification app");
-          }
-
-          return '';
-        },
-        __path__(options) {
-          if (options.pod) {
-            throw new Error("Pods aren't supported within a module unification app");
-          }
-          return path.join('ui', 'components', options.dasherizedModuleName);
-        },
-      };
-    } else {
-      return {
-        __root__() {
-          return 'tests';
-        },
-        __testType__(options) {
-          return options.locals.testType || 'integration';
-        },
-        __path__(options) {
-          if (options.pod) {
-            return path.join(options.podPath, options.locals.path, options.dasherizedModuleName);
-          }
-          return 'components';
-        },
-      };
-    }
+    return {
+      __root__() {
+        return 'tests';
+      },
+      __testType__(options) {
+        return options.locals.testType || 'integration';
+      },
+      __path__(options) {
+        if (options.pod) {
+          return path.join(options.podPath, options.locals.path, options.dasherizedModuleName);
+        }
+        return 'components';
+      },
+    };
   },
 
   locals: function(options) {
@@ -93,16 +65,6 @@ module.exports = useTestFrameworkDetector({
 
     if (options.pod && options.path !== 'components' && options.path !== '') {
       componentPathName = [options.path, dasherizedModuleName].filter(Boolean).join('/');
-    } else if (isModuleUnificationProject(this.project)) {
-      if (options.inRepoAddon) {
-        componentPathName = `${options.inRepoAddon}::${dasherizedModuleName}`;
-        templateInvocation = `${stringUtil.classify(options.inRepoAddon)}::${classifiedModuleName}`;
-      } else if (this.project.isEmberCLIAddon()) {
-        componentPathName = `${this.project.pkg.name}::${dasherizedModuleName}`;
-        templateInvocation = `${stringUtil.classify(
-          this.project.pkg.name
-        )}::${classifiedModuleName}`;
-      }
     }
 
     if (needsCurlyBracketInvocation(options)) {
