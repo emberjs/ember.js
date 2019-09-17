ec01cd191a49e64fbce2ac4299ce686973c3da70
Godfrey Chan <godfreykfc@gmail.com>
2019-09-16 19:22:45 -0700

    [BUGFIX beta] Don't import templates in Glimmer Components
    (cherry picked from commit f6e4bcabd49e17e9b1be8ff3642ab95ab302c437)

--- blueprints/component/index.js
+++ blueprints/component/index.js
@@ -185,7 +185,6 @@ module.exports = {
     let importComponent = '';
     let importTemplate = '';
     let defaultExport = '';
-    let contents = '';
 
     // if we're in an addon, build import statement
     if (options.project.isEmberCLIAddon() || (options.inRepoAddon && !options.inDummy)) {
@@ -197,8 +196,6 @@ module.exports = {
           'templates/components/' +
           stringUtil.dasherize(options.entity.name);
       }
-      importTemplate = "import layout from '" + templatePath + "';" + EOL;
-      contents = EOL + '  layout';
     }
 
     let componentClass = this.EMBER_GLIMMER_SET_COMPONENT_TEMPLATE
@@ -208,13 +205,17 @@ module.exports = {
     switch (componentClass) {
       case '@ember/component':
         importComponent = `import Component from '@ember/component';`;
-        defaultExport = `Component.extend({${contents}\n});`;
+        if (templatePath) {
+          importTemplate = `import layout from '${templatePath}';${EOL}`;
+          defaultExport = `Component.extend({${EOL}  layout${EOL}});`;
+        } else {
+          defaultExport = `Component.extend({${EOL}});`;
+        }
         break;
       case '@glimmer/component':
         importComponent = `import Component from '@glimmer/component';`;
-        defaultExport = `class ${classifiedModuleName}Component extends Component {\n}`;
+        defaultExport = `class ${classifiedModuleName}Component extends Component {${EOL}}`;
         break;
-      case '':
       case '@ember/component/template-only':
         importComponent = `import templateOnly from '@ember/component/template-only';`;
         defaultExport = `templateOnly();`;
