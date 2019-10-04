fc7cb2d8ef082d3e691107d401a1a353f27f7b9b
Alon Bukai <alonzorz@gmail.com>
2019-10-04 16:44:13 +0300

    feat: Assert "Cannot pass QPs object in @models and @query at same time

--- packages/@ember/-internals/routing/lib/system/query_params.ts
+++ packages/@ember/-internals/routing/lib/system/query_params.ts
@@ -1,7 +1,13 @@
+import { Option } from '@glimmer/interfaces';
+
 export default class QueryParams {
   values: null | object;
   isQueryParams = true;
-  constructor(values = null) {
+  constructor(values: Option<object> = null) {
     this.values = values;
   }
+}
+
+export function isQueryParams(obj: unknown): obj is QueryParams {
+  return typeof obj === 'object' && obj !== null && obj['isQueryParams'] === true;
 }
