f5ca232b73324cb1dcfe90eeb66652afe870ee65
Robert Wagner <rwwagner90@gmail.com>
2019-10-26 09:00:52 -0400

    Move to correct spot

--- packages/@ember/-internals/routing/lib/system/route.ts
+++ packages/@ember/-internals/routing/lib/system/route.ts
@@ -445,6 +445,11 @@ class Route extends EmberObject implements IRoute {
       }
     });
     ```
+    
+    To allow the `didTransition` event to continue bubbling to the
+    parent route, use `return true;`. When the `didTransition` method
+    has a return value of true then the parent route's `didTransition`
+    method will be fired, enabling "bubbling" behavior for the event.
 
     @event didTransition
     @since 1.2.0
@@ -473,11 +478,6 @@ class Route extends EmberObject implements IRoute {
       }
     });
     ```
-
-    To allow the `didTransition` event to continue bubbling to the
-    parent route, use `return true;`. When the `didTransition` method
-    has a return value of true then the parent route's `didTransition`
-    method will be fired, enabling "bubbling" behavior for the event.
 
     @event loading
     @param {Transition} transition
