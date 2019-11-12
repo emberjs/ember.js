ac14dfe33ba0e9b864d37ee90e1c0d8915dc4c5f
Chris Garrett <cgarrett@linkedin.com>
2019-11-11 20:54:08 -0800

    updates

--- packages/@ember/-internals/glimmer/lib/glimmer-component-docs.ts
+++ packages/@ember/-internals/glimmer/lib/glimmer-component-docs.ts
@@ -1,84 +1,165 @@
 /**
-A component is a reusable UI element that consists of a 
-`.hbs` template and an optional JavaScript class that defines its behavior.
-For example, someone might make a `button` in the template and handle
-the click behavior in the JavaScript file that shares the same name
-as the template.
+  A component is a reusable UI element that consists of a `.hbs` template and an
+  optional JavaScript class that defines its behavior. For example, someone
+  might make a `button` in the template and handle the click behavior in the
+  JavaScript file that shares the same name as the template.
 
-The APIs available in a component
-vary depending on whether they import from `@glimmer/component`
-or the older "classic" type, `@ember/component`.
-The documentation below covers 100% of the available methods, hooks,
-and properties of `@glimmer/component`.
+  The APIs available in a component vary depending on whether they import from
+  `@glimmer/component` or the older "classic" type, `@ember/component`. The
+  documentation below covers 100% of the available methods, hooks, and
+  properties of `@glimmer/component`.
 
-## Defining a component
+  ## Defining a component
 
-To define a component, subclass `Component` and add your own properties,
-methods and lifecycle hooks:
+  To define a component, subclass `Component` and add your own properties,
+  methods and lifecycle hooks:
 
-```javascript
-import Component from '@glimmer/component';
+  ```javascript
+  import Component from '@glimmer/component';
 
-export default class SomeComponent extends Component {
-  // your code here 
-}
-```
+  export default class SomeComponent extends Component {
+    // your code here
+  }
+  ```
 
-@module @glimmer/component
-@public
+  @module @glimmer/component
+  @public
 */
 
 /**
- @class GlimmerComponent
- @public
- @static
+  @class GlimmerComponent
+  @public
+  @static
 */
 
 /**
-Constructs a new component and assigns itself the passed properties.
-@method constructor
-@static
-@for @glimmer/component
-@public
+  Constructs a new component and assigns itself the passed properties. The
+  constructor is run whenever a new instance of the component is created, and
+  can be used to setup the initial state of the component.
 
-```javascript
-import Component from '@glimmer/component';
+  ```javascript
+  import Component from '@glimmer/component';
 
-export default class SomeComponent extends Component {
-  constructor(owner, args) {
-    super(...arguments);
-    if (this.args.fadeIn === true) {
-      // fade in logic here
+  export default class SomeComponent extends Component {
+    constructor(owner, args) {
+      super(owner, args);
+
+      if (this.args.displayMode === 'list') {
+        this.items = [];
+      }
     }
   }
-}
-```
+  ```
+
+  Service injections and arguments are available in the constructor.
+
+  ```javascript
+  import Component from '@glimmer/component';
+  import { inject as service } from '@ember/service';
+
+  export default class SomeComponent extends Component {
+    @service animation;
+
+    constructor(owner, args) {
+      super(owner, args);
+
+      if (this.args.fadeIn === true) {
+        this.animation.register(this, 'fade-in');
+      }
+    }
+  }
+  ```
+
+  @method constructor
+  @for @glimmer/component
+  @param {Object} owner An owner object.
+  @param {Object} args An args object.
+  @public
 */
 
 /**
-`didInsertElement` is called when the component has been inserted into the DOM.
-Override this function to do any set up that requires an element in the document body.
-@method didInsertElement
-@static
-@for @glimmer/component
-@public
+  `willDestroy` is called after the component has been removed from the DOM, but
+  before the component is fully destroyed. This lifecycle hook can be used to
+  cleanup the component and any related state.
 
+  ```javascript
+  import Component from '@glimmer/component';
+  import { inject as service } from '@ember/service';
 
+  export default class SomeComponent extends Component {
+    @service animation;
+
+    willDestroy() {
+      this.animation.unregister(this);
+    }
+  }
+  ```
+
+  @method willDestroy
+  @for @glimmer/component
+  @public
 */
 
 /**
-`didUpdate` is called when the component has updated and rerendered itself.
-It is called only during a rerender, not during an initial render.
-@method didUpdate
-@static
-@for @glimmer/component
-@public
+  The `args` property of Glimmer components contains the _arguments_ that are
+  passed to the component. For instance, the following component usage:
+
+  ```handlebars
+  <SomeComponent @fadeIn={{true}} />
+  ```
+
+  Would result in the following `args` object to be passed to the component:
+
+  ```javascript
+  { fadeIn: true }
+  ```
+
+  `args` can be accessed at any point in the component lifecycle, including
+  `constructor` and `willDestroy`. They are also automatically marked as tracked
+  properties, and they can be depended on as computed property dependencies:
+
+  ```javascript
+  import Component from '@glimmer/component';
+  import { tracked } from '@ember/tracking';
+  import { computed } from '@ember/object';
+
+  export default class SomeComponent extends Component {
+
+    @computed('args.someValue')
+    get computedGetter() {
+      // updates whenever args.someValue updates
+      return this.args.someValue;
+    }
+
+    get standardGetter() {
+      // updates whenever args.anotherValue updates (Ember 3.13+)
+      return this.args.anotherValue;
+    }
+  }
+  ```
+
+  @property args
+  @for @glimmer/component
+  @type Object
+  @public
 */
 
 /**
-`willDestroy` is called before the component has been removed from the DOM.
-@method willDestroy
-@static
-@for @glimmer/component
-@public
+  Flag to tell if the component is in the process of destroying. This is set to
+  true before `willDestroy` is called.
+
+  @property isDestroying
+  @for @glimmer/component
+  @type boolean
+  @public
+*/
+
+/**
+  Flag to tell if the component has been fully destroyed. This is set to true
+  after `willDestroy` is called.
+
+  @property isDestroyed
+  @for @glimmer/component
+  @type boolean
+  @public
 */
--- packages/@ember/-internals/glimmer/lib/glimmer-tracking-docs.ts
+++ packages/@ember/-internals/glimmer/lib/glimmer-tracking-docs.ts
@@ -1,47 +1,106 @@
 /**
-In order to tell Ember a value might change, we need to mark it as trackable.
-Trackable values are values that:
+  In order to tell Ember a value might change, we need to mark it as trackable.
+  Trackable values are values that:
 
-- Can change over their component’s lifetime and
-- Should cause Ember to rerender if and when they change
+  - Can change over their component’s lifetime and
+  - Should cause Ember to rerender if and when they change
 
-We can do this by marking the field with the @tracked decorator.
+  We can do this by marking the field with the @tracked decorator.
 
-@module @glimmer/tracking
-@public
+  @module @glimmer/tracking
+  @public
 */
 
 /**
- @class GlimmerTracking
- @public
- @static
-*/
+  Marks a property as tracked. By default, values that are rendered in Ember app
+  templates are _static_, meaning that updates to them won't cause the
+  application to rerender. Marking a property as tracked means that when that
+  property changes, any templates that used that property, directly or
+  indirectly, will rerender. For instance, consider this component:
 
-/**
-Marks a property as tracked. For example, by default,
-a component's properties are expected to be static,
-meaning you are not able to update them and have the template update
-accordingly. Marking a property as tracked means that when that
-property changes, a rerender of the component is scheduled so the
-template is kept up to date.
+  ```handlebars
+  <div>Count: {{this.count}}</div>
+  <div>Times Ten: {{this.timesTen}}</div>
+  <div>
+    <button {{on "click" this.plusOne}}>
+      Plus One
+    </button>
+  </div>
+  ```
 
-```javascript
-import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
+  ```javascript
+  import Component from '@glimmer/component';
+  import { tracked } from '@glimmer/tracking';
+  import { action } from '@ember/object';
 
-export default class SomeComponent extends Component {
-  @tracked item = 1;
+  export default class Counter extends Component {
+    @tracked count = 0;
 
-  @action
-  plusOne() {
-    this.item += 1;
+    get timesTen() {
+      return this.count * 10;
+    }
+
+    @action
+    plusOne() {
+      this.item += 1;
+    }
   }
-}
-```
+  ```
 
-@method tracked
-@static
-@for @glimmer/tracking
-@public
+  Both the `{{this.count}}` and the `{{this.timesTen}}` properties in the
+  template will update whenever the button is clicked. Any tracked properties
+  that are used in any way to calculate a value that is used in the template
+  will cause a rerender when updated - this includes through method calls and
+  other means:
+
+  ```javascript
+  import Component from '@glimmer/component';
+  import { tracked } from '@glimmer/tracking';
+  import { action } from '@ember/object';
+
+  class Entries {
+    @tracked name;
+    @tracked phoneNumber;
+
+    constructor(name, phoneNumber) {
+      this.name = name;
+      this.phoneNumber = phoneNumber;
+    }
+  }
+
+  export default class PhoneBook extends Component {
+    entries = [
+      new Entry('Pizza Palace', 5551234),
+      new Entry('1st Street Cleaners', 5554321),
+      new Entry('Plants R Us', 5552468),
+    ];
+
+    // Any usage of this property will update whenever any of the names in the
+    // entries arrays are updated
+    get names() {
+      return this.entries.map(e => e.name);
+    }
+
+    // Any usage of this property will update whenever any of the numbers in the
+    // entries arrays are updated
+    get numbers() {
+      return this.getFormattedNumbers();
+    }
+
+    getFormattedNumbers() {
+      return this.entries
+        .map(e => e.phoneNumber)
+        .map(number => {
+          let numberString = '' + number;
+
+          return numberString.slice(0, 3) + '-' + numberString.slice(3);
+        });
+    }
+  }
+  ```
+
+  @method tracked
+  @static
+  @for @glimmer/tracking
+  @public
 */
