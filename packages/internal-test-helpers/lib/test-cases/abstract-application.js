4f5e3811ebc11d7cc7a5455c66fc5439b2d5084d
Robert Jackson <rjackson@linkedin.com>
2019-11-15 16:41:32 -0500

    Remove run wrapping from test helpers's `visit`
    Co-authored-by: Kris Selden <kselden@linkedin.com>

--- packages/internal-test-helpers/lib/test-cases/abstract-application.js
+++ packages/internal-test-helpers/lib/test-cases/abstract-application.js
@@ -18,9 +18,7 @@ export default class AbstractApplicationTestCase extends AbstractTestCase {
 
   async visit(url, options) {
     // Create the instance
-    let instance = await this._ensureInstance(options).then(instance =>
-      runTask(() => instance.visit(url))
-    );
+    let instance = await this._ensureInstance(options).then(instance => instance.visit(url));
 
     // Await all asynchronous actions
     await runLoopSettled();
