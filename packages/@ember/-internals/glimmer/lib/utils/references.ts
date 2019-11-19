d60ed1c575970f2c616083831b4194af3a7fa5d7
Beau Davenport <rdavenport@linkedin.com>
2019-11-18 22:04:13 -0800

    [BUGFIX beta] add 'symbol' to references primitive type check

--- packages/@ember/-internals/glimmer/lib/utils/references.ts
+++ packages/@ember/-internals/glimmer/lib/utils/references.ts
@@ -520,7 +520,8 @@ function ensurePrimitive(value: unknown) {
         value === null ||
         typeof value === 'boolean' ||
         typeof value === 'number' ||
-        typeof value === 'string'
+        typeof value === 'string' ||
+        typeof value === 'symbol'
     );
   }
 }
