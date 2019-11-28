52480f166760c39858dabf8bf2d8244f772ed8da
Chris Garrett <cgarrett@linkedin.com>
2019-11-27 19:08:14 -0800

    update tracking, add transaction

--- packages/@glimmer/validator/lib/meta.ts
+++ packages/@glimmer/validator/lib/meta.ts
@@ -1,44 +1,49 @@
 import {
   dirty,
-  update,
   createUpdatableTag,
   UpdatableTag,
   CONSTANT_TAG,
-  isConstTag,
   ConstantTag,
 } from './validators';
+import { propertyDidChange } from './tracking';
 
-type Tags = Map<PropertyKey, UpdatableTag>;
-const TRACKED_TAGS = new WeakMap<object, Tags>();
 
 function isObject<T>(u: T): u is object & T {
   return typeof u === 'object' && u !== null;
 }
 
-export function dirtyTag<T>(obj: T, key: keyof T): void {
-  if (isObject(obj)) {
-    let tag = tagFor(obj, key);
+///////////
 
-    if (tag === undefined) {
-      updateTag(obj, key, createUpdatableTag());
-    } else if (isConstTag(tag)) {
-      throw new Error(`BUG: Can't update a constant tag`);
-    } else {
-      dirty(tag);
+type Tags = Map<PropertyKey, UpdatableTag>;
+
+const TRACKED_TAGS = new WeakMap<object, Tags>();
+
+export function dirtyTagFor<T>(obj: T, key: keyof T | string | symbol): void {
+  if (isObject(obj)) {
+    let tags = TRACKED_TAGS.get(obj);
+
+    // No tags have been setup for this object yet, return
+    if (tags === undefined) return;
+
+    // Dirty the tag for the specific property if it exists
+    let propertyTag = tags.get(key);
+
+    if (propertyTag !== undefined) {
+      dirty(propertyTag);
+      propertyDidChange();
     }
   } else {
     throw new Error(`BUG: Can't update a tag for a primitive`);
   }
 }
 
-export function tagFor<T extends object>(obj: T, key: keyof T): UpdatableTag;
-export function tagFor<T>(obj: T, key: string): ConstantTag;
-export function tagFor<T>(obj: T, key: keyof T): UpdatableTag | ConstantTag {
+export function tagFor<T>(obj: T, key: keyof T | string | symbol): UpdatableTag | ConstantTag {
   if (isObject(obj)) {
     let tags = TRACKED_TAGS.get(obj);
 
     if (tags === undefined) {
       tags = new Map();
+
       TRACKED_TAGS.set(obj, tags);
     } else if (tags.has(key)) {
       return tags.get(key)!;
@@ -46,24 +51,9 @@ export function tagFor<T>(obj: T, key: keyof T): UpdatableTag | ConstantTag {
 
     let tag = createUpdatableTag();
     tags.set(key, tag);
+
     return tag;
   } else {
     return CONSTANT_TAG;
-  }
-}
-
-export function updateTag<T>(obj: T, key: keyof T, newTag: UpdatableTag): UpdatableTag {
-  if (isObject(obj)) {
-    let tag = tagFor(obj, key);
-
-    if (isConstTag(tag)) {
-      throw new Error(`BUG: Can't update a constant tag`);
-    } else {
-      update(tag, newTag);
-    }
-
-    return tag;
-  } else {
-    throw new Error(`BUG: Can't update a tag for a primitive`);
   }
 }
--- packages/@glimmer/validator/lib/tracking.ts
+++ packages/@glimmer/validator/lib/tracking.ts
@@ -1,6 +1,7 @@
-import { Tag, combine, CONSTANT_TAG } from './validators';
-import { createTag, dirty } from './validators';
-import { tagFor, dirtyTag } from './meta';
+import { DEBUG } from '@glimmer/env';
+import { Tag, combine, createTag, dirty, CONSTANT_TAG } from './validators';
+import { tagFor, dirtyTagFor } from './meta';
+import { markTagAsConsumed, runInAutotrackingTransaction } from './debug';
 
 type Option<T> = T | null;
 
@@ -28,6 +29,11 @@ class Tracker {
 
   add(tag: Tag) {
     this.tags.add(tag);
+
+    if (DEBUG) {
+      markTagAsConsumed!(tag, new Error());
+    }
+
     this.last = tag;
   }
 
@@ -46,14 +52,20 @@ class Tracker {
   }
 }
 
-export function track(callback: () => void): Tag {
+//////////
+
+export function track(callback: () => void, debuggingContext?: string | false): Tag {
   let parent = CURRENT_TRACKER;
   let current = new Tracker();
 
   CURRENT_TRACKER = current;
 
   try {
-    callback();
+    if (DEBUG) {
+      runInAutotrackingTransaction!(callback, debuggingContext);
+    } else {
+      callback();
+    }
   } finally {
     CURRENT_TRACKER = parent;
   }
@@ -67,12 +79,34 @@ export function consume(tag: Tag) {
   }
 }
 
+export function isTracking() {
+  return CURRENT_TRACKER !== null;
+}
+
+export function untrack(callback: () => void) {
+  let parent = CURRENT_TRACKER;
+  CURRENT_TRACKER = null;
+
+  try {
+    callback();
+  } finally {
+    CURRENT_TRACKER = parent;
+  }
+}
+
+
 //////////
 
 export const EPOCH = createTag();
 
 export type Getter<T, K extends keyof T> = (self: T) => T[K] | undefined;
 export type Setter<T, K extends keyof T> = (self: T, value: T[K]) => void;
+
+export let propertyDidChange = function() {};
+
+export function setPropertyDidChange(cb: () => void) {
+  propertyDidChange = cb;
+}
 
 export function trackedData<T extends object, K extends keyof T>(
   key: K,
@@ -99,8 +133,9 @@ export function trackedData<T extends object, K extends keyof T>(
 
   function setter(self: T, value: T[K]): void {
     dirty(EPOCH);
-    dirtyTag(self, key);
+    dirtyTagFor(self, key);
     values.set(self, value);
+    propertyDidChange();
   }
 
   return { getter, setter };
