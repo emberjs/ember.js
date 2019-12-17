a8fd60695070be9080771ffdd8b77faad73f43f0
Chris Garrett <cgarrett@linkedin.com>
2019-12-17 12:21:03 -0800

    Object.values is not supported in IE11

---
+++ packages/@glimmer/reference/test/utils/platform.ts
@@ -0,0 +1,7 @@
+export default function objectValues(obj: any) {
+  if (typeof Object.values === 'function') {
+    return Object.values(obj);
+  } else {
+    return Object.keys(obj).map(k => obj[k]);
+  }
+}
