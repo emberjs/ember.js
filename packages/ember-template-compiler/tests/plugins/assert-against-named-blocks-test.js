2fe2d01ad3312d1d28d4723d3f170357b2f7f688
Chris Garrett <cgarrett@linkedin.com>
2020-01-28 15:16:49 -0800

    add assertion for yield, fix assertion for usage

--- packages/ember-template-compiler/tests/plugins/assert-against-named-blocks-test.js
+++ packages/ember-template-compiler/tests/plugins/assert-against-named-blocks-test.js
@@ -10,6 +10,10 @@ moduleFor(
       compile(`<Foo><:named></:named></Foo>`, {
         moduleName: 'baz/foo-bar',
       });
+
+      compile(`{{yield to="bar"}}`, {
+        moduleName: 'baz/bar-baz',
+      });
     }
 
     [`@feature(!EMBER_NAMED_BLOCKS) is enabled when feature flag is disabled`]() {
@@ -17,7 +21,13 @@ moduleFor(
         compile(`<Foo><:named></:named></Foo>`, {
           moduleName: 'baz/foo-bar',
         });
-      }, `Named blocks are not currently available, attempted to use the {{:named}} named block. ('baz/foo-bar' @ L1:C5) `);
+      }, `Named blocks are not currently available, attempted to use the <:named> named block. ('baz/foo-bar' @ L1:C5) `);
+
+      expectAssertion(() => {
+        compile(`{{yield to="bar"}}`, {
+          moduleName: 'baz/bar-baz',
+        });
+      }, `Named blocks are not currently available, attempted to yield to a named block other than "default" or "inverse": {{yield to="bar"}}. ('baz/bar-baz' @ L1:C0) `);
     }
   }
 );
