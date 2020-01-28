ef6fc46bb010f72d0b7c10e22ad31afd33f8edf4
Chris Garrett <cgarrett@linkedin.com>
2020-01-28 15:16:49 -0800

    add assertion for yield, fix assertion for usage
    (cherry picked from commit 2fe2d01ad3312d1d28d4723d3f170357b2f7f688)

--- packages/ember-template-compiler/lib/plugins/assert-against-named-blocks.ts
+++ packages/ember-template-compiler/lib/plugins/assert-against-named-blocks.ts
@@ -22,15 +22,34 @@ export default function assertAgainstNamedBlocks(env: ASTPluginEnvironment): AST
     visitor: {
       ElementNode(node: AST.ElementNode) {
         if (node.tag[0] === ':') {
-          assert(assertMessage(node.tag, moduleName, node));
+          let sourceInformation = calculateLocationDisplay(moduleName, node.loc);
+
+          assert(
+            `Named blocks are not currently available, attempted to use the <${node.tag}> named block. ${sourceInformation}`
+          );
+        }
+      },
+
+      MustacheStatement(node: AST.MustacheStatement) {
+        if (node.path.type === 'PathExpression' && node.path.original === 'yield') {
+          let to = node.hash.pairs.filter(pair => pair.key === 'to')[0];
+
+          // Glimmer template compiler ensures yield must receive a string literal,
+          // so we only need to check if it is not "default" or "inverse"
+          if (
+            to &&
+            to.value.type === 'StringLiteral' &&
+            to.value.original !== 'default' &&
+            to.value.original !== 'inverse'
+          ) {
+            let sourceInformation = calculateLocationDisplay(moduleName, node.loc);
+
+            assert(
+              `Named blocks are not currently available, attempted to yield to a named block other than "default" or "inverse": {{yield to="${to.value.original}"}}. ${sourceInformation}`
+            );
+          }
         }
       },
     },
   };
-}
-
-function assertMessage(tagName: string, moduleName: string, node: AST.ElementNode) {
-  let sourceInformation = calculateLocationDisplay(moduleName, node.loc);
-
-  return `Named blocks are not currently available, attempted to use the {{${tagName}}} named block. ${sourceInformation}`;
 }
