f7c3b90e3341d226f7763cd63df940d0aded5c63
Chris Garrett <cgarrett@linkedin.com>
2020-02-12 12:32:18 -0800

    [REFACTOR] Updates local-debug-flags to be more descriptive
    We recently let through a regression that would have failed tests, if
    the CI flag to enable LOCAL_DEBUG was on. This PR refactors the flags so
    that:
    
    1. They are distinguished from `DEBUG`, which is now imported from
       `@glimmer/env` and published.
    2. They have clear separate purposes.
       * `LOCAL_DEBUG` is for code that is meant to verify correctness and
       assert if things are not correct. It is enabled by default.
       * `LOCAL_SHOULD_LOG` is for logging things for local debugging. It
       is not enabled by default, and should not be used to verify
       correctness as such.

--- packages/@glimmer/local-debug-flags/index.ts
+++ packages/@glimmer/local-debug-flags/index.ts
@@ -1,9 +1,15 @@
-export const DEBUG = (() => {
+export const LOCAL_DEBUG = (() => {
   let location = typeof window !== 'undefined' && window.location;
-  if (location && /[?&]glimmer_logging/.test(window.location.search)) {
+  if (location && /[?&]disable_local_debug/.test(window.location.search)) {
+    return false;
+  }
+  return true;
+})();
+
+export const LOCAL_SHOULD_LOG = (() => {
+  let location = typeof window !== 'undefined' && window.location;
+  if (location && /[?&]enable_local_should_log/.test(window.location.search)) {
     return true;
   }
   return false;
 })();
-
-export const DEVMODE = true;
