a3e1c7e8b9cae1867eb2f7cef22ca739009d874e
Robert Jackson <rjackson@linkedin.com>
2020-02-27 20:49:26 -0500

    Fix tests failing when enabling template-only-glimmer-components.
    These tests were attempting to modify the
    `ENV._TEMPLATE_ONLY_GLIMMER_COMPONENTS` value at runtime, but load
    different code when it is and isn't set. So now that the test suite is setting
    `template-only-glimmer-components` when the Ember code loads we can no
    longer "fake" these tests.
    
    However, the changes to the test runner mean that we still test this
    both on and off so we are not loosing coverage.

--- packages/@ember/-internals/glimmer/tests/integration/components/template-only-components-test.js
+++ packages/@ember/-internals/glimmer/tests/integration/components/template-only-components-test.js
@@ -15,276 +15,256 @@ class TemplateOnlyComponentsTest extends RenderingTestCase {
   }
 }
 
-moduleFor(
-  'Components test: template-only components (glimmer components)',
-  class extends TemplateOnlyComponentsTest {
-    constructor() {
-      super(...arguments);
-      this._TEMPLATE_ONLY_GLIMMER_COMPONENTS = ENV._TEMPLATE_ONLY_GLIMMER_COMPONENTS;
-      ENV._TEMPLATE_ONLY_GLIMMER_COMPONENTS = true;
+if (ENV._TEMPLATE_ONLY_GLIMMER_COMPONENTS) {
+  moduleFor(
+    'Components test: template-only components (glimmer components)',
+    class extends TemplateOnlyComponentsTest {
+      ['@test it can render a template-only component']() {
+        this.registerTemplateOnlyComponent('foo-bar', 'hello');
+
+        this.render('{{foo-bar}}');
+
+        this.assertInnerHTML('hello');
+
+        this.assertStableRerender();
+      }
+
+      ['@test it can render named arguments']() {
+        this.registerTemplateOnlyComponent('foo-bar', '|{{@foo}}|{{@bar}}|');
+
+        this.render('{{foo-bar foo=foo bar=bar}}', {
+          foo: 'foo',
+          bar: 'bar',
+        });
+
+        this.assertInnerHTML('|foo|bar|');
+
+        this.assertStableRerender();
+
+        runTask(() => this.context.set('foo', 'FOO'));
+
+        this.assertInnerHTML('|FOO|bar|');
+
+        runTask(() => this.context.set('bar', 'BAR'));
+
+        this.assertInnerHTML('|FOO|BAR|');
+
+        runTask(() => this.context.setProperties({ foo: 'foo', bar: 'bar' }));
+
+        this.assertInnerHTML('|foo|bar|');
+      }
+
+      ['@test it does not reflected arguments as properties']() {
+        this.registerTemplateOnlyComponent('foo-bar', '|{{foo}}|{{this.bar}}|');
+
+        this.render('{{foo-bar foo=foo bar=bar}}', {
+          foo: 'foo',
+          bar: 'bar',
+        });
+
+        this.assertInnerHTML('|||');
+
+        this.assertStableRerender();
+
+        runTask(() => this.context.set('foo', 'FOO'));
+
+        this.assertInnerHTML('|||');
+
+        runTask(() => this.context.set('bar', null));
+
+        this.assertInnerHTML('|||');
+
+        runTask(() => this.context.setProperties({ foo: 'foo', bar: 'bar' }));
+
+        this.assertInnerHTML('|||');
+      }
+
+      ['@test it does not have curly component features']() {
+        this.registerTemplateOnlyComponent('foo-bar', 'hello');
+
+        this.render('{{foo-bar tagName="p" class=class}}', {
+          class: 'foo bar',
+        });
+
+        this.assertInnerHTML('hello');
+
+        this.assertStableRerender();
+
+        runTask(() => this.context.set('class', 'foo'));
+
+        this.assertInnerHTML('hello');
+
+        runTask(() => this.context.set('class', null));
+
+        this.assertInnerHTML('hello');
+
+        runTask(() => this.context.set('class', 'foo bar'));
+
+        this.assertInnerHTML('hello');
+      }
+
+      ['@test it has the correct bounds']() {
+        this.registerTemplateOnlyComponent('foo-bar', 'hello');
+
+        this.render('outside {{#if this.isShowing}}before {{foo-bar}} after{{/if}} outside', {
+          isShowing: true,
+        });
+
+        this.assertInnerHTML('outside before hello after outside');
+
+        this.assertStableRerender();
+
+        runTask(() => this.context.set('isShowing', false));
+
+        this.assertInnerHTML('outside <!----> outside');
+
+        runTask(() => this.context.set('isShowing', null));
+
+        this.assertInnerHTML('outside <!----> outside');
+
+        runTask(() => this.context.set('isShowing', true));
+
+        this.assertInnerHTML('outside before hello after outside');
+      }
+
+      ['@test asserts when a shared dependency is changed during rendering, and keeps original context']() {
+        this.registerComponent('x-outer', {
+          ComponentClass: Component.extend({
+            value: 1,
+            wrapper: EmberObject.create({ content: null }),
+          }),
+          template:
+            '<div id="outer-value">{{x-inner-template-only value=this.wrapper.content wrapper=wrapper}}</div>{{x-inner value=value wrapper=wrapper}}',
+        });
+
+        this.registerComponent('x-inner', {
+          ComponentClass: Component.extend({
+            didReceiveAttrs() {
+              this.get('wrapper').set('content', this.get('value'));
+            },
+            value: null,
+          }),
+          template: '<div id="inner-value">{{wrapper.content}}</div>',
+        });
+
+        this.registerTemplateOnlyComponent('x-inner-template-only', '{{@value}}');
+
+        let expectedBacktrackingMessage = backtrackingMessageFor('content', '<.+?>', {
+          renderTree: ['x-outer', 'this.wrapper.content'],
+        });
+
+        expectAssertion(() => {
+          this.render('{{x-outer}}');
+        }, expectedBacktrackingMessage);
+      }
     }
+  );
+} else {
+  moduleFor(
+    'Components test: template-only components (curly components)',
+    class extends TemplateOnlyComponentsTest {
+      ['@test it can render a template-only component']() {
+        this.registerTemplateOnlyComponent('foo-bar', 'hello');
 
-    teardown() {
-      super.teardown();
-      ENV._TEMPLATE_ONLY_GLIMMER_COMPONENTS = this._TEMPLATE_ONLY_GLIMMER_COMPONENTS;
+        this.render('{{foo-bar}}');
+
+        this.assertComponentElement(this.firstChild, { content: 'hello' });
+
+        this.assertStableRerender();
+      }
+
+      ['@test it can render named arguments']() {
+        this.registerTemplateOnlyComponent('foo-bar', '|{{@foo}}|{{@bar}}|');
+
+        this.render('{{foo-bar foo=foo bar=bar}}', {
+          foo: 'foo',
+          bar: 'bar',
+        });
+
+        this.assertComponentElement(this.firstChild, { content: '|foo|bar|' });
+
+        this.assertStableRerender();
+
+        runTask(() => this.context.set('foo', 'FOO'));
+
+        this.assertComponentElement(this.firstChild, { content: '|FOO|bar|' });
+
+        runTask(() => this.context.set('bar', 'BAR'));
+
+        this.assertComponentElement(this.firstChild, { content: '|FOO|BAR|' });
+
+        runTask(() => this.context.setProperties({ foo: 'foo', bar: 'bar' }));
+
+        this.assertComponentElement(this.firstChild, { content: '|foo|bar|' });
+      }
+
+      ['@test it renders named arguments as reflected properties']() {
+        this.registerTemplateOnlyComponent('foo-bar', '|{{foo}}|{{this.bar}}|');
+
+        this.render('{{foo-bar foo=foo bar=bar}}', {
+          foo: 'foo',
+          bar: 'bar',
+        });
+
+        this.assertComponentElement(this.firstChild, { content: '|foo|bar|' });
+
+        this.assertStableRerender();
+
+        runTask(() => this.context.set('foo', 'FOO'));
+
+        this.assertComponentElement(this.firstChild, { content: '|FOO|bar|' });
+
+        runTask(() => this.context.set('bar', null));
+
+        this.assertComponentElement(this.firstChild, { content: '|FOO||' });
+
+        runTask(() => this.context.setProperties({ foo: 'foo', bar: 'bar' }));
+
+        this.assertComponentElement(this.firstChild, { content: '|foo|bar|' });
+      }
+
+      ['@test it has curly component features']() {
+        this.registerTemplateOnlyComponent('foo-bar', 'hello');
+
+        this.render('{{foo-bar tagName="p" class=class}}', {
+          class: 'foo bar',
+        });
+
+        this.assertComponentElement(this.firstChild, {
+          tagName: 'p',
+          attrs: { class: classes('foo bar ember-view') },
+          content: 'hello',
+        });
+
+        this.assertStableRerender();
+
+        runTask(() => this.context.set('class', 'foo'));
+
+        this.assertComponentElement(this.firstChild, {
+          tagName: 'p',
+          attrs: { class: classes('foo ember-view') },
+          content: 'hello',
+        });
+
+        runTask(() => this.context.set('class', null));
+
+        this.assertComponentElement(this.firstChild, {
+          tagName: 'p',
+          attrs: { class: classes('ember-view') },
+          content: 'hello',
+        });
+
+        runTask(() => this.context.set('class', 'foo bar'));
+
+        this.assertComponentElement(this.firstChild, {
+          tagName: 'p',
+          attrs: { class: classes('foo bar ember-view') },
+          content: 'hello',
+        });
+      }
     }
-
-    ['@test it can render a template-only component']() {
-      this.registerTemplateOnlyComponent('foo-bar', 'hello');
-
-      this.render('{{foo-bar}}');
-
-      this.assertInnerHTML('hello');
-
-      this.assertStableRerender();
-    }
-
-    ['@test it can render named arguments']() {
-      this.registerTemplateOnlyComponent('foo-bar', '|{{@foo}}|{{@bar}}|');
-
-      this.render('{{foo-bar foo=foo bar=bar}}', {
-        foo: 'foo',
-        bar: 'bar',
-      });
-
-      this.assertInnerHTML('|foo|bar|');
-
-      this.assertStableRerender();
-
-      runTask(() => this.context.set('foo', 'FOO'));
-
-      this.assertInnerHTML('|FOO|bar|');
-
-      runTask(() => this.context.set('bar', 'BAR'));
-
-      this.assertInnerHTML('|FOO|BAR|');
-
-      runTask(() => this.context.setProperties({ foo: 'foo', bar: 'bar' }));
-
-      this.assertInnerHTML('|foo|bar|');
-    }
-
-    ['@test it does not reflected arguments as properties']() {
-      this.registerTemplateOnlyComponent('foo-bar', '|{{foo}}|{{this.bar}}|');
-
-      this.render('{{foo-bar foo=foo bar=bar}}', {
-        foo: 'foo',
-        bar: 'bar',
-      });
-
-      this.assertInnerHTML('|||');
-
-      this.assertStableRerender();
-
-      runTask(() => this.context.set('foo', 'FOO'));
-
-      this.assertInnerHTML('|||');
-
-      runTask(() => this.context.set('bar', null));
-
-      this.assertInnerHTML('|||');
-
-      runTask(() => this.context.setProperties({ foo: 'foo', bar: 'bar' }));
-
-      this.assertInnerHTML('|||');
-    }
-
-    ['@test it does not have curly component features']() {
-      this.registerTemplateOnlyComponent('foo-bar', 'hello');
-
-      this.render('{{foo-bar tagName="p" class=class}}', {
-        class: 'foo bar',
-      });
-
-      this.assertInnerHTML('hello');
-
-      this.assertStableRerender();
-
-      runTask(() => this.context.set('class', 'foo'));
-
-      this.assertInnerHTML('hello');
-
-      runTask(() => this.context.set('class', null));
-
-      this.assertInnerHTML('hello');
-
-      runTask(() => this.context.set('class', 'foo bar'));
-
-      this.assertInnerHTML('hello');
-    }
-
-    ['@test it has the correct bounds']() {
-      this.registerTemplateOnlyComponent('foo-bar', 'hello');
-
-      this.render('outside {{#if this.isShowing}}before {{foo-bar}} after{{/if}} outside', {
-        isShowing: true,
-      });
-
-      this.assertInnerHTML('outside before hello after outside');
-
-      this.assertStableRerender();
-
-      runTask(() => this.context.set('isShowing', false));
-
-      this.assertInnerHTML('outside <!----> outside');
-
-      runTask(() => this.context.set('isShowing', null));
-
-      this.assertInnerHTML('outside <!----> outside');
-
-      runTask(() => this.context.set('isShowing', true));
-
-      this.assertInnerHTML('outside before hello after outside');
-    }
-
-    ['@test asserts when a shared dependency is changed during rendering, and keeps original context']() {
-      this.registerComponent('x-outer', {
-        ComponentClass: Component.extend({
-          value: 1,
-          wrapper: EmberObject.create({ content: null }),
-        }),
-        template:
-          '<div id="outer-value">{{x-inner-template-only value=this.wrapper.content wrapper=wrapper}}</div>{{x-inner value=value wrapper=wrapper}}',
-      });
-
-      this.registerComponent('x-inner', {
-        ComponentClass: Component.extend({
-          didReceiveAttrs() {
-            this.get('wrapper').set('content', this.get('value'));
-          },
-          value: null,
-        }),
-        template: '<div id="inner-value">{{wrapper.content}}</div>',
-      });
-
-      this.registerTemplateOnlyComponent('x-inner-template-only', '{{@value}}');
-
-      let expectedBacktrackingMessage = backtrackingMessageFor('content', '<.+?>', {
-        renderTree: ['x-outer', 'this.wrapper.content'],
-      });
-
-      expectAssertion(() => {
-        this.render('{{x-outer}}');
-      }, expectedBacktrackingMessage);
-    }
-  }
-);
-
-moduleFor(
-  'Components test: template-only components (curly components)',
-  class extends TemplateOnlyComponentsTest {
-    constructor() {
-      super(...arguments);
-      this._TEMPLATE_ONLY_GLIMMER_COMPONENTS = ENV._TEMPLATE_ONLY_GLIMMER_COMPONENTS;
-      ENV._TEMPLATE_ONLY_GLIMMER_COMPONENTS = false;
-    }
-
-    teardown() {
-      super.teardown();
-      ENV._TEMPLATE_ONLY_GLIMMER_COMPONENTS = this._TEMPLATE_ONLY_GLIMMER_COMPONENTS;
-    }
-
-    ['@test it can render a template-only component']() {
-      this.registerTemplateOnlyComponent('foo-bar', 'hello');
-
-      this.render('{{foo-bar}}');
-
-      this.assertComponentElement(this.firstChild, { content: 'hello' });
-
-      this.assertStableRerender();
-    }
-
-    ['@test it can render named arguments']() {
-      this.registerTemplateOnlyComponent('foo-bar', '|{{@foo}}|{{@bar}}|');
-
-      this.render('{{foo-bar foo=foo bar=bar}}', {
-        foo: 'foo',
-        bar: 'bar',
-      });
-
-      this.assertComponentElement(this.firstChild, { content: '|foo|bar|' });
-
-      this.assertStableRerender();
-
-      runTask(() => this.context.set('foo', 'FOO'));
-
-      this.assertComponentElement(this.firstChild, { content: '|FOO|bar|' });
-
-      runTask(() => this.context.set('bar', 'BAR'));
-
-      this.assertComponentElement(this.firstChild, { content: '|FOO|BAR|' });
-
-      runTask(() => this.context.setProperties({ foo: 'foo', bar: 'bar' }));
-
-      this.assertComponentElement(this.firstChild, { content: '|foo|bar|' });
-    }
-
-    ['@test it renders named arguments as reflected properties']() {
-      this.registerTemplateOnlyComponent('foo-bar', '|{{foo}}|{{this.bar}}|');
-
-      this.render('{{foo-bar foo=foo bar=bar}}', {
-        foo: 'foo',
-        bar: 'bar',
-      });
-
-      this.assertComponentElement(this.firstChild, { content: '|foo|bar|' });
-
-      this.assertStableRerender();
-
-      runTask(() => this.context.set('foo', 'FOO'));
-
-      this.assertComponentElement(this.firstChild, { content: '|FOO|bar|' });
-
-      runTask(() => this.context.set('bar', null));
-
-      this.assertComponentElement(this.firstChild, { content: '|FOO||' });
-
-      runTask(() => this.context.setProperties({ foo: 'foo', bar: 'bar' }));
-
-      this.assertComponentElement(this.firstChild, { content: '|foo|bar|' });
-    }
-
-    ['@test it has curly component features']() {
-      this.registerTemplateOnlyComponent('foo-bar', 'hello');
-
-      this.render('{{foo-bar tagName="p" class=class}}', {
-        class: 'foo bar',
-      });
-
-      this.assertComponentElement(this.firstChild, {
-        tagName: 'p',
-        attrs: { class: classes('foo bar ember-view') },
-        content: 'hello',
-      });
-
-      this.assertStableRerender();
-
-      runTask(() => this.context.set('class', 'foo'));
-
-      this.assertComponentElement(this.firstChild, {
-        tagName: 'p',
-        attrs: { class: classes('foo ember-view') },
-        content: 'hello',
-      });
-
-      runTask(() => this.context.set('class', null));
-
-      this.assertComponentElement(this.firstChild, {
-        tagName: 'p',
-        attrs: { class: classes('ember-view') },
-        content: 'hello',
-      });
-
-      runTask(() => this.context.set('class', 'foo bar'));
-
-      this.assertComponentElement(this.firstChild, {
-        tagName: 'p',
-        attrs: { class: classes('foo bar ember-view') },
-        content: 'hello',
-      });
-    }
-  }
-);
+  );
+}
 
 if (EMBER_GLIMMER_SET_COMPONENT_TEMPLATE) {
   moduleFor(
