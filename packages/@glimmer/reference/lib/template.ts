c4215f44400bec335da143f1526151fe57d23149
Chris Garrett <cgarrett@linkedin.com>
2020-05-15 12:39:47 -0700

    add todo

--- packages/@glimmer/reference/lib/template.ts
+++ packages/@glimmer/reference/lib/template.ts
@@ -309,6 +309,7 @@ export class IterationItemReference<T = unknown> implements TemplatePathReferenc
   update(value: T) {
     let { itemValue } = this;
 
+    // TODO: refactor this https://github.com/glimmerjs/glimmer-vm/issues/1101
     if (value !== itemValue) {
       dirtyTag(this.tag);
       this.itemValue = value;
