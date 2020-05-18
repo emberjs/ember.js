76b22ac8225db578869784f5ace36a245577f7b7
Chris Garrett <cgarrett@linkedin.com>
2020-05-18 16:19:59 -0700

    [BUGFIX] Adds an assertion to prevent duplicate validator packages
    Adds an assertion that will throw if duplicate validator packages are detected.
    This is because validator depends on a few global values for things to work properly
    at the moment. We may be able to relax this restriction in the future, mainly adding
    it now to prevent confusion when things aren't working properly.

--- packages/@glimmer/validator/index.ts
+++ packages/@glimmer/validator/index.ts
@@ -1,3 +1,17 @@
+import { symbolFor, getGlobal } from './lib/utils';
+
+const globalObj = getGlobal();
+
+const GLIMMER_VALIDATOR_REGISTRATION = symbolFor('GLIMMER_VALIDATOR_REGISTRATION');
+
+if (globalObj[GLIMMER_VALIDATOR_REGISTRATION] === true) {
+  throw new Error(
+    'The `@glimmer/validator` library has been included twice in this application. It could be different versions of the package, or the same version included twice by mistake. `@glimmer/validator` depends on having a single copy of the package in use at any time in an application, even if they are the same version. You must dedupe your build to remove the duplicate packages in order to prevent this error.'
+  );
+}
+
+globalObj[GLIMMER_VALIDATOR_REGISTRATION] = true;
+
 export {
   ALLOW_CYCLES,
   bump,
