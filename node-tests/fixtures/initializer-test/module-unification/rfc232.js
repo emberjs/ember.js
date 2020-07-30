fa35230d082e4b9667f04b214e77f314385d18e7
Mehul Kar <mehulkar@apple.com>
2020-07-30 12:06:37 -0700

    Pass resolver to Test app in generated initializer test
    The default resolver (the globals resolver is deprecated), so we
    always need to pass this in.
    
    Closes #19046

--- node-tests/fixtures/initializer-test/module-unification/rfc232.js
+++ node-tests/fixtures/initializer-test/module-unification/rfc232.js
@@ -2,17 +2,18 @@ import Application from '@ember/application';
 
 import { initialize } from 'my-app/init/initializers/foo';
 import { module, test } from 'qunit';
+import Resolver from 'ember-resolver';
 import { run } from '@ember/runloop';
 
 module('Unit | Initializer | foo', function(hooks) {
   hooks.beforeEach(function() {
-    this.TestApplication = Application.extend();
+    this.TestApplication = class TestApplication extends Application {}
     this.TestApplication.initializer({
       name: 'initializer under test',
       initialize
     });
 
-    this.application = this.TestApplication.create({ autoboot: false });
+    this.application = this.TestApplication.create({ autoboot: false, Resolver });
   });
 
   hooks.afterEach(function() {
