ed8977a2c6f14c39b32a56e520a163a9c6b895a6
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-08-14 10:35:35 -0400

    Resolves #19004: RouterService#isActive() now consumes currentURL on the private router

--- packages/@ember/-internals/routing/lib/services/router.ts
+++ packages/@ember/-internals/routing/lib/services/router.ts
@@ -3,6 +3,7 @@ import { assert } from '@ember/debug';
 import { readOnly } from '@ember/object/computed';
 import Service from '@ember/service';
 import { DEBUG } from '@glimmer/env';
+import { consumeTag, tagFor } from '@glimmer/validator';
 import { Transition } from 'router_js';
 import EmberRouter, { QueryParam } from '../system/router';
 import { extractRouteArgs, resemblesURL, shallowEqual } from '../utils';
@@ -307,6 +308,18 @@ export default class RouterService extends Service {
   isActive(...args: any[]) {
     let { routeName, models, queryParams } = extractRouteArgs(args);
     let routerMicrolib = this._router._routerMicrolib;
+
+    // When using isActive() in a getter, we want to entagle with the auto-tracking system
+    // for example,
+    // in
+    // get isBarActive() {
+    //   return isActive('foo.bar');
+    // }
+    //
+    // you'd expect isBarActive to be dirtied when the route changes.
+    //
+    // https://github.com/emberjs/ember.js/issues/19004
+    consumeTag(tagFor(this._router, 'currentURL'));
 
     // UNSAFE: casting `routeName as string` here encodes the existing
     // assumption but may be wrong: `extractRouteArgs` correctly returns it as
