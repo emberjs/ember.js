69a88308a64995452415c2f68ad5da4102d077f2
Chris Garrett <cgarrett@linkedin.com>
2020-09-28 12:58:27 -0700

    [BUGFIX lts] Entangles custom EmberArray implementations when accessed
    Restores the previous logic that existed for entangling custom
    EmberArray implementations when they are accessed, along with a test
    for the functionality.
    
    (cherry picked from commit ef26b1fa43edcce2bd04a78ef087ff5615683af2)

--- packages/@ember/-internals/utils/tests/trackable-object-test.js
+++ packages/@ember/-internals/utils/tests/trackable-object-test.js
@@ -1,12 +1,15 @@
-import { EMBER_ARRAY, isEmberArray } from '..';
+import { setEmberArray, isEmberArray } from '..';
 import { moduleFor, AbstractTestCase } from 'internal-test-helpers';
 
 moduleFor(
   '@ember/-internals/utils Trackable Object',
   class extends AbstractTestCase {
     ['@test classes'](assert) {
-      class Test {}
-      Test.prototype[EMBER_ARRAY] = true;
+      class Test {
+        constructor() {
+          setEmberArray(this);
+        }
+      }
 
       let instance = new Test();
 
