5dccd3181b7204055d79ed2b6577a7cce601edd2
Chris Garrett <cgarrett@linkedin.com>
2020-11-02 11:14:47 -0800

    [UPSTREAM] DebugRenderTree
    Upstreams the DebugRenderTree from Ember, creating it directly on the
    Envirnoment. Also removes the `extra` property from the environment,
    instead allowing `Owner` to be accessed (this is the last remaining
    value that is being passed around by the environment).

--- packages/@glimmer/interfaces/lib/runtime/environment.d.ts
+++ packages/@glimmer/interfaces/lib/runtime/environment.d.ts
@@ -3,7 +3,9 @@ import { ComponentInstanceState } from '../components';
 import { ComponentManager } from '../components/component-manager';
 import { Option } from '../core';
 import { GlimmerTreeChanges, GlimmerTreeConstruction } from '../dom/changes';
+import { DebugRenderTree } from './debug-render-tree';
 import { ModifierManager } from './modifier';
+import { Owner } from './owner';
 
 export interface EnvironmentOptions {
   document?: SimpleDocument;
@@ -19,7 +21,7 @@ export interface Transaction {}
 declare const TransactionSymbol: unique symbol;
 export type TransactionSymbol = typeof TransactionSymbol;
 
-export interface Environment<Extra = unknown> {
+export interface Environment<O extends Owner = Owner> {
   [TransactionSymbol]: Option<Transaction>;
 
   didCreate(component: InternalComponent, manager: InternalComponentManager): void;
@@ -35,5 +37,6 @@ export interface Environment<Extra = unknown> {
   getAppendOperations(): GlimmerTreeConstruction;
 
   isInteractive: boolean;
-  extra: Extra;
+  debugRenderTree: DebugRenderTree;
+  owner: O;
 }
