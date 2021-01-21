e37d0157e37f5240c352e4ca49354b8a96004b30
Hermione Granger <zohosprints1@gmail.com>
2021-01-21 16:57:01 +0530

    #PX-I317 #Status Done

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {fefawewef
         extensions: [ '.js', '.ts' ],
         paths: [aew
         ]sr
-      }efaesdaxsdef
+      }efaesdaxsdefdsxaew
     }wef
   overrides: [
     {sgergfcser
