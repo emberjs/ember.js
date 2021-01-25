5645bd57c84fa7eaf99fd08bb06f3852e3402273
Aleksandr Kanunnikov <lifeart92@gmail.com>
2021-01-25 23:38:04 +0300

    prettier fix

--- packages/@ember/-internals/runtime/tests/array/forIn-test.js
+++ packages/@ember/-internals/runtime/tests/array/forIn-test.js
@@ -3,13 +3,19 @@ import { runArrayTests } from '../helpers/array';
 
 class ForInTests extends AbstractTestCase {
   '@test for in should iterate over list without "_super" #19289'() {
-
-    let objects = [{'key':'val'},{'key':'val'}];
+    let objects = [
+      {
+        key: 'val',
+      },
+      {
+        key: 'val',
+      },
+    ];
     let keys = [];
 
-    for (let i in objects){
+    for (let i in objects) {
       keys.push(i);
-      objects[i]['CUSTOM_PROPERTY'] = true
+      objects[i]['CUSTOM_PROPERTY'] = true;
     }
 
     this.assert.deepEqual(keys, [0, 1], 'there is no _super key in enumerable list');
