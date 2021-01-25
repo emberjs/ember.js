9907572b05baa0cae5910fdefd3bfccf1463199e
Aleksandr Kanunnikov <lifeart92@gmail.com>
2021-01-25 23:32:45 +0300

    attempt to ad unit test

---
+++ packages/@ember/-internals/runtime/tests/array/forIn-test.js
@@ -0,0 +1,19 @@
+import { AbstractTestCase } from 'internal-test-helpers';
+import { runArrayTests } from '../helpers/array';
+
+class ForInTests extends AbstractTestCase {
+  '@test for in should iterate over list without "_super" #19289'() {
+
+    let objects = [{'key':'val'},{'key':'val'}];
+    let keys = [];
+
+    for (let i in objects){
+      keys.push(i);
+      objects[i]['CUSTOM_PROPERTY'] = true
+    }
+
+    this.assert.deepEqual(keys, [0, 1], 'there is no _super key in enumerable list');
+  }
+}
+
+runArrayTests('forIn', ForInTests);
