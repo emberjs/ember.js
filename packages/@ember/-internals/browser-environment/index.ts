07357ed691575a70be9b0d23fc3891c65d156efa
Chris Garrett <cgarrett@linkedin.com>
2021-01-26 15:45:26 -0800

    [DEPRECATION] Deprecates old browser support policy
    * Adds the deprecation specified in
      https://github.com/emberjs/rfcs/blob/master/text/0685-new-browser-support-policy.md
    * Updates the test harness to avoid failing CI for deprecations issued
      on IE11

--- packages/@ember/-internals/browser-environment/index.ts
+++ packages/@ember/-internals/browser-environment/index.ts
@@ -3,6 +3,8 @@ import hasDom from './lib/has-dom';
 declare const InstallTrigger: unknown;
 declare const chrome: unknown;
 declare const opera: unknown;
+declare const MSInputMethodContext: unknown;
+declare const documentMode: unknown;
 
 export { default as hasDOM } from './lib/has-dom';
 export const window = hasDom ? self : null;
@@ -11,3 +13,6 @@ export const history = hasDom ? self.history : null;
 export const userAgent = hasDom ? self.navigator.userAgent : 'Lynx (textmode)';
 export const isChrome = hasDom ? typeof chrome === 'object' && !(typeof opera === 'object') : false;
 export const isFirefox = hasDom ? typeof InstallTrigger !== 'undefined' : false;
+export const isIE = hasDom
+  ? typeof MSInputMethodContext !== 'undefined' && typeof documentMode !== 'undefined'
+  : false;
