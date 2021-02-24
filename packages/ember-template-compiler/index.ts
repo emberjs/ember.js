bcea56f2efae51c2dfe9a85e9037f4a3552ce07c
Robert Jackson <rjackson@linkedin.com>
2021-02-24 18:07:39 -0500

    [BUGFIX release] Expose `@glimmer/syntax` from template compiler privately for use in Embroider.
    This enables Embroider to remove some gnarly hacks (or at least only
    resort to them when operating on older Ember).
    
    (cherry picked from commit b46eee7764d7007bbc55c18a5f3053266d88a335)

--- packages/ember-template-compiler/index.ts
+++ packages/ember-template-compiler/index.ts
@@ -1,5 +1,6 @@
 import { context, ENV } from '@ember/-internals/environment';
 import { FEATURES } from '@ember/canary-features';
+import * as _GlimmerSyntax from '@glimmer/syntax';
 import VERSION from 'ember/version';
 
 export const _Ember =
@@ -24,11 +25,18 @@ export { default as precompile } from './lib/system/precompile';
 export { default as compile } from './lib/system/compile';
 export {
   default as compileOptions,
+  buildCompileOptions as _buildCompileOptions,
+  transformsFor as _transformsFor,
   registerPlugin,
   unregisterPlugin,
 } from './lib/system/compile-options';
 export { RESOLUTION_MODE_TRANSFORMS, STRICT_MODE_TRANSFORMS } from './lib/plugins/index';
 export { EmberPrecompileOptions } from './lib/types';
+
+export { preprocess as _preprocess, print as _print } from '@glimmer/syntax';
+export { precompile as _precompile } from '@glimmer/compiler';
+
+export { _GlimmerSyntax, VERSION };
 
 // used to bootstrap templates
 import './lib/system/bootstrap';
