9edaec34327bd0e5ec0f5bdf5300c4c15d2298be
Bert De Block <bert.deblock@bagaar.be>
2021-03-16 17:43:43 +0100

    Rename helper arguments in blueprints

--- node-tests/fixtures/helper/mu-helper.js
+++ node-tests/fixtures/helper/mu-helper.js
@@ -1,7 +1,7 @@
 import { helper as buildHelper } from '@ember/component/helper';
 
-export function fooBarBaz(params/*, hash*/) {
-  return params;
+export function fooBarBaz(positional/*, named*/) {
+  return positional;
 }
 
 export const helper = buildHelper(fooBarBaz);
