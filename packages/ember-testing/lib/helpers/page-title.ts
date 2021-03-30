26b0558ec4c40548def42b6a275d8b8ff10bbfe9
Angel <angelconcepcion50@gmail.com>
2021-03-30 17:28:35 -0400

    [DOC beta] Added page-title.ts to helpers and  expected docs

--- packages/ember-testing/lib/helpers/page-title.ts
+++ packages/ember-testing/lib/helpers/page-title.ts
@@ -3,7 +3,7 @@
 */
 
 /**
-  `page-title` is an addon helper that allows you to change and set the titles of your application's pages. 
+  `page-title` is an addon helper that allows you to change and set the titles of your application's pages.
 
   To learn more about the `page-title` helper, read the documentation https://github.com/ember-cli/ember-page-title.
 
