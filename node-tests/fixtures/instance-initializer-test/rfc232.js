c69365b0574a9a16cad18d1ef27669fd11680a4a
Jonas Metzener <jonas.metzener@adfinis.com>
2021-05-12 09:37:15 +0200

    [BUGFIX] pass module prefix to initializer test blueprints

--- node-tests/fixtures/instance-initializer-test/rfc232.js
+++ node-tests/fixtures/instance-initializer-test/rfc232.js
@@ -1,27 +1,32 @@
 import Application from '@ember/application';
 
+import config from 'my-app/config/environment';
 import { initialize } from 'my-app/instance-initializers/foo';
 import { module, test } from 'qunit';
 import Resolver from 'ember-resolver';
 import { run } from '@ember/runloop';
 
-module('Unit | Instance Initializer | foo', function(hooks) {
-  hooks.beforeEach(function() {
-    this.TestApplication = class TestApplication extends Application {}
+module('Unit | Instance Initializer | foo', function (hooks) {
+  hooks.beforeEach(function () {
+    this.TestApplication = class TestApplication extends Application {
+      modulePrefix = config.modulePrefix;
+      podModulePrefix = config.podModulePrefix;
+      Resolver = Resolver;
+    };
     this.TestApplication.instanceInitializer({
       name: 'initializer under test',
-      initialize
+      initialize,
     });
-    this.application = this.TestApplication.create({ autoboot: false, Resolver });
+    this.application = this.TestApplication.create({ autoboot: false });
     this.instance = this.application.buildInstance();
   });
-  hooks.afterEach(function() {
+  hooks.afterEach(function () {
     run(this.instance, 'destroy');
     run(this.application, 'destroy');
   });
 
   // TODO: Replace this with your real tests.
-  test('it works', async function(assert) {
+  test('it works', async function (assert) {
     await this.instance.boot();
 
     assert.ok(true);
