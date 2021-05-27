825d7b92e1587afb09df43be3a1509d2b50c4c49
Peter Wagenet <peter.wagenet@gmail.com>
2021-05-27 11:32:49 -0700

    Prepare for public @ember/object defineProperty method type

--- packages/@ember/object/index.d.ts
+++ packages/@ember/object/index.d.ts
@@ -4,4 +4,14 @@ export let action: MethodDecorator;
 // However, it is more permissive than we want.
 export function computed(...dependentKeys: string[]): MethodDecorator;
 
+// NOTE: The internal version of defineProperty is TypeScript.
+// However, it is more permissive than we want.
+export function defineProperty(
+  obj: object,
+  keyName: string,
+  desc?: PropertyDescriptor,
+  data?: any,
+  meta?: any
+): void;
+
 export { default } from '@ember/-internals/runtime/types/index';
