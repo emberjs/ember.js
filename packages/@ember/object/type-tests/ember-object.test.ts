76789a81888695ff03efa0c335d7abc7469194b8
Peter Wagenet <peter.wagenet@gmail.com>
2021-06-04 09:47:24 -0700

    Fix lint error

--- packages/@ember/object/type-tests/ember-object.test.ts
+++ packages/@ember/object/type-tests/ember-object.test.ts
@@ -99,7 +99,6 @@ Person.reopen({ fullName: 6 });
 // NOTE: This is marked as @internal and will not be publicly available
 Person.reopenClass({ fullName: 6 });
 
-
 class MyComponent extends EmberObject {
   foo = 'bar';
 
