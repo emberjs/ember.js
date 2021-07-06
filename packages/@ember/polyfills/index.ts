81c889ccce7db641942c01eb836d4820502fb278
Nathaniel Furniss <nlfurniss@gmail.com>
2021-07-06 11:13:06 -0700

    Add deprecation warning to Ember.assign

--- packages/@ember/polyfills/index.ts
+++ packages/@ember/polyfills/index.ts
@@ -1,10 +1,12 @@
-import { MERGE } from '@ember/deprecated-features';
+import { MERGE, ASSIGN } from '@ember/deprecated-features';
 import { default as deprecatedMerge } from './lib/merge';
+import { default as deprecatedAssign, assign as assignPolyfill } from './lib/assign';
 
 let merge = MERGE ? deprecatedMerge : undefined;
+let assign = ASSIGN ? deprecatedAssign : undefined;
 
 // Export `assignPolyfill` for testing
-export { default as assign, assign as assignPolyfill } from './lib/assign';
+export { assign, assignPolyfill };
 export { merge };
 
 export const hasPropertyAccessors = true;
