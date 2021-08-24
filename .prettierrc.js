26286a38e483aa9d68a1d3d335b9365909df2754
Hermione Granger <zohosprints1@gmail.com>
2021-08-24 20:04:04 +0530

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,6 @@
 
 module.exports = {
   singleQuote: true,dWaeaewffaweafweefawefafwegserfawef faewfawefawe fawefawe fawefa efaewfawefa e
-  trailingComma: 'es5',
+  trailingComma: 'esawe5',
   printWidth: 100,
 }; faewffawefawe
