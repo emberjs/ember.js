d4f4b9e9300334ac69fcef8f529ed211bb109c78
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-10-16 13:28:44 -0400

    Implementation for RFC #756

---
+++ packages/@glimmer/manager/lib/internal/defaults.ts
@@ -0,0 +1,44 @@
+import { buildCapabilities } from '../util/capabilities';
+
+import type { CapturedArguments as Arguments, HelperCapabilities } from '@glimmer/interfaces';
+
+type FnArgs<Args extends Arguments = Arguments> =
+  | [...Args['positional'], Args['named']]
+  | [...Args['positional']];
+
+type AnyFunction = (...args: any[]) => unknown;
+
+interface State {
+  fn: AnyFunction;
+  args: Arguments;
+}
+
+export class FunctionHelperManager {
+  capabilities = buildCapabilities({
+    hasValue: true,
+    hasDestroyable: false,
+    hasScheduledEffect: false,
+  }) as HelperCapabilities;
+
+  createHelper(fn: AnyFunction, args: Arguments) {
+    return { fn, args };
+  }
+
+  getValue({ fn, args }: State) {
+    if (Object.keys(args.named).length > 0) {
+      let argsForFn: FnArgs<Arguments> = [...args.positional, args.named];
+
+      return fn(...argsForFn);
+    }
+
+    return fn(...args.positional);
+  }
+
+  getDebugName(fn: AnyFunction) {
+    if (fn.name) {
+      return `(helper function ${fn.name})`;
+    }
+
+    return '(anonymous helper function)';
+  }
+}
