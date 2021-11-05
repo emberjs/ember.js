emberjs/ember.js

commit 179a388d69af6fcd1bc906de396bde55e4925933

[cleanup]: this. property fallback this-property-fallback

packages/@glimmer/opcode-compiler/lib/syntax/expressions.ts
@@ -12,7 +12,6 @@ import { isGetFreeHelper } from '../opcode-builder/helpers/resolution';
 import { SimpleArgs } from '../opcode-builder/helpers/shared';
 import { Call, CallDynamic, Curry, PushPrimitiveReference } from '../opcode-builder/helpers/vm';
 import { Compilers, PushExpressionOp } from './compilers';
-import { DEBUG } from '@glimmer/env';
 
 export const EXPRESSIONS = new Compilers<PushExpressionOp, ExpressionSexpOpcode>();
 
@@ -57,23 +56,7 @@ EXPRESSIONS.add(SexpOpcodes.GetStrictFree, (op, [, sym, _path]) => {
   });
 });
 
-EXPRESSIONS.add(SexpOpcodes.GetFreeAsFallback, (op, [, freeVar, path]) => {
-  op(HighLevelResolutionOpcode.ResolveLocal, freeVar, (name: string, moduleName: string) => {
-    if (DEBUG) {
-      let propertyPath = path ? [name, ...path].join('.') : name;
-
-      deprecate(
-        `The \`${propertyPath}\` property path was used in the \`${moduleName}\` template without using \`this\`. This fallback behavior has been deprecated, all properties must be looked up on \`this\` when used in the template: {{this.${propertyPath}}}`,
-        false,
-        {
-          id: 'this-property-fallback',
-        }
-      );
-    }
-
-    op(Op.GetVariable, 0);
-    op(Op.GetProperty, name);
-  });
+EXPRESSIONS.add(SexpOpcodes.GetFreeAsFallback, (op, [, , path]) => {
   withPath(op, path);
 });
 
@@ -92,19 +75,6 @@ EXPRESSIONS.add(SexpOpcodes.GetFreeAsHelperHeadOrThisFallback, (op, expr) => {
     op(HighLevelResolutionOpcode.ResolveOptionalHelper, expr, {
       ifHelper: (handle: number) => {
         Call(op, handle, null, null);
-      },
-
-      ifFallback: (name: string, moduleName: string) => {
-        deprecate(
-          `The \`${name}\` property was used in the \`${moduleName}\` template without using \`this\`. This fallback behavior has been deprecated, all properties must be looked up on \`this\` when used in the template: {{this.${name}}}`,
-          false,
-          {
-            id: 'this-property-fallback',
-          }
-        );
-
-        op(Op.GetVariable, 0);
-        op(Op.GetProperty, name);
       },
     });
   });
@@ -139,19 +109,6 @@ EXPRESSIONS.add(SexpOpcodes.GetFreeAsDeprecatedHelperHeadOrThisFallback, (op, ex
         );
 
         Call(op, handle, null, null);
-      },
-
-      ifFallback: (name: string, moduleName: string) => {
-        deprecate(
-          `The \`${name}\` property was used in the \`${moduleName}\` template without using \`this\`. This fallback behavior has been deprecated, all properties must be looked up on \`this\` when used in the template: {{this.${name}}}`,
-          false,
-          {
-            id: 'this-property-fallback',
-          }
-        );
-
-        op(Op.GetVariable, 0);
-        op(Op.GetProperty, name);
       },
     });
   });

packages/@glimmer/opcode-compiler/lib/opcode-builder/helpers/resolution.ts
@@ -309,7 +309,7 @@ export function resolveOptionalHelper(
   resolver: CompileTimeResolver,
   constants: CompileTimeConstants & ResolutionTimeConstants,
   meta: ContainingMetadata,
-  [, expr, { ifHelper, ifFallback }]: ResolveOptionalHelperOp
+  [, expr, { ifHelper }]: ResolveOptionalHelperOp
 ): void {
   assert(
     isGetFreeOptionalHelper(expr) || isGetFreeDeprecatedHelper(expr),
@@ -320,9 +320,7 @@ export function resolveOptionalHelper(
   let name = upvars[expr[1]];
   let helper = resolver.lookupHelper(name, owner);
 
-  if (helper === null) {
-    ifFallback(name, meta.moduleName);
-  } else {
+  if (helper) {
     ifHelper(constants.helper(helper, name), name, meta.moduleName);
   }
 }
@@ -334,7 +332,7 @@ export function resolveOptionalComponentOrHelper(
   resolver: CompileTimeResolver,
   constants: CompileTimeConstants & ResolutionTimeConstants,
   meta: ContainingMetadata,
-  [, expr, { ifComponent, ifHelper, ifValue, ifFallback }]: ResolveOptionalComponentOrHelperOp
+  [, expr, { ifComponent, ifHelper, ifValue }]: ResolveOptionalComponentOrHelperOp
 ): void {
   assert(
     isGetFreeOptionalComponentOrHelper(expr),
@@ -396,10 +394,7 @@ export function resolveOptionalComponentOrHelper(
 
     if (helper !== null) {
       ifHelper(constants.helper(helper, name));
-      return;
     }
-
-    ifFallback(name);
   }
 }
 

packages/@glimmer/interfaces/lib/compile/encoder.d.ts
@@ -88,7 +88,6 @@ export type ResolveOptionalHelperOp = [
   op1: WireFormat.Expressions.Expression,
   op2: {
     ifHelper: (handle: number, name: string, moduleName: string) => void;
-    ifFallback: (name: string, moduleName: string) => void;
   }
 ];
 
@@ -99,7 +98,6 @@ export type ResolveOptionalComponentOrHelperOp = [
     ifComponent: (component: CompileTimeComponent) => void;
     ifHelper: (handle: number) => void;
     ifValue: (handle: number) => void;
-    ifFallback: (name: string) => void;
   }
 ];
 

packages/@glimmer/opcode-compiler/lib/syntax/statements.ts
@@ -1,4 +1,3 @@
-import { deprecate } from '@glimmer/global-context';
 import {
   CompileTimeComponent,
   ContentType,
@@ -159,24 +158,6 @@ STATEMENTS.add(SexpOpcodes.Append, (op, [, value]) => {
         op(MachineOp.PushFrame);
         op(Op.ConstantReference, handle);
         op(MachineOp.InvokeStatic, stdlibOperand('cautious-non-dynamic-append'));
-        op(MachineOp.PopFrame);
-      },
-
-      ifFallback(_name: string) {
-        op(MachineOp.PushFrame);
-        op(HighLevelResolutionOpcode.ResolveLocal, value[1], (name: string, moduleName: string) => {
-          deprecate(
-            `The \`${name}\` property was used in the \`${moduleName}\` template without using \`this\`. This fallback behavior has been deprecated, all properties must be looked up on \`this\` when used in the template: {{this.${name}}}`,
-            false,
-            {
-              id: 'this-property-fallback',
-            }
-          );
-
-          op(Op.GetVariable, 0);
-          op(Op.GetProperty, name);
-        });
-        op(MachineOp.InvokeStatic, stdlibOperand('cautious-append'));
         op(MachineOp.PopFrame);
       },
     });

commit cfc4b0cf7e29bb2ded6f3ac9bfbd5c027721355f

drop GetFreeAsFallback
GetFreeAsFallback got broken by 179a388d69af6fcd1bc906de396bde55e4925933

As far as I can tell, it's never correct to call `withPath` by itself, it's always supposed to come after an opcode that leaves a value on the stack that needs path support.

Rather than fix it, I'm going to try to cut the rest of it out, since it exists only to support `this` fallback.

packages/@glimmer/compiler/lib/builder/builder.ts
@@ -327,7 +327,7 @@ function buildKeyword(
       return [Op.If, expect(params, 'if requires params')[0], block, inverse];
     case 'each':
       let keyExpr = normalized.hash ? normalized.hash['key'] : null;
-      let key = keyExpr ? buildExpression(keyExpr, 'Generic', symbols) : null;
+      let key = keyExpr ? buildExpression(keyExpr, 'Strict', symbols) : null;
 
       return [Op.Each, expect(params, 'if requires params')[0], key, block, inverse];
     default:
@@ -489,7 +489,6 @@ type ExprResolution =
   | 'TrustedAppend'
   | 'AttrValue'
   | 'SubExpression'
-  | 'Generic'
   | 'Strict';
 
 function varContext(context: ExprResolution, bare: boolean): VarResolution {
@@ -528,7 +527,7 @@ export function buildExpression(
       let builtHash = buildHash(expr.hash, symbols);
       let builtExpr = buildCallHead(
         expr.head,
-        context === 'Generic' ? 'SubExpression' : varContext(context, false),
+        context === 'Strict' ? 'SubExpression' : varContext(context, false),
         symbols
       );
 
@@ -540,7 +539,7 @@ export function buildExpression(
         Op.HasBlock,
         buildVar(
           { kind: VariableKind.Block, name: expr.name, mode: 'loose' },
-          VariableResolutionContext.LooseFreeVariable,
+          VariableResolutionContext.Strict,
           symbols
         ),
       ];
@@ -551,7 +550,7 @@ export function buildExpression(
         Op.HasBlockParams,
         buildVar(
           { kind: VariableKind.Block, name: expr.name, mode: 'loose' },
-          VariableResolutionContext.LooseFreeVariable,
+          VariableResolutionContext.Strict,
           symbols
         ),
       ];
@@ -583,12 +582,7 @@ export function buildCallHead(
 }
 
 export function buildGetPath(head: NormalizedPath, symbols: Symbols): Expressions.GetPath {
-  return buildVar(
-    head.path.head,
-    VariableResolutionContext.LooseFreeVariable,
-    symbols,
-    head.path.tail
-  );
+  return buildVar(head.path.head, VariableResolutionContext.Strict, symbols, head.path.tail);
 }
 
 type VarResolution =
@@ -600,7 +594,6 @@ type VarResolution =
   | 'AttrValueBare'
   | 'AttrValueInvoke'
   | 'SubExpression'
-  | 'Generic'
   | 'Strict';
 
 export function buildVar(
@@ -640,8 +633,6 @@ export function buildVar(
         op = SexpOpcodes.GetFreeAsHelperHead;
       } else if (context === 'SubExpression') {
         op = SexpOpcodes.GetFreeAsHelperHead;
-      } else if (context === 'Generic') {
-        op = SexpOpcodes.GetFreeAsFallback;
       } else {
         op = expressionContextOp(context);
       }
@@ -688,8 +679,6 @@ export function expressionContextOp(context: VariableResolutionContext): GetCont
       return Op.GetFreeAsComponentOrHelperHead;
     case VariableResolutionContext.AmbiguousInvoke:
       return Op.GetFreeAsHelperHeadOrThisFallback;
-    case VariableResolutionContext.LooseFreeVariable:
-      return Op.GetFreeAsFallback;
     case VariableResolutionContext.ResolveAsCallHead:
       return Op.GetFreeAsHelperHead;
     case VariableResolutionContext.ResolveAsModifierHead:
@@ -707,7 +696,7 @@ export function buildParams(
 ): Option<WireFormat.Core.Params> {
   if (exprs === null || !isPresent(exprs)) return null;
 
-  return exprs.map((e) => buildExpression(e, 'Generic', symbols)) as WireFormat.Core.ConcatParams;
+  return exprs.map((e) => buildExpression(e, 'Strict', symbols)) as WireFormat.Core.ConcatParams;
 }
 
 export function buildConcat(
@@ -724,7 +713,7 @@ export function buildHash(exprs: Option<NormalizedHash>, symbols: Symbols): Wire
 
   Object.keys(exprs).forEach((key) => {
     out[0].push(key);
-    out[1].push(buildExpression(exprs[key], 'Generic', symbols));
+    out[1].push(buildExpression(exprs[key], 'Strict', symbols));
   });
 
   return out as WireFormat.Core.Hash;

packages/@glimmer/compiler/lib/wire-format-debug.ts
@@ -171,9 +171,6 @@ export default class WireFormatDebugger {
         case Op.GetStrictFree:
           return ['get-strict-free', this.upvars[opcode[1]], opcode[2]];
 
-        case Op.GetFreeAsFallback:
-          return ['GetFreeAsFallback', this.upvars[opcode[1]], opcode[2]];
-
         case Op.GetFreeAsComponentOrHelperHeadOrThisFallback:
           return [
             'GetFreeAsComponentOrHelperHeadOrThisFallback',

packages/@glimmer/interfaces/lib/compile/wire-format.d.ts
@@ -20,7 +20,6 @@ export const enum VariableResolutionContext {
   AmbiguousAppend = 1,
   AmbiguousAppendInvoke = 2,
   AmbiguousInvoke = 3,
-  LooseFreeVariable = 4,
   ResolveAsCallHead = 5,
   ResolveAsModifierHead = 6,
   ResolveAsComponentHead = 7,
@@ -69,9 +68,6 @@ export const enum SexpOpcodes {
   // Free variables are only keywords in strict mode
   GetStrictFree = 31,
 
-  // falls back to `this.` (or locals in the case of partials), but
-  // never turns into a component or helper invocation
-  GetFreeAsFallback = 33,
   // `{{x}}` in append position (might be a helper or component invocation, otherwise fall back to `this`)
   GetFreeAsComponentOrHelperHeadOrThisFallback = 34,
   // a component or helper (`{{<expr> x}}` in append position)
@@ -117,7 +113,6 @@ export type GetContextualFreeOp =
   | SexpOpcodes.GetFreeAsHelperHead
   | SexpOpcodes.GetFreeAsModifierHead
   | SexpOpcodes.GetFreeAsComponentHead
-  | SexpOpcodes.GetFreeAsFallback
   | SexpOpcodes.GetStrictFree;
 
 export type AttrOp =
@@ -167,7 +162,6 @@ export namespace Expressions {
   export type GetSymbol = [SexpOpcodes.GetSymbol, number];
   export type GetTemplateSymbol = [SexpOpcodes.GetTemplateSymbol, number];
   export type GetStrictFree = [SexpOpcodes.GetStrictFree, number];
-  export type GetFreeAsFallback = [SexpOpcodes.GetFreeAsFallback, number];
   export type GetFreeAsComponentOrHelperHeadOrThisFallback = [
     SexpOpcodes.GetFreeAsComponentOrHelperHeadOrThisFallback,
     number
@@ -186,7 +180,6 @@ export namespace Expressions {
   export type GetFreeAsComponentHead = [SexpOpcodes.GetFreeAsComponentHead, number];
 
   export type GetContextualFree =
-    | GetFreeAsFallback
     | GetFreeAsComponentOrHelperHeadOrThisFallback
     | GetFreeAsComponentOrHelperHead
     | GetFreeAsHelperHeadOrThisFallback
@@ -200,7 +193,6 @@ export namespace Expressions {
   export type GetPathSymbol = [SexpOpcodes.GetSymbol, number, Path];
   export type GetPathTemplateSymbol = [SexpOpcodes.GetTemplateSymbol, number, Path];
   export type GetPathStrictFree = [SexpOpcodes.GetStrictFree, number, Path];
-  export type GetPathFreeAsFallback = [SexpOpcodes.GetFreeAsFallback, number, Path];
   export type GetPathFreeAsComponentOrHelperHeadOrThisFallback = [
     SexpOpcodes.GetFreeAsComponentOrHelperHeadOrThisFallback,
     number,
@@ -226,7 +218,6 @@ export namespace Expressions {
   export type GetPathFreeAsComponentHead = [SexpOpcodes.GetFreeAsComponentHead, number, Path];
 
   export type GetPathContextualFree =
-    | GetPathFreeAsFallback
     | GetPathFreeAsComponentOrHelperHeadOrThisFallback
     | GetPathFreeAsComponentOrHelperHead
     | GetPathFreeAsHelperHeadOrThisFallback

packages/@glimmer/opcode-compiler/lib/syntax/expressions.ts
@@ -56,10 +56,6 @@ EXPRESSIONS.add(SexpOpcodes.GetStrictFree, (op, [, sym, _path]) => {
   });
 });
 
-EXPRESSIONS.add(SexpOpcodes.GetFreeAsFallback, (op, [, , path]) => {
-  withPath(op, path);
-});
-
 EXPRESSIONS.add(SexpOpcodes.GetFreeAsComponentOrHelperHeadOrThisFallback, () => {
   // TODO: The logic for this opcode currently exists in STATEMENTS.Append, since
   // we want different wrapping logic depending on if we are invoking a component,

packages/@glimmer/syntax/lib/v2-a/objects/resolution.ts
@@ -145,7 +145,7 @@ export class LooseModeResolution {
 
   resolution(): GetContextualFreeOp {
     if (this.ambiguity.namespaces.length === 0) {
-      return SexpOpcodes.GetFreeAsFallback;
+      return SexpOpcodes.GetStrictFree;
     } else if (this.ambiguity.namespaces.length === 1) {
       if (this.ambiguity.fallback) {
         // simple namespaced resolution with fallback must be attr={{x}}