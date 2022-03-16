2590b2bcd80d259f9d36ea4414bc001ef61d7ba3
Chris Krycho <hello@chriskrycho.com>
2022-03-16 13:20:44 -0600

    [BUGFIX beta] remove unneeded `setFactoryFor(this, this)`
    Identified as part of the work on #20025, this particular `setFactoryFor`
    was needed at one time but is now defunct. Remove it.

--- packages/@ember/-internals/container/lib/container.ts
+++ packages/@ember/-internals/container/lib/container.ts
@@ -558,7 +558,6 @@ class FactoryManager<T, C> {
     this.normalizedName = normalizedName;
     this.madeToString = undefined;
     this.injections = undefined;
-    setFactoryFor(this, this);
   }
 
   toString(): string {
