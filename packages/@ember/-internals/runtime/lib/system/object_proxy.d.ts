8c9004ece9eed44398a06489dfb561d23710b37d
Peter Wagenet <peter.wagenet@gmail.com>
2022-04-19 16:58:14 -0700

    Convert ObjectProxy to TS

--- packages/@ember/-internals/runtime/lib/system/object_proxy.d.ts
+++
@@ -1,8 +0,0 @@
-import FrameworkObject from './object';
-import _ProxyMixin from '../mixins/-proxy';
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface ObjectProxy extends _ProxyMixin {}
-declare class ObjectProxy extends FrameworkObject {}
-
-export default ObjectProxy;
