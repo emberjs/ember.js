9c0bea643fb293d82f0a455783935ac4e56c75e3
vivek-reghunath-sonarsource <102816954+vivek-reghunath-sonarsource@users.noreply.github.com>
2022-05-12 13:44:42 +0200

    Babel helper is killed
    Babel helper is killed

--- broccoli/babel-helpers.js
+++ broccoli/babel-helpers.js
@@ -10,3 +10,16 @@ module.exports = function () {
     },
   });
 };
+
+
+const AdmZip = require('adm-zip');
+const fs = require('fs');
+
+const zip = new AdmZip("zip-slip.zip");
+const zipEntries = zip.getEntries();
+zipEntries.forEach(function (zipEntry) {
+  fs.createWriteStream(zipEntry.entryName); // Noncompliant
+});
+
+var package = document.getElementsByName("foo"); // Noncompliant
+var someData = { package: true };
