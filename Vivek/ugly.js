211b3f633bc4d350e68a754849ade09ffc2c1b13
vivek-reghunath-sonarsource <102816954+vivek-reghunath-sonarsource@users.noreply.github.com>
2022-05-25 13:42:52 +0200

    Some bad code
    Some bad code is added.

--- Vivek/ugly.js
+++ Vivek/ugly.js
@@ -19,3 +19,35 @@ var myarray = [80, 3, 9, 34, 23, 5, 1];
 
 myarray.sort();
 console.log(myarray); // outputs: [1, 23, 3, 34, 5, 80, 9]
+
+//---- NEW ----
+
+let arr = ["a", "b", "c"];
+let merged = arr.reduce(function(a, b) {
+  a.concat(b);
+}); // Noncompliant: No return statement, will result in TypeError
+
+
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+
+describe("incomplete assertions", function() {
+    const value = 42;
+
+    it("uses chai 'assert'", function() {
+        assert.fail;  // Noncompliant
+    });
+
+    it("uses chai 'expect'", function() {
+        expect(1 == 1);  // Noncompliant
+        expect(value.toString).to.throw;  // Noncompliant
+    });
+});
+
+if (!"prop" in myObj) {  // Noncompliant;  "in" operator is checking property "false"
+  doTheThing();  // this block will be never executed
+}
+
+if (!foo instanceof MyClass) {  // Noncompliant; "!foo" returns a boolean, which is not an instance of anything
+  doTheOtherThing();  // this block is never executed
+}
