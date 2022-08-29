20c6148c42df5514e8e8a5712e3dbe522a952d8f
Chris Krycho <hello@chriskrycho.com>
2022-08-29 11:47:33 -0600

    Fix preview types/tests for @ember/service

--- types/preview/ember/test/inject.ts
+++ types/preview/ember/test/inject.ts
@@ -1,41 +1,41 @@
 import Ember from 'ember';
 
 class AuthService extends Ember.Service {
-    declare isAuthenticated: boolean;
+  declare isAuthenticated: boolean;
 }
 
 class ApplicationController extends Ember.Controller {
-    model = {};
-    declare string: string;
-    transitionToLogin() {}
+  model = {};
+  declare string: string;
+  transitionToLogin() {}
 }
 
 declare module '@ember/service' {
-    interface Registry {
-        auth: AuthService;
-    }
+  interface Registry {
+    auth: AuthService;
+  }
 }
 
 declare module '@ember/controller' {
-    interface Registry {
-        application: ApplicationController;
-    }
+  interface Registry {
+    application: ApplicationController;
+  }
 }
 
 class LoginRoute extends Ember.Route {
-    auth = Ember.inject.service('auth');
-    application = Ember.inject.controller('application');
+  auth = Ember.inject.service('auth');
+  application = Ember.inject.controller('application');
 
-    didTransition() {
-        if (!this.get('auth').get('isAuthenticated')) {
-            this.get('application').transitionToLogin();
-        }
+  didTransition() {
+    if (!this.get('auth').get('isAuthenticated')) {
+      this.get('application').transitionToLogin();
     }
+  }
 
-    anyOldMethod() {
-        this.get('application').set('string', 'must be a string');
-        this.controllerFor('application'); // $ExpectType Controller
-    }
+  anyOldMethod() {
+    this.get('application').set('string', 'must be a string');
+    this.controllerFor('application'); // $ExpectType Controller
+  }
 }
 
 // New module injection style.
@@ -44,17 +44,19 @@ import Service, { inject as service } from '@ember/service';
 import { assertType } from './lib/assert';
 
 class ComponentInjection extends Ember.Component {
-    applicationController = controller('application');
-    auth = service('auth');
-    router = service('router');
-    misc = service();
+  applicationController = controller('application');
+  auth = service('auth');
+  router = service('router');
+  misc = service();
 
-    testem() {
-        assertType<Ember.Service>(this.get('misc'));
-        const url = this.get('router').urlFor('some-route', 1, 2, 3, { queryParams: { seriously: 'yes' } });
-        assertType<string>(url);
-        if (!this.get('auth').isAuthenticated) {
-            this.get('applicationController').transitionToLogin();
-        }
+  testem() {
+    assertType<Ember.Service>(this.get('misc'));
+    const url = this.get('router').urlFor('some-route', 1, 2, 3, {
+      queryParams: { seriously: 'yes' },
+    });
+    assertType<string>(url);
+    if (!this.get('auth').isAuthenticated) {
+      this.get('applicationController').transitionToLogin();
     }
+  }
 }
