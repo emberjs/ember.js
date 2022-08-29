43181368622fd77fa942f87a5c2911f8bc662a4f
Chris Krycho <hello@chriskrycho.com>
2022-08-29 09:30:09 -0600

    Update preview type tests for EmberObject.create()

--- types/preview/ember/test/create.ts
+++ types/preview/ember/test/create.ts
@@ -1,55 +1,57 @@
 import Ember from 'ember';
-import { assertType } from './lib/assert';
-import { UnwrapComputedPropertyGetter } from "@ember/object/-private/types";
+import { expectTypeOf } from 'expect-type';
 
 /**
  * Zero-argument case
  */
 const o = Ember.Object.create();
 // create returns an object
-assertType<object>(o);
+expectTypeOf(o).toBeObject();
 // object returned by create type-checks as an instance of Ember.Object
-assertType<boolean>(o.isDestroyed); // from instance
-assertType<boolean>(o.isDestroying); // from instance
-assertType<<K extends keyof Ember.Object>(key: K) => UnwrapComputedPropertyGetter<Ember.Object[K]>>(o.get); // from prototype
+expectTypeOf(o.isDestroyed).toBeBoolean();
+expectTypeOf(o.isDestroying).toBeBoolean();
+expectTypeOf(o.get).toMatchTypeOf<<K extends keyof Ember.Object>(key: K) => Ember.Object[K]>();
 
 /**
  * One-argument case
  */
-const o1 = Ember.Object.create({ x: 9, y: 'hello', z: false });
-assertType<number>(o1.x);
-assertType<string>(o1.y);
-o1.y; // $ExpectType string
-o1.z; // $ExpectType boolean
-
-const obj = Ember.Object.create({ a: 1 }, { b: 2 }, { c: 3 });
-assertType<number>(obj.b);
-assertType<number>(obj.a);
-assertType<number>(obj.c);
-
-export class Person extends Ember.Object.extend({
-    fullName: Ember.computed('firstName', 'lastName', function () {
-        return [this.firstName + this.lastName].join(' ');
-    }),
-}) {
-    declare firstName: string;
-    declare lastName: string;
-    declare age: number;
+class O1 extends Ember.Object {
+  declare x: number;
+  declare y: string;
+  declare z: boolean;
 }
-const p = new Person();
 
-assertType<string>(p.firstName);
-assertType<Ember.ComputedProperty<string>>(p.fullName);
-assertType<string>(p.get('fullName'));
+const o1 = O1.create({ x: 9, y: 'hello', z: false });
+expectTypeOf(o1.x).toBeNumber();
+expectTypeOf(o1.y).toBeString();
+expectTypeOf(o1.z).toBeBoolean();
 
-const p2 = Person.create({ firstName: 'string' });
-const p2b = Person.create({}, { firstName: 'string' });
-const p2c = Person.create({}, {}, { firstName: 'string' });
+class O2 extends Ember.Object {
+  declare a: number;
+  declare b: number;
+  declare c: number;
+}
+const obj = O2.create({ a: 1 }, { b: 2 }, { c: 3 });
+expectTypeOf(obj.b).toBeNumber();
+expectTypeOf(obj.a).toBeNumber();
+expectTypeOf(obj.c).toBeNumber();
 
-export class PersonWithNumberName extends Person.extend({
-    fullName: 6,
-}) {}
+export class Person extends Ember.Object {
+  declare firstName: string;
+  declare lastName: string;
+  declare age: number;
 
-const p4 = new PersonWithNumberName();
-assertType<string>(p4.firstName);
-assertType<number>(p4.fullName);
+  @Ember.computed('firstName', 'lastName')
+  get fullName() {
+    return [this.firstName + this.lastName].join(' ');
+  }
+}
+const p = Person.create();
+
+expectTypeOf(p.firstName).toBeString();
+expectTypeOf(p.fullName).toBeString();
+expectTypeOf(p.get('fullName')).toBeString();
+
+Person.create({ firstName: 'string' });
+Person.create({}, { firstName: 'string' });
+Person.create({}, {}, { firstName: 'string' });
