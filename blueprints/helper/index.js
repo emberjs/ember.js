7bad4388304b61be2f09de2ab0177986bbaf9bf1
Bert De Block <bert.deblock@bagaar.be>
2022-08-30 21:46:54 +0200

    Clean up `helper` and `helper-test` blueprints

--- blueprints/helper/index.js
+++ blueprints/helper/index.js
@@ -4,8 +4,6 @@ const normalizeEntityName = require('ember-cli-normalize-entity-name');
 
 const maybePolyfillTypeScriptBlueprints = require('../-maybe-polyfill-typescript-blueprints');
 
-const path = require('path');
-
 module.exports = {
   description: 'Generates a helper function.',
 
@@ -14,19 +12,6 @@ module.exports = {
   init() {
     this._super && this._super.init.apply(this, arguments);
     maybePolyfillTypeScriptBlueprints(this);
-  },
-
-  filesPath() {
-    let rootPath = 'files';
-    return path.join(this.path, rootPath);
-  },
-
-  fileMapTokens() {
-    return {
-      __collection__() {
-        return 'helpers';
-      },
-    };
   },
 
   normalizeEntityName: function (entityName) {
