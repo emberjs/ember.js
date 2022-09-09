3577c74054456c2fedcd4919f15b45dca87c029a
layang <layang@linkedin.com>
2022-09-09 15:04:29 -0400

    moving docs to interface method

--- packages/@ember/object/observable.ts
+++ packages/@ember/object/observable.ts
@@ -91,29 +91,113 @@ export type ObserverMethod<Target, Sender> =
 */
 interface Observable {
   /**
-   * Retrieves the value of a property from the object.
-   */
+    Retrieves the value of a property from the object.
+
+    This method is usually similar to using `object[keyName]` or `object.keyName`,
+    however it supports both computed properties and the unknownProperty
+    handler.
+
+    Because `get` unifies the syntax for accessing all these kinds
+    of properties, it can make many refactorings easier, such as replacing a
+    simple property with a computed property, or vice versa.
+
+    ### Computed Properties
+
+    Computed properties are methods defined with the `property` modifier
+    declared at the end, such as:
+
+    ```javascript
+    import { computed } from '@ember/object';
+
+    fullName: computed('firstName', 'lastName', function() {
+      return this.get('firstName') + ' ' + this.get('lastName');
+    })
+    ```
+
+    When you call `get` on a computed property, the function will be
+    called and the return value will be returned instead of the function
+    itself.
+
+    ### Unknown Properties
+
+    Likewise, if you try to call `get` on a property whose value is
+    `undefined`, the `unknownProperty()` method will be called on the object.
+    If this method returns any value other than `undefined`, it will be returned
+    instead. This allows you to implement "virtual" properties that are
+    not defined upfront.
+  */
   get(key: string): unknown;
 
   /**
-   * To get the values of multiple properties at once, call `getProperties`
-   * with a list of strings or an array:
-   */
+    To get the values of multiple properties at once, call `getProperties`
+    with a list of strings or an array:
+
+    ```javascript
+    record.getProperties('firstName', 'lastName', 'zipCode');
+    // { firstName: 'John', lastName: 'Doe', zipCode: '10011' }
+    ```
+
+    is equivalent to:
+
+    ```javascript
+    record.getProperties(['firstName', 'lastName', 'zipCode']);
+    // { firstName: 'John', lastName: 'Doe', zipCode: '10011' }
+    ```
+  */
   getProperties<L extends string[]>(list: L): { [Key in L[number]]: unknown };
   getProperties<L extends string[]>(...list: L): { [Key in L[number]]: unknown };
 
   // NOT TYPE SAFE!
   /**
-   * Sets the provided key or path to the value.
-   */
+    Sets the provided key or path to the value.
+
+    ```javascript
+    record.set("key", value);
+    ```
+
+    This method is generally very similar to calling `object["key"] = value` or
+    `object.key = value`, except that it provides support for computed
+    properties, the `setUnknownProperty()` method and property observers.
+
+    ### Computed Properties
+
+    If you try to set a value on a key that has a computed property handler
+    defined (see the `get()` method for an example), then `set()` will call
+    that method, passing both the value and key instead of simply changing
+    the value itself. This is useful for those times when you need to
+    implement a property that is composed of one or more member
+    properties.
+
+    ### Unknown Properties
+
+    If you try to set a value on a key that is undefined in the target
+    object, then the `setUnknownProperty()` handler will be called instead. This
+    gives you an opportunity to implement complex "virtual" properties that
+    are not predefined on the object. If `setUnknownProperty()` returns
+    undefined, then `set()` will simply set the value on the object.
+
+    ### Property Observers
+
+    In addition to changing the property, `set()` will also register a property
+    change with the object. Unless you have placed this call inside of a
+    `beginPropertyChanges()` and `endPropertyChanges(),` any "local" observers
+    (i.e. observer methods declared on the same object), will be called
+    immediately. Any "remote" observers (i.e. observer methods declared on
+    another object) will be placed in a queue and called at a later time in a
+    coalesced manner.
+  */
   set<T>(key: string, value: T): T;
 
   // NOT TYPE SAFE!
   /**
-   * Sets a list of properties at once. These properties are set inside
-   * a single `beginPropertyChanges` and `endPropertyChanges` batch, so
-   * observers will be buffered.
-   */
+    Sets a list of properties at once. These properties are set inside
+    a single `beginPropertyChanges` and `endPropertyChanges` batch, so
+    observers will be buffered.
+
+    ```javascript
+    record.setProperties({ firstName: 'Charles', lastName: 'Jolley' });
+    ```
+  */
   setProperties<T extends Record<string, any>>(hash: T): T;
 
   /**
@@ -123,8 +207,80 @@ interface Observable {
   notifyPropertyChange(keyName: string): this;
 
   /**
-   * Adds an observer on a property.
-   */
+    Adds an observer on a property.
+
+    This is the core method used to register an observer for a property.
+
+    Once you call this method, any time the key's value is set, your observer
+    will be notified. Note that the observers are triggered any time the
+    value is set, regardless of whether it has actually changed. Your
+    observer should be prepared to handle that.
+
+    There are two common invocation patterns for `.addObserver()`:
+
+    - Passing two arguments:
+      - the name of the property to observe (as a string)
+      - the function to invoke (an actual function)
+    - Passing three arguments:
+      - the name of the property to observe (as a string)
+      - the target object (will be used to look up and invoke a
+        function on)
+      - the name of the function to invoke on the target object
+        (as a string).
+
+    ```app/components/my-component.js
+    import Component from '@ember/component';
+
+    export default Component.extend({
+      init() {
+        this._super(...arguments);
+
+        // the following are equivalent:
+
+        // using three arguments
+        this.addObserver('foo', this, 'fooDidChange');
+
+        // using two arguments
+        this.addObserver('foo', (...args) => {
+          this.fooDidChange(...args);
+        });
+      },
+
+      fooDidChange() {
+        // your custom logic code
+      }
+    });
+    ```
+
+    ### Observer Methods
+
+    Observer methods have the following signature:
+
+    ```app/components/my-component.js
+    import Component from '@ember/component';
+
+    export default Component.extend({
+      init() {
+        this._super(...arguments);
+        this.addObserver('foo', this, 'fooDidChange');
+      },
+
+      fooDidChange(sender, key, value, rev) {
+        // your code
+      }
+    });
+    ```
+
+    The `sender` is the object that changed. The `key` is the property that
+    changes. The `value` property is currently reserved and unused. The `rev`
+    is the last property revision of the object when it changed, which you can
+    use to detect if the key value has really changed or not.
+
+    Usually you will not need the value or revision parameters at
+    the end. In this case, it is common to write observer methods that take
+    only a sender and key value as parameters or, if you aren't interested in
+    any of these values, to write an observer that has no parameters at all.
+  */
   addObserver<Target>(key: keyof this, target: Target, method: ObserverMethod<Target, this>): this;
   addObserver(key: keyof this, method: ObserverMethod<this, this>): this;
 
@@ -142,21 +298,35 @@ interface Observable {
 
   // NOT TYPE SAFE!
   /**
-   * Set the value of a property to the current value plus some amount.
-   */
+    Set the value of a property to the current value plus some amount.
+
+    ```javascript
+    person.incrementProperty('age');
+    team.incrementProperty('score', 2);
+    ```
+  */
   incrementProperty(keyName: keyof this, increment?: number): number;
 
   // NOT TYPE SAFE!
   /**
-   * Set the value of a property to the current value minus some amount.
-   */
+    Set the value of a property to the current value minus some amount.
+
+    ```javascript
+    player.decrementProperty('lives');
+    orc.decrementProperty('health', 5);
+    ```
+  */
   decrementProperty(keyName: keyof this, decrement?: number): number;
 
   // NOT TYPE SAFE!
   /**
-   * Set the value of a boolean property to the opposite of its
-   * current value.
-   */
+    Set the value of a boolean property to the opposite of its
+    current value.
+
+    ```javascript
+    starship.toggleProperty('warpDriveEngaged');
+    ```
+  */
   toggleProperty(keyName: keyof this): boolean;
 
   /**
