d3b97886e346551362af13a5ec6f84369ef06a1a
qwan <qiansan@gmail.com>
2022-09-12 14:51:06 -0400

    move

--- packages/@ember/object/promise-proxy-mixin.ts
+++ packages/@ember/object/promise-proxy-mixin.ts
@@ -108,6 +108,10 @@ function tap<T>(proxy: PromiseProxyMixin<T>, promise: RSVP.Promise<T>) {
   @public
 */
 interface PromiseProxyMixin<T> {
+  /**
+    If the proxied promise is rejected this will contain the reason
+    provided.
+  */
   reason: unknown;
 
   /**
@@ -167,76 +171,20 @@ interface PromiseProxyMixin<T> {
   finally: this['promise']['finally'];
 }
 const PromiseProxyMixin = Mixin.create({
-  /**
-    If the proxied promise is rejected this will contain the reason
-    provided.
-
-    @property reason
-    @default null
-    @public
-  */
   reason: null,
 
-  /**
-    Once the proxied promise has settled this will become `false`.
-
-    @property isPending
-    @default true
-    @public
-  */
   isPending: computed('isSettled', function () {
     return !get(this, 'isSettled');
   }).readOnly(),
 
-  /**
-    Once the proxied promise has settled this will become `true`.
-
-    @property isSettled
-    @default false
-    @public
-  */
   isSettled: computed('isRejected', 'isFulfilled', function () {
     return get(this, 'isRejected') || get(this, 'isFulfilled');
   }).readOnly(),
 
-  /**
-    Will become `true` if the proxied promise is rejected.
-
-    @property isRejected
-    @default false
-    @public
-  */
   isRejected: false,
 
-  /**
-    Will become `true` if the proxied promise is fulfilled.
-
-    @property isFulfilled
-    @default false
-    @public
-  */
   isFulfilled: false,
 
-  /**
-    The promise whose fulfillment value is being proxied by this object.
-
-    This property must be specified upon creation, and should not be
-    changed once created.
-
-    Example:
-
-    ```javascript
-    import ObjectProxy from '@ember/object/proxy';
-    import PromiseProxyMixin from '@ember/object/promise-proxy-mixin';
-
-    ObjectProxy.extend(PromiseProxyMixin).create({
-      promise: <thenable>
-    });
-    ```
-
-    @property promise
-    @public
-  */
   promise: computed({
     get() {
       throw new EmberError("PromiseProxy's promise must be set");
@@ -246,42 +194,10 @@ const PromiseProxyMixin = Mixin.create({
     },
   }),
 
-  /**
-    An alias to the proxied promise's `then`.
-
-    See RSVP.Promise.then.
-
-    @method then
-    @param {Function} callback
-    @return {RSVP.Promise}
-    @public
-  */
   then: promiseAlias('then'),
 
-  /**
-    An alias to the proxied promise's `catch`.
-
-    See RSVP.Promise.catch.
-
-    @method catch
-    @param {Function} callback
-    @return {RSVP.Promise}
-    @since 1.3.0
-    @public
-  */
   catch: promiseAlias('catch'),
 
-  /**
-    An alias to the proxied promise's `finally`.
-
-    See RSVP.Promise.finally.
-
-    @method finally
-    @param {Function} callback
-    @return {RSVP.Promise}
-    @since 1.3.0
-    @public
-  */
   finally: promiseAlias('finally'),
 });
 
