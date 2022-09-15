c845dbe05ee86d1b5944cb7600394c22387e5ec7
Chris Krycho <hello@chriskrycho.com>
2022-09-15 06:54:49 -0600

    [BUGFIX beta] Provide a `.d.ts` file at types/stable
    This file currently does nothing, but in the months ahead will look more
    and more like the `/types/preview/index.d.ts` while that one empties.
    This is just here so that someone who writes the import we recommend--
    
    ```ts
    import 'ember-source';
    import 'ember-source/preview';
    ```
    
    --will not get warnings from TypeScript while we wait on putting actual
    things here!

---
+++ types/stable/index.d.ts
@@ -0,0 +1,12 @@
+// Future home of stable Ember types! This file currently does nothing, but in
+// the months ahead will look more and more like the `/types/preview/index.d.ts`
+// while that one empties. This is just here so that someone who writes the
+// import we recommend--
+//
+// ```ts
+// import 'ember-source';
+// import 'ember-source/preview';
+// ```
+//
+// --will not get warnings from TypeScript while we wait on putting actual
+// things here!
