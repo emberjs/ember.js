072830abb6a9a683f7a95dc8b6e2535bf39d3686
Chris Krycho <hello@chriskrycho.com>
2022-11-09 12:41:04 -0700

    Rework `MethodsOf` type utilities for robustness
    1. Using the keyof constraint feature introduced in 4.1, avoid ever
       introducing types with properties whose type is `never` for the
       non-method fields.
    
    2. Handle recursively applying this constraint to nested methods for
       the `EmberMethodsOf` type, which has additional complexity to handle
       Ember's string-based dispatch. Combined with (1), this actually
       *simplifies* the types, eliminating one layer of conditional types.

--- packages/@ember/-internals/utils/types.ts
+++ packages/@ember/-internals/utils/types.ts
@@ -1,10 +1,18 @@
 export type AnyFn = (...args: any[]) => any;
 
-export type MethodNamesOf<T> = {
-  [K in keyof T]: T[K] extends AnyFn ? K : never;
-}[keyof T];
+// The formatting here is designed to help make this type actually be
+// comprehensible to mortals, including the mortals who came up with it.
+// prettier-ignore
+export type MethodsOf<T> = {
+  // This `keyof` check is the thing which gives us *only* these keys, and no
+  // `foo: never` appears in the final type.
+  [K in keyof T as T[K] extends AnyFn ? K : never]:
+    // While this makes sure the resolved type only has `AnyFn` in it, so that
+    // the resulting type is known to be only function types.
+    T[K] extends AnyFn ? T[K] : never;
+};
 
-export type MethodsOf<O> = Pick<O, MethodNamesOf<O>>;
+export type MethodNamesOf<T> = keyof MethodsOf<T>;
 
 export type MethodParams<T, M extends MethodNamesOf<T>> = Parameters<MethodsOf<T>[M]>;
 
