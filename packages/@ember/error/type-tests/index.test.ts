045f46fb9130c87c83740a6e502a28bf628b31a8
Peter Wagenet <peter.wagenet@gmail.com>
2023-01-03 12:33:00 -0800

    Deprecate @ember/error per RFC 889

--- packages/@ember/error/type-tests/index.test.ts
+++ packages/@ember/error/type-tests/index.test.ts
@@ -1,4 +1,5 @@
 import EmberError from '@ember/error';
 import { expectTypeOf } from 'expect-type';
 
-expectTypeOf(EmberError).toEqualTypeOf<ErrorConstructor>();
+expectTypeOf(EmberError("Blah")).toEqualTypeOf<Error>();
+expectTypeOf(new EmberError("Blah")).toEqualTypeOf<Error>();
