e6a9f628d1396e92549dbdd13fb69d41c950d7d4
Katie Gengler <katie@kmg.io>
2023-01-05 14:41:06 -0500

    Deprecate Ember.STRINGS
    - Remove `getString` from string_registry, it was unused internally and
      not exposed publicly.

--- packages/@ember/string/lib/string_registry.ts
+++ packages/@ember/string/lib/string_registry.ts
@@ -1,16 +1,28 @@
+import { deprecate } from '@ember/debug';
+
 // STATE within a module is frowned upon, this exists
 // to support Ember.STRINGS but shield ember internals from this legacy global
 // API.
 let STRINGS: { [key: string]: string } = {};
 
 export function setStrings(strings: { [key: string]: string }) {
+  deprecateEmberStrings();
   STRINGS = strings;
 }
 
 export function getStrings(): { [key: string]: string } {
+  deprecateEmberStrings();
   return STRINGS;
 }
 
-export function getString(name: string): string | undefined {
-  return STRINGS[name];
+function deprecateEmberStrings() {
+  deprecate('Ember.STRINGS is deprecated. It is no longer used by Ember.', false, {
+    id: 'ember-strings',
+    for: 'ember-source',
+    since: {
+      available: '4.10',
+      enabled: '4.10.',
+    },
+    until: '5.0.0',
+  });
 }
