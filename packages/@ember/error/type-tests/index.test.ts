4e64f7178d241e1375b33c31473700f9bafa81cd
Katie Gengler <katie@kmg.io>
2023-01-11 19:53:40 -0500

    Lint fixup

--- packages/@ember/error/type-tests/index.test.ts
+++ packages/@ember/error/type-tests/index.test.ts
@@ -1,5 +1,5 @@
 import EmberError from '@ember/error';
 import { expectTypeOf } from 'expect-type';
 
-expectTypeOf(EmberError("Blah")).toEqualTypeOf<Error>();
-expectTypeOf(new EmberError("Blah")).toEqualTypeOf<Error>();
+expectTypeOf(EmberError('Blah')).toEqualTypeOf<Error>();
+expectTypeOf(new EmberError('Blah')).toEqualTypeOf<Error>();
