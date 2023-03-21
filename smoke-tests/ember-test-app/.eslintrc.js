66e9edd39b43e99de167bf64fefd1cf028af12ef
Peter Wagenet <peter.wagenet@gmail.com>
2023-03-20 20:15:15 -0700

    Update smoke-tests to Ember CLI 4.8

--- smoke-tests/ember-test-app/.eslintrc.js
+++ smoke-tests/ember-test-app/.eslintrc.js
@@ -50,7 +50,7 @@ module.exports = {
       },
     },
     {
-      // Test files:
+      // test files
       files: ['tests/**/*-test.{js,ts}'],
       extends: ['plugin:qunit/recommended'],
     },
