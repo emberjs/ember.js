dd907e9bedaa35f53bad637495d365227bcfa3b9
Chris Krycho <hello@chriskrycho.com>
2023-04-25 16:55:58 -0600

    Update a `set()` type test: it allows dynamic setting
    This matches the runtime behavior. We do not catch setting things this
    way, as a result, but in a world where native property access works
    everywhere, this is actually somewhat useful: it means you can set
    anything on a target object, and although the type will not be updated
    with that change, it will at least type check in a way that matches the
    dynamic runtime behavior.

--- type-tests/@ember/object-test/get-set.ts
+++ type-tests/@ember/object-test/get-set.ts
@@ -6,8 +6,12 @@ const basicPojo = { greeting: 'hello' };
 expectTypeOf(get(basicPojo, 'greeting')).toEqualTypeOf<string>();
 expectTypeOf(get(basicPojo, 'salutation')).toEqualTypeOf<unknown>();
 expectTypeOf(set(basicPojo, 'greeting', 'ahoy')).toEqualTypeOf<string>();
-// @ts-expect-error
-set(basicPojo, 'salutation', 'heyo');
+// This matches the runtime behavior. We do not catch setting things this way,
+// as a result, but in a world where native property access works everywhere,
+// this is actually somewhat useful: it means you can set anything on a target
+// object, and although the type will not be updated with that change, it will
+// at least type check in a way that matches the dynamic runtime behavior.
+expectTypeOf(set(basicPojo, 'salutation', 'heyo')).toBeString();
 
 declare let whoKnows: unknown;
 expectTypeOf(get(whoKnows, 'any-string')).toEqualTypeOf<unknown>();
