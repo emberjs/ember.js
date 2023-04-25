b94db2cbdc18da5bde612719cc11eb8c45e07366
Chris Krycho <hello@chriskrycho.com>
2023-04-25 16:28:19 -0600

    Handle optionality of `Ember.Test` in type tests

--- type-tests/ember/test.ts
+++ type-tests/ember/test.ts
@@ -1,32 +1,46 @@
 import Ember from 'ember';
 
 const pending = 0;
-Ember.Test.registerWaiter(() => pending !== 0);
 
 declare const MyDb: {
   hasPendingTransactions(): boolean;
 };
-Ember.Test.registerWaiter(MyDb, MyDb.hasPendingTransactions);
 
-Ember.Test.promise((resolve) => {
-  window.setTimeout(resolve, 500);
-});
+if (Ember.Test) {
+  Ember.Test.registerWaiter(() => pending !== 0);
 
-Ember.Test.registerHelper('boot', (app) => {
-  Ember.run(app, app.advanceReadiness);
-});
+  Ember.Test.registerWaiter(MyDb, MyDb.hasPendingTransactions);
 
-Ember.Test.registerAsyncHelper('boot', (app) => {
-  Ember.run(app, app.advanceReadiness);
-});
+  Ember.Test.promise((resolve) => {
+    window.setTimeout(resolve, 500);
+  });
 
-Ember.Test.registerAsyncHelper('waitForPromise', (app, promise) => {
-  return new Ember.Test.Promise((resolve) => {
-    Ember.Test.adapter.asyncStart();
+  Ember.Test.registerHelper('boot', (app) => {
+    Ember.run(app, app.advanceReadiness);
+  });
 
-    promise.then(() => {
-      Ember.run.schedule('afterRender', null, resolve);
-      Ember.Test.adapter.asyncEnd();
+  Ember.Test.registerAsyncHelper('boot', (app) => {
+    Ember.run(app, app.advanceReadiness);
+  });
+
+  Ember.Test.registerAsyncHelper('waitForPromise', (app, promise) => {
+    if (!Ember.Test) {
+      return;
+    }
+
+    return new Ember.Test.Promise((resolve) => {
+      if (!Ember.Test) {
+        return;
+      }
+      Ember.Test.Adapter.asyncStart();
+
+      promise.then(() => {
+        if (!Ember.Test) {
+          return;
+        }
+
+        Ember.Test.Adapter.asyncEnd();
+      });
     });
   });
-});
+}
