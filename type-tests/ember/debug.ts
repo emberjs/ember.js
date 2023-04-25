de0f67a5107168298bc332706a04ff1e09c7092c
Chris Krycho <hello@chriskrycho.com>
2023-04-25 16:12:56 -0600

    Update the Ember NS `debug` tests to match modules

--- type-tests/ember/debug.ts
+++ type-tests/ember/debug.ts
@@ -20,10 +20,8 @@ expectTypeOf(runInDebug(() => console.log('Should not show up in prod'))).toBeVo
 
 // Log a warning if we have more than 3 tomsters
 const tomsterCount = 2;
-// @ts-expect-error
 warn('Too many tomsters!');
 expectTypeOf(warn('Too many tomsters!', { id: 'some-warning' })).toBeVoid();
-// @ts-expect-error
 warn('Too many tomsters!', tomsterCount <= 3);
 expectTypeOf(warn('Too many tomsters!', tomsterCount <= 3, { id: 'some-warning' })).toBeVoid();
 expectTypeOf(
@@ -74,6 +72,14 @@ expectTypeOf(
   })
 ).toBeVoid();
 
+type ExpectedOptions = {
+  id: string;
+  until: string;
+  url?: string;
+  for: string;
+  since: { available: string } | { available: string; enabled: string };
+};
+
 // next is not called, so no warnings get the default behavior
 // @ts-expect-error
 registerDeprecationHandler();
@@ -81,16 +87,16 @@ expectTypeOf(registerDeprecationHandler(() => {})).toBeVoid();
 expectTypeOf(
   registerDeprecationHandler((message, options, next) => {
     expectTypeOf(message).toBeString();
-    expectTypeOf(options).toEqualTypeOf<{ id: string; until: string } | undefined>();
+    expectTypeOf(options).toEqualTypeOf<ExpectedOptions | undefined>();
     expectTypeOf(next).toEqualTypeOf<
-      (message: string, options?: { id: string; until: string } | undefined) => void
+      (message: string, options?: ExpectedOptions | undefined) => void
     >();
   })
 ).toBeVoid();
 expectTypeOf(
   registerDeprecationHandler((message, options, next) => {
     expectTypeOf(message).toBeString();
-    expectTypeOf(options).toEqualTypeOf<{ id: string; until: string } | undefined>();
+    expectTypeOf(options).toEqualTypeOf<ExpectedOptions | undefined>();
     // @ts-expect-error
     next();
   })
@@ -98,14 +104,14 @@ expectTypeOf(
 expectTypeOf(
   registerDeprecationHandler((message, options, next) => {
     expectTypeOf(message).toBeString();
-    expectTypeOf(options).toEqualTypeOf<{ id: string; until: string } | undefined>();
+    expectTypeOf(options).toEqualTypeOf<ExpectedOptions | undefined>();
     expectTypeOf(next(message)).toBeVoid();
   })
 ).toBeVoid();
 expectTypeOf(
   registerDeprecationHandler((message, options, next) => {
     expectTypeOf(message).toBeString();
-    expectTypeOf(options).toEqualTypeOf<{ id: string; until: string } | undefined>();
+    expectTypeOf(options).toEqualTypeOf<ExpectedOptions | undefined>();
     expectTypeOf(next(message, options)).toBeVoid();
   })
 ).toBeVoid();
