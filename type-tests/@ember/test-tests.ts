cb8f6fd55c636959af1444b7e74682bb4b77a8d4
Chris Krycho <hello@chriskrycho.com>
2023-04-26 15:34:57 -0600

    Improve types and type tests for `@ember/test`
    - Provide the correct type for the first param for the callback passed
      to `registerHelper()`.
    - Add a cast and an explanation for type test on `TestAdapter`

--- type-tests/@ember/test-tests.ts
+++ type-tests/@ember/test-tests.ts
@@ -34,7 +34,15 @@ registerAsyncHelper('waitForPromise', (app, promise) => {
   app.deferReadiness();
   app.register('foo', class {});
   return new Promise(() => {
-    const adapter = new TestAdapter();
+    // This needs a cast to itself, weird though that looks, because it is
+    // defined `EmberObject.extend()` not `class extends EmberObject`, and our
+    // types for `.extend` do not produce new classes (as described in the RFC
+    // for official TS support). Unfortunately, the "zebra-striping" problem,
+    // where doing `SomeNativeClass.extend()` can have weird behaviors in some
+    // edge cases, means that changing `TestAdapter` to use a native class would
+    // technically be a breaking change. Once the entire framework switches to
+    // native classes end to end, this cast will be unnecessary.
+    const adapter = new TestAdapter() as TestAdapter;
     adapter.asyncStart();
     return promise.then(() => {
       adapter.asyncEnd();
