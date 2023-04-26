be2596f8efada9fdf7c0deec552bb82d5513f2d1
Chris Krycho <hello@chriskrycho.com>
2023-04-26 15:33:47 -0600

    Improve correctness and usability of `@ember/modifier` types
    - Re-export the relevant types from `@glimmer/interfaces` as well as
      wiring them up to the appropriate points in the exported functions.
    - Add the hooks necessary to make the exported `on` modifier visible to
      Glint, including adding an `Opaque` internal type.

--- packages/@ember/-internals/utility-types/index.ts
+++ packages/@ember/-internals/utility-types/index.ts
@@ -19,3 +19,12 @@ export type MethodParams<T, M extends MethodNamesOf<T>> = Parameters<MethodsOf<T
 export type MethodReturns<T, M extends MethodNamesOf<T>> = ReturnType<MethodsOf<T>[M]>;
 
 export type OmitFirst<F> = F extends [any, ...infer R] ? R : [];
+
+// A way of representing non-user-constructible types. You can conveniently use
+// this by doing `interface Type extends Data<'some-type-name'> { ... }` for
+// simple types, and/or you can type-parameterize it as makes sense for your use
+// case (see e.g. `@ember/component/helper`'s use with functional helpers).
+declare const Data: unique symbol;
+export class Opaque<Data> {
+  private declare [Data]: Data;
+}
