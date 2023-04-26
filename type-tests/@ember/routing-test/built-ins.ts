ad9e569ded034537d47274cc2a0d4354e571d0d7
Chris Krycho <hello@chriskrycho.com>
2023-04-26 16:09:00 -0600

    Correctly handle opaque LinkTo type in type tests

--- type-tests/@ember/routing-test/built-ins.ts
+++ type-tests/@ember/routing-test/built-ins.ts
@@ -2,4 +2,9 @@ import { LinkTo } from '@ember/routing';
 import { expectTypeOf } from 'expect-type';
 
 // Basic check taht we export both type and value correctly.
-expectTypeOf(LinkTo.create()).toEqualTypeOf<LinkTo>();
+expectTypeOf(LinkTo).not.toBeUndefined();
+expectTypeOf<LinkTo>().not.toBeUndefined();
+
+// And that it is opaquified as we expect.
+expectTypeOf(LinkTo.create()).toEqualTypeOf<never>();
+expectTypeOf(LinkTo.toString()).toBeString();
