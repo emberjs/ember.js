ae6d85d68a88d812e66d7cba42fe801f12053eb0
Chris Krycho <hello@chriskrycho.com>
2023-05-04 09:46:28 -0600

    Fix typo in description of `Opaque`
    Co-authored-by: Dan Freeman <dfreeman@salsify.com>

--- packages/@ember/-internals/utility-types/index.ts
+++ packages/@ember/-internals/utility-types/index.ts
@@ -21,7 +21,7 @@ export type MethodReturns<T, M extends MethodNamesOf<T>> = ReturnType<MethodsOf<
 export type OmitFirst<F> = F extends [any, ...infer R] ? R : [];
 
 // A way of representing non-user-constructible types. You can conveniently use
-// this by doing `interface Type extends Data<'some-type-name'> { ... }` for
+// this by doing `interface Type extends Opaque<'some-type-name'> { ... }` for
 // simple types, and/or you can type-parameterize it as makes sense for your use
 // case (see e.g. `@ember/component/helper`'s use with functional helpers).
 declare const Data: unique symbol;
