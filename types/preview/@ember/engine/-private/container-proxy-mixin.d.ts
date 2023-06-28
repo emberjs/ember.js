5e94c9225429823a726157a57c6c6a68f508cc46
Chris Krycho <hello@chriskrycho.com>
2023-06-28 15:08:26 -0600

    [BUGFIX LTS] Backport some stable type import locations
    The `ContainerProxyMixin` and `RegistryProxyMixin` private types are
    used as "intimate API" by `@ember/test-helpers`, which aims to support
    both the preview and stable type definitions. Provide both these types
    in both their legacy locations (where they were published from until
    4.10) and their correct locations. *Only* provide the types, however,
    since the runtime location has moved.

--- types/preview/@ember/engine/-private/container-proxy-mixin.d.ts
+++ types/preview/@ember/engine/-private/container-proxy-mixin.d.ts
@@ -1,11 +1,3 @@
 declare module '@ember/engine/-private/container-proxy-mixin' {
-  import { ContainerProxy } from '@ember/-internals/owner';
-  import Mixin from '@ember/object/mixin';
-
-  /**
-   * Given a fullName return a factory manager.
-   */
-  interface ContainerProxyMixin extends ContainerProxy {}
-  const ContainerProxyMixin: Mixin;
-  export default ContainerProxyMixin;
+  export type { default } from '@ember/-internals/runtime/lib/mixins/container_proxy';
 }
