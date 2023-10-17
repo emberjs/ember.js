2e000415ff1681ff7fe45659bc6254aa999f3cf5
Edward Faulkner <edward@eaf4.com>
2023-10-17 16:49:10 -0400

    don't rely on sloppy transitive deps

--- broccoli/find-package.js
+++ broccoli/find-package.js
@@ -1,57 +1,27 @@
 'use strict';
-const fs = require('fs');
+const { PackageCache } = require('@embroider/shared-internals');
 const path = require('path');
-const resolve = require('resolve');
 
-const cache = new Map();
+const packageCache = PackageCache.shared('ember-source', __dirname);
 
-module.exports = function findPackage(name, from) {
-  let key = from === void 0 ? name : name + '\0' + from;
-  let info = cache.get(key);
-  if (info === undefined) {
-    let basedir = from === void 0 ? __dirname : findPackage(from).dir;
-    let resolved = resolve.sync(name + '/package.json', {
-      basedir: basedir,
-    });
-    info = new PackageInfo(fs.realpathSync(resolved));
-    cache.set(key, info);
+exports.findFromProject = function findFromProject(...names) {
+  let current = packageCache.get(packageCache.appRoot);
+  for (let name of names) {
+    current = packageCache.resolve(name, current);
   }
-  return info;
+  return current;
 };
 
-class PackageInfo {
-  constructor(resolved) {
-    let config = require(resolved);
-    this.name = config.name;
-    this.config = config;
-    this.dir = path.dirname(resolved);
+exports.entrypoint = function moduleEntrypoint(pkg, which) {
+  let module = pkg.packageJSON[which];
+  if (!module) {
+    return;
   }
-
-  get main() {
-    return this.parseResolve(this.config.main);
-  }
-
-  get module() {
-    return this.parseResolve(this.config.module);
-  }
-
-  get dependencies() {
-    return this.config.dependencies && Object.keys(this.config.dependencies);
-  }
-
-  resolve(relative) {
-    if (!relative) return;
-    return path.resolve(this.dir, relative);
-  }
-
-  parseResolve(relative) {
-    if (!relative) return;
-    let resolved = this.resolve(relative);
-    let parsed = path.parse(resolved);
-    return {
-      dir: parsed.dir,
-      base: parsed.base,
-      path: resolved,
-    };
-  }
-}
+  let resolved = path.resolve(pkg.root, module);
+  let { dir, base } = path.parse(resolved);
+  return {
+    dir,
+    base,
+    path: resolved,
+  };
+};
