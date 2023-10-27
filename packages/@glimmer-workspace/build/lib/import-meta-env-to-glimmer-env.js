b773292a8d65ad3fb40b332d22d93469d71bb535
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-10-27 17:46:38 -0400

    Split the dev from prod build

---
+++ packages/@glimmer-workspace/build/lib/import-meta-env-to-glimmer-env.js
@@ -0,0 +1,16 @@
+// Ember will use the development build.
+//
+// But ember doesn't yet support import.meta.env
+// and we can't require that apps gain support for it until a major of ember.
+//
+// So for the development build, we'll swap all
+//   import.meta.env.DEV with DEBUG and
+//   import.meta.env.PROD with !DEBUG
+// using the existing babel-plugin-debug-macros with fake-import from `@glimmer/env`
+export default function importMetaEnvToGlimmerEnv(options = {}) {
+  return {
+    transform(code) {
+      console.log(code);
+    },
+  };
+}
