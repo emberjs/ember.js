896c4c845c074090476b3ff029bea56bba1efb8c
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-10-30 10:48:13 -0400

    Setup more publishConfig

---
+++ bin/setup-packages.mjs
@@ -0,0 +1,25 @@
+import { project, packageJson } from 'ember-apply';
+
+let workspaces = await project.getWorkspaces();
+
+for (let workspace of workspaces) {
+  if (!workspace.includes('packages/@glimmer')) continue;
+
+  packageJson.modify((json) => {
+    json.type = 'module';
+
+    json.devDependencies ||= {};
+    json.devDependencies.publint = '^0.2.5';
+
+    json.scripts ||= {};
+    json.scripts['test:publint'] = 'publint';
+
+    json.publishConfig ||= {};
+    json.publishConfig.exports ||= {};
+    json.publishConfig.exports['.'] ||= {};
+    json.publishConfig.exports['.'].types = './dist/dev/index.d.ts';
+    json.publishConfig.exports['.'].development ||= {};
+    json.publishConfig.exports['.'].development.default = './dist/dev/index.js';
+    json.publishConfig.exports['.'].default = './dist/prod/index.d.ts';
+  }, workspace);
+}
