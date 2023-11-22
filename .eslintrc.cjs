e712ae331000de3f3d4bb8facdb6e579a25f77cf
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-11-22 10:42:26 -0500

    Sort package.json files to reduce diff from the Error Recovery PR (#1506)
    * Set config
    
    * sort package json files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -64,6 +64,50 @@ module.exports = {
         '@typescript-eslint/no-var-requires': 'off',
       },
     },
+    {
+      files: ['./package.json', '**/package.json'],
+      parser: 'jsonc-eslint-parser',
+      extends: ['plugin:jsonc/recommended-with-json', 'plugin:jsonc/prettier'],
+      rules: {
+        // Enforce order in the scripts object
+        // https://ota-meshi.github.io/eslint-plugin-jsonc/rules/sort-keys.html
+        'jsonc/sort-keys': [
+          'error',
+          {
+            pathPattern: '^$',
+            order: [
+              'name',
+              'version',
+              'license',
+              'description',
+              'repository',
+              'author',
+              'type',
+              'main',
+              'types',
+              'module',
+              'exports',
+              'publishConfig',
+              'files',
+              'scripts',
+              'dependencies',
+              'peerDependencies',
+              'devDependencies',
+              'release-it',
+              'changelog',
+              'engines',
+              'volta',
+            ],
+          },
+          {
+            pathPattern:
+              'scripts|devDependencies|peerDependencies|optionalDependencies|pnpm|overrides|peerDependencyRules|patchedDependencies|dependenciesMeta',
+            order: { type: 'asc' },
+          },
+          // ...
+        ],
+      },
+    },
 
     {
       // these packages need to be fixed to avoid these warnings, but in the
