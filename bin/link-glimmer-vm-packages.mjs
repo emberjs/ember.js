355e278dbaade26d4f314b2a852aabdc416fcc09
Godfrey Chan <godfreykfc@gmail.com>
2023-12-11 19:09:24 -0800

    Improve link script for glimmer-vm packages
    Since packages/** are real packages now, it is not sufficient to
    only link from the root (e.g. for TypeScript checking), and instead
    we will need to link each sub-package separately.
    
    In the future, https://github.com/pnpm/rfcs/blob/main/text/0001-catalogs.md
    may help, but that is not yet implemented.

--- bin/link-glimmer-vm-packages.mjs
+++ bin/link-glimmer-vm-packages.mjs
@@ -1,15 +1,47 @@
 import { readFileSync } from 'node:fs';
+import path from 'node:path';
+import chalk from 'chalk';
 import execa from 'execa';
+import glob from 'glob';
 
-const packageJsonPath = new URL('../package.json', import.meta.url).pathname;
-const packageJson = JSON.parse(await readFileSync(packageJsonPath, { encoding: 'utf8' }));
+const rootDir = new URL('..', import.meta.url).pathname;
+
+const packageJsonPaths = glob.sync('**/package.json', {
+  cwd: rootDir,
+  ignore: '**/node_modules/**',
+});
 
 function shouldLink(dep) {
-  return dep.startsWith('@glimmer/') && dep !== '@glimmer/component' && dep !== '@glimmer/env';
+  return (
+    dep.startsWith('@glimmer/') &&
+    dep !== '@glimmer/component' &&
+    dep !== '@glimmer/env' &&
+    dep !== '@glimmer/tracking'
+  );
 }
 
-for (const [dep] of Object.entries(packageJson.dependencies)) {
-  if (shouldLink(dep)) {
-    await execa('pnpm', ['link', '--global', dep], { stdio: 'inherit' });
+const link = packageJsonPaths.map(async (packageJsonPath) => {
+  const packagePath = path.dirname(packageJsonPath);
+
+  try {
+    const packageJson = JSON.parse(await readFileSync(packageJsonPath, { encoding: 'utf8' }));
+
+    for (const [dep] of Object.entries(packageJson.dependencies ?? {})) {
+      if (shouldLink(dep)) {
+        // eslint-disable-next-line no-console
+        console.log(`Linking ${chalk.yellow(dep)} from ${chalk.grey(packagePath)}`);
+        await execa('pnpm', ['link', '--global', dep], { cwd: packagePath });
+      }
+    }
+  } catch (error) {
+    let message = `Failed to link ${packagePath}`;
+
+    if (error instanceof Error) {
+      message += `\n\n${error.stack}`;
+    }
+
+    throw new Error(message);
   }
-}
+});
+
+await Promise.all(link);
