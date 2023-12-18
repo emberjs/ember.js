612b0f8d492a8b389991dc6fa3cea50ac9609bbd
Edward Faulkner <edward@eaf4.com>
2023-12-18 11:27:42 -0500

    matching babel config more closely

--- babel.config.mjs
+++ babel.config.mjs
@@ -1,5 +1,11 @@
 import { resolve, dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { createRequire } from 'node:module';
+import vmBabelPlugins from '@glimmer/vm-babel-plugins';
+
+const require = createRequire(import.meta.url);
+const buildDebugMacroPlugin = require('./lib/build-debug-macro-plugin.js');
+const isProduction = process.env.EMBER_ENV === 'production';
 
 export default {
   plugins: [
@@ -19,5 +25,7 @@ export default {
         ),
       },
     ],
+    buildDebugMacroPlugin(!isProduction),
+    ...vmBabelPlugins({ isDebug: !isProduction }),
   ],
 };
