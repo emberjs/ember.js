e42fb31832d49f34938f97676226c4f7db24200d
Edward Faulkner <edward@eaf4.com>
2023-12-20 16:17:34 -0500

    add ember-testing

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -18,6 +18,7 @@ export default [
     './lib/amd-compat-entrypoints/ember-template-compiler.js',
     'ember-template-compiler.js'
   ),
+  legacyBundleConfig('./lib/amd-compat-entrypoints/ember-testing.js', 'ember-testing.js'),
 ];
 
 function esmConfig() {
