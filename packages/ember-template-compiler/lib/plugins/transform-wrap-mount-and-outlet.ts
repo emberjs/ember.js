f7c53bf3e731476c37e288af8405c61f2a121905
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-03-27 15:05:33 -0400

    Remove extraneous import

--- packages/ember-template-compiler/lib/plugins/transform-wrap-mount-and-outlet.ts
+++ packages/ember-template-compiler/lib/plugins/transform-wrap-mount-and-outlet.ts
@@ -1,4 +1,4 @@
-import type { AST, ASTPlugin, ASTPluginBuilder } from '@glimmer/syntax';
+import type { AST, ASTPlugin } from '@glimmer/syntax';
 import type { EmberASTPluginEnvironment } from '../types';
 import { isPath, trackLocals } from './utils';
 
