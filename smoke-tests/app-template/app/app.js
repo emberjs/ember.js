9663d641f172b7597a0a8ff5de3d3c9c1a22f81b
Edward Faulkner <edward@eaf4.com>
2024-05-24 02:14:36 -0400

    wip

--- smoke-tests/app-template/app/app.js
+++ smoke-tests/app-template/app/app.js
@@ -1,10 +1,11 @@
 import Application from '@ember/application';
-import Resolver from '@ember/resolver';
+import Resolver from 'ember-resolver';
 import config from 'ember-test-app/config/environment';
+import modules from '@ember/compat-modules';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
-  Resolver = Resolver;
+  Resolver = Resolver.withModules(modules);
 }
 
