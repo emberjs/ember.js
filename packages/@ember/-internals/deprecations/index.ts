45ce7c42c9adafa2b07f7289a530c7a22644d58f
Simon Ihmig <simon.ihmig@kaliber5.de>
2024-06-03 18:07:15 +0200

    Move deprecation to avoid circular module imports
    (cherry picked from commit 1e686b7f962898e9e1d401b45a15b3457379c013)

--- packages/@ember/-internals/deprecations/index.ts
+++ packages/@ember/-internals/deprecations/index.ts
@@ -154,3 +154,14 @@ export function deprecateUntil(message: string, deprecation: DeprecationObject) 
   }
   deprecate(message, deprecation.test, options);
 }
+
+const { EXTEND_PROTOTYPES } = ENV as {
+  EXTEND_PROTOTYPES: { Array?: boolean };
+};
+
+if (EXTEND_PROTOTYPES.Array !== false) {
+  deprecateUntil(
+    'Array prototype extensions are deprecated. Follow the deprecation guide for migration instructions, and set EmberENV.EXTEND_PROTOTYPES to false in your config/environment.js',
+    DEPRECATIONS.DEPRECATE_ARRAY_PROTOTYPE_EXTENSIONS
+  );
+}
