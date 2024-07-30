af2db065872ee3170eef5dd4819e48efe4e04fd1
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-07-30 14:59:30 -0400

    Update index.ts

--- packages/@ember/deprecated-features/index.ts
+++ packages/@ember/deprecated-features/index.ts
@@ -1,6 +1,3 @@
 /* eslint-disable no-implicit-coercion */
 
-// These versions should be the version that the deprecation was _introduced_,
-// not the version that the feature will be removed.
-
-export const ASSIGN = !!'4.0.0-beta.1';
+export const ASSIGN = true;
