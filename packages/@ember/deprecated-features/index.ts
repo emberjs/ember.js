a3bb306c88311ed0b2501b30ee031151cf464dc0
Peter Wagenet <peter@wagenet.us>
2024-08-21 15:42:34 -0700

    Directly assign a boolean value in deprecated-features

--- packages/@ember/deprecated-features/index.ts
+++ packages/@ember/deprecated-features/index.ts
@@ -3,4 +3,5 @@
 // These versions should be the version that the deprecation was _introduced_,
 // not the version that the feature will be removed.
 
-export const ASSIGN = !!'4.0.0-beta.1';
+/** Introduced in 4.0.0-beta.1 */
+export const ASSIGN = true;
