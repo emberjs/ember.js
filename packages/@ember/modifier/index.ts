49c0436ba7bee3bc1f1bc0a4ce55c9a1ba27f31c
Edward Faulkner <edward@eaf4.com>
2024-08-26 17:48:12 -0400

    break another cycle

--- packages/@ember/modifier/index.ts
+++ packages/@ember/modifier/index.ts
@@ -1,22 +1,9 @@
-import { on as glimmerOn } from '@glimmer/runtime';
 import { setModifierManager as glimmerSetModifierManager } from '@glimmer/manager';
 
-import type { Opaque } from '@ember/-internals/utility-types';
 import type Owner from '@ember/owner';
 import type { ModifierManager } from '@glimmer/interfaces';
 
-// In normal TypeScript, this modifier is essentially an opaque token that just
-// needs to be importable. Declaring it with a unique interface like this,
-// however, gives tools like Glint (that *do* have a richer notion of what it
-// is) a place to install more detailed type information.
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface OnModifier extends Opaque<'modifier:on'> {}
-
-// SAFETY: at the time of writing, the cast here is from `{}` to `OnModifier`,
-// which makes it strictly safer to use outside this module because it is not
-// usable as "any non-null item", which is what `{}` means, without loss of any
-// information from the type itself.
-export const on = glimmerOn as OnModifier;
+export { on, type OnModifier } from './on';
 
 // NOTE: this uses assignment to *require* that the `glimmerSetModifierManager`
 // is legally assignable to this type, i.e. that variance is properly upheld.
