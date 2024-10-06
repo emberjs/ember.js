36242f3713f3bacbcdf749b21d39a0e06029e0f2
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2024-10-06 13:53:06 +0200

    Remove `--pod` support from the component generator
    The pod layout is deprecated for components.

--- blueprints/component/index.js
+++ blueprints/component/index.js
@@ -1,13 +1,10 @@
 'use strict';
 
 const chalk = require('chalk');
-const path = require('path');
 const SilentError = require('silent-error');
 const stringUtil = require('ember-cli-string-utils');
-const pathUtil = require('ember-cli-path-utils');
 const getPathOption = require('ember-cli-get-component-path-option');
 const normalizeEntityName = require('ember-cli-normalize-entity-name');
-const { EOL } = require('os');
 const { has } = require('@ember/edition-utils');
 const { generateComponentSignature } = require('../-utils');
 
@@ -137,22 +134,7 @@ module.exports = {
   fileMapTokens(options) {
     let commandOptions = this.options;
 
-    if (commandOptions.pod) {
-      return {
-        __path__() {
-          return path.join(options.podPath, options.locals.path, options.dasherizedModuleName);
-        },
-        __templatepath__() {
-          return path.join(options.podPath, options.locals.path, options.dasherizedModuleName);
-        },
-        __templatename__() {
-          return 'template';
-        },
-      };
-    } else if (
-      this.EMBER_GLIMMER_SET_COMPONENT_TEMPLATE &&
-      commandOptions.componentStructure === 'flat'
-    ) {
+    if (this.EMBER_GLIMMER_SET_COMPONENT_TEMPLATE && commandOptions.componentStructure === 'flat') {
       return {
         __path__() {
           return 'components';
@@ -209,19 +191,6 @@ module.exports = {
   },
 
   locals(options) {
-    // if we're in an addon, build import statement
-    let templatePath = '';
-    if (options.project.isEmberCLIAddon() || (options.inRepoAddon && !options.inDummy)) {
-      if (options.pod) {
-        templatePath = './template';
-      } else {
-        templatePath =
-          pathUtil.getRelativeParentPath(options.entity.name) +
-          'templates/components/' +
-          stringUtil.dasherize(options.entity.name);
-      }
-    }
-
     let componentClass = this.EMBER_GLIMMER_SET_COMPONENT_TEMPLATE
       ? options.componentClass
       : '@ember/component';
@@ -237,12 +206,7 @@ module.exports = {
     switch (componentClass) {
       case '@ember/component':
         importComponent = `import Component from '@ember/component';`;
-        if (templatePath) {
-          importTemplate = `import layout from '${templatePath}';${EOL}`;
-          defaultExport = `Component.extend({${EOL}  layout${EOL}});`;
-        } else {
-          defaultExport = `Component.extend({});`;
-        }
+        defaultExport = `Component.extend({});`;
         break;
       case '@glimmer/component':
         importComponent = `import Component from '@glimmer/component';`;
