f528512ccd6f0a61e5cb3904184f15383d90b643
Yehuda Katz <wycats@gmail.com>
2024-10-11 12:47:26 -0700

    Document type signature and consolidate plugins
    1. Document the type signature of the `template()` options.
    2. Consolidate plugins into `@ember/template-compiler` and import them
       from `ember-template-compiler`.
    
    This commit also creates a few new entry points in
    `@ember/template-compiler` to satisfy the use-cases in the
    consolidation.

--- packages/ember-template-compiler/lib/public-api.ts
+++ packages/ember-template-compiler/lib/public-api.ts
@@ -10,7 +10,10 @@ export {
   buildCompileOptions as _buildCompileOptions,
   transformsFor as _transformsFor,
 } from './system/compile-options';
-export { RESOLUTION_MODE_TRANSFORMS, STRICT_MODE_TRANSFORMS } from './plugins';
+export {
+  RESOLUTION_MODE_TRANSFORMS,
+  STRICT_MODE_TRANSFORMS,
+} from '@ember/template-compiler/primitives';
 export type { EmberPrecompileOptions } from './types';
 
 export { preprocess as _preprocess, print as _print } from '@glimmer/syntax';
