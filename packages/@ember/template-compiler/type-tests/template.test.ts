90ce57d80842ff2fd53aa2bc1e7a52d986ea5ecc
Yehuda Katz <wycats@gmail.com>
2024-10-11 13:02:52 -0700

    Start writing better type tests

--- packages/@ember/template-compiler/type-tests/template.test.ts
+++ packages/@ember/template-compiler/type-tests/template.test.ts
@@ -1,6 +1,11 @@
 import { template, type EmberPrecompileOptions } from '@ember/template-compiler';
+import type { TemplateOnlyComponent } from '@ember/component/template-only';
 import { expectTypeOf } from 'expect-type';
 
-expectTypeOf(template).toEqualTypeOf<
-  (template: string, options?: Partial<EmberPrecompileOptions>) => object
->();
+const hello = 'hello';
+
+expectTypeOf(template('hello world')).toEqualTypeOf<TemplateOnlyComponent>();
+expectTypeOf(template('hello world', { scope: () => ({}) })).toEqualTypeOf<TemplateOnlyComponent>();
+expectTypeOf(
+  template('{{hello}} world', { scope: () => ({ hello }) })
+).toEqualTypeOf<TemplateOnlyComponent>();
