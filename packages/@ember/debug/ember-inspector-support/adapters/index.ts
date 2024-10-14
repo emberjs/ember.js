680ac237f0b659f2cfad9b7ac0c97824b265b6d0
patrick <pa.trickjlp@gmail.com>
2024-10-14 13:45:10 +0200

    rename

--- packages/@ember/debug/ember-inspector-support/adapters/index.ts
+++ packages/@ember/debug/ember-inspector-support/adapters/index.ts
@@ -1,13 +1,15 @@
 import { default as BasicAdapter } from './basic';
 import { default as BookmarkletAdapter } from './bookmarklet';
 import { default as ChromeAdapter } from './chrome';
+import { default as FirefoxAdapter } from './firefox';
 import { default as WebsocketAdapter } from './websocket';
 import { default as WebExtensionAdapter } from './web-extension';
 
 export default {
-  BasicAdapter,
-  BookmarkletAdapter,
-  ChromeAdapter,
-  WebExtensionAdapter,
-  WebsocketAdapter,
+  basic: BasicAdapter,
+  bookmarklet: BookmarkletAdapter,
+  chrome: ChromeAdapter,
+  firefox: FirefoxAdapter,
+  webExtension: WebExtensionAdapter,
+  websocket: WebsocketAdapter,
 };
