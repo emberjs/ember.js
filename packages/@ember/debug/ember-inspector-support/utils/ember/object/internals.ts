c6cdc3218397e73d37a471d75d2f497f1177dd42
patrick <pa.trickjlp@gmail.com>
2024-10-14 15:27:54 +0200

    fix custom guidFor

--- packages/@ember/debug/ember-inspector-support/utils/ember/object/internals.ts
+++ packages/@ember/debug/ember-inspector-support/utils/ember/object/internals.ts
@@ -2,7 +2,7 @@ import { guidFor as emberGuidFor } from '@ember/-internals/utils';
 
 // it can happen that different ember apps/iframes have the same id for different objects
 // since the implementation is just a counter, so we add a prefix per iframe & app
-let perIframePrefix = Math.random().toString() + '-';
+let perIframePrefix = parseInt(String(Math.random() * 1000)).toString() + '-';
 let prefix = '';
 let guidFor = (obj: any, pref?: string) =>
   `${perIframePrefix + (pref || prefix)}-${emberGuidFor(obj)}`;
