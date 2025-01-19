d4487929ff7c4f076af7833d425f3659952bd413
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-01-19 16:22:56 -0500

    Progress?

--- packages/@ember/-internals/metal/lib/storage.ts
+++ packages/@ember/-internals/metal/lib/storage.ts
@@ -1,21 +1,77 @@
-import { trackedData } from '@glimmer/validator';
+import type { UpdatableTag } from '@glimmer/interfaces';
 
-interface Storage<Value> {
-  /**
-   * This is not a real property. Don't use it.
-   * This is functioning as a brand-type for use with
-   *   - createStorage
-   *   - getValue
-   *   - setValue
-   *
-   * from @glimmer/trcking/primitives/storage
-   */
-  __storage: Value;
+import { consumeTag } from './tracking';
+import { createUpdatableTag, DIRTY_TAG } from './validators';
+import { assert } from '@ember/debug';
+
+const SET = Symbol.for('TrackedStorage.set');
+const READ = Symbol.for('TrackedStorage.read');
+
+function tripleEq<Value>(a: Value, b: Value): boolean {
+  return a === b;
+}
+
+class Storage<Value> {
+  #tag: UpdatableTag;
+  #value: Value;
+  #lastValue: Value;
+  #isEqual: (a: Value, b: Value) => boolean;
+
+  get #current() {
+    consumeTag(this.#tag);
+    return this.#value;
+  }
+  set #current(value) {
+    if (this.#isEqual(this.#value, this.#lastValue)) {
+      return;
+    }
+
+    this.#value = this.#lastValue = value;
+
+    DIRTY_TAG(this.#tag);
+  }
+
+  constructor(initialValue: Value, isEqual?: (a: Value, b: Value) => boolean) {
+    this.#tag = createUpdatableTag();
+    this.#value = this.#lastValue = initialValue;
+    this.#isEqual = isEqual ?? tripleEq;
+  }
+
+  [READ]() {
+    return this.#current;
+  }
+
+  [SET](value: Value) {
+    this.#current = value;
+  }
 }
 
 export function createStorage<Value>(
-  initialValue?: Value,
+  initialValue: Value,
   isEqual?: (oldValue: Value, newValue: Value) => boolean
-): Storage<Value> {}
-export function getValue<Value>(storage: Storage<Value>): Value {}
-export function setValue<Value>(storage: Storage<Value>, value: Value): void {}
+): Storage<Value> {
+  assert(
+    'the second parameter to `createStorage` must be an equality function or undefined',
+    isEqual === undefined || typeof isEqual === 'function'
+  );
+
+  return new Storage(initialValue, isEqual);
+}
+
+export function getValue<Value>(storage: Storage<Value>): Value {
+  assert(
+    'getValue must be passed a tracked store created with `createStorage`.',
+    storage instanceof Storage
+  );
+
+  return storage[READ]();
+}
+
+export function setValue<Value>(storage: Storage<Value>, value: Value): void {
+  assert(
+    'setValue must be passed a tracked store created with `createStorage`.',
+    storage instanceof Storage
+  );
+
+  storage[SET](value);
+}
