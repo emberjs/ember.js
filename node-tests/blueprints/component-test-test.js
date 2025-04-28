57902a6d8d4250164e4252ff9f0e8cebf6204034
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-04-28 14:56:09 +0200

    fix attempt 2.. mondays <3

--- node-tests/blueprints/component-test-test.js
+++ node-tests/blueprints/component-test-test.js
@@ -49,86 +49,92 @@ describe('Blueprint: component-test', function () {
         }
       );
     });
-  });
 
-  it('component-test x-foo --unit', function () {
-    return emberGenerateDestroy(['component-test', 'x-foo', '--unit'], (_file) => {
-      expect(_file('tests/unit/components/x-foo-test.js')).to.equal(
-        fixture('component-test/unit.js')
-      );
-    });
-  });
-});
-
-describe('in addon', function () {
-  beforeEach(function () {
-    return emberNew({ target: 'addon' });
-  });
-
-  it('component-test foo', function () {
-    return emberGenerateDestroy(['component-test', 'foo'], (_file) => {
-      expect(_file('tests/integration/components/foo-test.js')).to.equal(
-        fixture('component-test/addon.js', {
-          replace: {
-            component: 'foo',
-            componentInvocation: 'Foo',
-          },
-        })
-      );
-    });
-  });
-
-  it('component-test foo --unit', function () {
-    return emberGenerateDestroy(['component-test', 'foo', '--unit'], (_file) => {
-      expect(_file('tests/unit/components/foo-test.js')).to.equal(
-        fixture('component-test/addon-unit.js')
-      );
-    });
-  });
-
-  it('component-test foo --strict', function () {
-    return emberGenerateDestroy(['component-test', 'foo', '--strict'], (_file) => {
-      expect(_file('tests/integration/components/foo-test.gjs')).to.equal(
-        fixture('component-test/addon.gjs')
-      );
-    });
-  });
-
-  it('component-test foo --strict --typescript', function () {
-    return emberGenerateDestroy(['component-test', 'foo', '--strict', '--typescript'], (_file) => {
-      expect(_file('tests/integration/components/foo-test.gts')).to.equal(
-        fixture('component-test/addon.gts')
-      );
-    });
-  });
-});
-
-describe('in in-repo-addon', function () {
-  beforeEach(function () {
-    return emberNew({ target: 'in-repo-addon' });
-  });
-
-  it('component-test foo --in-repo-addon=my-addon', function () {
-    return emberGenerateDestroy(['component-test', 'foo', '--in-repo-addon=my-addon'], (_file) => {
-      expect(_file('tests/integration/components/foo-test.js')).to.equal(
-        fixture('component-test/app.js', {
-          replace: {
-            component: 'foo',
-            componentInvocation: 'Foo',
-          },
-        })
-      );
-    });
-  });
-
-  it('component-test x-foo --in-repo-addon=my-addon --unit', function () {
-    return emberGenerateDestroy(
-      ['component-test', 'x-foo', '--in-repo-addon=my-addon', '--unit'],
-      (_file) => {
+    it('component-test x-foo --unit', function () {
+      return emberGenerateDestroy(['component-test', 'x-foo', '--unit'], (_file) => {
         expect(_file('tests/unit/components/x-foo-test.js')).to.equal(
           fixture('component-test/unit.js')
         );
-      }
-    );
+      });
+    });
+  });
+
+  describe('in addon', function () {
+    beforeEach(function () {
+      return emberNew({ target: 'addon' });
+    });
+
+    it('component-test foo', function () {
+      return emberGenerateDestroy(['component-test', 'foo'], (_file) => {
+        expect(_file('tests/integration/components/foo-test.js')).to.equal(
+          fixture('component-test/addon.js', {
+            replace: {
+              component: 'foo',
+              componentInvocation: 'Foo',
+            },
+          })
+        );
+      });
+    });
+
+    it('component-test foo --unit', function () {
+      return emberGenerateDestroy(['component-test', 'foo', '--unit'], (_file) => {
+        expect(_file('tests/unit/components/foo-test.js')).to.equal(
+          fixture('component-test/addon-unit.js')
+        );
+      });
+    });
+
+    it('component-test foo --strict', function () {
+      return emberGenerateDestroy(['component-test', 'foo', '--strict'], (_file) => {
+        expect(_file('tests/integration/components/foo-test.gjs')).to.equal(
+          fixture('component-test/addon.gjs')
+        );
+      });
+    });
+
+    it('component-test foo --strict --typescript', function () {
+      return emberGenerateDestroy(
+        ['component-test', 'foo', '--strict', '--typescript'],
+        (_file) => {
+          expect(_file('tests/integration/components/foo-test.gts')).to.equal(
+            fixture('component-test/addon.gts')
+          );
+        }
+      );
+    });
+  });
+
+  describe('in in-repo-addon', function () {
+    beforeEach(function () {
+      return emberNew({ target: 'in-repo-addon' });
+    });
+
+    it('component-test foo --in-repo-addon=my-addon', function () {
+      return emberGenerateDestroy(
+        ['component-test', 'foo', '--in-repo-addon=my-addon'],
+        (_file) => {
+          expect(_file('tests/integration/components/foo-test.js')).to.equal(
+            fixture('component-test/app.js', {
+              replace: {
+                component: 'foo',
+                componentInvocation: 'Foo',
+              },
+            })
+          );
+        }
+      );
+    });
+
+    it('component-test x-foo --in-repo-addon=my-addon --unit', function () {
+      return emberGenerateDestroy(
+        ['component-test', 'x-foo', '--in-repo-addon=my-addon', '--unit'],
+        (_file) => {
+          expect(_file('tests/unit/components/x-foo-test.js')).to.equal(
+            fixture('component-test/unit.js')
+          );
+        }
+      );
+    });
   });
 });
