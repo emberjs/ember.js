699eebb24fb751bbb753aaa79b55ae771528c529
Edward Faulkner <edward@eaf4.com>
2025-09-02 15:26:11 -0400

    getting type-checking for glimmer-vm working here

--- packages/internal-test-helpers/lib/module-for.ts
+++ packages/internal-test-helpers/lib/module-for.ts
@@ -33,7 +33,6 @@ export function moduleForDevelopment<T extends AbstractTestCase, M extends Gener
   TestClass: TestClass<T>,
   ...mixins: Mixin<M>[]
 ) {
-  // @ts-expect-error Our tests run in vite, vite supports this
   if (import.meta.env.MODE === 'development') {
     moduleFor(description, TestClass, ...mixins);
   }
