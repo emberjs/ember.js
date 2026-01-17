e10078e835fe2a1d9ed8802240c71e7e6197b4d9
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-01-17 01:34:48 -0500

    GetStrictKeyword

--- packages/@glimmer/opcode-compiler/lib/opcode-builder/helpers/resolution.ts
+++ packages/@glimmer/opcode-compiler/lib/opcode-builder/helpers/resolution.ts
@@ -221,8 +221,6 @@ export function resolveModifier(
       expr[1]
     ];
 
-    definition ??= modifierKeywords[expr[1]];
-
     then(constants.modifier(definition as object, lexical?.at(expr[1]) ?? undefined));
   } else if (type === SexpOpcodes.GetStrictKeyword) {
     let {
@@ -230,6 +228,8 @@ export function resolveModifier(
     } = assertResolverInvariants(meta);
     let name = unwrap(upvars[expr[1]]);
     let modifier = resolver?.lookupBuiltInModifier?.(name) ?? null;
+
+    modifier ??= modifierKeywords[name] ?? null;
 
     if (DEBUG && modifier === null) {
       localAssert(
