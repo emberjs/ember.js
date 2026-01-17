095af41ba9c51385b4ec2f4c5f565b6f04ab7719
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-01-17 00:11:11 -0500

    Remove related files

--- packages/ember-template-compiler/index.ts
+++ packages/ember-template-compiler/index.ts
@@ -3,7 +3,3 @@ import * as ETC from './lib/public-api';
 import { __registerTemplateCompiler } from '@ember/template-compilation';
 
 __registerTemplateCompiler(ETC);
-
-// add domTemplates initializer (only does something if `ember-template-compiler`
-// is loaded already)
-import './lib/system/initializer';
