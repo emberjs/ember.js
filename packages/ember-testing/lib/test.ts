3ac6c1f70207353248ce37bafc45c8b9e5c19372
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-01-20 14:18:28 -0500

    Remove helpers

--- packages/ember-testing/lib/test.ts
+++ packages/ember-testing/lib/test.ts
@@ -1,7 +1,6 @@
 /**
   @module ember
 */
-import { helpers, registerHelper, registerAsyncHelper, unregisterHelper } from './test/helpers';
 import { onInjectHelpers } from './test/on_inject_helpers';
 import TestPromise, { promise, resolve } from './test/promise';
 import { checkWaiters, registerWaiter, unregisterWaiter } from './test/waiters';
@@ -21,18 +20,6 @@ import { getAdapter, setAdapter } from './test/adapter';
   @public
 */
 const Test = {
-  /**
-    Hash containing all known test helpers.
-
-    @property _helpers
-    @private
-    @since 1.7.0
-  */
-  _helpers: helpers,
-
-  registerHelper,
-  registerAsyncHelper,
-  unregisterHelper,
   onInjectHelpers,
   Promise: TestPromise,
   promise,
