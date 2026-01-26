1246da4b166aabf23c33ae00e40e908e1e44210c
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-01-26 17:49:54 -0500

    Add jsutils.import to new transform

---
+++ packages/@ember/template-compiler/lib/plugins/auto-import-builtins.ts
@@ -0,0 +1,53 @@
+import type { AST, ASTPlugin } from '@glimmer/syntax';
+import type { Builders, EmberASTPluginEnvironment } from '../types';
+import { isPath, trackLocals } from './utils';
+
+/**
+ @module ember
+*/
+
+/**
+  A Glimmer2 AST transformation that replaces all instances of
+
+  ```handlebars
+ <button {{action 'foo'}}>
+ <button onblur={{action 'foo'}}>
+ <button onblur={{action (action 'foo') 'bar'}}>
+  ```
+
+  with
+
+  ```handlebars
+ <button {{action this 'foo'}}>
+ <button onblur={{action this 'foo'}}>
+ <button onblur={{action this (action this 'foo') 'bar'}}>
+  ```
+
+  @private
+  @class TransformActionSyntax
+*/
+
+export default function transformActionSyntax(env: EmberASTPluginEnvironment): ASTPlugin {
+  let { hasLocal, visitor } = trackLocals(env);
+
+  return {
+    name: 'auto-import-built-ins',
+
+    visitor: {
+      ...visitor,
+      ElementModifierStatement(node: AST.ElementModifierStatement) {
+        if (isOn(node, hasLocal)) {
+          // @ts-expect-error doesn't exist on types
+          env.meta?.jsutils.import('@ember/modifier', 'on', 'on');
+        }
+      },
+    },
+  };
+}
+
+function isOn(
+  node: AST.ElementModifierStatement | AST.MustacheStatement | AST.SubExpression,
+  hasLocal: (k: string) => boolean
+) {
+  return isPath(node.path) && node.path.original === 'on' && !hasLocal('on');
+}
