b303485adcbcaba254f7380c7f2ede5d9c43a873
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-01-29 22:26:31 -0500

    wip

---
+++ packages/@ember/runtime/index.ts
@@ -0,0 +1,15 @@
+export function emitText() {}
+export function emitComponent() {}
+
+export function emitElement() {}
+
+/**
+ * to elements and components
+ */
+export function applyAttributes() {}
+export function applyModifier() {}
+
+/**
+ * e.g.: <template shadowrootmode="open">
+ */
+export function emitTemplate() {}
