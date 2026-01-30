e832439c6249868e7cbe60184ea7a1116ae216be
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-01-29 22:39:20 -0500

    Planning

--- packages/@ember/runtime/index.ts
+++ packages/@ember/runtime/index.ts
@@ -1,15 +1,31 @@
-export function emitText() {}
-export function emitComponent() {}
+/**
+ *
+ * Can we design a runtime that is both fast and eliminates complexity from our language server tooling?
+ *
+ */
+export function emitText(text: string): void {}
+export function emitComponent<Signature>(
+  component: ComponentLike<Signature>,
+  args: Args
+): ElementFor<Signature> {}
 
-export function emitElement() {}
+export function emitElement<TagName extends string>(
+  tagName: TagName,
+  staticAttributes: AttributesFor<TagName>
+): Element {}
 
 /**
  * to elements and components
  */
-export function applyAttributes() {}
-export function applyModifier() {}
+export function applyAttributes<Element>(
+  element: Element,
+  dynamicAttributes: Record<string, () => string>
+) {}
+export function applyModifier<Element>(element: Element, modifier: ModifierLike, args: Args) {}
 
 /**
  * e.g.: <template shadowrootmode="open">
+ *
+ * TODO: I don't know how this would work yet
  */
 export function emitTemplate() {}
