b01c3a72ce7e3e12eb749242877ffa5b9f2d4b4a
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-02-06 16:50:27 -0500

    Repro for RFC#1070 - none of the default globals are accessible in eval-mode

--- packages/@ember/-internals/glimmer/tests/integration/components/runtime-template-compiler-implicit-test.ts
+++ packages/@ember/-internals/glimmer/tests/integration/components/runtime-template-compiler-implicit-test.ts
@@ -1,4 +1,5 @@
 import { template } from '@ember/template-compiler/runtime';
+import { ALLOWED_GLOBALS } from '@ember/template-compiler';
 import { RenderingTestCase, defineSimpleModifier, moduleFor } from 'internal-test-helpers';
 import GlimmerishComponent from '../../utils/glimmerish-component';
 import { on } from '@ember/modifier/on';
@@ -464,6 +465,29 @@ moduleFor(
       this.assertStableRerender();
     }
   }
+);
+
+moduleFor(
+  'Strict Mode - Runtime Template Compiler (implicit) - allowed globals from RFC#1070',
+  (function () {
+    class AllowedGlobalsTest extends RenderingTestCase {}
+
+    for (let globalName of ALLOWED_GLOBALS) {
+      AllowedGlobalsTest.prototype[`@test Can use ${globalName}`] = async function () {
+        await this.renderComponentModule(() => {
+          return template(`{{log ${globalName}}}`, {
+            eval() {
+              return eval(arguments[0]);
+            },
+          });
+        });
+
+        this.assertStableRerender();
+      };
+    }
+
+    return AllowedGlobalsTest;
+  })()
 );
 
 /**
