9e8d4a4112ef14f44520a694167fbc1e843f3f8f
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-06-03 10:48:58 -0400

    Remove userland way to try to achieve the same tree shaking

--- smoke-tests/v2-app-hello-world-template/vite.config.mjs
+++ smoke-tests/v2-app-hello-world-template/vite.config.mjs
@@ -3,20 +3,6 @@ import { extensions, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 
 export default defineConfig({
-  build: {
-    rolldownOptions: {
-      treeshake: {
-        moduleSideEffects: [
-          { test: /\.css$/, sideEffects: true },
-          { test: /@ember/, sideEffects: false, external: false },
-          { test: /router_js/, sideEffects: false, external: false },
-          { test: /rsvp/, sideEffects: false, external: false },
-          { test: /backburner/, sideEffects: false, external: false },
-        ],
-
-      }
-    }
-  },
   plugins: [
     ember(),
     babel({
