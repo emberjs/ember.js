39a9acddda092fc4185b93d3d08ad32721fed899
Katie Gengler <katie@kmg.io>
2026-06-11 16:19:36 -0400

    Update @glimmer/component/index API docs to use template tag
    (cherry picked from commit 781d3128d47b093ffcca718e7f0b01d57a51a700)

--- packages/@glimmer/component/src/index.ts
+++ packages/@glimmer/component/src/index.ts
@@ -5,10 +5,10 @@ import _GlimmerComponent, { type Args } from './-private/component';
 import { setOwner, type default as Owner } from '@ember/owner';
 
 /**
-  A component is a reusable UI element that consists of a `.hbs` template and an
+  A component is a reusable UI element that consists of a template and an
   optional JavaScript class that defines its behavior. For example, someone
   might make a `button` in the template and handle the click behavior in the
-  JavaScript file that shares the same name as the template.
+  JavaScript.
 
   Components are broken down into two categories:
 
@@ -27,41 +27,41 @@ import { setOwner, type default as Owner } from '@ember/owner';
   Below is the documentation for Template-only and Glimmer components. If you
   are looking for the API documentation for Classic components, it is
   [available here](/ember/release/classes/Component). The source code for
-  Glimmer components can be found in [`@glimmer/component`](https://github.com/glimmerjs/glimmer.js/tree/master/packages/%40glimmer/component).
+  Glimmer components can be found in [`@glimmer/component`](https://github.com/emberjs/ember.js/tree/main/packages/%40glimmer/component).
 
+  Note: Prior to Ember 6.8, by default, components were authored in paired `.hbs` and `.js` 
+  files. This is still supported, but the default authoring format is now `.gjs` or "template tag".
+  To read more about how components were previously authored, see the 
+  [6.7 version of this API documentation](https://api.emberjs.com/ember/6.7/modules/@glimmer%2Fcomponent).
+    
   ## Defining a Template-only Component
 
-  The simplest way to create a component is to create a template file in
-  `app/templates/components`. For example, if you name a template
-  `app/templates/components/person-profile.hbs`:
+  The simplest way to create a component is to create a `gjs` file in
+  `app/components` with a `<template>` tag wrapper:
 
-  ```app/templates/components/person-profile.hbs
-  <h1>{{@person.name}}</h1>
-  <img src={{@person.avatar}}>
-  <p class='signature'>{{@person.signature}}</p>
+  ```app/components/person-profile.gjs
+  <template>
+    <h1>{{@person.name}}</h1>
+    <img src={{@person.avatar}}>
+    <p class='signature'>{{@person.signature}}</p>
+  </template>
   ```
 
   You will be able to use `<PersonProfile />` to invoke this component elsewhere
   in your application:
 
-  ```app/templates/application.hbs
-  <PersonProfile @person={{this.currentUser}} />
+  ```app/templates/application.gjs
+  import PersonProfile from '../components/person-profile';
+    
+  <template>
+    <PersonProfile @person={{@model.currentUser}} />
+  </template>
   ```
 
   Note that component names are capitalized here in order to distinguish them
-  from regular HTML elements, but they are dasherized in the file system.
+  from regular HTML elements.
 
-  While the angle bracket invocation form is generally preferred, it is also
-  possible to invoke the same component with the `{{person-profile}}` syntax:
-
-  ```app/templates/application.hbs
-  {{person-profile person=this.currentUser}}
-  ```
-
-  Note that with this syntax, you use dashes in the component name and
-  arguments are passed without the `@` sign.
-
-  In both cases, Ember will render the content of the component template we
+  Ember will render the content of the component template we
   created above. The end result will be something like this:
 
   ```html
@@ -72,20 +72,24 @@ import { setOwner, type default as Owner } from '@ember/owner';
 
   ## File System Nesting
 
-  Components can be nested inside sub-folders for logical groupping. For
-  example, if we placed our template in
-  `app/templates/components/person/short-profile.hbs`, we can invoke it as
-  `<Person::ShortProfile />`:
+  In Ember templates, “invokables” are things you can invoke in a template. These include components, 
+  helpers, and modifiers. In the template tag format, these invokables need to be imported before 
+  they can be used. This makes it easier to understand where values come from and what they do, as 
+  well as unlocks build optimizations.
 
-  ```app/templates/application.hbs
-  <Person::ShortProfile @person={{this.currentUser}} />
-  ```
+  When making use of the `PersonProfile` component as defined before in a different component file, 
+  it first needs to be imported. This is done using the import statement, just like you would import 
+  any other JavaScript module -- from your local project or from an external package.
 
-  Or equivalently, `{{person/short-profile}}`:
-
-  ```app/templates/application.hbs
-  {{person/short-profile person=this.currentUser}}
-  ```
+  Since they are imported, Components can be placed anywhere in your project on the filesystem, but 
+  they are conventionally placed within `app/components` for reusable components and within 
+  `app/templates` for Route components. 
+    
+  Route components must be named after the route they are associated with -- and placed in the 
+  `app/templates` directory. For example, a `Person` route would have a corresponding `Person` 
+  component defined in `app/templates/person.gjs`. This component would then be rendered whenever 
+  the user visits the `/person` route. For more information, see the 
+  [Routing](https://guides.emberjs.com/release/routing/) section of the guides.
 
   ## Using Blocks
 
@@ -93,27 +97,25 @@ import { setOwner, type default as Owner } from '@ember/owner';
   attached to the component. For instance, if we added a `{{yield}}` to our
   component like so:
 
-  ```app/templates/components/person-profile.hbs
-  <h1>{{@person.name}}</h1>
-  {{yield}}
+  ```app/components/person-profile.gjs
+  <template>
+    <h1>{{@person.name}}</h1>
+    {{yield}}
+  </template>
   ```
 
   We could then invoke it like this:
 
-  ```handlebars
-  <PersonProfile @person={{this.currentUser}}>
-    <p>Admin mode</p>
-  </PersonProfile>
+  ```gjs
+  import PersonProfile from '../components/person-profile';
+    
+  <template>
+    <PersonProfile @person={{@model.currentUser}}>
+      <p>Admin mode</p>
+    </PersonProfile>
+  </template>
   ```
-
-  or with curly syntax like this:
-
-  ```handlebars
-  {{#person-profile person=this.currentUser}}
-    <p>Admin mode</p>
-  {{/person-profile}}
-  ```
-
+    
   And the content passed in between the brackets of the component would be
   rendered in the same place as the `{{yield}}` within it, replacing it.
 
@@ -125,17 +127,23 @@ import { setOwner, type default as Owner } from '@ember/owner';
   You can also pass positional parameters to `{{yield}}`, which are then made
   available in the block:
 
-  ```app/templates/components/person-profile.hbs
-  <h1>{{@person.name}}</h1>
-  {{yield @person.signature}}
+  ```app/components/person-profile.gjs
+  <template>
+    <h1>{{@person.name}}</h1>
+    {{yield @person.signature}}
+  </template>
   ```
 
   We can then use this value in the block like so:
 
-  ```handlebars
-  <PersonProfile @person={{this.currentUser}} as |signature|>
-    {{signature}}
-  </PersonProfile>
+  ```gjs
+  import PersonProfile from '../components/person-profile';
+    
+  <template>
+    <PersonProfile @person={{@model.currentUser}} as |signature|>
+      {{signature}}
+    </PersonProfile>
+  </template>
   ```
 
   ### Passing multiple blocks
@@ -145,18 +153,24 @@ import { setOwner, type default as Owner } from '@ember/owner';
   we wanted to add a way for users to customize the title of our
   `<PersonProfile>` component, we could add a named block inside of the header:
 
-  ```app/templates/components/person-profile.hbs
-  <h1>{{yield to="title"}}</h1>
-  {{yield}}
+  ```app/components/person-profile.gjs
+  <template>
+    <h1>{{yield to="title"}}</h1>
+    {{yield}}
+  </template>
   ```
 
   This component could then be invoked like so:
 
-  ```handlebars
-  <PersonProfile @person={{this.currentUser}}>
-    <:title>{{this.currentUser.name}}</:title>
-    <:default>{{this.currentUser.signature}}</:default>
-  </PersonProfile>
+  ```gjs
+  import PersonProfile from '../components/person-profile';
+
+  <template>
+    <PersonProfile @person={{@model.currentUser}}>
+      <:title>{{@model.currentUser.name}}</:title>
+      <:default>{{@model.currentUser.signature}}</:default>
+    </PersonProfile>
+  </template>
   ```
 
   When passing named blocks, you must name every block, including the `default`
@@ -168,18 +182,24 @@ import { setOwner, type default as Owner } from '@ember/owner';
 
   You can also pass parameters to named blocks:
 
-  ```app/templates/components/person-profile.hbs
-  <h1>{{yield @person.name to="title"}}</h1>
-  {{yield @person.signature}}
+  ```app/components/person-profile.gjs
+  <template>
+    <h1>{{yield @person.name to="title"}}</h1>
+    {{yield @person.signature}}
+  </template>
   ```
 
   These parameters can then be used like so:
 
-  ```handlebars
-  <PersonProfile @person={{this.currentUser}}>
-    <:title as |name|>{{name}}</:title>
-    <:default as |signature|>{{signature}}</:default>
-  </PersonProfile>
+  ```gjs
+  import PersonProfile from '../components/person-profile';
+
+  <template>
+    <PersonProfile @person={{@model.currentUser}}>
+      <:title as |name|>{{name}}</:title>
+      <:default as |signature|>{{signature}}</:default>
+    </PersonProfile>
+  </template>
   ```
 
   ### Checking to see if a block exists
@@ -187,44 +207,50 @@ import { setOwner, type default as Owner } from '@ember/owner';
   You can also check to see if a block exists using the `(has-block)` keyword,
   and conditionally use it, or provide a default template instead.
 
-  ```app/templates/components/person-profile.hbs
-  <h1>
-    {{#if (has-block "title")}}
-      {{yield @person.name to="title"}}
+  ```app/components/person-profile.gjs
+  <template>
+    <h1>
+      {{#if (has-block "title")}}
+        {{yield @person.name to="title"}}
+      {{else}}
+        {{@person.name}}
+      {{/if}}
+    </h1>
+  
+    {{#if (has-block)}}
+      {{yield @person.signature}}
     {{else}}
-      {{@person.name}}
+      {{@person.signature}}
     {{/if}}
-  </h1>
-
-  {{#if (has-block)}}
-    {{yield @person.signature}}
-  {{else}}
-    {{@person.signature}}
-  {{/if}}
+  </template>
   ```
 
   With this template, we can then optionally pass in one block, both blocks, or
   none at all:
 
-  ```handlebars
-  {{! passing both blocks }}
-  <PersonProfile @person={{this.currentUser}}>
-    <:title as |name|>{{name}}</:title>
-    <:default as |signature|>{{signature}}</:default>
-  </PersonProfile>
+  ```gjs
+  import PersonProfile from '../components/person-profile';
 
-  {{! passing just the title block }}
-  <PersonProfile @person={{this.currentUser}}>
-    <:title as |name|>{{name}}</:title>
-  </PersonProfile>
-
-  {{! passing just the default block }}
-  <PersonProfile @person={{this.currentUser}} as |signature|>
-    {{signature}}
-  </PersonProfile>
-
-  {{! not passing any blocks }}
-  <PersonProfile @person={{this.currentUser}}/>
+  <template>
+    {{! passing both blocks }}
+    <PersonProfile @person={{@model.currentUser}}>
+      <:title as |name|>{{name}}</:title>
+      <:default as |signature|>{{signature}}</:default>
+    </PersonProfile>
+  
+    {{! passing just the title block }}
+    <PersonProfile @person={{@model.currentUser}}>
+      <:title as |name|>{{name}}</:title>
+    </PersonProfile>
+  
+    {{! passing just the default block }}
+    <PersonProfile @person={{@model.currentUser}} as |signature|>
+      {{signature}}
+    </PersonProfile>
+  
+    {{! not passing any blocks }}
+    <PersonProfile @person={{@model.currentUser}}/>
+  </template>
   ```
 
   ### Checking to see if a block has parameters
@@ -232,27 +258,26 @@ import { setOwner, type default as Owner } from '@ember/owner';
   We can also check if a block receives parameters using the `(has-block-params)`
   keyword, and conditionally yield different values if so.
 
-  ```app/templates/components/person-profile.hbs
-  {{#if (has-block-params)}}
-    {{yield @person.signature}}
-  {{else}}
-    {{yield}}
-  {{/if}}
+  ```app/components/person-profile.gjs
+  <template>
+    {{#if (has-block-params)}}
+      {{yield @person.signature}}
+    {{else}}
+      {{yield}}
+    {{/if}}
+  </template>
   ```
 
   ## Customizing Components With JavaScript
 
-  To add JavaScript to a component, create a JavaScript file in the same
-  location as the template file, with the same name, and export a subclass
-  of `Component` as the default value. For example, to add Javascript to the
-  `PersonProfile` component which we defined above, we would create
-  `app/components/person-profile.js` and export our class as the default, like
-  so:
+  To add JavaScript to a component, add a class extending from `@glimmer/component` to the `gjs` 
+  file, wrapping your `<template>` tag.
+  For example, to add JavaScript to the `PersonProfile` component which we defined above:
 
-  ```app/components/person-profile.js
+  ```app/components/person-profile.gjs
   import Component from '@glimmer/component';
 
-  export default class PersonProfileComponent extends Component {
+  export default class PersonProfile extends Component {
     get displayName() {
       let { title, firstName, lastName } = this.args.person;
 
@@ -262,19 +287,19 @@ import { setOwner, type default as Owner } from '@ember/owner';
         return `${firstName} ${lastName}`;
       }
     })
+    
+    <template>
+      <h1>{{this.displayName}}</h1>
+      {{yield}}
+    </template>
   }
   ```
 
   You can add your own properties, methods, and lifecycle hooks to this
   subclass to customize its behavior, and you can reference the instance of the
-  class in your template using `{{this}}`. For instance, we could access the
+  class in your template using `{{this}}`. In the example above, we access the
   `displayName` property of our `PersonProfile` component instance in the
-  template like this:
-
-  ```app/templates/components/person-profile.hbs
-  <h1>{{this.displayName}}</h1>
-  {{yield}}
-  ```
+  template.
 
   ## `constructor`
 
@@ -284,7 +309,7 @@ import { setOwner, type default as Owner } from '@ember/owner';
   constructor is run whenever a new instance of the component is created, and
   can be used to setup the initial state of the component.
 
-  ```javascript
+  ```gjs
   import Component from '@glimmer/component';
 
   export default class SomeComponent extends Component {
@@ -300,7 +325,7 @@ import { setOwner, type default as Owner } from '@ember/owner';
 
   Service injections and arguments are available in the constructor.
 
-  ```javascript
+  ```gjs
   import Component from '@glimmer/component';
   import { service } from '@ember/service';
 
@@ -322,8 +347,11 @@ import { setOwner, type default as Owner } from '@ember/owner';
   `willDestroy` is called after the component has been removed from the DOM, but
   before the component is fully destroyed. This lifecycle hook can be used to
   cleanup the component and any related state.
+    
+  You may prefer the [@ember/destroyable](/ember/release/modules//@ember%2Fdestroyable) APIs for
+  this purpose.
 
-  ```javascript
+  ```gjs
   import Component from '@glimmer/component';
   import { service } from '@ember/service';
 
@@ -344,8 +372,10 @@ import { setOwner, type default as Owner } from '@ember/owner';
   _arguments_ that are passed to the component. For instance, the
   following component usage:
 
-  ```handlebars
-  <SomeComponent @fadeIn={{true}} />
+  ```gjs
+  <template>
+    <SomeComponent @fadeIn={{true}} />
+  </template>
   ```
 
   Would result in the following `args` object to be passed to the component:
@@ -356,26 +386,22 @@ import { setOwner, type default as Owner } from '@ember/owner';
 
   `args` can be accessed at any point in the component lifecycle, including
   `constructor` and `willDestroy`. They are also automatically marked as tracked
-  properties, and they can be depended on as computed property dependencies:
+  properties, and they can be depended on to update as any other tracked property:
 
-  ```javascript
+  ```gjs
   import Component from '@glimmer/component';
   import { computed } from '@ember/object';
 
   export default class SomeComponent extends Component {
-
-    @computed('args.someValue')
-    get computedGetter() {
-      // updates whenever args.someValue updates
-      return this.args.someValue;
-    }
-
     get standardGetter() {
-      // updates whenever args.anotherValue updates (Ember 3.13+)
+      // updates whenever args.anotherValue updates
       return this.args.anotherValue;
     }
   }
   ```
+    
+  A components arguments are accessible in the template using the `@` prefix - for example, 
+  `this.args.fadeIn` from the example above can be accessed in the template as `@fadeIn`.
 
   ## `isDestroying`
 
